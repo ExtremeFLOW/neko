e8fc1c81fe3037d721d4a4e24a3996eb5d90df23
Niclas Jansson <leifniclas.jansson@riken.jp>
2018-04-25 16:41:52 +0900

    Initial work on conversion programs

---
+++ contrib/rea2nbin/rea2nbin.f90
@@ -0,0 +1,69 @@
+program rea2nbin
+  use num_types
+  use mesh
+  implicit none
+
+  integer :: i, j, ierr
+  integer :: ndim, nparam, nskip, nlogic
+  integer :: nelgs, nelgv
+  character(len=80) :: fname, output, opt
+  type(mesh_t) :: msh
+  
+  if (iargc() .lt. 2) then
+     write(*,*) 'Usage: ./rea2nbin <reafile> <neko mesh>'
+     stop
+  end if
+  
+  call getarg(1, fname)
+  call getarg(2, output)
+
+  open(unit=9,file=trim(fname), status='old', iostat=ierr)
+  write(*, '(A,A)') ' Reading ', fname
+  
+  read(9, *)
+  read(9, *)
+  read(9, *) ndim
+  read(9, *) nparam
+  
+  ! Skip parameters
+  do i = 1, nparam
+     read(9, *)
+  end do
+
+  ! Skip passive scalars
+  read(9, *) nskip
+  do i = 1, nskip
+     read(9, *)
+  end do
+
+  ! Skip logic switches
+  read(9, *) nlogic
+  do i = 1, nlogic
+     read(9, *)
+  end do
+
+  ! Read mesh info
+  read(9, *)
+  read(9, *)
+  read(9, *) nelgs,ndim, nelgv
+  write(*,*) nelgs, ndim, nelgv
+
+  call mesh_init_coordinates(msh, ndim, nelgv)
+
+  do i = 1, nelgv
+     read(9, *)
+     if (ndim .eq. 2) then
+        read(9, *) (msh%xc(j, i),j=1,4)
+        read(9, *) (msh%yc(j, i),j=1,4)
+     else if (ndim .eq. 3) then
+        read(9, *) (msh%xc(j, i),j=1,4)
+        read(9, *) (msh%yc(j, i),j=1,4)
+        read(9, *) (msh%zc(j, i),j=1,4)
+     end if
+  end do
+
+  call mesh_free(msh)
+
+  close(9)
+
+end program rea2nbin
--- src/mesh.f90
+++ src/mesh.f90
@@ -12,7 +12,49 @@ module mesh
      integer :: ly1
      integer :: lz1
 
+     real(kind=dp), allocatable :: xc(:,:)   !< X-coordinates
+     real(kind=dp), allocatable :: yc(:,:)   !< Y-coordinates
+     real(kind=dp), allocatable :: zc(:,:)   !< Z-coordinates
+
   end type mesh_t
+
+contains 
+
+  subroutine mesh_init_coordinates(m, ndim, nelv)
+    type(mesh_t), intent(inout) :: m
+    integer, intent(in) :: ndim
+    integer, intent(in) :: nelv
+    
+    if (.not. allocated(m%xc)) then
+       allocate(m%xc(4, nelv))
+    end if
+
+    if (.not. allocated(m%yc)) then
+       allocate(m%yc(4, nelv))
+    end if
+
+    if (ndim .gt. 2 .and. (.not. allocated(m%zc))) then
+       allocate(m%zc(4, nelv))
+    end if
+    
+  end subroutine mesh_init_coordinates
+  
+  subroutine mesh_free(m)
+    type(mesh_t), intent(inout) :: m
+
+    if (allocated(m%xc)) then
+       deallocate(m%xc)
+    end if
+
+    if (allocated(m%yc)) then
+       deallocate(m%yc)
+    end if
+
+    if (allocated(m%zc)) then
+       deallocate(m%zc)
+    end if
+
+  end subroutine mesh_free
 
 
 end module mesh
