2e5a75002ed63267233b6910f0d7beaf51abbc2f
Niclas Jansson <leifniclas.jansson@riken.jp>
2019-02-21 13:49:35 +0100

    Added missing files

---
+++ src/file.f90
@@ -0,0 +1,82 @@
+module file
+  use generic_file
+  use rea
+  use mesh
+  implicit none
+  
+  type file_t
+     class(*), private, pointer :: file_
+     class(generic_file_t), allocatable :: file_type
+   contains
+     procedure :: write => file_write
+     procedure :: read => file_read
+     procedure :: free => file_free
+  end type file_t
+
+  interface file_t
+     module procedure file_init
+  end interface file_t
+
+contains
+
+  !> File reader/writer constructor
+  !! @param fname Filename
+  function file_init(fname) result(this)
+    character(len=*), intent(inout) :: fname
+    type(file_t), target :: this
+    integer :: fname_len
+    character(len=80) :: suffix
+    integer suffix_pos
+    class(generic_file_t), pointer :: q
+    
+    fname_len = len_trim(fname)
+    suffix_pos = scan(trim(fname), '.', back=.true.)
+    suffix = trim(fname(suffix_pos + 1:fname_len))
+    
+    if (allocated(this%file_type)) then
+       deallocate(this%file_type)
+    end if
+    
+    if (suffix .eq. "rea") then
+       allocate(rea_t::this%file_type)
+    else
+       write(*,*) 'Unknown file format'
+    end if
+
+    q => this%file_type
+    call q%init(fname)
+
+  end function file_init
+
+  !> File operation destructor
+  subroutine file_free(this)
+    class(file_t), intent(inout) :: this
+
+
+  end subroutine file_free
+
+  !> Write @a data to a file
+  !! @param data Data to be written
+  subroutine file_write(this, data)
+    class(file_t), target :: this
+    class(*), intent(inout) :: data
+    class(generic_file_t), pointer :: q
+    
+    q => this%file_type
+    call q%write(data)
+    
+   end subroutine file_write
+   
+  !> Read @a data from a file
+  !! @param data Read data
+  subroutine file_read(this, data)
+    class(file_t), target :: this
+    class(*), intent(inout) :: data
+    class(generic_file_t), pointer :: q
+
+    q => this%file_type
+    call q%read(data)
+
+  end subroutine file_read
+
+end module file
---
+++ src/generic_file.f90
@@ -0,0 +1,43 @@
+module generic_file
+  use num_types
+  implicit none
+  
+  type, abstract :: generic_file_t
+     character(len=80) :: fname
+     integer :: counter
+   contains
+     procedure :: init => generic_file_init
+     procedure(generic_file_write), deferred :: write
+     procedure(generic_file_read), deferred :: read
+  end type generic_file_t
+
+  abstract interface
+     subroutine generic_file_write(this, data)
+       import :: generic_file_t
+       class(generic_file_t) :: this
+       class(*), intent(in) :: data
+     end subroutine generic_file_write
+  end interface
+  
+  abstract interface
+     subroutine generic_file_read(this, data)
+       import :: generic_file_t
+       class(generic_file_t) :: this
+       class(*), intent(inout) :: data
+     end subroutine generic_file_read
+  end interface
+
+contains
+  
+  !> Generic file constructor
+  !! @param fname Filename
+  subroutine generic_file_init(this, fname)
+    class(generic_file_t) :: this
+    character(len=80) :: fname
+    
+    this%fname = fname
+    this%counter = 0
+    
+  end subroutine generic_file_init
+
+end module generic_file
