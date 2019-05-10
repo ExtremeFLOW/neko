be901315a1915e19e39b2a0ba4786fc52af4d922
Niclas Jansson <leifniclas.jansson@riken.jp>
2019-05-10 17:07:12 +0200

    Add hooks to read the re2 mesh from rea files

--- src/rea.f90
+++ src/rea.f90
@@ -1,13 +1,13 @@
 !
 !> NEKTON session data
 !! @details This module is used to represent NEKTON session data
-!
 module rea
   use generic_file
   use num_types
   use utils
   use mesh
-  use point  
+  use point
+  use re2_file
   implicit none
   private
 
@@ -51,6 +51,8 @@ contains
     logical :: read_param
     real(kind=dp) :: xc(8), yc(8), zc(8)
     type(point_t) :: p(8)
+    type(re2_file_t) :: re2_file
+    character(len=80) :: re2_fname
 
     select type(data)
     type is (rea_t)
@@ -102,54 +104,55 @@ contains
     read(9, *)
     read(9, *) nelgs,ndim, nelgv
     if (nelgs .lt. 0) then
-       !> @todo Add support to load binary NEKTON meshes
-       call neko_error('Binary NEKTON meshes are not supported')
-    end if
-
-    write(*,1) ndim, nelgv
-1   format(1x,'ndim = ', i1, ', nelements =', i7)
-    call mesh_init(msh, ndim, nelgv)
-
-    el_idx = 1
-    pt_idx = 1
-    do i = 1, nelgv
-       read(9, *)
-       if (ndim .eq. 2) then
-          read(9, *) (xc(j),j=1,4)
-          read(9, *) (yc(j),j=1,4)
-          do j = 1, 4
-             p(j) = point_t(xc(j), yc(j), 0d0, pt_idx)
-             pt_idx = pt_idx
-          end do 
-          call mesh_add_element(msh, el_idx, p(1), p(2), p(3), p(4))
-       else if (ndim .eq. 3) then
-          read(9, *) (xc(j),j=1,4)
-          read(9, *) (yc(j),j=1,4)
-          read(9, *) (zc(j),j=1,4)
-          read(9, *) (xc(j),j=5,8)
-          read(9, *) (yc(j),j=5,8)
-          read(9, *) (zc(j),j=5,8)
-          do j = 1, 8
-             p(j) = point_t(xc(j), yc(j), zc(j), pt_idx)
-             pt_idx = pt_idx + 1
-          end do
-          call mesh_add_element(msh, el_idx, &
-               p(1), p(2), p(3), p(4), p(5), p(6), p(7), p(8))
-       end if
-       el_idx = el_idx + 1
-    end do
-    write(*,*) 'Done'
-
-    
-    !> @todo Add support for curved side data
-
-    close(9)
+       re2_fname = trim(this%fname(1:scan(trim(this%fname), '.', back=.true.)))//'re2' 
+       call re2_file%init(re2_fname)
+       call re2_file%read(msh)
+    else       
+       write(*,1) ndim, nelgv
+1      format(1x,'ndim = ', i1, ', nelements =', i7)
+       call mesh_init(msh, ndim, nelgv)
+       
+       el_idx = 1
+       pt_idx = 1
+       do i = 1, nelgv
+          read(9, *)
+          if (ndim .eq. 2) then
+             read(9, *) (xc(j),j=1,4)
+             read(9, *) (yc(j),j=1,4)
+             do j = 1, 4
+                p(j) = point_t(xc(j), yc(j), 0d0, pt_idx)
+                pt_idx = pt_idx
+             end do
+             call mesh_add_element(msh, el_idx, p(1), p(2), p(3), p(4))
+          else if (ndim .eq. 3) then
+             read(9, *) (xc(j),j=1,4)
+             read(9, *) (yc(j),j=1,4)
+             read(9, *) (zc(j),j=1,4)
+             read(9, *) (xc(j),j=5,8)
+             read(9, *) (yc(j),j=5,8)
+             read(9, *) (zc(j),j=5,8)
+             do j = 1, 8
+                p(j) = point_t(xc(j), yc(j), zc(j), pt_idx)
+                pt_idx = pt_idx + 1
+             end do
+             call mesh_add_element(msh, el_idx, &
+                  p(1), p(2), p(3), p(4), p(5), p(6), p(7), p(8))
+          end if
+          el_idx = el_idx + 1
+       end do
+       write(*,*) 'Done'
+       
+       
+       !> @todo Add support for curved side data
+       
+       close(9)
+    endif
     
   end subroutine rea_file_read
-
 
   subroutine rea_file_write(this, data)
     class(rea_file_t), intent(in) :: this
     class(*), target, intent(in) :: data
   end subroutine rea_file_write
+
 end module rea
