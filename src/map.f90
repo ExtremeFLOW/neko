a4c655b54b72a5c5e8cd8f8d157decc0ce8f6342
Niclas Jansson <leifniclas.jansson@riken.jp>
2019-09-03 16:26:06 +0200

    Reworked Nekton vertex mappings

--- src/map.f90
+++ src/map.f90
@@ -1,6 +1,7 @@
 !> NEKTON map
 !! @todo Figure out a better name for this module
 module map
+  use mesh
   implicit none
 
   !> NEKTON vertex mapping
@@ -10,9 +11,25 @@ module map
      integer, allocatable :: vertex(:,:)
   end type map_t
 
+  interface map_init
+     module procedure map_init_nel_nelv, map_init_mesh
+  end interface map_init
 contains
 
-  subroutine map_init(m, nel, nlv)
+  subroutine map_init_mesh(m, msh)
+    type(map_t), intent(inout) :: m
+    type(mesh_t), intent(in) :: msh
+
+    call map_free(m)
+
+    m%nel = msh%nelv
+    m%nlv = msh%npts
+    
+    call map_init_common(m)
+
+  end subroutine map_init_mesh
+
+  subroutine map_init_nel_nelv(m, nel, nlv)
     type(map_t), intent(inout) :: m
     integer, intent(in) :: nel
     integer, intent(in) :: nlv
@@ -21,12 +38,19 @@ contains
 
     m%nel = nel
     m%nlv = nlv
+
+    call map_init_common(m)
+
+  end subroutine map_init_nel_nelv
+
+  subroutine map_init_common(m)
+    type(map_t), intent(inout) :: m
     
     allocate(m%imap(m%nel))
     
     allocate(m%vertex(m%nlv, m%nel))
     
-  end subroutine map_init
+  end subroutine map_init_common
 
   subroutine map_free(m)
     type(map_t), intent(inout) :: m
