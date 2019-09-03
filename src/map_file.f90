957c622d0e5eab598292f4181802fdbe884eeb8e
Niclas Jansson <leifniclas.jansson@riken.jp>
2019-09-03 16:06:44 +0200

    Rename to avoid name clashes

--- src/map_file.f90
+++ src/map_file.f90
@@ -2,7 +2,6 @@
 !! @details This module is used to read/write NEKTON vertex mapping data
 module map_file
   use generic_File
-  use num_types
   use utils
   use map
   implicit none
@@ -21,12 +20,12 @@ contains
   subroutine map_file_read(this, data)
     class(map_file_t) :: this
     class(*), target, intent(inout) :: data
-    type(map_t), pointer :: map
+    type(map_t), pointer :: nm
     integer :: j, k, neli, nnzi, ierr
     
     select type(data)
     type is (map_t)
-       map => data
+       nm => data
     class default
        call neko_error("Invalid output data")
     end select
@@ -38,8 +37,8 @@ contains
 
     !> @todo Check if neli matches map%nel
     
-    do j = 1, map%nel
-       read(9, *) map%imap(j),(map%vertex(k, j), k=1,map%nlv)
+    do j = 1, nm%nel
+       read(9, *) nm%imap(j),(nm%vertex(k, j), k=1,nm%nlv)
     end do
     
     close(unit=9)
