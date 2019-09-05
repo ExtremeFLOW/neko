fc8969dc9cf0006cd1295de63279101504df2d58
Niclas Jansson <njansson@kth.se>
2019-09-05 20:23:24 +0200

    Work on conversion program

--- contrib/rea2nbin/rea2nbin.f90
+++ contrib/rea2nbin/rea2nbin.f90
@@ -2,27 +2,30 @@ program rea2nbin
   use neko
   implicit none
 
-  integer :: i, j, ierr
-  integer :: ndim, nparam, nskip, nlogic
-  integer :: nelgs, nelgv
-  character(len=NEKO_FNAME_LEN) :: fname, output, opt
+  character(len=NEKO_FNAME_LEN) :: fname, output
   type(mesh_t) :: msh
-  type(file_t) :: rea_file
+  type(file_t) :: rea_file, nmsh_file
   
   if (command_argument_count() .lt. 2) then
      write(*,*) 'Usage: ./rea2nbin <reafile> <neko mesh>'
      stop
   end if
   
-  call get_command_argument(1, fname)
+  call neko_init 
+  
+  call get_command_argument(1, fname) 
   call get_command_argument(2, output)
-
+  
   rea_file = file_t(fname)
-
+  
   call rea_file%read(msh)
-
+  
+  nmsh_file = file_t(output)
+  
+  call nmsh_file%write(msh)
+  
   call mesh_free(msh)
-
-  close(9)
+  
+  call neko_finalize
 
 end program rea2nbin
