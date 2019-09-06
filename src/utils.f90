602e4bfbb4d9e39986e75ec2a8dd1b68bb50ee43
Niclas Jansson <njansson@kth.se>
2019-09-06 14:08:20 +0200

    Add various filename utilities

--- src/utils.f90
+++ src/utils.f90
@@ -11,6 +11,32 @@ module utils
   
 contains
   
+  !> Find position (in the string) of a filename's suffix
+  pure function filename_suffix_pos(fname) result(suffix_pos)
+    character(len=*), intent(in) :: fname
+    integer :: suffix_pos
+    suffix_pos = scan(trim(fname), '.', back=.true.)    
+  end function filename_suffix_pos
+
+  !> Extract a filename's suffix
+  subroutine filename_suffix(fname, suffix)
+    character(len=*) :: fname
+    character(len=*) :: suffix
+    suffix = trim(fname(filename_suffix_pos(fname) + 1:len_trim(fname)))
+  end subroutine filename_suffix
+
+  !> Change a filename's suffix
+  subroutine filename_chsuffix(fname, new_fname, new_suffix)
+    character(len=*) :: fname
+    character(len=*) :: new_fname
+    character(len=*) :: new_suffix
+    integer :: suffix_pos
+
+    suffix_pos = filename_suffix_pos(fname)
+    new_fname = trim(fname(1:suffix_pos))//new_suffix
+
+  end subroutine filename_chsuffix
+
   subroutine neko_error_plain(error_code)
     integer, optional :: error_code
     integer :: code
