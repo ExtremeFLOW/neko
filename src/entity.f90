7ac925f7cbea7ec341dae399a9f626936c3b4da4
Niclas Jansson <njansson@kth.se>
2019-11-25 11:02:08 +0100

    Store a global index for each mesh entity

--- src/entity.f90
+++ src/entity.f90
@@ -5,25 +5,42 @@ module entity
   !> Base type for an entity
   type, public, abstract :: entity_t 
      integer, private :: id_ = -1 !< Entity index
+     integer, private :: glb_id_ = -1 !< Global entitiy index
    contains
      procedure, pass(this) :: id => entity_id
+     procedure, pass(this) :: glb_id => entity_glb_id
      procedure, pass(this) :: set_id => entity_set_id     
+     procedure, pass(this) :: set_glb_id => entity_set_glb_id
   end type entity_t
 
 contains
 
-  !> Return @a id_ of an entity
+  !> Return the index of an entity
   pure function entity_id(this) result(id)
     class(entity_t), intent(in) :: this
     integer :: id
     id = this%id_
   end function entity_id
 
-  !> Update @a id_ of an entity
+  !> Return the global index of an entity
+  pure function entity_glb_id(this) result(glb_id)
+    class(entity_t), intent(in) :: this
+    integer :: glb_id
+    glb_id = this%glb_id_
+  end function entity_glb_id
+
+  !> Update the index of an entity
   subroutine entity_set_id(this, id)
     class(entity_t), intent(inout) :: this
     integer, intent(in) :: id
     this%id_ = id
   end subroutine entity_set_id
+
+  !> Update the global index of an entitiy
+  subroutine entity_set_glb_id(this, glb_id)
+    class(entity_t), intent(inout) :: this
+    integer, intent(in) :: glb_id
+    this%glb_id_ = glb_id
+  end subroutine entity_set_glb_id
     
 end module entity
