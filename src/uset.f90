10c9ec3d8153fafe13a4c9ec4e9b45df9ef928b5
Niclas Jansson <njansson@kth.se>
2019-12-03 19:18:27 +0100

    rework data type

--- src/uset.f90
+++ src/uset.f90
@@ -6,8 +6,19 @@ module uset
   implicit none
   private
 
+  !> Base type for an unordered set
+  type, private, abstract :: uset_t
+   contains
+     procedure(uset_init), pass(this), deferred :: init
+     procedure(uset_free), pass(this), deferred :: free
+     procedure(uset_size), pass(this), deferred :: size
+     procedure(uset_clear), pass(this), deferred :: clear
+     procedure(uset_element), pass(this), deferred :: element
+     procedure(uset_add), pass(this), deferred :: add
+  end type uset_t
+
   !> Integer based unordered set
-  type, public :: uset_i4_t
+  type, extends(uset_t), public :: uset_i4_t
      type(htable_i4_t) :: t
    contains
      procedure, pass(this) :: init => uset_i4_init
@@ -19,7 +30,7 @@ module uset
   end type uset_i4_t
 
   !> Double precision unordered set
-  type, public :: uset_r8_t
+  type, extends(uset_t), public :: uset_r8_t
      type(htable_r8_t) :: t
    contains
      procedure, pass(this) :: init => uset_r8_init
@@ -29,6 +40,59 @@ module uset
      procedure, pass(this) :: element => uset_r8_element
      procedure, pass(this) :: add => uset_r8_add
   end type uset_r8_t
+
+  !> Interface for initializing an unordered set
+  abstract interface
+     subroutine uset_init(this, n) 
+       import uset_t
+       class(uset_t), intent(inout) :: this
+       integer, value, optional :: n
+     end subroutine uset_init
+  end interface
+
+  !> Interface for destroying an unordered set
+  abstract interface
+     subroutine uset_free(this) 
+       import uset_t
+       class(uset_t), intent(inout) :: this
+     end subroutine uset_free
+  end interface
+
+  !> Interface for getting the cardinality of an unordered set
+  abstract interface
+     pure function uset_size(this) result(entries)
+       import uset_t
+       class(uset_t), intent(in) :: this
+       integer :: entries
+     end function uset_size
+  end interface
+
+  !> Interface for clearing an unordered set
+  abstract interface
+     subroutine uset_clear(this) 
+       import uset_t
+       class(uset_t), intent(inout) :: this
+     end subroutine uset_clear
+  end interface
+
+  !> Interface for checking if @a key is an element of an unordered set
+  abstract interface
+     function uset_element(this, key) result(res)
+       import uset_t
+       class(uset_t), intent(inout) :: this
+       class(*), intent(inout) :: key
+       logical :: res
+     end function uset_element
+  end interface
+
+  !> Inteface for adding @a key to an unorderd set
+  abstract interface     
+     subroutine uset_add(this, key)
+       import uset_t
+       class(uset_t), intent(inout) :: this
+       class(*) :: key
+     end subroutine uset_add
+ end interface
 
 contains
 
@@ -54,7 +118,7 @@ contains
   end subroutine uset_i4_free
 
   !> Return the cardinality of an integer based unordered set
-  function uset_i4_size(this) result(entries)
+  pure function uset_i4_size(this) result(entries)
     class(uset_i4_t), intent(in) :: this
     integer :: entries
 
@@ -109,7 +173,7 @@ contains
   end subroutine uset_r8_free
 
   !> Return the cardinality of a double precision based unordered set
-  function uset_r8_size(this)  result(entries)
+  pure function uset_r8_size(this)  result(entries)
     class(uset_r8_t), intent(in) :: this
     integer :: entries
 
