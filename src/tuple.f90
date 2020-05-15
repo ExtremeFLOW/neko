d4e4fb7a3c817a445efd32236abb4979f73a2176
Niclas Jansson <njansson@kth.se>
2020-05-15 23:02:55 +0200

    Add integer based 4-tuple (for face data)

--- src/tuple.f90
+++ src/tuple.f90
@@ -24,6 +24,15 @@ module tuple
      procedure, pass(this) :: equal => tuple_i4_equal
   end type tuple_i4_t
 
+    !> Integer based 4-tuple 
+  type, extends(tuple_t), public :: tuple4_i4_t
+     integer :: x(4)
+   contains
+     procedure, pass(this) :: assign_tuple => tuple4_i4_assign_tuple
+     procedure, pass(this) :: assign_vector => tuple4_i4_assign_vector
+     procedure, pass(this) :: equal => tuple4_i4_equal
+  end type tuple4_i4_t
+
   !> Double precision based 2-tuple 
   type, extends(tuple_t), public :: tuple_r8_t
      real(kind=dp) :: x(2)
@@ -92,6 +101,38 @@ contains
        res = .false.
     end if    
   end function tuple_i4_equal
+
+    !> Assign an integer 4-tuple to a tuple
+  pure subroutine tuple4_i4_assign_tuple(this, other)
+    class(tuple4_i4_t), intent(inout) :: this
+    type(tuple4_i4_t), intent(in) :: other
+    this%x = other%x
+  end subroutine tuple4_i4_assign_tuple
+
+  !> Assign an integer vector to a tuple
+  pure subroutine tuple4_i4_assign_vector(this, x)
+    class(tuple4_i4_t), intent(inout) :: this
+    class(*), dimension(4), intent(in) :: x
+    select type(x)
+    type is (integer)
+       this%x = x
+    end select    
+  end subroutine tuple4_i4_assign_vector
+
+  !> Check if two integer based tuples are equal
+  pure function tuple4_i4_equal(this, other) result(res)
+    class(tuple4_i4_t), intent(in) :: this
+    type(tuple4_i4_t), intent(in) :: other
+    logical :: res    
+    if ((this%x(1) .eq. other%x(1)) .and. &
+         (this%x(2) .eq. other%x(2)) .and. &
+         (this%x(3) .eq. other%x(3)) .and. &
+         (this%x(4) .eq. other%x(4))) then
+       res = .true.
+    else
+       res = .false.
+    end if    
+  end function tuple4_i4_equal
 
   !> Assign a double precision 2-tuple to a tuple
   pure subroutine tuple_r8_assign_tuple(this, other)
