f727842da0f72ba2f671a3527782311b3224bc1f
Niclas Jansson <njansson@kth.se>
2020-06-09 22:43:41 +0200

    Add container for storing edge information

--- src/distdata.f90
+++ src/distdata.f90
@@ -7,7 +7,9 @@ module distdata
   
   type, public :: distdata_t
      type(stack_i4t2_t) :: shared_facet !< Elemenets with shared facets
-     type(uset_i4_t) :: shared_point   !< List of shared points
+     type(stack_i4t2_t) :: shared_edge  !< Elements with shared edges
+     type(uset_i4_t) :: shared_point    !< List of shared points
+
   end type distdata_t
 
 contains
@@ -17,6 +19,7 @@ contains
     type(distdata_t), intent(inout) :: distdata
 
     call distdata%shared_facet%init()
+    call distdata%shared_edge%init()   
     call distdata%shared_point%init()
     
   end subroutine distdata_init
@@ -26,6 +29,7 @@ contains
     type(distdata_t), intent(inout) :: distdata
 
     call distdata%shared_facet%free()
+    call distdata%shared_edge%free()
     call distdata%shared_point%free()
     
   end subroutine distdata_free
@@ -41,6 +45,19 @@ contains
     call distdata%shared_facet%push(t)
     
   end subroutine distdata_set_shared_facet
+
+  !> Mark an element's edge as shared
+  !! @attention only defined for elements where facet .ne. edges
+  subroutine distdata_set_shared_edge(distdata, element, edge)
+    type(distdata_t), intent(inout) :: distdata
+    integer, intent(in), value :: element !< Element index (local numbering)
+    integer, intent(in), value :: edge    !< Edge index
+    type(tuple_i4_t) :: t
+
+    t = (/ element, edge /)
+    call distdata%shared_edge%push(t)
+    
+  end subroutine distdata_set_shared_edge
 
   !> Mark a point as shared
   subroutine distdata_set_shared_point(distdata, point)
