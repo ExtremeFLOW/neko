01ebb68b5424a7df5cb04a945d3758e099e2a58e
Niclas Jansson <njansson@kth.se>
2020-07-03 23:37:04 +0200

    Work on infra for a nekbone like benchmark

---
+++ bench/nekbone/driver.f90
@@ -0,0 +1,63 @@
+program nekobone
+  use neko
+  implicit none
+
+  character(len=NEKO_FNAME_LEN) :: fname, lxchar
+  type(mesh_t) :: msh
+  type(file_t) :: nmsh_file
+  type(space_t) :: Xh
+  type(dofmap_t) :: dm
+  type(gs_t) :: gs_h
+  type(field_t) :: x, msk
+  integer :: argc, lx, n
+  character(len=80) :: suffix
+  real(kind=dp), allocatable :: f(:), c(:)
+  
+  argc = command_argument_count()
+
+  if ((argc .lt. 2) .or. (argc .gt. 2)) then
+     write(*,*) 'Usage: ./nekobone <neko mesh> <N>'
+     stop
+  end if
+  
+  call neko_init 
+  
+  call get_command_argument(1, fname)
+  call get_command_argument(2, lxchar)
+  read(lxchar, *) lx
+  
+  nmsh_file = file_t(fname)
+  call nmsh_file%read(msh)  
+  call mesh_generate_conn(msh)
+
+  call space_init(Xh, 1, GLL, lx, lx, lx)
+  dm = dofmap_t(msh, Xh)
+  call gs_init(gs_h, dm)
+
+  call field_init(x, msh, Xh, "x")
+  call field_init(msk, msh, Xh, "mask")
+
+  msk = 1d0
+  call set_mask(msk)
+  
+  n = Xh%lx * Xh%ly * Xh%lz * msh%nelv
+  allocate(f(n), c(n))
+
+  call set_multiplicity(c, n, gs_h)
+  call set_f(f, c, n, gs_h)
+
+
+  !! Add cg loop here
+  
+  deallocate(f, c)
+  call space_free(Xh)
+  call field_free(x)
+  call field_free(msk)
+  call mesh_free(msh)
+  
+  call neko_finalize
+
+end program nekobone
+
+
+
---
+++ bench/nekbone/setup.f90
@@ -0,0 +1,110 @@
+! Set mask for Dirichlet conditions
+subroutine set_mask(msk)
+  use field
+  use mesh
+  implicit none
+  
+  type(field_t), target, intent(inout) :: msk
+  type(mesh_t), pointer :: msh
+  integer :: i, j, k, l
+  integer :: lx, ly, lz
+  msh => msk%msh
+  lx = msk%Xh%lx
+  ly = msk%Xh%ly
+  lz = msk%Xh%lz
+
+  do i = 1, msh%nelv
+     if (msh%facet_neigh(1, i) .eq. 0) then
+        do l = 1, lz
+           do k = 1, ly
+              msk%x(1, k, l, i) = 0d0
+           end do
+        end do
+     end if
+
+     if (msh%facet_neigh(2, i) .eq. 0) then
+        do l = 1, lz
+           do k = 1, ly
+              msk%x(lx, k, l, i) = 0d0
+           end do
+        end do
+     end if
+
+     if (msh%facet_neigh(3, i) .eq. 0) then
+        do l = 1, lz
+           do j = 1, lx
+              msk%x(j, 1, l, i) = 0d0
+           end do
+        end do
+     end if
+
+     if (msh%facet_neigh(4, i) .eq. 0) then
+        do l = 1, lz
+           do j = 1, lx
+              msk%x(j, ly, l, i) = 0d0
+           end do
+        end do
+     end if
+
+     if (msh%facet_neigh(5, i) .eq. 0) then
+        do k = 1, ly
+           do j = 1, lx
+              msk%x(j, k, 1, i) = 0d0
+           end do
+        end do
+     end if
+
+     if (msh%facet_neigh(6, i) .eq. 0) then
+        do k = 1, ly
+           do j = 1, lx
+              msk%x(j, k, lz, i) = 0d0
+           end do
+        end do
+     end if
+  end do
+
+end subroutine set_mask
+
+! Inverse of counting matrix
+subroutine set_multiplicity(c, n, gs_h)
+  use gather_scatter
+  use num_types
+  implicit none
+  
+  real(kind=dp), intent(inout), dimension(n) :: c
+  type(gs_t), intent(inout) :: gs_h
+  integer, intent(inout) :: n
+  integer :: i
+
+  call rone(c, n)
+  call gs_op(gs_h, c, n, GS_OP_ADD)
+
+  do i = 1, n
+     c(i) = 1d0 / c(i)
+  end do
+
+end subroutine set_multiplicity
+
+! Setup rhs
+subroutine set_f(f, c, n, gs_h)
+  use gather_scatter
+  use num_types
+  implicit none
+  
+  real(kind=dp), intent(inout), dimension(n) :: f
+  real(kind=dp), intent(inout), dimension(n) :: c
+  integer,  intent(inout) :: n
+  type(gs_t), intent(inout) :: gs_h
+  real(kind=dp) :: arg
+  integer :: i
+
+  do i = 1, n
+     arg = 1d9 * (i * i)
+     arg = 1d9 * cos(arg)
+     f(i) = sin(arg)
+  end do
+
+  call gs_op(gs_h, f, n, GS_OP_ADD)
+  call col2(f,c,n)
+
+end subroutine set_f
