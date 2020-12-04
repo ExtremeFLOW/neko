4a0dd089da04c4d0dfe2ff6a73980a434251156e
Martin Karp <martinkarp@github.com>
2020-12-04 11:13:34 +0100

    axhelm evaluation

---
+++ src/ax_helm.f90
@@ -0,0 +1,90 @@
+module ax_helm
+  use ax_product
+  implicit none
+
+  type, public, extends(ax_t) :: ax_helm_t
+  contains
+     procedure, nopass :: compute => ax_helm_compute
+  end type ax_helm_t
+
+contains 
+  
+  subroutine ax_helm_compute(w, u, coef, msh, Xh)
+    type(mesh_t), intent(inout) :: msh
+    type(space_t), intent(inout) :: Xh
+    type(coef_t), intent(inout) :: coef
+    real(kind=dp), intent(inout) :: w(Xh%lx, Xh%ly, Xh%lz, msh%nelv)
+    real(kind=dp), intent(inout) :: u(Xh%lx, Xh%ly, Xh%lz, msh%nelv)
+  
+    real(kind=dp) :: dudr(Xh%lx,Xh%ly,Xh%lz)
+    real(kind=dp) :: duds(Xh%lx,Xh%ly,Xh%lz)
+    real(kind=dp) :: dudt(Xh%lx,Xh%ly,Xh%lz)
+    real(kind=dp) :: tmp1(Xh%lx,Xh%ly,Xh%lz)
+    real(kind=dp) :: tmp2(Xh%lx,Xh%ly,Xh%lz)
+    real(kind=dp) :: tmp3(Xh%lx,Xh%ly,Xh%lz)
+    real(kind=dp) :: tm1(Xh%lx,Xh%ly,Xh%lz)
+    real(kind=dp) :: tm2(Xh%lx,Xh%ly,Xh%lz)
+    real(kind=dp) :: tm3(Xh%lx,Xh%ly,Xh%lz)
+    integer :: e, k, lxy, lxz, lyz, lxyz
+
+    lxy = Xh%lx*Xh%ly
+    lxz = Xh%lx*Xh%lz
+    lyz = Xh%ly*Xh%lz
+    lxyz = Xh%lx*Xh%ly*Xh%lz
+
+    call rzero(w,coef%dof%n_dofs)
+  
+    do e = 1, msh%nelv
+       if(msh%gdim .eq. 2) then
+         call mxm  (Xh%dx,Xh%lx,u(1,1,1,e),Xh%lx,dudr,lyz)
+         call mxm  (u(1,1,1,e),Xh%lx,Xh%dyt,Xh%ly,duds,Xh%ly)
+         call col3 (tmp1,dudr,coef%G1(1,1,1,e),lxyz)
+         call col3 (tmp2,duds,coef%G2(1,1,1,e),lxyz)
+         if (msh%dfrmd_el(e)) then
+            call addcol3 (tmp1,duds,coef%G4(1,1,1,e),lxyz)
+            call addcol3 (tmp2,dudr,coef%G4(1,1,1,e),lxyz)
+         end if
+         call col2 (tmp1,coef%h1(1,1,1,e),lxyz)
+         call col2 (tmp2,coef%h1(1,1,1,e),lxyz)
+         call mxm  (Xh%dxt,Xh%lx,tmp1,Xh%lx,tm1,lyz)
+         call mxm  (tmp2,Xh%lx,Xh%dy,Xh%ly,tm2,Xh%ly)
+         call add2 (w(1,1,1,e),tm1,lxyz)
+         call add2 (w(1,1,1,e),tm2,lxyz)
+
+       ! 3D evaluation!
+       else
+         call mxm(Xh%dx,Xh%lx,u(1,1,1,e),Xh%lx,dudr,lyz)
+         do k=1,Xh%lz
+            call mxm(u(1,1,k,e),Xh%lx,Xh%dyt,Xh%ly,duds(1,1,k),Xh%ly)
+         end do
+         call mxm     (u(1,1,1,e),lxy,Xh%dzt,Xh%lz,dudt,Xh%lz)
+         call col3    (tmp1,dudr,coef%G1(1,1,1,e),lxyz)
+         call col3    (tmp2,duds,coef%G2(1,1,1,e),lxyz)
+         call col3    (tmp3,dudt,coef%G3(1,1,1,e),lxyz)
+         if (msh%dfrmd_el(e)) then
+            call addcol3 (tmp1,duds,coef%G4(1,1,1,e),lxyz)
+            call addcol3 (tmp1,dudt,coef%G5(1,1,1,e),lxyz)
+            call addcol3 (tmp2,dudr,coef%G4(1,1,1,e),lxyz)
+            call addcol3 (tmp2,dudt,coef%G6(1,1,1,e),lxyz)
+            call addcol3 (tmp3,dudr,coef%G5(1,1,1,e),lxyz)
+            call addcol3 (tmp3,duds,coef%G6(1,1,1,e),lxyz)
+         end if
+         call col2 (tmp1,coef%h1(1,1,1,e),lxyz)
+         call col2 (tmp2,coef%h1(1,1,1,e),lxyz)
+         call col2 (tmp3,coef%h1(1,1,1,e),lxyz)
+         call mxm  (Xh%dxt,Xh%lx,tmp1,Xh%lx,tm1,lyz)
+         do k=1,Xh%lz
+            call mxm(tmp2(1,1,k),Xh%lx,Xh%dy,Xh%ly,tm2(1,1,k),Xh%ly)
+         end do
+         call mxm  (tmp3,lxy,Xh%dz,Xh%lz,tm3,Xh%lz)
+         call add2 (w(1,1,1,e),tm1,lxyz)
+         call add2 (w(1,1,1,e),tm2,lxyz)
+         call add2 (w(1,1,1,e),tm3,lxyz)
+       end if 
+    end do
+    
+    if (coef%ifh2) call addcol4 (w,coef%h2,coef%B,u,coef%dof%n_dofs)
+
+  end subroutine ax_helm_compute
+  
+end module ax_helm
