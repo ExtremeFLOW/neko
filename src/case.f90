5704428515d5eb67b1a30fcfbaa9c98bac391426
Martin Karp <martinkarp@github.com>
2021-02-04 19:46:15 +0100

    user functions

--- src/case.f90
+++ src/case.f90
@@ -112,6 +112,7 @@ contains
     ! Setup user defined functions
     !
     call C%usr%init()
+    call C%usr%usr_msh_setup(C%msh)
 
     !
     ! Setup fluid scheme
@@ -156,6 +157,18 @@ contains
        else if (trim(initial_condition) .eq. 'user') then
           call C%usr%fluid_usr_ic(C%fluid%u, C%fluid%v, &
                C%fluid%w, C%fluid%p, C%params)
+          call gs_op_vector(C%fluid%gs_Xh, C%fluid%u%x, C%fluid%dm_Xh%n_dofs, GS_OP_ADD) 
+          call col2(C%fluid%u%x, C%fluid%c_Xh%mult,C%fluid%dm_Xh%n_dofs) 
+          call gs_op_vector(C%fluid%gs_Xh, C%fluid%v%x, C%fluid%dm_Xh%n_dofs, GS_OP_ADD) 
+          call col2(C%fluid%v%x, C%fluid%c_Xh%mult,C%fluid%dm_Xh%n_dofs) 
+          call gs_op_vector(C%fluid%gs_Xh, C%fluid%w%x, C%fluid%dm_Xh%n_dofs, GS_OP_ADD) 
+          call col2(C%fluid%w%x, C%fluid%c_Xh%mult,C%fluid%dm_Xh%n_dofs) 
+          select type(f => C%fluid)
+          type is(fluid_plan4_t)
+            call copy(f%ulag, C%fluid%u%x, C%fluid%dm_Xh%n_dofs)
+            call copy(f%vlag, C%fluid%v%x, C%fluid%dm_Xh%n_dofs)
+            call copy(f%wlag, C%fluid%w%x, C%fluid%dm_Xh%n_dofs)
+          end select
        else
           call neko_error('Invalid initial condition')
        end if
@@ -191,54 +204,6 @@ contains
     call C%s%add(C%f_out)
     
   end subroutine case_init
-
-  function pipe_ic(x, y, z) result(uvw)
-    real(kind=dp) :: x, y, z
-    real(kind=dp) :: uvw(3)
-    real(kind=dp) :: rand, ux, uy, uz, xr, yr, rr, zo
-    real(kind=dp) :: amp_z, freq_z, freq_t, amp_tht, amp_clip, blt
-    real(kind=dp) :: phase_z, arg_tht, amp_sin, pi, th
-
-    pi = 4d0 * atan(1d0)
-    xr = x
-    yr = y
-    rr = xr*xr + yr*yr
-    if (rr.gt.0) rr=sqrt(rr)
-    th = atan2(y,x)
-    zo = 2*pi*z/25d0
-
-    uz = 6d0*(1d0-rr**6d0)/5d0
-
-    ! Assign a wiggly shear layer near the wall
-    amp_z    = 35d-2  ! Fraction of 2pi for z-based phase modification
-    freq_z   = 4d0     ! Number of wiggles in axial- (z-) direction
-    freq_t   = 9d0     ! Frequency of wiggles in azimuthal-direction
-
-    amp_tht  = 5d0     ! Amplification factor for clipped sine function
-    amp_clip = 2d-1   ! Clipped amplitude
-
-    blt      = 7d-2  ! Fraction of boundary layer with momentum deficit
-
-    phase_z = amp_z*(2d0*pi)*sin(freq_z*zo)
-
-    arg_tht = freq_t*th + phase_z
-    amp_sin = 5d0*sin(arg_tht)
-    if (amp_sin.gt. amp_clip) amp_sin =  amp_clip
-    if (amp_sin.lt.-amp_clip) amp_sin = -amp_clip
-
-    if (rr.gt.(1-blt)) uz = uz + amp_sin
-    call random_number(rand)
-
-    ux   = 5d-2*rand*rand
-    uy   = 1d-1*rand*rand*rand
-    uz   = uz + 1d-2*rand
-    
-    uvw(1) = ux
-    uvw(2) = uy
-    uvw(3) = uz
-
-  end function pipe_ic
-
   !> Deallocate a case 
   subroutine case_free(C)
     type(case_t), intent(inout) :: C
