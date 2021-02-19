a5f7fa33061bbbb089f8c9b0e97d300e126ff029
Niclas Jansson <njansson@kth.se>
2021-02-19 22:29:05 +0100

    Added constrained graph partitioning

--- src/parmetis_wrapper.c
+++ src/parmetis_wrapper.c
@@ -37,3 +37,30 @@ int ParMETIS_V3_PartGeom_wrapper(int *vtxdist, int *ndims,
   return rcode;
   
 }
+
+/*!
+  Fortran wrapper for ParMETIS PartMeshKway
+*/
+int ParMETIS_V3_PartMeshKway_wrapper(int *elmdist, int *eptr, int *eind,
+				     int *elmwgt, int *wgtflag, int *numflag,
+				     int *ncon, int *ncommonnodes, int *nparts,
+				     float *tpwgts, float *ubvec, int *options,
+				     int *edgecut, int *part)
+{
+
+  int rcode;
+  MPI_Comm comm;
+  MPI_Comm_dup(MPI_COMM_WORLD, &comm);
+
+  rcode = METIS_ERROR;
+
+#ifdef HAVE_PARMETIS
+  
+  rcode = ParMETIS_V3_PartMeshKway(elmdist, eptr, eind, elmwgt, wgtflag,
+				   numflag, ncon, ncommonnodes, nparts,tpwgts,
+				   ubvec, options, edgecut, part, &comm);
+#endif
+
+  return rcode;
+
+}
