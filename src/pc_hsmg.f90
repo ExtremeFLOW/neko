a604c98dae911d9362af2f1e68043a8f437b0e13
Martin <martin.karp@outlook.com>
2021-02-24 21:44:20 +0100

    remove loops

--- src/pc_hsmg.f90
+++ src/pc_hsmg.f90
@@ -233,26 +233,24 @@ contains
     class(hsmg_t), intent(inout) :: this
     real(kind=dp), dimension(n), intent(inout) :: z
     real(kind=dp), dimension(n), intent(inout) :: r
-    integer :: i, l
+    integer :: i
     type(ksp_monitor_t) :: crs_info
 
 
     !OVERLAPPING Schwarz exchange and solve
     call this%grids(3)%schwarz%compute(z,r)      
     ! DOWNWARD Leg of V-cycle, we are pretty hardcoded here but w/e
-    do l = this%nlvls-1,2,-1                         !          T
-       !In original code they do not do col2 but only on faces
-       call col2(r, this%grids(l+1)%coef%mult, &
-                 this%grids(l+1)%dof%n_dofs)
-       !Restrict   
-       call tnsr1_3d(r,this%grids(l)%Xh%lx,this%grids(l+1)%Xh%lx,&
-                     this%jht(1,l),this%jh(1,l), this%jh(1,l), &
-                     this%msh%nelv)
-       call gs_op_vector(this%grids(l)%gs_h, r, &
-                         this%grids(l)%dof%n_dofs, GS_OP_ADD)
-       !OVERLAPPING Schwarz exchange and solve
-       call this%grids(l)%schwarz%compute(this%grids(l)%e%x,r)  
-    enddo
+    !In original code they do not do col2 but only on faces
+    call col2(r, this%grids(3)%coef%mult, &
+                 this%grids(3)%dof%n_dofs)
+    !Restrict to middle level
+    call tnsr1_3d(r,this%grids(2)%Xh%lx,this%grids(3)%Xh%lx,&
+                  this%jht(1,2),this%jh(1,2), this%jh(1,2), &
+                  this%msh%nelv)
+    call gs_op_vector(this%grids(2)%gs_h, r, &
+                      this%grids(2)%dof%n_dofs, GS_OP_ADD)
+    !OVERLAPPING Schwarz exchange and solve
+    call this%grids(2)%schwarz%compute(this%grids(2)%e%x,r)  
                                                      !         T
     call col2(r, this%grids(2)%coef%mult, this%grids(2)%dof%n_dofs)
     !restrict residual to crs
@@ -272,21 +270,18 @@ contains
     call bc_list_apply_scalar(this%grids(1)%bclst, this%grids(1)%e%x,&
                               this%grids(1)%dof%n_dofs)
     ! UNWIND.  No smoothing.    
-    do l = 2,this%nlvls-1                          
-       !Interpolate
-       call tnsr3d(this%w,this%grids(l)%Xh%lx,this%grids(l-1)%e%x, &
-                   this%grids(l-1)%Xh%lx,this%jh(1,l-1), &
-                   this%jht(1,l-1), this%jht(1,l-1), this%msh%nelv)
-       call add2(this%grids(l)%e%x,this%w,this%grids(l)%dof%n_dofs)
-    enddo
-
-    l = this%nlvls 
-    call tnsr3d(this%w,this%grids(l)%Xh%lx,this%grids(l-1)%e%x, &
-                this%grids(l-1)%Xh%lx,this%jh(1,l-1), &
-                this%jht(1,l-1), this%jht(1,l-1), this%msh%nelv)
-    call add2(z, this%w, this%grids(l)%dof%n_dofs)
-    call gs_op_vector(this%grids(l)%gs_h, z, &
-                      this%grids(l)%dof%n_dofs, GS_OP_ADD)
-    call col2(z, this%grids(l)%coef%mult, this%grids(l)%dof%n_dofs)
+    !to middle level
+    call tnsr3d(this%w,this%grids(2)%Xh%lx,this%grids(1)%e%x, &
+                this%grids(1)%Xh%lx,this%jh(1,1), &
+                this%jht(1,1), this%jht(1,1), this%msh%nelv)
+    call add2(this%grids(2)%e%x,this%w,this%grids(2)%dof%n_dofs)
+    !to original grid
+    call tnsr3d(this%w,this%grids(3)%Xh%lx,this%grids(2)%e%x, &
+                this%grids(2)%Xh%lx,this%jh(1,2), &
+                this%jht(1,2), this%jht(1,2), this%msh%nelv)
+    call add2(z, this%w, this%grids(3)%dof%n_dofs)
+    call gs_op_vector(this%grids(3)%gs_h, z, &
+                      this%grids(3)%dof%n_dofs, GS_OP_ADD)
+    call col2(z, this%grids(3)%coef%mult, this%grids(3)%dof%n_dofs)
   end subroutine hsmg_solve
 end module hsmg
