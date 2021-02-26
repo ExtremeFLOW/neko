5b21beb094eb0b5225aff33d36f49f47be2cb91a
MartinKarp <martin.karp@outlook.com>
2021-02-26 16:35:53 +0100

    curve struct

---
+++ src/structs.f90
@@ -0,0 +1,12 @@
+!> Defines structs that are used... Dont know if we should keep it though.
+module structs
+  use num_types
+  implicit none
+  
+  type, public :: struct_curve_t
+    real(kind=dp) :: curve_data(6,12)
+    integer :: curve_type(12)
+    integer :: el_idx
+  end type struct_curve_t
+end module structs
+
