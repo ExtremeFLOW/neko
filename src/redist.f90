fded9f451e48e4073a6644303dc0dc4be11ca90d
Niclas Jansson <njansson@kth.se>
2021-03-03 22:31:01 +0100

    Add redist. routines for periodic zones! #23

--- src/redist.f90
+++ src/redist.f90
@@ -26,16 +26,20 @@ contains
     type(nmsh_zone_t), pointer :: zp(:)
     type(nmsh_zone_t), allocatable :: recv_buf_zone(:)
     class(element_t), pointer :: ep
+    integer, allocatable :: recv_buf_idx(:), send_buf_idx(:)
     integer :: status(MPI_STATUS_SIZE)
-    integer :: i, j, ierr, max_recv_msh, max_recv_zone
-    integer :: src, dst, recv_size, gdim, tmp, new_el_idx
+    integer :: i, j, k, ierr, max_recv_msh, max_recv_zone, max_recv_idx
+    integer :: src, dst, recv_size, gdim, tmp, new_el_idx, new_pel_idx
     type(point_t) :: p(8)
-    type(htable_i4_t) :: el_map
+    type(htable_i4_t) :: el_map, glb_map
+    type(stack_i4_t) :: pe_lst
 
     !
     ! Extract new zone distributions
     !     
 
+    call pe_lst%init()
+    
     allocate(new_zone_dist(0:pe_size - 1))
     do i = 0, pe_size - 1
        call new_zone_dist(i)%init()
@@ -45,8 +49,7 @@ contains
     call redist_zone(msh, msh%inlet, 2, parts, new_zone_dist)
     call redist_zone(msh, msh%outlet, 3, parts, new_zone_dist)
     call redist_zone(msh, msh%sympln, 4, parts, new_zone_dist)
-    call redist_zone(msh, msh%periodic, 5, parts, new_zone_dist)
-
+    call redist_zone(msh, msh%periodic, 5, parts, new_zone_dist, pe_lst)
     
     !
     ! Extract new mesh distribution
@@ -109,13 +112,26 @@ contains
 
        do j = 1, recv_size
           call new_zone_dist(pe_rank)%push(recv_buf_zone(j))
-       end do
-              
+       end do              
+    end do
+    
+    deallocate(recv_buf_msh)
+    deallocate(recv_buf_zone)
+
+    do i = 0, pe_size - 1
+       if (i .ne. pe_rank) then
+          call new_mesh_dist(i)%free
+          call new_zone_dist(i)%free
+       end if
     end do
 
+    !
+    ! Create a new mesh based on the given distribution
+    !
     call mesh_init(msh, gdim, new_mesh_dist(pe_rank)%size())
 
     call el_map%init(new_mesh_dist(pe_rank)%size())
+    call glb_map%init(new_mesh_dist(pe_rank)%size())
 
     np => new_mesh_dist(pe_rank)%array()
     do i = 1, new_mesh_dist(pe_rank)%size()
@@ -126,13 +142,64 @@ contains
             p(1), p(2), p(3), p(4), p(5), p(6), p(7), p(8))
 
        if (el_map%get(np(i)%el_idx, tmp) .gt. 0) then
+          ! Old glb to new local
           tmp = i
           call el_map%set(np(i)%el_idx, tmp)
+
+          ! Old glb to new glb
+          tmp = msh%elements(i)%e%id()
+          call glb_map%set(np(i)%el_idx,  tmp)
        else
           call neko_error('Global element id already defined')
        end if       
     end do
+    call new_mesh_dist(pe_rank)%free()
 
+
+    !
+    ! Figure out new mesh distribution (necessary for periodic zones)
+    !
+    max_recv_idx = pe_lst%size()
+    call MPI_Allreduce(MPI_IN_PLACE, max_recv_idx, 1, MPI_INTEGER, &
+         MPI_MAX, NEKO_COMM, ierr)
+    allocate(recv_buf_idx(max_recv_idx))
+    allocate(send_buf_idx(2*max_recv_idx))
+    
+    do i = 1, pe_size - 1
+       src = modulo(pe_rank - i + pe_size, pe_size)
+       dst = modulo(pe_rank + i, pe_size)
+
+       call MPI_Sendrecv(pe_lst%array(), &
+            pe_lst%size(), MPI_INTEGER, dst, 0, recv_buf_idx, &
+            max_recv_idx, MPI_INTEGER, src, 0, NEKO_COMM, status, ierr)
+       call MPI_Get_count(status, MPI_INTEGER, recv_size, ierr)
+
+       k = 0
+       do j = 1, recv_size
+          if (glb_map%get(recv_buf_idx(j), tmp) .eq. 0) then
+             k = k + 1
+             send_buf_idx(k) = recv_buf_idx(j)
+             k = k + 1
+             send_buf_idx(k) = tmp
+          end if
+       end do
+
+       call MPI_Sendrecv(send_buf_idx, k, MPI_INTEGER, src, 0, &
+            recv_buf_idx, max_recv_idx, MPI_INTEGER, dst, 0, &
+            NEKO_COMM, status, ierr)
+       call MPI_Get_count(status, MPI_INTEGER, recv_size, ierr)
+
+       do j = 1, recv_size, 2
+          call glb_map%set(recv_buf_idx(j),  recv_buf_idx(j+1))
+       end do              
+    end do
+    deallocate(recv_buf_idx)
+    deallocate(send_buf_idx)
+    call pe_lst%free()
+    
+    !
+    ! Add zone data for new mesh distribution
+    !     
     zp => new_zone_dist(pe_rank)%array()
     do i = 1, new_zone_dist(pe_rank)%size()
        if (el_map%get(zp(i)%e, new_el_idx) .gt. 0) then
@@ -147,19 +214,30 @@ contains
           call mesh_mark_outlet_facet(msh, zp(i)%f, new_el_idx)
        case(4)
           call mesh_mark_sympln_facet(msh, zp(i)%f, new_el_idx)
+       case(5)
+
+          if (glb_map%get(zp(i)%p_e, new_pel_idx) .gt. 0) then
+             call neko_error('Missing periodic element after redistribution')
+          end if
+          
+          call mesh_apply_periodic_facet(msh, zp(i)%f, new_el_idx, &
+               zp(i)%p_f, new_pel_idx, zp(i)%glb_pt_ids)
        end select
     end do
+    call new_zone_dist(pe_rank)%free()
 
     call mesh_finalize(msh)
     
   end subroutine redist_mesh
 
-  subroutine redist_zone(msh, z, type, parts, new_dist)
+  !> Fill redistribution list for zone data
+  subroutine redist_zone(msh, z, type, parts, new_dist, pel)
     type(mesh_t), intent(inout) :: msh
     class(zone_t), intent(in) :: z
     integer, intent(in) :: type
     type(mesh_fld_t), intent(in) :: parts
     type(stack_nz_t), intent(inout), allocatable :: new_dist(:)
+    type(stack_i4_t), intent(inout), optional :: pel
     type(nmsh_zone_t) :: nmsh_zone
     integer :: i, j, zone_el
 
@@ -180,13 +258,11 @@ contains
           nmsh_zone%p_e = zp%p_facet_el(i)%x(2)
           nmsh_zone%p_f = zp%p_facet_el(i)%x(1)
           nmsh_zone%glb_pt_ids = zp%p_ids(i)%x          
-          nmsh_zone%type = type
+          nmsh_zone%type = type          
           call new_dist(parts%data(zone_el))%push(nmsh_zone)
+          call pel%push(nmsh_zone%p_e)
        end do
     end select
-
-
-       
   end subroutine redist_zone
 
 end module redist
