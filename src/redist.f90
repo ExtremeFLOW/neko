6fc151d5c54c57c8dc93a898fb4d552442708421
Niclas Jansson <njansson@kth.se>
2021-03-05 22:36:26 +0100

    Fix buffers and add curved elements to redist. routines

--- src/redist.f90
+++ src/redist.f90
@@ -5,13 +5,17 @@ module redist
   use htable
   use point
   use stack
+  use curve
   use comm
   use mesh
   use nmsh
   use zone
   use mpi  
   implicit none
+  private
 
+  public :: redist_mesh
+  
 contains
 
   !> Redistribute a mesh @a msh according to new partitions
@@ -20,16 +24,20 @@ contains
     type(mesh_fld_t), intent(in) :: parts         !< Partitions
     type(stack_nh_t), allocatable :: new_mesh_dist(:)
     type(stack_nz_t), allocatable :: new_zone_dist(:)
+    type(stack_nc_t), allocatable :: new_curve_dist(:)
     type(nmsh_hex_t) :: el
     type(nmsh_hex_t), pointer :: np(:)
     type(nmsh_hex_t), allocatable :: recv_buf_msh(:)
     type(nmsh_zone_t), pointer :: zp(:)
     type(nmsh_zone_t), allocatable :: recv_buf_zone(:)
+    type(nmsh_curve_el_t), pointer :: cp(:)
+    type(nmsh_curve_el_t), allocatable :: recv_buf_curve(:)
     class(element_t), pointer :: ep
     integer, allocatable :: recv_buf_idx(:), send_buf_idx(:)
     integer :: status(MPI_STATUS_SIZE)
-    integer :: i, j, k, ierr, max_recv_msh, max_recv_zone, max_recv_idx
+    integer :: i, j, k, ierr, max_recv_idx
     integer :: src, dst, recv_size, gdim, tmp, new_el_idx, new_pel_idx
+    integer :: max_recv(3)
     type(point_t) :: p(8)
     type(htable_i4_t) :: el_map, glb_map
     type(stack_i4_t) :: pe_lst
@@ -50,6 +58,16 @@ contains
     call redist_zone(msh, msh%outlet, 3, parts, new_zone_dist)
     call redist_zone(msh, msh%sympln, 4, parts, new_zone_dist)
     call redist_zone(msh, msh%periodic, 5, parts, new_zone_dist, pe_lst)
+
+    !
+    ! Extract new curve info. distributions
+    !
+    allocate(new_curve_dist(0:pe_size - 1))
+    do i = 0, pe_size - 1
+       call new_curve_dist(i)%init()
+    end do
+
+    call redist_curve(msh, msh%curve, parts, new_curve_dist)
     
     !
     ! Extract new mesh distribution
@@ -74,31 +92,26 @@ contains
     gdim = msh%gdim    
     call mesh_free(msh)
 
-    max_recv_msh = 0
+    max_recv(1) = 0
     do i = 0, pe_size - 1
-       max_recv_msh = max(max_recv_msh, new_mesh_dist(i)%size())
+       max_recv(1) = max(max_recv(1), new_mesh_dist(i)%size())
+       max_recv(2) = max(max_recv(2), new_zone_dist(i)%size())
+       max_recv(3) = max(max_recv(3), new_curve_dist(i)%size())
     end do
     
-    call MPI_Allreduce(MPI_IN_PLACE, max_recv_msh, 1, MPI_INTEGER, &
+    call MPI_Allreduce(MPI_IN_PLACE, max_recv, 3, MPI_INTEGER, &
          MPI_MAX, NEKO_COMM, ierr)
-    allocate(recv_buf_msh(max_recv_msh))
-
-    max_recv_zone = 0
-    do i = 0, pe_size - 1
-       max_recv_zone = max(max_recv_zone, new_zone_dist(i)%size())
-    end do
+    allocate(recv_buf_msh(max_recv(1)))
+    allocate(recv_buf_zone(max_recv(2)))
+    allocate(recv_buf_curve(max_recv(3)))
     
-    call MPI_Allreduce(MPI_IN_PLACE, max_recv_zone, 1, MPI_INTEGER, &
-         MPI_MAX, NEKO_COMM, ierr)
-    allocate(recv_buf_zone(max_recv_zone))
-
     do i = 1, pe_size - 1
        src = modulo(pe_rank - i + pe_size, pe_size)
        dst = modulo(pe_rank + i, pe_size)
 
        call MPI_Sendrecv(new_mesh_dist(dst)%array(), &
             new_mesh_dist(dst)%size(), MPI_NMSH_HEX, dst, 0, recv_buf_msh, &
-            max_recv_msh, MPI_NMSH_HEX, src, 0, NEKO_COMM, status, ierr)
+            max_recv(1), MPI_NMSH_HEX, src, 0, NEKO_COMM, status, ierr)
        call MPI_Get_count(status, MPI_NMSH_HEX, recv_size, ierr)
 
        do j = 1, recv_size
@@ -106,22 +119,33 @@ contains
        end do
 
        call MPI_Sendrecv(new_zone_dist(dst)%array(), &
-            new_zone_dist(dst)%size(), MPI_NMSH_ZONE, dst, 0, recv_buf_zone,&
-            max_recv_zone, MPI_NMSH_ZONE, src, 0, NEKO_COMM, status, ierr)
+            new_zone_dist(dst)%size(), MPI_NMSH_ZONE, dst, 1, recv_buf_zone,&
+            max_recv(2), MPI_NMSH_ZONE, src, 1, NEKO_COMM, status, ierr)
        call MPI_Get_count(status, MPI_NMSH_ZONE, recv_size, ierr)
 
        do j = 1, recv_size
           call new_zone_dist(pe_rank)%push(recv_buf_zone(j))
-       end do              
+       end do
+
+       call MPI_Sendrecv(new_curve_dist(dst)%array(), &
+            new_curve_dist(dst)%size(), MPI_NMSH_CURVE, dst, 2, recv_buf_curve,&
+            max_recv(3), MPI_NMSH_CURVE, src, 2, NEKO_COMM, status, ierr)
+       call MPI_Get_count(status, MPI_NMSH_CURVE, recv_size, ierr)
+
+       do j = 1, recv_size
+          call new_curve_dist(pe_rank)%push(recv_buf_curve(j))
+      end do              
     end do
     
     deallocate(recv_buf_msh)
     deallocate(recv_buf_zone)
+    deallocate(recv_buf_curve)
 
     do i = 0, pe_size - 1
        if (i .ne. pe_rank) then
           call new_mesh_dist(i)%free
           call new_zone_dist(i)%free
+          call new_curve_dist(i)%free
        end if
     end do
 
@@ -159,11 +183,11 @@ contains
     !
     ! Figure out new mesh distribution (necessary for periodic zones)
     !
-    max_recv_idx = pe_lst%size()
+    max_recv_idx = 2 * pe_lst%size()
     call MPI_Allreduce(MPI_IN_PLACE, max_recv_idx, 1, MPI_INTEGER, &
          MPI_MAX, NEKO_COMM, ierr)
     allocate(recv_buf_idx(max_recv_idx))
-    allocate(send_buf_idx(2*max_recv_idx))
+    allocate(send_buf_idx(max_recv_idx))
     
     do i = 1, pe_size - 1
        src = modulo(pe_rank - i + pe_size, pe_size)
@@ -184,8 +208,8 @@ contains
           end if
        end do
 
-       call MPI_Sendrecv(send_buf_idx, k, MPI_INTEGER, src, 0, &
-            recv_buf_idx, max_recv_idx, MPI_INTEGER, dst, 0, &
+       call MPI_Sendrecv(send_buf_idx, k, MPI_INTEGER, src, 1, &
+            recv_buf_idx, max_recv_idx, MPI_INTEGER, dst, 1, &
             NEKO_COMM, status, ierr)
        call MPI_Get_count(status, MPI_INTEGER, recv_size, ierr)
 
@@ -215,7 +239,6 @@ contains
        case(4)
           call mesh_mark_sympln_facet(msh, zp(i)%f, new_el_idx)
        case(5)
-
           if (glb_map%get(zp(i)%p_e, new_pel_idx) .gt. 0) then
              call neko_error('Missing periodic element after redistribution')
           end if
@@ -225,6 +248,18 @@ contains
        end select
     end do
     call new_zone_dist(pe_rank)%free()
+
+    !
+    ! Add curve element information for new mesh distribution
+    !
+    cp => new_curve_dist(pe_rank)%array()
+    do i = 1, new_curve_dist(pe_rank)%size()
+       if (el_map%get(cp(i)%e, new_el_idx) .gt. 0) then
+          call neko_error('Missing element after redistribution')
+       end if
+       call mesh_mark_curve_element(msh, new_el_idx, cp(i)%curve_data, cp(i)%type)
+    end do
+    call new_curve_dist(pe_rank)%free()
 
     call mesh_finalize(msh)
     
@@ -242,16 +277,8 @@ contains
     integer :: i, j, zone_el
 
     select type(zp => z)
-    type is (zone_t)
-       do i = 1, z%size
-          zone_el =  zp%facet_el(i)%x(2)
-          nmsh_zone%e = zp%facet_el(i)%x(2) + msh%offset_el
-          nmsh_zone%f = zp%facet_el(i)%x(1)
-          nmsh_zone%type = type
-          call new_dist(parts%data(zone_el))%push(nmsh_zone)
-       end do
     type is (zone_periodic_t)
-       do i = 1, z%size
+       do i = 1, zp%size
           zone_el =  zp%facet_el(i)%x(2)
           nmsh_zone%e = zp%facet_el(i)%x(2) + msh%offset_el
           nmsh_zone%f = zp%facet_el(i)%x(1)
@@ -262,7 +289,34 @@ contains
           call new_dist(parts%data(zone_el))%push(nmsh_zone)
           call pel%push(nmsh_zone%p_e)
        end do
+    type is (zone_t)
+       do i = 1, zp%size
+          zone_el =  zp%facet_el(i)%x(2)
+          nmsh_zone%e = zp%facet_el(i)%x(2) + msh%offset_el
+          nmsh_zone%f = zp%facet_el(i)%x(1)
+          nmsh_zone%type = type
+          call new_dist(parts%data(zone_el))%push(nmsh_zone)
+       end do
     end select
   end subroutine redist_zone
+
+  !> Fill redistribution list for zone data
+  subroutine redist_curve(msh, c, parts, new_dist)
+    type(mesh_t), intent(inout) :: msh
+    type(curve_t), intent(in) :: c
+    type(mesh_fld_t), intent(in) :: parts
+    type(stack_nc_t), intent(inout), allocatable :: new_dist(:)
+    type(nmsh_curve_el_t) :: nmsh_curve
+    integer :: i, j, curve_el
+
+    do i = 1, c%size
+       curve_el = c%curve_el(i)%el_idx
+       nmsh_curve%e = curve_el + msh%offset_el
+       nmsh_curve%curve_data = c%curve_el(i)%curve_data
+       nmsh_curve%type = c%curve_el(i)%curve_type
+       call new_dist(parts%data(curve_el))%push(nmsh_curve)
+    end do
+    
+  end subroutine redist_curve
 
 end module redist
