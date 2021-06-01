f296fb8f2edcf35925ea6743eb91bfb501d2378c
Niclas Jansson <njansson@kth.se>
2021-06-01 22:49:06 +0200

    Add CUDA device layer, #113

--- src/device.F90
+++ src/device.F90
@@ -1,5 +1,6 @@
 module device
   use num_types
+  use cuda_intf
   use hip_intf
   use utils
   use, intrinsic :: iso_c_binding
@@ -17,6 +18,10 @@ contains
     if (hipmalloc(x_d, s) .ne. HIP_SUCCESS) then
        call neko_error('Memory allocation on device failed')
     end if
+#elif HAVE_CUDA
+    if (cudamalloc(x_d, s) .ne. CUDA_SUCCESS) then
+       call neko_error('Memory allocation on device failed')
+    end if
 #endif
   end subroutine device_alloc
 
@@ -25,6 +30,10 @@ contains
     type(c_ptr), intent(inout) :: x_d
 #ifdef HAVE_HIP
     if (hipfree(x_d) .ne. HIP_SUCCESS) then
+       call neko_error('Memory deallocation on device failed')
+    end if
+#elif HAVE_CUDA
+    if (cudafree(x_d) .ne. CUDA_SUCCESS) then
        call neko_error('Memory deallocation on device failed')
     end if
 #endif
@@ -36,20 +45,35 @@ contains
     class(*), intent(inout), target :: x(n)
     type(c_ptr), intent(inout) :: x_d
     integer, intent(in), value :: dir
+    type(c_ptr) :: ptr_h
     integer(c_size_t) :: s
 
     select type(x)
     type is (integer)
        s = n * 4
-    type is (integer(8))
+       ptr_h = c_loc(x)
+    type is (integer(8))       
        s = n * 8
+       ptr_h = c_loc(x)
     type is (real)
        s = n * 4
+       ptr_h = c_loc(x)
     type is (double precision)
        s = n * 8
+       ptr_h = c_loc(x)
     end select
 #ifdef HAVE_HIP
-    if (hipMemcpy(c_loc(x), x_d, s, dir) .ne. HIP_SUCCESS) then
+    if (hipmemcpy(ptr_h, x_d, s, dir) .ne. HIP_SUCCESS) then
+       if (dir .eq. HOST_TO_DEVICE) then
+          call neko_error('Device memcpy (host-to-device) failed')
+       else if (dir .eq. DEVICE_TO_HOST) then
+          call neko_error('Device memcpy (device-to-host) failed')
+       else
+          call neko_error('Device memcpy failed (invalid direction')
+       end if
+    end if
+#elif HAVE_CUDA
+    if (cudamemcpy(ptr_h, x_d, s, dir) .ne. CUDA_SUCCESS) then
        if (dir .eq. HOST_TO_DEVICE) then
           call neko_error('Device memcpy (host-to-device) failed')
        else if (dir .eq. DEVICE_TO_HOST) then
@@ -65,6 +89,10 @@ contains
   subroutine device_sync()
 #ifdef HAVE_HIP
     if (hipdevicesynchronize() .ne. HIP_SUCCESS) then
+       call neko_error('Error during device sync')
+    end if
+#elif HAVE_CUDA
+    if (cudadevicesynchronize() .ne. CUDA_SUCCESS) then
        call neko_error('Error during device sync')
     end if
 #endif
