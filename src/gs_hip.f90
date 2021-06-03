5a350aeb3d507a6bcec34698d224a004809b0815
Niclas Jansson <njansson@kth.se>
2021-06-03 09:33:12 +0200

    Allocate and associate device pointer on first call to gather, #119

--- src/gs_hip.f90
+++ src/gs_hip.f90
@@ -10,11 +10,17 @@ module gs_hip
 
   !> Gather-scatter backend for HIP enabled devices
   type, public, extends(gs_bcknd_t) :: gs_hip_t
-     real(kind=rp), allocatable :: local_wrk(:)
-     real(kind=rp), allocatable :: shared_wrk(:)
-     type(c_ptr) :: local_wrk_d
-     type(c_ptr) :: shared_wrk_d
-     integer :: nlocal
+     real(kind=rp), allocatable :: local_wrk(:)  !< Scratch array for local
+     real(kind=rp), allocatable :: shared_wrk(:) !< Scratch array for shared
+     type(c_ptr) :: local_gs_d      !< Device ptr for local gs-ops
+     type(c_ptr) :: local_dof_gs_d  !< Device ptr for local dof to gs mapping
+     type(c_ptr) :: local_gs_dof_d  !< Device ptr for local gs to dof mapping
+     type(c_ptr) :: shared_gs_d     !< Device ptr for shared gs-ops
+     type(c_ptr) :: shared_dof_gs_d !< Device ptr for shared dof to gs mapping
+     type(c_ptr) :: shared_gs_dof_d !< Device ptr for shared gs to dof mapping
+     type(c_ptr) :: local_wrk_d     !< Device ptr for local scratch array
+     type(c_ptr) :: shared_wrk_d    !< Device ptr for shared scratch array
+     integer :: nlocal              
      integer :: nshared
    contains
      procedure, pass(this) :: init => gs_hip_init
@@ -67,6 +73,13 @@ contains
     n = nshared * 8
     call device_alloc(this%shared_wrk_d, n)
     call device_associate(this%shared_wrk, this%shared_wrk_d, nshared)
+
+    this%local_gs_d = C_NULL_PTR
+    this%local_dof_gs_d = C_NULL_PTR
+    this%local_gs_dof_d = C_NULL_PTR
+    this%shared_gs_d = C_NULL_PTR
+    this%shared_dof_gs_d = C_NULL_PTR
+    this%shared_gs_dof_d = C_NULL_PTR    
       
   end subroutine gs_hip_init
 
@@ -80,6 +93,18 @@ contains
 
     if (allocated(this%shared_wrk)) then
        deallocate(this%shared_wrk)
+    end if
+
+    if (c_associated(this%local_gs_d)) then
+       call device_free(this%local_gs_d)
+    end if
+
+    if (c_associated(this%local_dof_gs_d)) then
+       call device_free(this%local_dof_gs_d)
+    end if
+
+    if (c_associated(this%local_gs_dof_d)) then
+       call device_free(this%local_gs_dof_d)
     end if
 
     this%nlocal = 0
@@ -100,20 +125,55 @@ contains
     integer, dimension(nb), intent(inout) :: b
     integer, intent(inout) :: o
     integer :: op
-    type(c_ptr) :: v_d, u_d, dg_d, gd_d
+    integer(c_size_t) :: buflen
+    integer(c_size_t) :: maplen
+    type(c_ptr) :: u_d
 
-    v_d = device_get_ptr(v, m)
+    buflen = m * 8
+    maplen = m * 4
+    
     u_d = device_get_ptr(u, n)
-
-    dg_d = device_get_ptr(dg, m)
-    gd_d = device_get_ptr(gd, m)
         
-    if (this%nlocal .eq. m) then
-       associate(w=>this%local_wrk, w_d=>this%local_wrk_d)
+    if (this%nlocal .eq. m) then       
+       associate(v_d=>this%local_gs_d, dg_d=>this%local_dof_gs_d, &
+            gd_d=>this%local_gs_dof_d, w_d=>this%local_wrk_d)
+
+         if (.not. c_associated(v_d)) then
+            call device_alloc(v_d, buflen)
+            call device_associate(v, v_d, m)
+         end if
+
+         if (.not. c_associated(dg_d)) then
+            call device_alloc(dg_d, maplen)
+            call device_associate(dg, dg_d, m)
+         end if
+
+         if (.not. c_associated(gd_d)) then
+            call device_alloc(gd_d, maplen)
+            call device_associate(gd, gd_d, m)
+         end if
+         
          call hip_gather_kernel(v_d, m, o, dg_d, u_d, n, gd_d, w_d, op) 
        end associate
     else if (this%nshared .eq. m) then
-       associate(w=>this%shared_wrk, w_d=>this%shared_wrk_d)
+       associate(v_d=>this%shared_gs_d, dg_d=>this%shared_dof_gs_d, &
+            gd_d=>this%shared_gs_dof_d, w_d=>this%shared_wrk_d)
+
+         if (.not. c_associated(v_d)) then
+            call device_alloc(v_d, buflen)
+            call device_associate(v, v_d, m)
+         end if
+
+         if (.not. c_associated(dg_d)) then
+            call device_alloc(dg_d, maplen)
+            call device_associate(dg, dg_d, m)
+         end if
+
+         if (.not. c_associated(gd_d)) then
+            call device_alloc(gd_d, maplen)
+            call device_associate(gd, gd_d, m)
+         end if
+         
          call hip_gather_kernel(v_d, m, o, dg_d, u_d, n, gd_d, w_d, op) 
        end associate
     end if
