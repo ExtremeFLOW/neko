6eb45b73f437380cf61f8bbd3651a55700ba73e4
Niclas Jansson <njansson@kth.se>
2021-09-01 21:32:06 +0200

    Add mean squared statistics

---
+++ src/field/mean_sqr_field.f90
@@ -0,0 +1,29 @@
+!> Defines a mean square field
+!
+module mean_sqr_field
+  use mean_field
+  use num_types
+  use field
+  use math
+  implicit none
+
+  type, extends(mean_field_t) :: mean_sqr_field_t
+   contains
+     procedure, pass(this) :: update => mean_sqr_field_update
+  end type mean_sqr_field_t
+
+contains    
+
+  !> Update a mean sqr field
+  subroutine mean_sqr_field_update(this, k)
+    class(mean_sqr_field_t), intent(inout) :: this
+    real(kind=dp), intent(in) :: k !< Time since last sample
+
+    this%mf%x = this%mf%x * this%time
+    call addsqr2s2(this%mf%x, this%f%x, k, this%mf%dof%n_dofs)
+    this%time = this%time + k
+    this%mf%x = this%mf%x / this%time
+
+  end subroutine mean_sqr_field_update
+
+end module mean_sqr_field
