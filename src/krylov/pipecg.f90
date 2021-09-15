396709e1de465c80de45496552ec15341fe76896
Niclas Jansson <njansson@kth.se>
2021-09-15 11:22:16 +0200

    Make the linter happy

--- src/krylov/pipecg.f90
+++ src/krylov/pipecg.f90
@@ -4,7 +4,7 @@ module pipecg
   use math
   use num_types
   implicit none
-
+  
   !> Pipelined preconditioned conjugate gradient method
   type, public, extends(ksp_t) :: pipecg_t
      real(kind=rp), allocatable :: p(:)
@@ -22,9 +22,9 @@ module pipecg
      procedure, pass(this) :: free => pipecg_free
      procedure, pass(this) :: solve => pipecg_solve
   end type pipecg_t
-
+  
 contains
-
+  
   !> Initialise a pipelined PCG solver
   subroutine pipecg_init(this, n, M, rel_tol, abs_tol)
     class(pipecg_t), intent(inout) :: this
@@ -32,9 +32,9 @@ contains
     integer, intent(in) :: n
     real(kind=rp), optional, intent(inout) :: rel_tol
     real(kind=rp), optional, intent(inout) :: abs_tol
-        
+    
     call this%free()
-
+    
     this%p_space = 50
     
     allocate(this%p(n))
@@ -49,7 +49,7 @@ contains
     if (present(M)) then 
        this%M => M
     end if
-
+    
     if (present(rel_tol) .and. present(abs_tol)) then
        call this%ksp_init(rel_tol, abs_tol)
     else if (present(rel_tol)) then
@@ -59,15 +59,15 @@ contains
     else
        call this%ksp_init()
     end if
-          
+    
   end subroutine pipecg_init
-
+  
   !> Deallocate a pipelined PCG solver
   subroutine pipecg_free(this)
     class(pipecg_t), intent(inout) :: this
-
+    
     call this%ksp_free()
-
+    
     if (allocated(this%p)) then
        deallocate(this%p)
     end if
@@ -95,7 +95,7 @@ contains
     if (allocated(this%ni)) then
        deallocate(this%ni)
     end if
-
+    
     nullify(this%M)
 
 
@@ -115,7 +115,8 @@ contains
     integer, optional, intent(in) :: niter
     integer :: iter, max_iter, i, j, k, ierr, p_cur, p_prev, u_prev
     real(kind=rp) :: rnorm, rtr, reduction(3), norm_fac
-    real(kind=rp) :: alpha(this%p_space), beta(this%p_space), gamma1, gamma2, delta
+    real(kind=rp) :: alpha(this%p_space), beta(this%p_space)
+    real(kind=rp) :: gamma1, gamma2, delta
     real(kind=rp) :: tmp1, tmp2, tmp3, x_plus(NEKO_BLK_SIZE)
     type(MPI_Request) :: request
     type(MPI_Status) :: status
@@ -127,7 +128,7 @@ contains
        max_iter = KSP_MAX_ITER
     end if
     norm_fac = 1.0_rp / sqrt(coef%volume)
-
+    
     associate(p => this%p, q => this%q, r => this%r, s => this%s, &
          u => this%u, w => this%w, z => this%z, mi => this%mi, ni => this%ni)
       
@@ -144,14 +145,14 @@ contains
       call Ax%compute(w, u(1,u_prev), coef, x%msh, x%Xh)
       call gs_op(gs_h, w, n, GS_OP_ADD)
       call bc_list_apply(blst, w, n)
-    
+      
       rtr = glsc3(r, coef%mult, r, n)
       rnorm = sqrt(rtr)*norm_fac
       ksp_results%res_start = rnorm
       ksp_results%res_final = rnorm
       ksp_results%iter = 0
       if(rnorm .eq. 0.0_rp) return
-
+      
       gamma1 = 0.0_rp
       tmp1 = 0.0_rp
       tmp2 = 0.0_rp
@@ -164,8 +165,7 @@ contains
       reduction(1) = tmp1
       reduction(2) = tmp2
       reduction(3) = tmp3
-
-
+      
       do iter = 1, max_iter
          call MPI_Iallreduce(MPI_IN_PLACE, reduction, 3, &
               MPI_REAL_PRECISION, MPI_SUM, NEKO_COMM, request, ierr)
@@ -174,16 +174,15 @@ contains
          call Ax%compute(ni, mi, coef, x%msh, x%Xh)
          call gs_op(gs_h, ni, n, GS_OP_ADD)
          call bc_list_apply(blst, ni, n)
-
+         
          call MPI_Wait(request, status, ierr)
          gamma2 = gamma1       
          gamma1 = reduction(1)
          delta = reduction(2)
          rtr = reduction(3)
-
+         
          rnorm = sqrt(rtr)*norm_fac
          if (rnorm .lt. this%abs_tol) exit
-
 
          if (iter .gt. 1) then
             beta(p_cur) = gamma1 / gamma2
@@ -192,17 +191,17 @@ contains
             beta(p_cur) = 0.0_rp
             alpha(p_cur) = gamma1/delta
          end if
-                  
+         
          !call add2s1(z, ni, beta, n)
          !call add2s1(q, mi, beta, n)
          !call add2s1(s, w, beta, n)
          !call add2s1(p, u, beta, n)
- 
+         
          !call add2s2(x%x, p, alpha, n)
          !call add2s2(r, s, -alpha, n)
          !call add2s2(u, q, -alpha, n)
          !call add2s2(w, z, -alpha, n)
-
+         
          tmp1 = 0.0_rp
          tmp2 = 0.0_rp
          tmp3 = 0.0_rp
@@ -233,9 +232,11 @@ contains
                end do
             end if
          end do
+         
          reduction(1) = tmp1
          reduction(2) = tmp2
          reduction(3) = tmp3
+         
          if (p_cur .eq. this%p_space) then
             do i = 0, n, NEKO_BLK_SIZE
                if (i + NEKO_BLK_SIZE .le. n) then
@@ -245,7 +246,7 @@ contains
                   p_prev = this%p_space+1
                   do j = 1, p_cur
                      do k = 1, NEKO_BLK_SIZE
-                        p(i+k) = beta(j)* p(i+k) + u(i+k,p_prev)
+                        p(i+k) = beta(j) * p(i+k) + u(i+k,p_prev)
                         x_plus(k) = x_plus(k) + alpha(j) * p(i+k)
                      end do
                      p_prev = j
@@ -259,7 +260,7 @@ contains
                      x_plus(1) = 0.0
                      p_prev = this%p_space + 1
                      do j = 1, p_cur
-                        p(i+k) = beta(j)* p(i+k) + u(i+k,p_prev)
+                        p(i+k) = beta(j) * p(i+k) + u(i+k,p_prev)
                         x_plus(1) = x_plus(1) + alpha(j) * p(i+k)
                         p_prev = j
                      end do
@@ -279,40 +280,41 @@ contains
             p_cur = p_cur + 1
          end if
       end do
+      
       if ( p_cur .ne. 1) then
-            do i = 0, n, NEKO_BLK_SIZE
-               if (i + NEKO_BLK_SIZE .le. n) then
+         do i = 0, n, NEKO_BLK_SIZE
+            if (i + NEKO_BLK_SIZE .le. n) then
+               do k = 1, NEKO_BLK_SIZE
+                  x_plus(k) = 0.0
+               end do
+               p_prev = this%p_space+1
+               do j = 1, p_cur
                   do k = 1, NEKO_BLK_SIZE
-                     x_plus(k) = 0.0
+                     p(i+k) = beta(j) * p(i+k) + u(i+k,p_prev)
+                     x_plus(k) = x_plus(k) + alpha(j) * p(i+k)
                   end do
-                  p_prev = this%p_space+1
+                  p_prev = j
+               end do
+               do k = 1, NEKO_BLK_SIZE
+                  x%x(i+k,1,1,1) = x%x(i+k,1,1,1) + x_plus(k)
+                  u(i+k,this%p_space+1) = u(i+k,this%p_space)
+               end do
+            else 
+               do k = 1, n-i
+                  x_plus(1) = 0.0
+                  p_prev = this%p_space + 1
                   do j = 1, p_cur
-                     do k = 1, NEKO_BLK_SIZE
-                        p(i+k) = beta(j)* p(i+k) + u(i+k,p_prev)
-                        x_plus(k) = x_plus(k) + alpha(j) * p(i+k)
-                     end do
+                     p(i+k) = beta(j) * p(i+k) + u(i+k,p_prev)
+                     x_plus(1) = x_plus(1) + alpha(j) * p(i+k)
                      p_prev = j
                   end do
-                  do k = 1, NEKO_BLK_SIZE
-                     x%x(i+k,1,1,1) = x%x(i+k,1,1,1) + x_plus(k)
-                     u(i+k,this%p_space+1) = u(i+k,this%p_space)
-                  end do
-               else 
-                  do k = 1, n-i
-                     x_plus(1) = 0.0
-                     p_prev = this%p_space + 1
-                     do j = 1, p_cur
-                        p(i+k) = beta(j)* p(i+k) + u(i+k,p_prev)
-                        x_plus(1) = x_plus(1) + alpha(j) * p(i+k)
-                        p_prev = j
-                     end do
-                     x%x(i+k,1,1,1) = x%x(i+k,1,1,1) + x_plus(1)
-                     u(i+k,this%p_space+1) = u(i+k,this%p_space)
-                  end do
-               end if
-            end do
-         end if
-
+                  x%x(i+k,1,1,1) = x%x(i+k,1,1,1) + x_plus(1)
+                  u(i+k,this%p_space+1) = u(i+k,this%p_space)
+               end do
+            end if
+         end do
+      end if
+      
       ksp_results%res_final = rnorm
       ksp_results%iter = iter
       
