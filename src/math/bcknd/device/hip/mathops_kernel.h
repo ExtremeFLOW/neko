ef3bd0030fb398052ae56a40dab74dedca5135f4
Niclas Jansson <njansson@kth.se>
2021-10-06 11:32:49 +0200

    Add device mathops and corresponding HIP kernels

---
+++ src/math/bcknd/device/hip/mathops_kernel.h
@@ -0,0 +1,161 @@
+
+/**
+ * Device kernel for opchsign
+ */
+template< typename T>
+__global__ void opchsign_kernel(T * __restrict__ a1, 
+				T * __restrict__ a2, 
+				T * __restrict__ a3,
+				const int gdim,
+				const int n) {
+
+  const int idx = blockIdx.x * blockDim.x + threadIdx.x;
+  const int str = blockDim.x * gridDim.x;
+
+  if (gdim == 3) {
+    for (int i = idx; i < n; i += str) {
+      a1[i] = -a1[i];
+      a2[i] = -a2[i];
+      a3[i] = -a3[i];
+    }
+  } 
+  else {
+    for (int i = idx; i < n; i += str) {
+      a1[i] = -a1[i];
+      a2[i] = -a2[i];
+    }
+  }
+
+}
+
+/**
+ * Device kernel for opcolv
+ */
+template< typename T>
+__global__ void opcolv_kernel(T * __restrict__ a1, 
+			      T * __restrict__ a2, 
+			      T * __restrict__ a3,
+			      const T * __restrict__ c,
+			      const int gdim,
+			      const int n) {
+
+  const int idx = blockIdx.x * blockDim.x + threadIdx.x;
+  const int str = blockDim.x * gridDim.x;
+
+  if (gdim == 3) {
+    for (int i = idx; i < n; i += str) {
+      a1[i] = a1[i] * c[i];
+      a2[i] = a2[i] * c[i];
+      a3[i] = a3[i] * c[i];
+    }
+  } 
+  else {
+    for (int i = idx; i < n; i += str) {
+      a1[i] = a1[i] * c[i];
+      a2[i] = a2[i] * c[i];
+    }
+  }
+
+}
+
+/**
+ * Device kernel for opcolv3c
+ */
+template< typename T>
+__global__ void opcolv3c_kernel(T * __restrict__ a1, 
+				T * __restrict__ a2, 
+				T * __restrict__ a3,
+				const T * __restrict__ b1, 
+				const T * __restrict__ b2, 
+				const T * __restrict__ b3,
+				const T * __restrict__ c,
+				const T d,
+				const int gdim,
+				const int n) {
+
+  const int idx = blockIdx.x * blockDim.x + threadIdx.x;
+  const int str = blockDim.x * gridDim.x;
+
+  if (gdim == 3) {
+    for (int i = idx; i < n; i += str) {
+      a1[i] = b1[i] * c[i] * d;
+      a2[i] = b2[i] * c[i] * d;
+      a3[i] = b3[i] * c[i] * d;
+    }
+  } 
+  else {
+    for (int i = idx; i < n; i += str) {
+      a1[i] = b1[i] * c[i] * d;
+      a2[i] = b2[i] * c[i] * d;
+    }
+  }
+
+}
+
+/**
+ * Device kernel for opadd2cm
+ */
+template< typename T>
+__global__ void opadd2cm_kernel(T * __restrict__ a1, 
+				T * __restrict__ a2, 
+				T * __restrict__ a3,
+				const T * __restrict__ b1, 
+				const T * __restrict__ b2, 
+				const T * __restrict__ b3,
+				const T c,
+				const int gdim,
+				const int n) {
+
+  const int idx = blockIdx.x * blockDim.x + threadIdx.x;
+  const int str = blockDim.x * gridDim.x;
+
+  if (gdim == 3) {
+    for (int i = idx; i < n; i += str) {
+      a1[i] = a1[i] + b1[i] * c;
+      a2[i] = a2[i] + b2[i] * c;
+      a3[i] = a3[i] + b3[i] * c;
+    }
+  } 
+  else {
+    for (int i = idx; i < n; i += str) {
+      a1[i] = a1[i] + b1[i] * c;
+      a2[i] = a2[i] + b2[i] * c;
+    }
+  }
+
+}
+
+
+/**
+ * Device kernel for opadd2col
+ */
+template< typename T>
+__global__ void opadd2col_kernel(T * __restrict__ a1, 
+				 T * __restrict__ a2, 
+				 T * __restrict__ a3,
+				 const T * __restrict__ b1, 
+				 const T * __restrict__ b2, 
+				 const T * __restrict__ b3,
+				 const T * __restrict__ c,
+				 const int gdim,
+				 const int n) {
+
+  const int idx = blockIdx.x * blockDim.x + threadIdx.x;
+  const int str = blockDim.x * gridDim.x;
+
+  if (gdim == 3) {
+    for (int i = idx; i < n; i += str) {
+      a1[i] = a1[i] + b1[i] * c[i];
+      a2[i] = a2[i] + b2[i] * c[i];
+      a3[i] = a3[i] + b3[i] * c[i];
+    }
+  } 
+  else {
+    for (int i = idx; i < n; i += str) {
+      a1[i] = a1[i] + b1[i] * c[i];
+      a2[i] = a2[i] + b2[i] * c[i];
+    }
+  }
+
+}
+
