2622cb56d393ed8a6bbdfe5da803e5ace0ec1416
Niclas Jansson <njansson@kth.se>
2021-10-30 15:31:14 +0200

    Remove generated file

--- src/scripts/cltostring.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-IN=$(basename $1)
-NAME=${IN%.cl}
-
-echo "const char *"${NAME}" =" > $1.h
-sed -e 's/\\/\\\\/g;s/"/\\"/g;s/^/"/;s/$/\\n"/;s/real/double/' \
-    $1 >> $1.h
-echo ";" >>$1.h
