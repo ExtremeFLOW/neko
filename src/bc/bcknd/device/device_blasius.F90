269789f7fb55843ad6c0e1fd0c1aafeea8e913d3
Niclas Jansson <njansson@kth.se>
2021-12-06 20:55:11 +0100

    Add HIP kernel for Blasius inflow profile

--- src/bc/bcknd/device/device_blasius.F90
+++ src/bc/bcknd/device/device_blasius.F90
@@ -5,6 +5,16 @@ module device_blasius
   implicit none
 
 #ifdef HAVE_HIP
+  interface
+     subroutine hip_blasius_apply_vector(msk, x, y, z, bla_x, bla_y, bla_z, m) &
+          bind(c, name='hip_blasius_apply_vector')
+       use, intrinsic :: iso_c_binding
+       import c_rp
+       implicit none
+       integer(c_int) :: m
+       type(c_ptr), value :: msk, x, y, z, bla_x, bla_y, bla_z
+     end subroutine hip_blasius_apply_vector
+  end interface
 #elif HAVE_CUDA
   interface
      subroutine cuda_blasius_apply_vector(msk, x, y, z, bla_x, bla_y, bla_z, m) &
@@ -26,6 +36,7 @@ contains
     type(c_ptr) :: msk, x, y, z, bla_x, bla_y, bla_z
 
 #ifdef HAVE_HIP
+    call hip_blasius_apply_vector(msk, x, y, z, bla_x, bla_y, bla_z, m)
 #elif HAVE_CUDA
     call cuda_blasius_apply_vector(msk, x, y, z, bla_x, bla_y, bla_z, m)
 #elif HAVE_OPENCL
