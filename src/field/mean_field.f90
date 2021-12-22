42189c009cfdc1e307758d8a93a380a6dc31ba44
Niclas Jansson <njansson@kth.se>
2021-12-22 20:03:45 +0100

    Fix failing field tests (device versions)

--- src/field/mean_field.f90
+++ src/field/mean_field.f90
@@ -53,11 +53,19 @@ contains
     class(mean_field_t), intent(inout) :: this
     real(kind=rp), intent(in) :: k !< Time since last sample
 
-    this%mf%x = this%mf%x * this%time
-    call add2s2(this%mf%x, this%f%x, k, this%mf%dof%n_dofs)
-    this%time = this%time + k
-    this%mf%x = this%mf%x / this%time
-
+    if ((NEKO_BCKND_HIP .eq. 1) .or. (NEKO_BCKND_CUDA .eq. 1) .or. &
+         (NEKO_BCKND_OPENCL .eq. 1)) then
+       call device_cmult(this%mf%x_d, this%time, size(this%mf%x))
+       call device_add2s2(this%mf%x_d, this%f%x_d, k, size(this%mf%x))
+       this%time = this%time + k
+       call device_cmult(this%mf%x_d, 1.0_rp / this%time, size(this%mf%x))
+    else
+       this%mf%x = this%mf%x * this%time
+       call add2s2(this%mf%x, this%f%x, k, this%mf%dof%n_dofs)
+       this%time = this%time + k
+       this%mf%x = this%mf%x / this%time
+    end if
+       
   end subroutine mean_field_update
   
 end module mean_field
