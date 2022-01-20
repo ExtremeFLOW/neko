f3f740afc14990511f94fd8c26a0ff4e9975e9ec
Martin Karp <martin.karp@outlook.com>
2022-01-20 10:20:48 +0100

    operator fixes

--- src/math/bcknd/device/hip/cdtp_kernel.h
+++ src/math/bcknd/device/hip/cdtp_kernel.h
@@ -23,8 +23,9 @@ __global__ void cdtp_kernel(T * __restrict__ dtx,
   __shared__ T shdyt[LX * LX];
   __shared__ T shdzt[LX * LX];
   
-  __shared__ T shjac[LX * LX * LX];
-  __shared__ T shB[LX * LX * LX];
+  __shared__ T shtar[LX * LX * LX];
+  __shared__ T shtas[LX * LX * LX];
+  __shared__ T shtat[LX * LX * LX];
   
   int i,j,k;
   
@@ -40,57 +41,32 @@ __global__ void cdtp_kernel(T * __restrict__ dtx,
 
   j = iii;
   while(j < (LX * LX * LX)) {
-    shx[j] = x[j + e * LX * LX * LX];
+     // We can probably avoid this division by use of jacinv instead.
+    T wx = (x[j + e * LX * LX * LX] * B[j + e * LX * LX * LX])/jac[j + e * LX * LX * LX];
 
-    shdr[j] = dr[j + e * LX * LX * LX];
-    shds[j] = ds[j + e * LX * LX * LX];
-    shdt[j] = dt[j + e * LX * LX * LX];
-
-    shB[j] = B[j + e * LX * LX * LX];
-    
-    shjac[j] = jac[j + e * LX * LX * LX];
+    shtar[j] = wx*dr[j + e * LX * LX * LX];
+    shtas[j] = wx*ds[j + e * LX * LX * LX];
+    shtat[j] = wx*dt[j + e * LX * LX * LX];
 
     j = j + CHUNKS;
   }
 
   __syncthreads();
-  
   for (int n = 0; n < nchunks; n++) {
     const int ijk = iii + n * CHUNKS;
     const int jk = ijk / LX;
     i = ijk - jk * LX;
     k = jk / LX;
     j = jk - k * LX;
-    if ( i < LX && j < LX && k < LX) {
+    if ( i < LX && j < LX && k < LX && ijk < LX*LX*LX) {
       T rtmp = 0.0;
       T stmp = 0.0;
       T ttmp = 0.0;
       for (int l = 0; l < LX; l++) {
-
-	// col3(wx, B, x) + invcol2(wx, jac) + col3(ta1, wx, dr)
-	const T ta1_r = (((shB[l + j * LX + k * LX * LX]
-				* shx[l + j * LX + k * LX * LX]) /
-			       shjac[l + j * LX + k * LX * LX]) *
-			      shdr[l + j * LX + k * LX * LX]);
-
-	// col3(wx, B, x) + invcol2(wx, jac) + col3(ta1, wx, ds)
-	const T ta1_s = (((shB[i + l * LX + k * LX * LX]
-				* shx[i + l * LX + k * LX * LX]) /
-			       shjac[i + l * LX + k * LX * LX])
-			      * shds[i + l * LX + k * LX * LX]);
-
-	// col3(wx, B, x) + invcol2(wx, jac) + col3(ta1, wx, dt)
-	const T ta1_t = (((shB[i + j * LX + l * LX * LX]
-				* shx[i + j * LX + l * LX * LX]) /
-			       shjac[i + j * LX + l * LX * LX])
-			      * shdt[i + j * LX + l * LX * LX]);
-		
-	rtmp += shdxt[i + l * LX] * ta1_r;	
-	stmp += shdyt[j + l * LX] * ta1_s;
-	ttmp += shdzt[k + l * LX] * ta1_t;
-
+	    rtmp += shdxt[i + l * LX] * shtar[l+j*LX+k*LX*LX];	
+	    stmp += shdyt[j + l * LX] * shtas[i+l*LX + k*LX*LX];
+	    ttmp += shdzt[k + l * LX] * shtat[i + j*LX + l*LX*LX];
       }
-      
       dtx[ijk + e * LX * LX * LX] = ( rtmp + stmp + ttmp );
       
     }
