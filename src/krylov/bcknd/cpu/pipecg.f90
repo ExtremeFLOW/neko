fd4ca23ad6fb891150fc5e3f9f5c57b665dadc59
Niclas Jansson <njansson@kth.se>
2022-01-31 22:15:17 +0100

    More work

--- src/krylov/bcknd/cpu/pipecg.f90
+++ src/krylov/bcknd/cpu/pipecg.f90
@@ -1,3 +1,35 @@
+! Copyright (c) 2021, The Neko Authors
+! All rights reserved.
+!
+! Redistribution and use in source and binary forms, with or without
+! modification, are permitted provided that the following conditions
+! are met:
+!
+!   * Redistributions of source code must retain the above copyright
+!     notice, this list of conditions and the following disclaimer.
+!
+!   * Redistributions in binary form must reproduce the above
+!     copyright notice, this list of conditions and the following
+!     disclaimer in the documentation and/or other materials provided
+!     with the distribution.
+!
+!   * Neither the name of the authors nor the names of its
+!     contributors may be used to endorse or promote products derived
+!     from this software without specific prior written permission.
+!
+! THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+! "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+! LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+! FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+! COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+! INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+! BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+! LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+! CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+! LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+! ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+! POSSIBILITY OF SUCH DAMAGE.
+!
 !> Defines a pipelined Conjugate Gradient methods
 module pipecg
   use krylov
@@ -191,16 +223,6 @@ contains
             beta(p_cur) = 0.0_rp
             alpha(p_cur) = gamma1/delta
          end if
-         
-         !call add2s1(z, ni, beta, n)
-         !call add2s1(q, mi, beta, n)
-         !call add2s1(s, w, beta, n)
-         !call add2s1(p, u, beta, n)
-         
-         !call add2s2(x%x, p, alpha, n)
-         !call add2s2(r, s, -alpha, n)
-         !call add2s2(u, q, -alpha, n)
-         !call add2s2(w, z, -alpha, n)
          
          tmp1 = 0.0_rp
          tmp2 = 0.0_rp
