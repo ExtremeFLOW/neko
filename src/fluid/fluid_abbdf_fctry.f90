49a1d98ec7ccd4d926cabf5a5215a0cd1de3e997
Niclas Jansson <njansson@kth.se>
2022-02-17 22:25:57 +0100

    Add CUDA kernels for abbdf

--- src/fluid/fluid_abbdf_fctry.f90
+++ src/fluid/fluid_abbdf_fctry.f90
@@ -1,6 +1,8 @@
 module fluid_abbdf_fctry
   use fluid_abbdf
   use fluid_abbdf_cpu
+  use fluid_abbdf_device
+  use neko_config
   implicit none
 
 contains
@@ -12,7 +14,12 @@ contains
        deallocate(sumab)
     end if
 
-    allocate(fluid_sumab_cpu_t::sumab)
+    if ((NEKO_BCKND_HIP .eq. 1) .or. (NEKO_BCKND_CUDA .eq. 1) .or. &
+         (NEKO_BCKND_OPENCL .eq. 1)) then
+       allocate(fluid_sumab_device_t::sumab)
+    else
+       allocate(fluid_sumab_cpu_t::sumab)
+    end if
     
   end subroutine fluid_sumab_fctry
 
@@ -23,7 +30,12 @@ contains
        deallocate(makeabf)
     end if
 
-    allocate(fluid_makeabf_cpu_t::makeabf)
+    if ((NEKO_BCKND_HIP .eq. 1) .or. (NEKO_BCKND_CUDA .eq. 1) .or. &
+         (NEKO_BCKND_OPENCL .eq. 1)) then
+       allocate(fluid_makeabf_device_t::makeabf)
+    else
+       allocate(fluid_makeabf_cpu_t::makeabf)
+    end if
     
   end subroutine fluid_makeabf_fctry
 
@@ -34,7 +46,12 @@ contains
        deallocate(makebdf)
     end if
 
-    allocate(fluid_makebdf_cpu_t::makebdf)
+    if ((NEKO_BCKND_HIP .eq. 1) .or. (NEKO_BCKND_CUDA .eq. 1) .or. &
+         (NEKO_BCKND_OPENCL .eq. 1)) then
+       allocate(fluid_makebdf_device_t::makebdf)
+    else       
+       allocate(fluid_makebdf_cpu_t::makebdf)
+    end if
     
   end subroutine fluid_makebdf_fctry
   
