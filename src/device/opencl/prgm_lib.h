886f474eb63604b8093772a7cd18d799e23e9f1b
Martin Karp <martin.karp@outlook.com>
2022-03-09 11:47:39 +0100

    fix now?

--- src/device/opencl/prgm_lib.h
+++ src/device/opencl/prgm_lib.h
@@ -27,7 +27,7 @@ extern void *symmetry_program;
 extern void *facet_normal_program;
 
 /** Device Blasius profile kernel */
-extern void *blasius_program;
+extern void *inom_dirichlet_program;
 
 /** Device Derivative kernels */
 extern void *dudxyz_program;
