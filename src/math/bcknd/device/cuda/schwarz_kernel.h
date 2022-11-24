2c11e8451a0b65b22c3aa737f20710d3637975af
Jonathan Vincent <jonvin@kth.se>
2022-11-24 14:37:21 +0200

    updated to use LX

--- src/math/bcknd/device/cuda/schwarz_kernel.h
+++ src/math/bcknd/device/cuda/schwarz_kernel.h
@@ -34,7 +34,7 @@
 
 /**
  * Device kernel for schwarz extrude
- * This can probably be done i a better way...
+ * This can probably be done in a better way...
  * We sum the "shell" of a1 that is l1 steps in scaled with f1
  * with the shell of a2 that is l2 steps in and scale with f3.
  * Right now we just thorw away all arrays that are not
@@ -42,7 +42,7 @@
  * It should be noted that a1, a2 are often the same array.
  * If l1,l2 are not the same or if one is not 0 this might lead to a race.
  */
-template< typename T, int NX>
+template< typename T, const int NX>
 __global__ void schwarz_extrude_kernel(T * a1,
                                        const int l1,
                                        const T f1,
@@ -50,68 +50,67 @@ __global__ void schwarz_extrude_kernel(T * a1,
                                        const int l2,
                                        const T f2){
 
-  const int nx=NX;	
-  __shared__ T x1[nx*nx],x2[nx*nx];
-  __shared__ T y1[nx*nx],y2[nx*nx];
-  __shared__ T z1[nx*nx],z2[nx*nx];
+  __shared__ T x1[NX*NX],x2[NX*NX];
+  __shared__ T y1[NX*NX],y2[NX*NX];
+  __shared__ T z1[NX*NX],z2[NX*NX];
   const int idx = threadIdx.x;
-  const int el = blockIdx.x*nx*nx*nx;
+  const int el = blockIdx.x*NX*NX*NX;
 
-  if(idx < nx*nx){
-    int i = idx%nx;
-    int k = idx/nx;
-    int idx_x1 = l2 + i*nx + k*nx*nx + el;
+  if(idx < NX*NX){
+    int i = idx%NX;
+    int k = idx/NX;
+    int idx_x1 = l2 + i*NX + k*NX*NX + el;
     x1[idx]=a2[idx_x1];
-    int idx_x2 = nx-1-l2 + i*nx + k*nx*nx + el;
+    int idx_x2 = NX-1-l2 + i*NX + k*NX*NX + el;
     x2[idx]=a2[idx_x2];
 
-    int idx_y1 = i + l2*nx + k*nx*nx + el;
+    int idx_y1 = i + l2*NX + k*NX*NX + el;
     y1[idx]=a2[idx_y1];
-    int idx_y2 = i + (nx-1-l2)*nx + k*nx*nx + el;
+    int idx_y2 = i + (NX-1-l2)*NX + k*NX*NX + el;
     y2[idx]=a2[idx_y2];
     
-    int idx_z1 = i + k*nx + l2*nx*nx + el;
+    int idx_z1 = i + k*NX + l2*NX*NX + el;
     z1[idx]=a2[idx_z1];
-    int idx_z2 = i + k*nx + (nx-l2-1)*nx*nx + el;
+    int idx_z2 = i + k*NX + (NX-l2-1)*NX*NX + el;
     z2[idx]=a2[idx_z2]; 
   }
   __syncthreads();
 
-  for(int ijk = idx; ijk<nx*nx*nx; ijk+=blockDim.x){
-     int jk = ijk/nx;
-     int i = ijk - nx*jk;
-     int k = jk/nx;
-     int j = jk -k*nx;
-     if(j>0 && j< nx-1 && k > 0 && k < nx -1){
-       int idx1 = i + j*nx + k*nx*nx + el;
+  for(int ijk = idx; ijk<NX*NX*NX; ijk+=blockDim.x){
+     int jk = ijk/NX;
+     int i = ijk - NX*jk;
+     int k = jk/NX;
+     int j = jk -k*NX;
+     if(j>0 && j< NX-1 && k > 0 && k < NX -1){
+       int idx1 = i + j*NX + k*NX*NX + el;
        if(i == l1){
-         int idx2 = j + k*nx;	 
+         int idx2 = j + k*NX;	 
          a1[idx1] = f1*a1[idx1] + f2*x1[idx2];
        }
-       if(i == nx-1-l1){
-         int idx2 = j + k*nx;
+       if(i == NX-1-l1){
+         int idx2 = j + k*NX;
          a1[idx1] = f1*a1[idx1] + f2*x2[idx2];
        }
      }
-     if( i > 0 && i < nx-1 && k > 0 && k < nx -1){
-       int idx1 = i + j*nx + k*nx*nx + el;
+     if( i > 0 && i < NX-1 && k > 0 && k < NX -1){
+       int idx1 = i + j*NX + k*NX*NX + el;
        if(j == l1){
-         int idx2 = i + k*nx;
+         int idx2 = i + k*NX;
          a1[idx1] = f1*a1[idx1] + f2*y1[idx2];
        }
-       if(j == nx-1-l1){
-          int idx2 = i + k*nx;
+       if(j == NX-1-l1){
+          int idx2 = i + k*NX;
           a1[idx1] = f1*a1[idx1] + f2*y2[idx2];
        }
      }
-     if( i > 0 && i < nx-1 && j>0 && j< nx-1 ){
-       int idx1 = i + j*nx + k*nx*nx + el;
+     if( i > 0 && i < NX-1 && j>0 && j< NX-1 ){
+       int idx1 = i + j*NX + k*NX*NX + el;
        if(k == l1){
-         int idx2 = i + j*nx;
+         int idx2 = i + j*NX;
          a1[idx1] = f1*a1[idx1] + f2*z1[idx2];
        }
-       if(k == nx-1-l1){
-           int idx2 = i + j*nx;
+       if(k == NX-1-l1){
+           int idx2 = i + j*NX;
            a1[idx1] = f1*a1[idx1] + f2*z2[idx2];
        }
      }
