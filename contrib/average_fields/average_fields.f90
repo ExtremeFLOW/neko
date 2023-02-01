3af63e3ec05f2faa36afb83a9e217369017e1faa
Martin Karp <martin.karp@outlook.com>
2023-02-01 17:42:11 +0100

    make it so only rank 0 writes

--- contrib/average_fields/average_fields.f90
+++ contrib/average_fields/average_fields.f90
@@ -13,12 +13,14 @@ program average_fields
   argc = command_argument_count()
 
   if ((argc .lt. 3) .or. (argc .gt. 3)) then
+     if (pe_rank .eq. 0) then
      write(*,*) 'Usage: ./average_fields field_series_name.fld start_time output_name.fld' 
      write(*,*) 'Example command: ./average_fields mean_field104.fld 103.2 mean_field_avg.fld'
      write(*,*) 'Computes the average field over the fld files described in mean_field104.nek5000'
      write(*,*) 'The start time is the time at which the first file startsto collect stats'
      write(*,*) 'The files need to be aranged chronological order.'
      write(*,*) 'The average field is then stored in a fld series, i.e. output_name.nek5000 and output_name.f00000'
+     end if
      stop
   end if
   
@@ -40,9 +42,11 @@ program average_fields
 
   do i = 1, fld_data_avg%meta_nsamples-1
      call fld_file%read(fld_data)
+     if (pe_rank .eq. 0) write(*,*) 'Reading file:', i+1
      call fld_data%scale(fld_data%time-fld_data_avg%time)
      call fld_data_avg%add(fld_data)
-     write(*,*) 'Reading file:', i, 'dt', fld_data%time - fld_data_avg%time
+
+     if (pe_rank .eq. 0) write(*,*) 'dt', fld_data%time - fld_data_avg%time
      fld_data_avg%time = fld_data%time
   end do
   call fld_data_avg%scale(1.0_rp/(fld_data_avg%time-start_time))
