5e6169a071b5019fa621ed8f32e326c1382c7b29
Victor Baconnet <baconnet@mech.kth.se>
2023-05-05 17:26:21 +0200

    Reuse vector_t and add matrix_t for csv and dat io

--- src/io/dat_file.f90
+++ src/io/dat_file.f90
@@ -36,7 +36,8 @@
 !! specified floating-point data
 module dat_file
   use probe
-  use dat_outputs
+  use vector
+  use matrix
   use logger
   use generic_file
   use comm
@@ -59,32 +60,27 @@ contains
     real(kind=rp), intent(in), optional :: t
 
     real(kind=rp) :: time
-    type(vector_output_t), pointer :: vec => null()
-    type(matrix_output_t), pointer :: mat => null()
+    type(vector_t), pointer :: vec => null()
+    type(matrix_t), pointer :: mat => null()
 
     ! Select which type of output we have
     select type(data)
-    type is (vector_output_t)
+    type is (vector_t)
 
-       if (.not. associated(data%data)) then
-          call neko_error("Vector is not associated! Use &
-          vector_output_t%init() to associate your array &
-          with the vector_output_t object")
+       if (.not. allocated(data%x)) then
+          call neko_error("Vector is not allocated!")
        end if
        vec => data
 
-    type is (matrix_output_t)
+    type is (matrix_t)
       
-       if (.not. associated(data%data)) then
-          call neko_error("Matrix is not associated! Use &
-          matrix_output_t%init() to associate your array &
-          with the vector_output_t object")
+       if (.not. allocated(data%x)) then
+          call neko_error("Matrix is not allocated!")
        end if
        mat => data
 
     class default
-       call neko_error("Invalid data. Expected vector_output_t or &
-       matrix_output_t")
+       call neko_error("Invalid data. Expected vector_t or matrix_t")
     end select    
 
     if (pe_rank .eq. 0) then
@@ -109,24 +105,20 @@ contains
   !> Writes a @ vector_output_t object to an output file
   subroutine dat_file_write_vector(d, data, t)
     class(dat_file_t), intent(inout) :: d
-    type(vector_output_t), intent(in) :: data
+    type(vector_t), intent(in) :: data
     real(kind=rp), intent(in), optional :: t
 
-    character(len=1024) :: fname
-    integer :: suffix_pos, file_unit, i, ierr
+    integer :: file_unit, i, ierr
 
-    suffix_pos = filename_suffix_pos(d%fname)
-    fname = trim(d%fname(1:suffix_pos-1)) // "-out.dat"
-
-    open(file=trim(fname), position="append", iostat=ierr, newunit=file_unit)
+    open(file=trim(d%fname), position="append", iostat=ierr, newunit=file_unit)
 
     if (present(t)) write (file_unit, '(E15.7)', advance="no") t
 
     do i=1, data%n - 1
-       write (file_unit, '(E15.7)', advance="no") data%data(i)
+       write (file_unit, '(E15.7)', advance="no") data%x(i)
     end do
 
-    write (file_unit, '(E15.7)') data%data(data%n)
+    write (file_unit, '(E15.7)') data%x(data%n)
 
     close(file_unit)
 
@@ -135,26 +127,21 @@ contains
   !> Writes a @ matrix_output_t object to an output file
   subroutine dat_file_write_matrix(d, data, t)
     class(dat_file_t), intent(inout) :: d
-    type(matrix_output_t), intent(in) :: data
+    type(matrix_t), intent(in) :: data
     real(kind=rp), intent(in), optional :: t
 
-    character(len=1024) :: fname
-    integer :: suffix_pos, file_unit, i,j, ierr
+    integer :: file_unit, i,j, ierr
 
-    ! The output file's name will be <input_filename>-out.dat
-    suffix_pos = filename_suffix_pos(d%fname)
-    fname = trim(d%fname(1:suffix_pos-1)) // "-out.dat"
+    open(file=trim(d%fname), position="append", iostat=ierr, newunit=file_unit)
 
-    open(file=trim(fname), position="append", iostat=ierr, newunit=file_unit)
-
-    do i = 1, data%ni
+    do i = 1, data%nrows
        if (present(t)) write (file_unit, '(E15.7)', advance="no") t
 
-       do j = 1, data%nj - 1
-          write (file_unit, '(E15.7)', advance="no") data%data(i,j)
+       do j = 1, data%ncols - 1
+          write (file_unit, '(E15.7)', advance="no") data%x(i,j)
        end do
 
-       write (file_unit, '(E15.7)') data%data(i,data%nj)
+       write (file_unit, '(E15.7)') data%x(i,data%ncols)
     end do
 
     close(file_unit)
@@ -162,33 +149,53 @@ contains
   end subroutine dat_file_write_matrix
 
   !> Reads data from an input file.
-  !> @note Only possible to read probe formatted text files!
+  !> Possible formats:
+  !> - probes (probe_t)
+  !> - vector (vector_t)
+  !> - matrix (matrix_t)
   subroutine dat_file_read(this, data)
     class(dat_file_t) :: this               !< dat_file_t object
     class(*), target, intent(inout) :: data !< data to read
 
-    type(probe_t), pointer :: pb => null() 
-    integer :: i, ierr, file_unit, npts
-    character(len=LOG_SIZE) :: log_buf
-    type(MPI_File) :: fh
-    type(MPI_Status) :: status
-    real(kind=rp), dimension(:,:), allocatable :: temp_send_coords
-
+    type(probe_t), pointer :: pb => null()
+    type(vector_t), pointer :: vec => null()
+    type(matrix_t), pointer :: mat => null()    
+  
     select type(data) 
     type is (probe_t) 
        pb => data
+    type is (vector_t)
+       vec => data
+    type is (matrix_t)
+       mat => data
     class default
        call neko_error("Invalid data type for dat_file (expected: probe_t)")
     end select
 
-    npts = -1
+    if (associated(pb)) then
+       call dat_file_read_probe(this, pb)
+    else if (associated(vec)) then
+       call dat_file_read_vector(this, vec)
+    else if (associated(mat)) then
+       call dat_file_read_matrix(this, mat)
+    end if
+
+  end subroutine dat_file_read
+
+  !> Read a probe from a dat file
+  subroutine dat_file_read_probe(d, pb)
+    type(dat_file_t), intent(in) :: d !< dat file object
+    type(probe_t), pointer, intent(inout) :: pb !< probe
+
+    real(kind=rp), dimension(:,:), allocatable :: temp_send_coords
+    integer :: i, ierr, file_unit, npts
 
     ! == First, get npts from the file.
     if (pe_rank .eq. 0) then
 
-      call neko_log%message("Reading dat file " // trim(this%fname))  
-      open(file=trim(this%fname), status='old', iostat=ierr, newunit=file_unit)
-      read(file_unit, *) npts
+       call neko_log%message("Reading dat file " // trim(d%fname))  
+       open(file=trim(d%fname), status='old', iostat=ierr, newunit=file_unit)
+       read(file_unit, *) npts
       
     end if ! end pe_rank
     
@@ -198,7 +205,7 @@ contains
     ! Initialize the probe object with an array of npts for coordinates
     call probe_init(pb, npts)
 
-    ! Allocate a temporary array that will store the points
+    ! Allocate a tempoqrary array that will store the points
     ! very ugly
     allocate(temp_send_coords(3,npts))
 
@@ -209,7 +216,7 @@ contains
         read (file_unit, *) pb%xyz_coords(i)%x(1), pb%xyz_coords(i)%x(2), &
         pb%xyz_coords(i)%x(3)
       end do
-
+                                                                            
       close(unit=file_unit)
       
       !very ugly
@@ -222,13 +229,46 @@ contains
     call MPI_Bcast(temp_send_coords, pb%npts, MPI_POINT, 0, NEKO_COMM, ierr)
 
     if (pe_rank .ne. 0) then
-      !very ugly
-      pb%xyz_coords(:)%x(1) = temp_send_coords(1,:)
-      pb%xyz_coords(:)%x(2) = temp_send_coords(2,:)
-      pb%xyz_coords(:)%x(3) = temp_send_coords(3,:)
+       !very ugly
+       pb%xyz_coords(:)%x(1) = temp_send_coords(1,:)
+       pb%xyz_coords(:)%x(2) = temp_send_coords(2,:)
+       pb%xyz_coords(:)%x(3) = temp_send_coords(3,:)
     end if
 
-  end subroutine dat_file_read
+    deallocate(temp_send_coords)
 
+  end subroutine dat_file_read_probe
+
+  !> Read a vector from a dat file
+  !> @TODO: Broadcast to other processes
+  subroutine dat_file_read_vector(d, vec)
+    type(dat_file_t), intent(in) :: d !< dat file object
+    type(vector_t), intent(inout) :: vec !< vector object, contains read data
+    integer :: ierr, file_unit
+
+    if ( pe_rank .eq. 0 ) then      
+       open(file=trim(d%fname), status='old', iostat=ierr, newunit=file_unit)
+       read (file_unit,*) vec%x
+       close(unit=file_unit)
+    end if
+
+  end subroutine dat_file_read_vector
+
+  !> Read a matrix from a dat file
+  !> @TODO: Broadcast to other processes.
+  subroutine dat_file_read_matrix(d, mat)
+   type(dat_file_t), intent(in) :: d !< dat file object
+   type(matrix_t), intent(inout) :: mat !< matrix object, contains read data
+   integer :: ierr, file_unit, i
+
+   if ( pe_rank .eq. 0 ) then      
+      open(file=trim(d%fname), status='old', iostat=ierr, newunit=file_unit)
+      do i=1, mat%nrows
+         read (file_unit,*) mat%x(i,:)
+      end do
+      close(unit=file_unit)
+   end if
+
+ end subroutine dat_file_read_matrix
 
 end module dat_file
---
+++ src/math/matrix.f90
@@ -0,0 +1,154 @@
+! Copyright (c) 2023, The Neko Authors
+! All rights reserved.
+!
+! Redistribution and use in source and binary forms, with or without
+! modification, are permitted provided that the following conditions
+! are met:
+!
+!   * Redistributions of source code must retain the above copyright
+!     notice, this list of conditions and the following disclaimer.
+!
+!   * Redistributions in binary form must reproduce the above
+!     copyright notice, this list of conditions and the following
+!     disclaimer in the documentation and/or other materials provided
+!     with the distribution.
+!
+!   * Neither the name of the authors nor the names of its
+!     contributors may be used to endorse or promote products derived
+!     from this software without specific prior written permission.
+!
+! THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+! "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+! LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+! FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+! COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+! INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+! BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+! LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+! CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+! LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+! ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+! POSSIBILITY OF SUCH DAMAGE.
+!
+!> Defines a matrix
+module matrix
+    use neko_config
+    use num_types
+    use device
+    use device_math
+    use utils
+    use, intrinsic :: iso_c_binding
+    implicit none
+    private
+    
+    type, public ::  matrix_t
+       real(kind=rp), allocatable :: x(:,:) !< matrix entrie
+    !    type(c_ptr) :: x_d = C_NULL_PTR    !< Device pointer
+       integer :: nrows  = 0                  !< Number of matrix rows
+       integer :: ncols  = 0                  !< Number of matrix columns
+     contains
+       procedure, pass(m) :: init => matrix_init
+       procedure, pass(m) :: free => matrix_free
+       procedure, pass(m) :: size => matrix_size
+       procedure, pass(m) :: matrix_assign_matrix
+       procedure, pass(m) :: matrix_assign_scalar
+       generic :: assignment(=) => matrix_assign_matrix, &
+            matrix_assign_scalar
+    end type matrix_t
+  
+  contains
+  
+    !> Initialise a matrix of size @a n
+    subroutine matrix_init(m, nrows, ncols)
+      class(matrix_t), intent(inout) :: m
+      integer, intent(in) :: nrows
+      integer, intent(in) :: ncols
+  
+      call m%free()
+  
+      allocate(m%x(nrows, ncols))
+      m%x = 0.0_rp
+     
+    !   if (NEKO_BCKND_DEVICE .eq. 1) then
+    !      call device_map(m%x, m%x_d, n)
+    !      call device_cfill(m%x_d, 0.0_rp, n)
+    !   end if
+  
+      m%nrows = nrows
+      m%ncols = ncols
+      
+    end subroutine matrix_init
+  
+    !> Deallocate a matrix
+    subroutine matrix_free(m)
+      class(matrix_t), intent(inout) :: m
+  
+      if (allocated(m%x)) then
+         deallocate(m%x)
+      end if
+  
+    !   if (c_associated(m%x_d)) then
+    !      call device_free(m%x_d)
+    !   end if
+  
+      m%nrows = 0
+      m%ncols = 0
+          
+    end subroutine matrix_free
+  
+    !> Return the number of entries in the matrix
+    function matrix_size(m) result(s)
+      class(matrix_t), intent(inout) :: m
+      integer :: s
+      s = m%nrows*m%ncols    
+    end function matrix_size
+  
+    !> Assignment \f$ m = w \f$
+    subroutine matrix_assign_matrix(m, w)
+      class(matrix_t), intent(inout) :: m
+      type(matrix_t), intent(in) :: w
+  
+      if (allocated(m%x)) then
+         call m%free()
+      end if
+  
+      if (.not. allocated(m%x)) then
+  
+         m%nrows = w%nrows
+         m%ncols = w%ncols
+         allocate(m%x(m%nrows, m%ncols))
+         
+        !  if (NEKO_BCKND_DEVICE .eq. 1) then
+        !     call device_map(m%x, m%x_d, m%n)
+        !  end if
+         
+      end if
+  
+    !   if (NEKO_BCKND_DEVICE .eq. 1) then
+    !      call device_copy(m%x_d, w%x_d, m%n)
+    !   else
+    !      m%x = w%x
+    !   end if
+  
+    end subroutine matrix_assign_matrix
+  
+    !> Assignment \f$ m = s \f$
+    subroutine matrix_assign_scalar(m, s)
+      class(matrix_t), intent(inout) :: m
+      real(kind=rp), intent(in) :: s
+  
+      if (.not. allocated(m%x)) then
+         call neko_error('matrix not allocated')
+      end if
+  
+    !   if (NEKO_BCKND_DEVICE .eq. 1) then
+        !  call device_cfill(m%x_d, s, m%n)
+    !   else
+         m%x = s
+    !   end if
+  
+    end subroutine matrix_assign_scalar
+  
+    
+  end module matrix
+  
