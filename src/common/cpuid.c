3389b694a535af60a51a5bb299d2aed7c1dda08b
Niclas Jansson <njansson@kth.se>
2023-05-07 13:24:07 +0200

    Fix include guard for systems without cpuid.h
    (cherry picked from commit 8581c21447c808d023fe0667762699c2f7b14051)

--- src/common/cpuid.c
+++ src/common/cpuid.c
@@ -4,7 +4,9 @@
 #include <sys/types.h>
 #include <unistd.h>
 #elif defined(__x86_64__)
+#if defined(HAVE_CPUID_H)
 #include <cpuid.h>
+#endif
 #endif
 
 #include <string.h>
@@ -15,7 +17,7 @@
 void system_cpuid(char *name, int len) {  
 #if defined(__APPLE__)
   sysctlbyname("machdep.cpu.brand_string", name,(size_t *) &len, NULL, 0);
-#elif defined(__x86_64__)
+#elif defined(__x86_64__) && defined(HAVE_CPUID_H)
   __get_cpuid(0x80000002, name+0x0, name+0x1, name+0x2, name+0x3);
   __get_cpuid(0x80000003, name+0x4, name+0x5, name+0x6, name+0x7);
   __get_cpuid(0x80000004, name+0x8, name+0x9, name+0xa, name+0xb);
