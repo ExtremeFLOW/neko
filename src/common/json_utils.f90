b23a74633d62ac06392c5a01f8c289c0cd2310c0
Timofey Mukha <timofey.mukha@protonmail.com>
2023-06-11 21:51:26 +0200

    Add array types to json_get

--- src/common/json_utils.f90
+++ src/common/json_utils.f90
@@ -43,7 +43,8 @@ module json_utils
   !> Retrieves a parameter by name or throughs an error
   interface json_get
     module procedure json_get_real, json_get_integer, json_get_logical, &
-                     json_get_string
+                     json_get_string, json_get_real_array,&
+                     json_get_integer_array, json_get_logical_array
   end interface json_get
 
   !> Retrieves a parameter by name or assigns a provided default value.
@@ -122,6 +123,57 @@ contains
        call neko_error("Parameter "//name//" missing from the case file")
     end if
   end subroutine json_get_string
+
+  !> Retrieves a real array parameter by name or throughs an error
+  !! @param json The json to retrieve the parameter from.
+  !! @param name The full path to the parameter.
+  !! @value value The variable to be populated with the retrieved parameter.
+  subroutine json_get_real_array(json, name, value)
+    type(json_file), intent(inout) :: json
+    character(len=*), intent(in) :: name
+    real(kind=rp), allocatable, intent(out) :: value(:)
+    logical :: found
+    
+    call json%get(name, value, found)
+    
+    if (.not. found) then
+       call neko_error("Parameter "//name//" missing from the case file")
+    end if
+  end subroutine json_get_real_array
+
+  !> Retrieves a integer array parameter by name or throughs an error
+  !! @param json The json to retrieve the parameter from.
+  !! @param name The full path to the parameter.
+  !! @value value The variable to be populated with the retrieved parameter.
+  subroutine json_get_integer_array(json, name, value)
+    type(json_file), intent(inout) :: json
+    character(len=*), intent(in) :: name
+    integer, allocatable, intent(out) :: value(:)
+    logical :: found
+    
+    call json%get(name, value, found)
+    
+    if (.not. found) then
+       call neko_error("Parameter "//name//" missing from the case file")
+    end if
+  end subroutine json_get_integer_array
+
+  !> Retrieves a logical array parameter by name or throughs an error
+  !! @param json The json to retrieve the parameter from.
+  !! @param name The full path to the parameter.
+  !! @value value The variable to be populated with the retrieved parameter.
+  subroutine json_get_logical_array(json, name, value)
+    type(json_file), intent(inout) :: json
+    character(len=*), intent(in) :: name
+    logical, allocatable, intent(out) :: value(:)
+    logical :: found
+    
+    call json%get(name, value, found)
+    
+    if (.not. found) then
+       call neko_error("Parameter "//name//" missing from the case file")
+    end if
+  end subroutine json_get_logical_array
 
   !> Retrieves a real parameter by name or assigns a provided default value.
   !! In the latter case also adds the missing paramter to the json.
