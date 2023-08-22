eba3b0d0c37ee04a07a8a01b9205d3b03c8359d1
Niclas Jansson <njansson@kth.se>
2023-08-22 18:53:11 +0900

    Fix cpu detection on PowerPC and A64FX

--- src/common/cpuid.c
+++ src/common/cpuid.c
@@ -7,10 +7,13 @@
 #if defined(HAVE_CPUID_H)
 #include <cpuid.h>
 #endif
-#endif
-
+#elif defined(_ARCH_PPC64) || defined(__aarch64__)
+#include <stdio.h>
 #include <string.h>
 #include <stdint.h>
+#endif
+
+#define MAXLEN 2048
 
 /**
  * Return the systems' CPU id
@@ -24,6 +27,44 @@ void system_cpuid(char *name, int len) {
   __get_cpuid(0x80000003, brand+0x4, brand+0x5, brand+0x6, brand+0x7);
   __get_cpuid(0x80000004, brand+0x8, brand+0x9, brand+0xa, brand+0xb);
   strncpy(name, (char *) brand, len);
+#elif defined(_ARCH_PPC64) || defined(__aarch64__)
+  FILE *fp = fopen("/proc/cpuinfo", "r");
+  char buf[MAXLEN];
+  const char *delim = ":\n";
+#if defined(__aarch64__)
+  /* Generic ARM unless we found something known */
+  strncpy(name, "ARM", len);
+  int cpufj = 0;
+#endif
+  while (fgets (buf, MAXLEN, fp)) {
+#if defined(_ARCH_PPC64)
+    if(strstr(buf, "cpu")) {
+      char *token = strtok (buf, delim);
+      token = strtok (NULL, delim);
+      strncpy(name, token+1, len);
+      break;
+    }
+#elif defined(__aarch64__)
+    if (strstr(buf, "CPU implementer")) {                   
+      char *token = strtok (buf, delim);
+      token = strtok (NULL, delim);
+      if (strstr(token, "0x46")) {
+        cpufj = 1;
+        continue;
+      }
+    }
+
+    if (strstr(buf, "CPU part") && cpufj) {
+      char *token = strtok (buf, delim);
+      token = strtok (NULL, delim);
+      if (strstr(token, "0x001")) {
+        strncpy(name, "A64FX", len);
+        break;
+      }
+    }   
+#endif
+  }
+  fclose(fp);
 #else
   strncpy(name, "Unknown CPU", len);
 #endif
