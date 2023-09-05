d4ecb3d84b259581205e9590703e226fbb5d29fe
Martin Karp <martin.karp@outlook.com>
2023-09-05 09:28:02 +0200

    lambda2 simcomp

--- src/simulation_components/lambda2.f90
+++ src/simulation_components/lambda2.f90
@@ -42,7 +42,7 @@ module lambda2
   use field_registry, only : neko_field_registry
   use field, only : field_t, field_free, field_init, field_ptr_t
   use field_list, only : field_list_t
-  use operators, only : opgrad
+  use operators, only : lambda2op
   use math, only : pi, sort
   use case, only : case_t
   use device
@@ -85,7 +85,6 @@ module lambda2
        class(case_t), intent(inout), target ::case 
        
        call this%init_base(json, case)
-       print *, 'hey'
 
        call lambda2_init_from_attributes(this)
   end subroutine lambda2_init_from_json
@@ -130,77 +129,38 @@ module lambda2
 
 
     !> Get some fields to work with
-    call this%case%fluid%scratch%request_field(grad(1,1)%f, temp_indices(1))
-    call this%case%fluid%scratch%request_field(grad(1,2)%f, temp_indices(2))
-    call this%case%fluid%scratch%request_field(grad(1,3)%f, temp_indices(3))
-    call this%case%fluid%scratch%request_field(grad(2,1)%f, temp_indices(4))
-    call this%case%fluid%scratch%request_field(grad(2,2)%f, temp_indices(5))
-    call this%case%fluid%scratch%request_field(grad(2,3)%f, temp_indices(6))
-    call this%case%fluid%scratch%request_field(grad(3,1)%f, temp_indices(7))
-    call this%case%fluid%scratch%request_field(grad(3,2)%f, temp_indices(8))
-    call this%case%fluid%scratch%request_field(grad(3,3)%f, temp_indices(9))
+    call lambda2op(this%lambda2, this%u, this%v, this%w, this%case%fluid%c_Xh)
+    !call this%case%fluid%scratch%request_field(grad(1,1)%f, temp_indices(1))
+    !call this%case%fluid%scratch%request_field(grad(1,2)%f, temp_indices(2))
+    !call this%case%fluid%scratch%request_field(grad(1,3)%f, temp_indices(3))
+    !call this%case%fluid%scratch%request_field(grad(2,1)%f, temp_indices(4))
+    !call this%case%fluid%scratch%request_field(grad(2,2)%f, temp_indices(5))
+    !call this%case%fluid%scratch%request_field(grad(2,3)%f, temp_indices(6))
+    !call this%case%fluid%scratch%request_field(grad(3,1)%f, temp_indices(7))
+    !call this%case%fluid%scratch%request_field(grad(3,2)%f, temp_indices(8))
+    !call this%case%fluid%scratch%request_field(grad(3,3)%f, temp_indices(9))
 
-    call opgrad(grad(1,1)%f%x,grad(1,2)%f%x,grad(1,2)%f%x,this%u%x,this%case%fluid%c_Xh)
-    call opgrad(grad(2,1)%f%x,grad(2,2)%f%x,grad(2,2)%f%x,this%v%x,this%case%fluid%c_Xh)
-    call opgrad(grad(3,1)%f%x,grad(3,2)%f%x,grad(3,2)%f%x,this%w%x,this%case%fluid%c_Xh)
+    !call opgrad(grad(1,1)%f%x,grad(1,2)%f%x,grad(1,2)%f%x,this%u%x,this%case%fluid%c_Xh)
+    !call opgrad(grad(2,1)%f%x,grad(2,2)%f%x,grad(2,2)%f%x,this%v%x,this%case%fluid%c_Xh)
+    !call opgrad(grad(3,1)%f%x,grad(3,2)%f%x,grad(3,2)%f%x,this%w%x,this%case%fluid%c_Xh)
 
-    if (NEKO_BCKND_DEVICE .eq. 1) then
-        call device_memcpy(grad(1,1)%f%x,grad(1,1)%f%x_d,grad(1,1)%f%dof%size(),DEVICE_TO_HOST)
-        call device_memcpy(grad(1,2)%f%x,grad(1,2)%f%x_d,grad(1,1)%f%dof%size(),DEVICE_TO_HOST)
-        call device_memcpy(grad(1,3)%f%x,grad(1,3)%f%x_d,grad(1,1)%f%dof%size(),DEVICE_TO_HOST)
-        call device_memcpy(grad(2,1)%f%x,grad(2,1)%f%x_d,grad(1,1)%f%dof%size(),DEVICE_TO_HOST)
-        call device_memcpy(grad(2,2)%f%x,grad(2,2)%f%x_d,grad(1,1)%f%dof%size(),DEVICE_TO_HOST)
-        call device_memcpy(grad(2,3)%f%x,grad(2,3)%f%x_d,grad(1,1)%f%dof%size(),DEVICE_TO_HOST)
-        call device_memcpy(grad(3,1)%f%x,grad(3,1)%f%x_d,grad(1,1)%f%dof%size(),DEVICE_TO_HOST)
-        call device_memcpy(grad(3,2)%f%x,grad(3,2)%f%x_d,grad(1,1)%f%dof%size(),DEVICE_TO_HOST)
-        call device_memcpy(grad(3,3)%f%x,grad(3,3)%f%x_d,grad(1,1)%f%dof%size(),DEVICE_TO_HOST)
-    end if
-    do e = 1, this%lambda2%msh%nelv
-
-       do i = 1, this%lambda2%Xh%lxyz
-          s11 = grad(1,1)%f%x(i,1,1,e)
-          s22 = grad(2,2)%f%x(i,1,1,e)
-          s33 = grad(3,3)%f%x(i,1,1,e)
-
-          
-          s12 = 0.5*(grad(1,2)%f%x(i,1,1,e)+grad(1,2)%f%x(i,1,1,e))
-          s13 = 0.5*(grad(1,3)%f%x(i,1,1,e)+grad(3,1)%f%x(i,1,1,e))
-          s23 = 0.5*(grad(2,3)%f%x(i,1,1,e)+grad(3,2)%f%x(i,1,1,e))
-
-          o12 = 0.5*(grad(1,2)%f%x(i,1,1,e)-grad(1,2)%f%x(i,1,1,e))
-          o13 = 0.5*(grad(1,3)%f%x(i,1,1,e)-grad(3,1)%f%x(i,1,1,e))
-          o23 = 0.5*(grad(2,3)%f%x(i,1,1,e)-grad(3,2)%f%x(i,1,1,e))
-
-          a11 = s11*s11 + s12*s12 + s13*s13 - o12*o12 - o13*o13
-          a12 = s11 * s12  +  s12 * s22  +  s13 * s23 - o13 * o23
-          a13 = s11 * s13  +  s12 * s23  +  s13 * s33 + o12 * o23
-
-          a22 = s12*s12 + s22*s22 + s23*s23 - o12*o12 - o23*o23
-          a23 = s12 * s13 + s22 * s23 + s23 * s33 - o12 * o13
-          a33 = s13*s13 + s23*s23 + s33*s33 - o13*o13 - o23*o23
-
-
-          B = -(a11 + a22 + a33)
-          C = -(a12*a12 + a13*a13 + a23*a23 - a11 * a22 - a11 * a33 - a22 * a33)
-          D = -(2.0 * a12 * a13 * a23 - a11 * a23*a23 - a22 * a13*a13 - a33 * a12*a12  +  a11 * a22 * a33)
-
-
-          q = (3.0 * C - B*B) / 9.0
-          r = (9.0 * C * B - 27.0 * D - 2.0 * B*B*B) / 54.0
-          theta = acos( r / sqrt(-q*q*q) )
-
-          eigen(1) = 2.0 * sqrt(-q) * cos(theta / 3.0) - B / 3.0
-          eigen(2) = 2.0 * sqrt(-q) * cos((theta + 2.0 * pi) / 3.0) - B / 3.0
-          eigen(3) = 2.0 * sqrt(-q) * cos((theta + 4.0 * pi) / 3.0) - B / 3.0
-
-          call sort(eigen,ind_sort,3)
-          this%lambda2%x(i,1,1,e) = eigen(2) 
-       end do
-    end do
-    if (NEKO_BCKND_DEVICE .eq. 1) then
-        call device_memcpy(this%lambda2%x,this%lambda2%x_d,this%lambda2%dof%size(),HOST_TO_DEVICE)
-    end if
-    call this%case%fluid%scratch%relinquish_field(temp_indices)
+    !if (NEKO_BCKND_DEVICE .eq. 1) then
+    !    call device_memcpy(grad(1,1)%f%x,grad(1,1)%f%x_d,grad(1,1)%f%dof%size(),DEVICE_TO_HOST)
+    !    call device_memcpy(grad(1,2)%f%x,grad(1,2)%f%x_d,grad(1,1)%f%dof%size(),DEVICE_TO_HOST)
+    !    call device_memcpy(grad(1,3)%f%x,grad(1,3)%f%x_d,grad(1,1)%f%dof%size(),DEVICE_TO_HOST)
+    !    call device_memcpy(grad(2,1)%f%x,grad(2,1)%f%x_d,grad(1,1)%f%dof%size(),DEVICE_TO_HOST)
+    !    call device_memcpy(grad(2,2)%f%x,grad(2,2)%f%x_d,grad(1,1)%f%dof%size(),DEVICE_TO_HOST)
+    !    call device_memcpy(grad(2,3)%f%x,grad(2,3)%f%x_d,grad(1,1)%f%dof%size(),DEVICE_TO_HOST)
+    !    call device_memcpy(grad(3,1)%f%x,grad(3,1)%f%x_d,grad(1,1)%f%dof%size(),DEVICE_TO_HOST)
+    !    call device_memcpy(grad(3,2)%f%x,grad(3,2)%f%x_d,grad(1,1)%f%dof%size(),DEVICE_TO_HOST)
+    !    call device_memcpy(grad(3,3)%f%x,grad(3,3)%f%x_d,grad(1,1)%f%dof%size(),DEVICE_TO_HOST)
+    !end if
+    !do e = 1, this%lambda2%msh%nelv
+    !   end do
+    !end do
+    !if (NEKO_BCKND_DEVICE .eq. 1) then
+    !    call device_memcpy(this%lambda2%x,this%lambda2%x_d,this%lambda2%dof%size(),HOST_TO_DEVICE)
+    !end if
 
   end subroutine lambda2_compute
   
