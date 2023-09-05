70b6df39ee7d855cf22711930ded6875036ecc29
Timofey Mukha <timofey.mukha@protonmail.com>
2023-09-05 12:14:10 +0300

    WIP const source term

--- src/source_terms/const_source_term.f90
+++ src/source_terms/const_source_term.f90
@@ -1,5 +1,5 @@
 
-! Copyright (c) 2020-2021, The Neko Authors
+! Copyright (c) 2023, The Neko Authors
 ! All rights reserved.
 !
 ! Redistribution and use in source and binary forms, with or without
@@ -35,23 +35,27 @@
 module const_source_term
   use num_types, only : rp
   use field_list, only : field_list_t
-  use json_module, only : json_file
+  use json_module, only : json_file, json_core, json_value
   use json_utils, only: json_get
   use source_term, only : source_term_t
   use coefs, only : coef_t
   use math, only : cadd
   use device_math, only : device_cadd
   use neko_config, only : NEKO_BCKND_DEVICE
+  use utils, only : neko_error
   implicit none
   private
 
+  !> A constant source term.
+  !! The strength is specified with the `values` keyword, which should be an
+  !! array, with a value for each component of the source.
   type, public, extends(source_term_t) :: const_source_term_t
      !> The value of the source term.
-     real(kind=rp) :: value
+     real(kind=rp), allocatable :: values(:)
    contains
-     !> The common constructor using a JSON dictionary.
+     !> The common constructor using a JSON object.
      procedure, pass(this) :: init => const_source_term_init_from_json
-     !> The construct from type components.
+     !> The constructor from type components.
      procedure, pass(this) :: init_from_compenents => & 
        const_source_term_init_from_components
      !> Destructor.
@@ -61,28 +65,42 @@ module const_source_term
   end type const_source_term_t
 
 contains
+  !> The common constructor using a JSON object.
+  !! @param json The JSON object for the source.
+  !! @param fields A list of fields for adding the source values.
+  !! @param coef The SEM coeffs.
   subroutine const_source_term_init_from_json(this, json, fields, coef) 
     class(const_source_term_t), intent(inout) :: this
     type(json_file), intent(inout) :: json
     class(field_list_t), intent(inout), target :: fields
     type(coef_t), intent(inout) :: coef
-    real(kind=rp) :: value
+    real(kind=rp), allocatable :: values(:)
+    ! Json low-level manipulator.
+    type(json_core) :: core
 
-    call json_get(json, "value", value)
-    call const_source_term_init_from_components(this, json, fields, value, coef)
+    call json_get(json, "values", values)
+    call const_source_term_init_from_components(this, fields, values, coef)
 
   end subroutine const_source_term_init_from_json
 
-  subroutine const_source_term_init_from_components(this, json, fields, value, &
+  !> The constructor from type components.
+  !! @param fields A list of fields for adding the source values.
+  !! @param values The array of values, one for each field.
+  !! @param coef The SEM coeffs.
+  subroutine const_source_term_init_from_components(this, fields, values, &
                                                     coef) 
     class(const_source_term_t), intent(inout) :: this
-    type(json_file), intent(inout) :: json
     class(field_list_t), intent(inout), target :: fields
-    real(kind=rp), intent(in) :: value
+    real(kind=rp), intent(in) :: values(:)
     type(coef_t) :: coef
 
     call this%init_base(fields, coef)
-    this%value = value
+
+    if (size(values) .ne. size(fields%fields)) then
+       call neko_error("Number of fields and values inconsistent.")
+    end if
+
+    this%values = values
   end subroutine const_source_term_init_from_components
 
   subroutine const_source_term_free(this) 
@@ -102,11 +120,11 @@ contains
 
     if (NEKO_BCKND_DEVICE .eq. 1) then
        do i=1, n_fields
-          call device_cadd(this%fields%fields(i)%f%x_d, this%value, n)
+          call device_cadd(this%fields%fields(i)%f%x_d, this%values(i), n)
        end do
     else
        do i=1, n_fields
-          call cadd(this%fields%fields(i)%f%x, this%value, n)
+          call cadd(this%fields%fields(i)%f%x, this%values(i), n)
        end do
     end if
   end subroutine const_source_term_compute
