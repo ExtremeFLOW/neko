a11cb81dbde028491097678cfb4bb92a4713c1f0
Adalberto Perez <adalberto0524@gmail.com>
2023-09-21 16:43:49 +0200

    uncommented for testing

--- src/io/data_streamer.F90
+++ src/io/data_streamer.F90
@@ -91,9 +91,9 @@ contains
     !nelb = nelb - nelv
 
 !#ifdef HAVE_ADIOS2
-!    call fortran_adios2_setup(npts,nelv,nelb,nelgv, &
-!                nelgv,coef%dof%x,coef%dof%y,  &
-!                coef%dof%z,if_asynch,NEKO_COMM)
+    call fortran_adios2_setup(npts,nelv,nelb,nelgv, &
+                nelgv,coef%dof%x,coef%dof%y,  &
+                coef%dof%z,if_asynch,NEKO_COMM)
 !#else
 !    call neko_error('NEKO needs to be built with ADIOS2 support')
 !#endif
@@ -108,7 +108,7 @@ contains
     if (allocated(this%lglel))        deallocate(this%lglel)
 
 !#ifdef HAVE_ADIOS2
-!    call fortran_adios2_finalize()
+    call fortran_adios2_finalize()
 !#else
 !    call neko_error('NEKO needs to be built with ADIOS2 support')
 !#endif
@@ -137,7 +137,7 @@ contains
     end if
 
 !#ifdef HAVE_ADIOS2
-!    call fortran_adios2_stream(this%lglel,p%x, u%x, v%x, w%x, coef%B, u%x)
+    call fortran_adios2_stream(this%lglel,p%x, u%x, v%x, w%x, coef%B, u%x)
 !#else
 !    call neko_error('NEKO needs to be built with ADIOS2 support')
 !#endif
