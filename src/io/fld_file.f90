3c0ed4c6e337e01eded6e21a886d76b0ee859a49
Martin Karp <martin.karp@outlook.com>
2023-10-11 16:30:12 +0200

    add use array_ptr_t

--- src/io/fld_file.f90
+++ src/io/fld_file.f90
@@ -38,10 +38,12 @@ module fld_file
   use field_list
   use dofmap
   use space
+  use structs, only: array_ptr_t
   use vector
   use fld_file_data
   use mean_flow
   use mean_sqr_flow
+  use structs
   use vector, only : vector_t
   use space, only : space_t
   use mesh, only : mesh_t
@@ -64,9 +66,6 @@ module fld_file
      procedure :: set_precision => fld_file_set_precision
   end type fld_file_t
 
-  type, private :: array_ptr_t
-     real(kind=rp), pointer :: x(:)
-  end type array_ptr_t
 
 contains
 
