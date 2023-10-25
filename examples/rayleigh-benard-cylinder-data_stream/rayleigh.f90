1e43c39273c7c589919f3e719845012384f3538f
Adalberto Perez <adalberto0524@gmail.com>
2023-10-25 16:51:32 +0200

    added everything from the neko side

---
+++ examples/rayleigh-benard-cylinder-data_stream/rayleigh.f90
@@ -0,0 +1,178 @@
+module user
+  use neko
+  implicit none
+
+  !> Variables to store the Rayleigh and Prandlt numbers
+  real(kind=rp) :: Ra = 0
+  real(kind=rp) :: Re = 0
+  real(kind=rp) :: Pr = 0
+
+  type(data_streamer_t) :: dstream
+
+contains
+  ! Register user defined functions (see user_intf.f90)
+  subroutine user_setup(u)
+    type(user_t), intent(inout) :: u
+    u%user_init_modules => user_initialize
+    u%user_finalize_modules => user_finalize
+    u%fluid_user_ic => set_initial_conditions_for_u_and_s
+    u%scalar_user_bc => set_scalar_boundary_conditions
+    u%fluid_user_f_vector => set_bousinesq_forcing_term
+    u%user_check => check
+    u%material_properties => set_material_properties
+  end subroutine user_setup
+
+  subroutine set_material_properties(t, tstep, rho, mu, cp, lambda, params)
+    real(kind=rp), intent(in) :: t
+    integer, intent(in) :: tstep
+    real(kind=rp), intent(inout) :: rho, mu, cp, lambda
+    type(json_file), intent(inout) :: params
+    real(kind=rp) :: Re
+
+    call json_get(params, "case.fluid.Ra", Ra)
+    call json_get(params, "case.scalar.Pr", Pr)
+
+
+    Re = sqrt(Ra / Pr)
+    mu = 1.0_rp / Re
+    lambda = mu / Pr
+    rho = 1.0_rp
+    cp = 1.0_rp
+  end subroutine set_material_properties
+   
+ 
+  subroutine set_scalar_boundary_conditions(s, x, y, z, nx, ny, nz, ix, iy, iz, ie, t, tstep)
+    real(kind=rp), intent(inout) :: s
+    real(kind=rp), intent(in) :: x
+    real(kind=rp), intent(in) :: y
+    real(kind=rp), intent(in) :: z
+    real(kind=rp), intent(in) :: nx
+    real(kind=rp), intent(in) :: ny
+    real(kind=rp), intent(in) :: nz
+    integer, intent(in) :: ix
+    integer, intent(in) :: iy
+    integer, intent(in) :: iz
+    integer, intent(in) :: ie
+    real(kind=rp), intent(in) :: t
+    integer, intent(in) :: tstep
+
+    !> Variables for bias
+    real(kind=rp) :: arg, bias
+    
+    ! This will be used on all zones without labels
+    ! e.g. the ones hardcoded to 'v', 'w', etcetc
+    s = 1.0_rp - z
+
+  end subroutine set_scalar_boundary_conditions
+
+  subroutine set_initial_conditions_for_u_and_s(u, v, w, p, params)
+    type(field_t), intent(inout) :: u
+    type(field_t), intent(inout) :: v
+    type(field_t), intent(inout) :: w
+    type(field_t), intent(inout) :: p
+    type(json_file), intent(inout) :: params
+    type(field_t), pointer :: s
+    integer :: i, j, k, e
+    real(kind=rp) :: rand, r,z
+    s => neko_field_registry%get_field('s')
+
+    !> Initialize with zero velocity
+    call rzero(u%x,u%dof%size())
+    call rzero(v%x,v%dof%size())
+    call rzero(w%x,w%dof%size())
+
+    !> Initialize with rand perturbations on temperature
+    call rzero(s%x,w%dof%size())
+    do i = 1, s%dof%size()
+       s%x(i,1,1,1) = 1-s%dof%z(i,1,1,1) 
+    end do
+    ! perturb not on element boundaries
+    ! Maybe not necessary, but lets be safe
+    do e = 1, s%msh%nelv
+       do k = 2,s%Xh%lx-1
+          do j = 2,s%Xh%lx-1
+             do i = 2,s%Xh%lx-1
+
+                !call random_number(rand)
+                !Somewhat random
+                rand = cos(real(e+s%msh%offset_el,rp)*real(i*j*k,rp))
+                r = sqrt(s%dof%x(i,j,k,e)**2+s%dof%y(i,j,k,e)**2)
+                z = s%dof%z(i,j,k,e)
+                s%x(i,j,k,e) = 1-z + 0.0001*rand*s%dof%x(i,j,k,e)*&
+                                                    sin(3*pi*r/0.05_rp)*sin(10*pi*z)
+             end do
+          end do
+       end do
+    end do
+    if (NEKO_BCKND_DEVICE .eq. 1) then
+       call device_memcpy(s%x,s%x_d,s%dof%size(),HOST_TO_DEVICE)
+    end if
+
+  end subroutine set_initial_conditions_for_u_and_s
+
+  subroutine user_initialize(t, u, v, w, p, coef, params)
+    real(kind=rp) :: t
+    type(field_t), intent(inout) :: u
+    type(field_t), intent(inout) :: v
+    type(field_t), intent(inout) :: w
+    type(field_t), intent(inout) :: p
+    type(coef_t), intent(inout) :: coef
+    type(json_file), intent(inout) :: params
+
+
+    !> Initialize the streamer object
+    call dstream%init(coef, 1)
+    
+
+  end subroutine user_initialize
+
+
+  subroutine user_finalize(t, param)
+    real(kind=rp) :: t
+    type(json_file), intent(inout) :: param
+
+    !> Finalize the stream object
+    call dstream%free()
+  
+  end subroutine user_finalize
+
+  subroutine set_bousinesq_forcing_term(f, t)
+    class(fluid_user_source_term_t), intent(inout) :: f
+    real(kind=rp), intent(in) :: t
+    integer :: i
+    type(field_t), pointer :: u, v, w, s
+    real(kind=rp) :: rapr, ta2pr
+    u => neko_field_registry%get_field('u')
+    v => neko_field_registry%get_field('v')
+    w => neko_field_registry%get_field('w')
+    s => neko_field_registry%get_field('s')
+
+    if (NEKO_BCKND_DEVICE .eq. 1) then
+       call device_rzero(f%u_d,f%dm%size())
+       call device_rzero(f%v_d,f%dm%size())
+       call device_copy(f%w_d,s%x_d,f%dm%size())
+    else
+       call rzero(f%u,f%dm%size())
+       call rzero(f%v,f%dm%size())
+       call copy(f%w,s%x,f%dm%size())
+    end if
+  end subroutine set_bousinesq_forcing_term
+
+  subroutine check(t, tstep, u, v, w, p, coef, params)
+    real(kind=rp), intent(in) :: t
+    integer, intent(in) :: tstep
+    type(coef_t), intent(inout) :: coef
+    type(json_file), intent(inout) :: params
+    type(field_t), intent(inout) :: u
+    type(field_t), intent(inout) :: v
+    type(field_t), intent(inout) :: w
+    type(field_t), intent(inout) :: p
+
+
+    !> Send data to data processor
+    call dstream%stream(u, v, w, p, coef)  
+
+
+  end subroutine check
+
+end module user
