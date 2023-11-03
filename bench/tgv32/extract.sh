8cab6c6d236b9ce29eb693233b814b2160bd7fb8
Martin Karp <martin.karp@outlook.com>
2023-11-03 14:02:37 +0100

    change text

--- bench/tgv32/extract.sh
+++ bench/tgv32/extract.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Extract time and enstrophy from neko output on stdin
+# Extract time and enstrophy from neko output from argument $1
 
 echo "# Time Enstrophy"
 awk '/enst:/ {print($3, $NF)}' $1
