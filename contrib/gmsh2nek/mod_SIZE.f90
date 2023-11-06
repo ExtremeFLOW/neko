0c6173a27c487dc6fda89516f14b622280cfe4d0
Timofey Mukha <timofey.mukha@protonmail.com>
2023-11-06 17:40:51 +0300

    Revert to compile.sh and makefile

--- contrib/gmsh2nek/mod_SIZE.f90
+++ contrib/gmsh2nek/mod_SIZE.f90
@@ -56,9 +56,7 @@
 ! herein do not necessarily state or reflect those of the United States 
 ! Government or UCHICAGO ARGONNE, LLC, and shall 
 ! not be used for advertising or product endorsement purposes.
-
 module SIZE
-       use neko 
 !
 !
 ! Gmsh msh file related variables:
@@ -69,11 +67,11 @@ module SIZE
       integer num_dim,num_elem
       integer aorb ! file type in fmsh file header, 0 for ascii, 1 for binary
  
-      real(kind=rp),save,allocatable,dimension(:,:)    ::node_xyz ! real data in msh binary file is 8 byte
+      real*8,save,allocatable,dimension(:,:)    ::node_xyz ! real data in msh binary file is 8 byte
       integer,save,allocatable,dimension(:,:)   ::node_quad,node_hex
       integer,save,allocatable,dimension(:,:)   ::quad_array,hex_array,hex_face_array
       integer,save,allocatable,dimension(:,:)   ::node_line
-      integer,save,allocatable,dimension(:,:)   ::line_array,quad_line_array
+	  integer,save,allocatable,dimension(:,:)   ::line_array,quad_line_array
       integer,save,allocatable,dimension(:)     ::r_or_l ! for 2d msh quad elements, right-hand or left-hand 
 
       integer,save,allocatable,dimension(:,:)     :: bcID ! bcID(1) = bcID, bcID(2) = surface total quad/lines elements number, bcID(3)=periodic bc id
@@ -81,8 +79,8 @@ module SIZE
 
 ! NEK CORE variables:
 !
-      real(kind=rp),save,allocatable,dimension(:,:,:)   ::  bc, curve
-      real(kind=rp),save,allocatable,dimension(:,:,:,:) ::  xm1, ym1, zm1
+      real,save,allocatable,dimension(:,:,:)   ::  bc, curve
+      real,save,allocatable,dimension(:,:,:,:) ::  xm1, ym1, zm1
 
       character(1),save,allocatable,dimension(:,:) :: ccurve
       character(3),save,allocatable,dimension(:,:) :: cbc
