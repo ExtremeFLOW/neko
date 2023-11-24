ba91bbe6dadbb9598aebd09ea161f99494356789
Adalberto Perez <adalberto0524@gmail.com>
2023-11-24 18:37:25 +0100

    added more calculations and made the whole thing prettier

--- examples/rbc_RA1e8/post_process/TimeSeries/time_series_data.py
+++ examples/rbc_RA1e8/post_process/TimeSeries/time_series_data.py
@@ -15,6 +15,9 @@ class time_series_data_c():
         self.dtdz_bot = abs(dat[:,3])
         self.lx = dat[:,4]
         self.Ra = dat[:,5]
+        self.nu_eps_t = dat[:,6]
+        self.nu_eps_k = dat[:,7]
+        self.tke = dat[:,8]
 
         ra = self.Ra[0]
         self.nu_v = 1 + np.sqrt(ra)*self.uzt
@@ -28,6 +31,10 @@ class time_series_data_c():
         self.nu_v_transient_index = adf_test(self.nu_v, granularity_for_iterations)
         self.nu_a_transient_index = adf_test(self.nu_a, granularity_for_iterations)
         self.nu_va_transient_index = adf_test(self.nu_v/self.nu_a, granularity_for_iterations)
+        self.nu_eps_t_transient_index = adf_test(self.nu_eps_t, granularity_for_iterations)
+        self.nu_eps_k_transient_index = adf_test(self.nu_eps_k, granularity_for_iterations)
+        self.tke_transient_index = adf_test(self.tke[np.where(self.tke > 0)], granularity_for_iterations)
+        #self.tke_transient_index = self.tke_transient_index + np.where(self.tke[self.tke<=0])[0][-1]
 
         return
 
@@ -38,6 +45,11 @@ class time_series_data_c():
 
         self.nu_a_batch = get_lag1_cor(self.t[self.nu_a_transient_index:],self.nu_a[self.nu_a_transient_index:], len(self.nu_a[self.nu_a_transient_index:]), plot)
 
+        self.nu_eps_t_batch = get_lag1_cor(self.t[self.nu_eps_t_transient_index:],self.nu_eps_t[self.nu_eps_t_transient_index:], len(self.nu_eps_t[self.nu_eps_t_transient_index:]), plot)
+
+        self.nu_eps_k_batch = get_lag1_cor(self.t[self.nu_eps_k_transient_index:],self.nu_eps_k[self.nu_eps_k_transient_index:], len(self.nu_eps_k[self.nu_eps_k_transient_index:]), plot)
+
+       
     def get_stats(self):
         
         mean, var, ci = get_SME_var(self.t[self.nu_v_transient_index:],self.nu_v[self.nu_v_transient_index:], self.nu_v_batch)
@@ -46,3 +58,8 @@ class time_series_data_c():
         mean, var, ci = get_SME_var(self.t[self.nu_a_transient_index:],self.nu_a[self.nu_a_transient_index:], self.nu_a_batch)
         self.stats_nu_a = [mean, var, ci]
 
+        mean, var, ci = get_SME_var(self.t[self.nu_eps_t_transient_index:],self.nu_eps_t[self.nu_eps_t_transient_index:], self.nu_eps_t_batch)
+        self.stats_nu_eps_t = [mean, var, ci]
+
+        mean, var, ci = get_SME_var(self.t[self.nu_eps_k_transient_index:],self.nu_eps_k[self.nu_eps_k_transient_index:], self.nu_eps_k_batch)
+        self.stats_nu_eps_k = [mean, var, ci]
--- examples/rbc_RA1e8/post_process/TimeSeries/time_series_plotter.py
+++ examples/rbc_RA1e8/post_process/TimeSeries/time_series_plotter.py
@@ -7,6 +7,8 @@ rcParams['text.usetex'] = False
 rcParams['lines.linewidth'] = 0.5
 rcParams['font.size'] = 16
 
+import numpy as np
+
 class time_series_plotter_c():
     
     def __init__(self):
@@ -50,34 +52,92 @@ class time_series_plotter_c():
         
         for which_ra in keys:
         
-            print("------ Now writing for Ra = "+repr(which_ra)+ "---------------")
+            fig, ax = plt.subplots(4, 2,figsize=(10, 12), dpi=300)
             
-            fig, ax = plt.subplots(2, 2,figsize=(10, 6), dpi=300)
-            i=-1
             for ts in ts_dict[which_ra]:
+
+                ra_string = '{:.0e}'.format(ts.Ra[-1])
+                fig.suptitle(r'Time series for $Ra = $' + ra_string, fontsize=15)
+
+                # Check if the time is always increasing, this could be broken if the run stopped suddenly and the time series was not cleaned.
+                increasing = np.all(np.diff(ts.t) > 0)
+                if increasing == False:
+                    print("the time series is not strictly increasing, overwriting instances in which the time is larger than a subsequent simulation")
+                    index_where_time_goes_back  = np.where(np.diff(ts.t) < 0)
+                    print(index_where_time_goes_back[0][0])
+                    index_that_should_be_deleted = np.where(ts.t[:index_where_time_goes_back[0][0]+1] >= ts.t[index_where_time_goes_back[0][0]+1])[0]
+                    print("The following entries (rows) of the time series will be deleted to favor the newer simulation")
+                    print(index_that_should_be_deleted)
+                    #print(ts.t[index_that_should_be_deleted])
+                    #print(ts.t[index_that_should_be_deleted[-1]:index_that_should_be_deleted[-1]+100])
+                    ts.t = np.delete(ts.t, index_that_should_be_deleted, axis=0)
+            
+                # Check again if delete worked
+                increasing = np.all(np.diff(ts.t) > 0)
+                print(increasing)
+            
+                unique_lx = (np.unique(ts.lx))
+                # Plot unique colors for different orders
+                for lx_ in unique_lx:
+                    ax[0,0].plot(ts.t[ts.lx == lx_] - ts.t[0], ts.nu_a[ts.lx == lx_])
+                    #ax[0,0].plot(ts.t, ts.nu_v)
+                    ax[0,0].set_xlabel(r'$t - t_{0}$')
+                    ax[0,0].set_ylabel(r'$Nu_{\langle A \rangle}$')
+                    #ax[0,0].set_xlim(50,600)
+                    ax[0,0].set_ylim(min(ts.nu_eps_k)*0.9,max(ts.nu_eps_k)*1.1)
+                            
+                    #ax[0,1].plot(ts.t[ts.lx == lx_] - ts.t[0], ts.nu_v[ts.lx == lx_]/ts.nu_a[ts.lx == lx_],)
+                    ax[0,1].plot(ts.t[ts.lx == lx_] - ts.t[0], ts.nu_v[ts.lx == lx_],)
+                    ax[0,1].set_xlabel(r'$t - t_{0}$')
+                    ax[0,1].set_ylabel(r'$Nu_{\langle V \rangle}$')
+                    #ax[0,1].set_ylim(0.5,1.5)
+                    ax[0,1].set_ylim(min(ts.nu_eps_k)*0.9,max(ts.nu_eps_k)*1.1)
+            
+                    ax[1,0].plot(ts.t[ts.lx == lx_] - ts.t[0], ts.nu_eps_t[ts.lx == lx_],)
+                    ax[1,0].set_xlabel(r'$t - t_{0}$')
+                    ax[1,0].set_ylabel(r'$eps_t$')
+                    #ax[0,1].set_ylim(0.5,1.5)
+                    ax[1,0].set_ylim(min(ts.nu_eps_k)*0.9,max(ts.nu_eps_k)*1.1)
+            
+                    ax[1,1].plot(ts.t[ts.lx == lx_] - ts.t[0], ts.nu_eps_k[ts.lx == lx_],)
+                    ax[1,1].set_xlabel(r'$t - t_{0}$')
+                    ax[1,1].set_ylabel(r'$eps_k$')
+                    #ax[0,1].set_ylim(0.5,1.5)
+                    ax[1,1].set_ylim(min(ts.nu_eps_k)*0.9,max(ts.nu_eps_k)*1.1)
+                            
+                    ax[2,0].plot(ts.t[ts.lx == lx_] - ts.t[0], ts.tke[ts.lx == lx_],)
+                    ax[2,0].set_xlabel(r'$t - t_{0}$')
+                    ax[2,0].set_ylabel(r'$tke$')
+                    ax[2,0].set_ylim(min(ts.tke[ts.tke>0])*0.9,max(ts.tke[ts.tke>0])*1.1)
+            
+                    ax[2,1].axis('off')
+                    
+                    ax[3,0].plot(ts.t[ts.lx == lx_] - ts.t[0], ts.lx[ts.lx == lx_])
+                    ax[3,0].set_xlabel(r'$t - t_{0}$')
+                    ax[3,0].set_ylabel(r'$lx$')
+                        
+                    ax[3,1].plot(ts.t[ts.lx == lx_] - ts.t[0], ts.Ra[ts.lx == lx_],)
+                    ax[3,1].set_xlabel(r'$t - t_{0}$')
+                    ax[3,1].set_ylabel(r'$Ra$')
+
+            if plot_transient_end==1:
+                ax[0,0].axvline(x=ts.t[ts.nu_a_transient_index], linestyle=':', color= "red", linewidth = 1)
+                ax[0,1].axvline(x=ts.t[ts.nu_v_transient_index], linestyle=':', color= "red", linewidth = 1)
+                ax[1,0].axvline(x=ts.t[ts.nu_eps_t_transient_index], linestyle=':', color= "red", linewidth = 1)
+                ax[1,1].axvline(x=ts.t[ts.nu_eps_k_transient_index], linestyle=':', color= "red", linewidth = 1)
+                # Correct for tke index in case some files did not have tke on them
+                tke_real_transient_index = np.where(ts.tke == 0)[0][-1] + ts.tke_transient_index
+                ax[2,0].axvline(x=ts.t[tke_real_transient_index], linestyle=':', color= "red", linewidth = 1)
+                    
                 
-                i+=1
-            
-                ax[0,0].plot(ts.t, ts.nu_a)
-                #ax[0,0].plot(ts.t, ts.nu_v)
-                ax[0,0].set_xlabel(r'$t$')
-                ax[0,0].set_ylabel(r'$Nu_{\langle A \rangle}$')
-                if plot_transient_end:
-                    ax[0,0].axvline(x=ts.t[ts.nu_a_transient_index], linestyle=':')
-                    
-                ax[0,1].plot(ts.t, ts.nu_v/ts.nu_a,)
-                ax[0,1].set_xlabel(r'$t$')
-                ax[0,1].set_ylabel(r'$Nu_{\langle V \rangle}/Nu_{\langle A \rangle}$')
-                ax[0,1].set_ylim(0.5,1.5)
-            
-                ax[1,0].plot(ts.t, ts.lx)
-                ax[1,0].set_xlabel(r'$t$')
-                ax[1,0].set_ylabel(r'$lx$')
-                    
-                ax[1,1].plot(ts.t, ts.Ra,)
-                ax[1,1].set_xlabel(r'$t$')
-                ax[1,1].set_ylabel(r'$Ra$')
-            
+            ax[0,0].grid(color = 'black', linestyle = '-', linewidth = 0.1)
+            ax[0,1].grid(color = 'black', linestyle = '-', linewidth = 0.1)
+            ax[1,0].grid(color = 'black', linestyle = '-', linewidth = 0.1)
+            ax[1,1].grid(color = 'black', linestyle = '-', linewidth = 0.1)
+            ax[2,0].grid(color = 'black', linestyle = '-', linewidth = 0.1)
+            ax[2,1].grid(color = 'black', linestyle = '-', linewidth = 0.1)
+            ax[3,0].grid(color = 'black', linestyle = '-', linewidth = 0.1)
+            ax[3,1].grid(color = 'black', linestyle = '-', linewidth = 0.1)
             plt.tight_layout()
             #plt.savefig("nu_timeseries.pdf", format="pdf", bbox_inches="tight")
             plt.show()
