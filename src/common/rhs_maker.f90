c16adce079a032b6837928cdcc8ea12dad378ca1
Tim Felle Olsen <tife@dtu.dk>
2023-12-04 10:49:25 +0100

    Revert "rhs_maker"
    This reverts commit 43b61c9d07e6fd19de8c06f3a3cb08d9099dbece.

--- src/common/rhs_maker.f90
+++ src/common/rhs_maker.f90
@@ -111,7 +111,7 @@ module rhs_maker
        import rp
        integer, intent(in) :: n, nbd
        type(field_t), intent(in) :: u, v, w
-       type(field_series_t), intent(in) :: ulag, vlag, wlag
+       type(field_series_t), intent(in) :: ulag, vlag, wlag        
        real(kind=rp), intent(inout) :: bfx(n), bfy(n), bfz(n)
        real(kind=rp), intent(in) :: B(n)
        real(kind=rp), intent(in) :: dt, rho, bd(4)
