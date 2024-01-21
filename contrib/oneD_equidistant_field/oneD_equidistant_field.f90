def0bc611f1b53478dc3e2cd61b717df5f05f387
Shiyu Du <115027341+Shiyu-Sandy-Du@users.noreply.github.com>
2024-01-21 09:08:33 +0100

    Update oneD_equidistant_field.f90

--- contrib/oneD_equidistant_field/oneD_equidistant_field.f90
+++ contrib/oneD_equidistant_field/oneD_equidistant_field.f90
@@ -2,7 +2,7 @@
 !> The number of points inside an element of the input and output field file is supposed to be the same
 !> Shiyu Du 16/01-24 note: the program is designed for Cartesian Box meshes with equidistant elements on the hom-dir,
 !                          the version for rotational coordinates is not yet been thought about.
-!> Shiyu Du 16/01-24 note: the program is currently tested on 1 core and MPI, GPU is not tested yet.
+!> Shiyu Du 16/01-24 note: the program is currently tested on CPUs, GPU is not tested yet.
 !! Shiyu Du 16/01-24
 program oneD_equidistant_field
   use neko
