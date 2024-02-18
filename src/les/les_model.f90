e57aca483f87f8cba4d55cd61224e3c15fe1c07a
Timofey Mukha <timofey.mukha@protonmail.com>
2024-02-18 13:47:08 +0300

    disable gsop in les delta for now

--- src/les/les_model.f90
+++ src/les/les_model.f90
@@ -182,7 +182,7 @@ contains
        enddo
     enddo
 
-    call this%coef%gs_h%op(this%delta, GS_OP_ADD)
+!    call this%coef%gs_h%op(this%delta%x, this%delta%dof%size(), GS_OP_ADD)
 
     if (NEKO_BCKND_DEVICE .eq. 1) then
        call device_memcpy(this%delta%x, this%delta%x_d, this%delta%dof%size(),&
