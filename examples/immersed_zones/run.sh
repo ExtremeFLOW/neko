6758e7c587678142142f022c4be39f1d9a1c7dcc
Tim Felle Olsen <tife@dtu.dk>
2024-02-21 14:26:50 +0100

    Updated examples

--- examples/immersed_zones/run.sh
+++ examples/immersed_zones/run.sh
@@ -10,44 +10,43 @@ function help() {
     echo -e "  directions, respectively."
     echo -e ""
     echo -e "  If no input arguments are provided, the default mesh size is"
-    echo -e "  16x8x8."
+    echo -e "  32x8x8."
     echo -e ""
     echo -e "  Example usage:"
-    echo -e "    run.sh 16 8 8"
+    echo -e "    run.sh -x32 -y8 -z8 case_name.case"
     echo -e ""
     echo -e " Options:"
     echo -e "  -h, --help  Show this help message and exit."
-    echo -e "  -x          Number of cells in the x direction."
-    echo -e "  -y          Number of cells in the y direction."
-    echo -e "  -z          Number of cells in the z direction."
+    echo -e "  -x#         Number of cells in the x direction."
+    echo -e "  -y#         Number of cells in the y direction."
+    echo -e "  -z#         Number of cells in the z direction."
     echo -e ""
     echo -e "  See Readme for additional details."
     exit 0
 }
 
-Nx=16
-Ny=8
-Nz=8
-
-# Search for "-h" or "--help" in the input arguments
-for arg in $@; do
-    case $arg in
-    -h | --help) help ;;
-    -x) Nx=$arg ;;
-    -y) Ny=$arg ;;
-    -z) Nz=$arg ;;
-    *) ;;
-    esac
+# Handle options
+Nx=32 && Ny=8 && Nz=8
+for arg in "$@"; do
+    if [ "${arg:0:2}" == "--" ]; then
+        case ${arg:2} in
+        help) help ;;
+        *) echo -e "Invalid option: $arg" >&2 && help ;;
+        esac
+    elif [ "${arg:0:1}" == "-" ]; then
+        case ${arg:1:1} in
+        h) help ;;
+        x) Nx=${arg:2} ;;
+        y) Ny=${arg:2} ;;
+        z) Nz=${arg:2} ;;
+        *) echo -e "Invalid option: ${arg:1}" >&2 && help ;;
+        esac
+    else
+        cases="$cases $arg"
+    fi
 done
 
-# Extract the cases to run
-cases=""
-for arg in $@; do
-    case $arg in
-    -h | --help | -x | -y | -z) continue ;;
-    *) cases+="$arg " ;;
-    esac
-done
+# ============================================================================ #
 
 # ============================================================================ #
 # Ensure Neko can be found and set default mesh size
@@ -71,7 +70,7 @@ genmeshbox 0 4 0 1 0 1 $Nx $Ny $Nz .false. .false. .false.
 
 for case in $cases; do
     echo "Running case: $case"
-    # neko $case >${case%.*}.log
+    neko $case >${case%.*}.log
 done
 
 # End of file
