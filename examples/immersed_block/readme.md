03541aa16d705cb23df286d2c956ca00f7530b4d
Tim Felle Olsen <tife@dtu.dk>
2024-02-26 08:45:54 +0100

    Prepare for point zone brinkman terms

---
+++ examples/immersed_block/readme.md
@@ -0,0 +1,13 @@
+# Immersed boundary of a Stanford Bunny
+
+This example illustrates the brinkman condition source term used to simulate an
+immersed boundary.
+
+## Immersed bunny example
+
+```
+Bunny url:              https://www.thingiverse.com/thing:151081
+Bunny domain:           [ [-24, 85], [-41, 45], [5, 112] ]
+Design domain:          [ [-100, 400], [-100, 100], [0, 200] ]
+```
+
---
+++ examples/immersed_block/run.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/bash
+
+# ============================================================================ #
+# Define the help function
+
+function help() {
+    echo -e "run.sh [Nx] [Ny] [Nz]"
+    echo -e "  Generate a mesh and run the immersed_block case."
+    echo -e "  The input arguments are the number of cells in the x, y, and z"
+    echo -e "  directions, respectively."
+    echo -e ""
+    echo -e "  If no input arguments are provided, the default mesh size is"
+    echo -e "  16x8x8."
+    echo -e ""
+    echo -e "  See Readme for additional details."
+    exit 0
+}
+
+# Search for "-h" or "--help" in the input arguments
+for arg in $@; do
+    if [[ $arg == "-h" ]] || [[ $arg == "--help" ]]; then
+        help
+    fi
+done
+
+# ============================================================================ #
+# Ensure Neko can be found and set default mesh size
+
+if [ "$NEKO_DIR" ]; then
+    export PATH=$NEKO_DIR:$PATH
+fi
+
+if [[ -z $(which neko) ]]; then
+    echo -e "Neko not found." >&2
+    echo -e "Please ensure Neko is installed and in your PATH." >&2
+    echo -e "Alternatively, set the NEKO_DIR environment variable." >&2
+    exit 1
+fi
+
+if [ $# -lt 1 ]; then Nx=16; else Nx=$1; fi
+if [ $# -lt 2 ]; then Ny=8; else Ny=$2; fi
+if [ $# -lt 3 ]; then Nz=8; else Nz=$3; fi
+
+# ============================================================================ #
+# Generate mesh and run case
+
+echo "Generating mesh with dimensions: $Nx $Ny $Nz"
+genmeshbox 0 4 0 1 0 1 $Nx $Ny $Nz .false. .false. .false.
+neko immersed_block.case
+
+# End of file
+# ============================================================================ #
