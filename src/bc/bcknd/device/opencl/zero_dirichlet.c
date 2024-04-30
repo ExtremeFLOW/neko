ef570cf746f7a6a98c15ae35fecfa011cd55b0f1
Timofey Mukha <timofey.mukha@protonmail.com>
2024-04-30 16:19:41 +0300

    Try to fix depends_device

--- src/bc/bcknd/device/opencl/zero_dirichlet.c
+++ src/bc/bcknd/device/opencl/zero_dirichlet.c
@@ -44,18 +44,18 @@
 #include <device/opencl/prgm_lib.h>
 #include <device/opencl/check.h>
 
-#include "no_slip_wall_kernel.cl.h"
+#include "zero_dirichlet_kernel.cl.h"
 
-/** 
+/**
  * Fortran wrapper for device no-slip wall apply vector
  */
 void opencl_no_slip_wall_apply_scalar(void *msk, void *x, int *m) {
-    
+
   cl_int err;
-  
+
   if (no_slip_wall_program == NULL)
     opencl_kernel_jit(no_slip_wall_kernel, (cl_program *) &no_slip_wall_program);
-  
+
   cl_kernel kernel = clCreateKernel(no_slip_wall_program,
                                     "no_slip_wall_apply_scalar_kernel", &err);
   CL_CHECK(err);
@@ -63,7 +63,7 @@ void opencl_no_slip_wall_apply_scalar(void *msk, void *x, int *m) {
   CL_CHECK(clSetKernelArg(kernel, 0, sizeof(cl_mem), (void *) &msk));
   CL_CHECK(clSetKernelArg(kernel, 1, sizeof(cl_mem), (void *) &x));
   CL_CHECK(clSetKernelArg(kernel, 2, sizeof(int), m));
-  
+
   const int nb = ((*m) + 256 - 1) / 256;
   const size_t global_item_size = 256 * nb;
   const size_t local_item_size = 256;
@@ -73,16 +73,16 @@ void opencl_no_slip_wall_apply_scalar(void *msk, void *x, int *m) {
                                   0, NULL, NULL));
 }
 
-/** 
+/**
  * Fortran wrapper for device no-slip wall apply vector
  */
 void opencl_no_slip_wall_apply_vector(void *msk, void *x, void *y,
                                       void *z, int *m) {
   cl_int err;
-  
+
   if (no_slip_wall_program == NULL)
     opencl_kernel_jit(no_slip_wall_kernel, (cl_program *) &no_slip_wall_program);
-  
+
   cl_kernel kernel = clCreateKernel(no_slip_wall_program,
                                     "no_slip_wall_apply_vector_kernel", &err);
   CL_CHECK(err);
@@ -92,7 +92,7 @@ void opencl_no_slip_wall_apply_vector(void *msk, void *x, void *y,
   CL_CHECK(clSetKernelArg(kernel, 2, sizeof(cl_mem), (void *) &y));
   CL_CHECK(clSetKernelArg(kernel, 3, sizeof(cl_mem), (void *) &z));
   CL_CHECK(clSetKernelArg(kernel, 4, sizeof(int), m));
-  
+
   const int nb = ((*m) + 256 - 1) / 256;
   const size_t global_item_size = 256 * nb;
   const size_t local_item_size = 256;
@@ -100,5 +100,5 @@ void opencl_no_slip_wall_apply_vector(void *msk, void *x, void *y,
   CL_CHECK(clEnqueueNDRangeKernel((cl_command_queue) glb_cmd_queue, kernel, 1,
                                   NULL, &global_item_size, &local_item_size,
                                   0, NULL, NULL));
- 
+
 }
