d45ed3c00e4afea83aa281fa58c8afcd1e515599
Niclas Jansson <njansson@kth.se>
2024-05-29 22:19:42 +0200

    Add CUDA vector version for device ax_helm

--- src/math/ax.f90
+++ src/math/ax.f90
@@ -82,7 +82,7 @@ module ax_product
   !! @param Xh Function space \f$ X_h \f$.
   abstract interface
      subroutine ax_compute_vector(this, au, av, aw, u, v, w, coef, msh, Xh)
-        import space_t
+       import space_t
        import mesh_t
        import coef_t
        import ax_t
--- src/math/bcknd/device/cuda/ax_helm_kernel.h
+++ src/math/bcknd/device/cuda/ax_helm_kernel.h
@@ -347,4 +347,375 @@ __global__ void ax_helm_kernel_kstep_padded(T * __restrict__ w,
   }
 }
 
+/*
+ * Vector versions
+ */
+
+template< typename T, const int LX >
+__global__ void ax_helm_kernel_vector_kstep(T * __restrict__ au,
+                                            T * __restrict__ av,
+                                            T * __restrict__ aw,
+                                            const T * __restrict__ u,
+                                            const T * __restrict__ v,
+                                            const T * __restrict__ w,
+                                            const T * __restrict__ dx,
+                                            const T * __restrict__ dy,
+                                            const T * __restrict__ dz,
+                                            const T * __restrict__ h1,
+                                            const T * __restrict__ g11,
+                                            const T * __restrict__ g22,
+                                            const T * __restrict__ g33,
+                                            const T * __restrict__ g12,
+                                            const T * __restrict__ g13,
+                                            const T * __restrict__ g23) {
+
+  __shared__ T shdx[LX * LX];
+  __shared__ T shdy[LX * LX];
+  __shared__ T shdz[LX * LX];
+    
+  __shared__ T shu[LX * LX];
+  __shared__ T shur[LX * LX];
+  __shared__ T shus[LX * LX];
+
+  __shared__ T shv[LX * LX];
+  __shared__ T shvr[LX * LX];
+  __shared__ T shvs[LX * LX];
+
+  __shared__ T shw[LX * LX];
+  __shared__ T shwr[LX * LX];
+  __shared__ T shws[LX * LX];
+
+  T ru[LX];
+  T rv[LX];
+  T rw[LX];
+  
+  T ruw[LX];
+  T rvw[LX];
+  T rww[LX];
+  
+  T rut;
+  T rvt;
+  T rwt;
+
+  const int e = blockIdx.x;
+  const int j = threadIdx.y;
+  const int i = threadIdx.x;
+  const int ij = i + j*LX;
+  const int ele = e*LX*LX*LX;
+
+  shdx[ij] = dx[ij];
+  shdy[ij] = dy[ij];
+  shdz[ij] = dz[ij];
+  
+#pragma unroll
+  for(int k = 0; k < LX; ++k){
+    ru[k] = u[ij + k*LX*LX + ele];
+    ruw[k] = 0.0;
+
+    rv[k] = v[ij + k*LX*LX + ele];
+    rvw[k] = 0.0;
+
+    rw[k] = w[ij + k*LX*LX + ele];
+    rww[k] = 0.0;
+  }
+
+
+  __syncthreads();
+#pragma unroll
+  for (int k = 0; k < LX; ++k){
+    const int ijk = ij + k*LX*LX; 
+    const T G00 = g11[ijk+ele];
+    const T G11 = g22[ijk+ele];
+    const T G22 = g33[ijk+ele]; 
+    const T G01 = g12[ijk+ele];
+    const T G02 = g13[ijk+ele];
+    const T G12 = g23[ijk+ele];
+    const T H1  = h1[ijk+ele];
+    T uttmp = 0.0;
+    T vttmp = 0.0;
+    T wttmp = 0.0;
+    shu[ij] = ru[k];
+    shv[ij] = rv[k];
+    shw[ij] = rw[k];
+    for (int l = 0; l < LX; l++){
+      uttmp += shdz[k+l*LX] * ru[l];
+      vttmp += shdz[k+l*LX] * rv[l];
+      wttmp += shdz[k+l*LX] * rw[l];
+    }
+    __syncthreads();
+    
+    T urtmp = 0.0;
+    T ustmp = 0.0;
+
+    T vrtmp = 0.0;
+    T vstmp = 0.0;
+
+    T wrtmp = 0.0;
+    T wstmp = 0.0;
+#pragma unroll
+    for (int l = 0; l < LX; l++){
+      urtmp += shdx[i+l*LX] * shu[l+j*LX];
+      ustmp += shdy[j+l*LX] * shu[i+l*LX];
+
+      vrtmp += shdx[i+l*LX] * shv[l+j*LX];
+      vstmp += shdy[j+l*LX] * shv[i+l*LX];
+
+      wrtmp += shdx[i+l*LX] * shw[l+j*LX];
+      wstmp += shdy[j+l*LX] * shw[i+l*LX];
+    }
+    
+    shur[ij] = H1
+	     * (G00 * urtmp
+		+ G01 * ustmp
+		+ G02 * uttmp);
+    shus[ij] = H1
+	     * (G01 * urtmp
+		+ G11 * ustmp
+		+ G12 * uttmp);
+    rut      = H1
+	     * (G02 * urtmp
+		+ G12 * ustmp 
+		+ G22 * uttmp);
+
+    shvr[ij] = H1
+	     * (G00 * vrtmp
+		+ G01 * vstmp
+		+ G02 * vttmp);
+    shvs[ij] = H1
+	     * (G01 * vrtmp
+		+ G11 * vstmp
+		+ G12 * vttmp);
+    rvt      = H1
+	     * (G02 * vrtmp
+		+ G12 * vstmp 
+		+ G22 * vttmp);
+
+    shwr[ij] = H1
+	     * (G00 * wrtmp
+		+ G01 * wstmp
+		+ G02 * wttmp);
+    shws[ij] = H1
+	     * (G01 * wrtmp
+		+ G11 * wstmp
+		+ G12 * wttmp);
+    rwt      = H1
+	     * (G02 * wrtmp
+		+ G12 * wstmp 
+		+ G22 * wttmp);
+
+    __syncthreads();
+
+    T uwijke = 0.0;
+    T vwijke = 0.0;
+    T wwijke = 0.0;
+#pragma unroll
+    for (int l = 0; l < LX; l++){
+      uwijke += shur[l+j*LX] * shdx[l+i*LX];
+      ruw[l] += rut * shdz[k+l*LX];
+      uwijke += shus[i+l*LX] * shdy[l + j*LX];
+
+      vwijke += shvr[l+j*LX] * shdx[l+i*LX];
+      rvw[l] += rvt * shdz[k+l*LX];
+      vwijke += shvs[i+l*LX] * shdy[l + j*LX];
+
+      wwijke += shwr[l+j*LX] * shdx[l+i*LX];
+      rww[l] += rwt * shdz[k+l*LX];
+      wwijke += shws[i+l*LX] * shdy[l + j*LX];
+    }
+    ruw[k] += uwijke;
+    rvw[k] += vwijke;
+    rww[k] += wwijke;
+  }
+#pragma unroll
+  for (int k = 0; k < LX; ++k){
+   au[ij + k*LX*LX + ele] = ruw[k];
+   av[ij + k*LX*LX + ele] = rvw[k];
+   aw[ij + k*LX*LX + ele] = rww[k]; 
+  }
+}
+
+template< typename T, const int LX >
+__global__ void ax_helm_kernel_vector_kstep_padded(T * __restrict__ au,
+                                                   T * __restrict__ av,
+                                                   T * __restrict__ aw,
+                                                   const T * __restrict__ u,
+                                                   const T * __restrict__ v,
+                                                   const T * __restrict__ w,
+                                                   const T * __restrict__ dx,
+                                                   const T * __restrict__ dy,
+                                                   const T * __restrict__ dz,
+                                                   const T * __restrict__ h1,
+                                                   const T * __restrict__ g11,
+                                                   const T * __restrict__ g22,
+                                                   const T * __restrict__ g33,
+                                                   const T * __restrict__ g12,
+                                                   const T * __restrict__ g13,
+                                                   const T * __restrict__ g23) {
+
+  __shared__ T shdx[LX * (LX+1)];
+  __shared__ T shdy[LX * (LX+1)];
+  __shared__ T shdz[LX * (LX+1)];
+    
+  __shared__ T shu[LX * (LX+1)];
+  __shared__ T shur[LX * LX];
+  __shared__ T shus[LX * (LX+1)];
+
+  __shared__ T shv[LX * (LX+1)];
+  __shared__ T shvr[LX * LX];
+  __shared__ T shvs[LX * (LX+1)];
+
+  __shared__ T shw[LX * (LX+1)];
+  __shared__ T shwr[LX * LX];
+  __shared__ T shws[LX * (LX+1)];
+
+  T ru[LX];
+  T rv[LX];
+  T rw[LX];
+  
+  T ruw[LX];
+  T rvw[LX];
+  T rww[LX];
+  
+  T rut;
+  T rvt;
+  T rwt;
+
+  const int e = blockIdx.x;
+  const int j = threadIdx.y;
+  const int i = threadIdx.x;
+  const int ij = i + j*LX;
+  const int ij_p = i + j*(LX+1);
+  const int ele = e*LX*LX*LX;
+
+  shdx[ij_p] = dx[ij];
+  shdy[ij_p] = dy[ij];
+  shdz[ij_p] = dz[ij];
+  
+#pragma unroll
+  for(int k = 0; k < LX; ++k){
+    ru[k] = u[ij + k*LX*LX + ele];
+    ruw[k] = 0.0;
+
+    rv[k] = v[ij + k*LX*LX + ele];
+    rvw[k] = 0.0;
+
+    rw[k] = w[ij + k*LX*LX + ele];
+    rww[k] = 0.0;
+  }
+
+
+  __syncthreads();
+#pragma unroll
+  for (int k = 0; k < LX; ++k){
+    const int ijk = ij + k*LX*LX; 
+    const T G00 = g11[ijk+ele];
+    const T G11 = g22[ijk+ele];
+    const T G22 = g33[ijk+ele]; 
+    const T G01 = g12[ijk+ele];
+    const T G02 = g13[ijk+ele];
+    const T G12 = g23[ijk+ele];
+    const T H1  = h1[ijk+ele];
+    T uttmp = 0.0;
+    T vttmp = 0.0;
+    T wttmp = 0.0;
+    shu[ij_p] = ru[k];
+    shv[ij_p] = rv[k];
+    shw[ij_p] = rw[k];
+    for (int l = 0; l < LX; l++){
+      uttmp += shdz[k+l*LX] * ru[l];
+      vttmp += shdz[k+l*LX] * rv[l];
+      wttmp += shdz[k+l*LX] * rw[l];
+    }
+    __syncthreads();
+    
+    T urtmp = 0.0;
+    T ustmp = 0.0;
+
+    T vrtmp = 0.0;
+    T vstmp = 0.0;
+
+    T wrtmp = 0.0;
+    T wstmp = 0.0;
+#pragma unroll
+    for (int l = 0; l < LX; l++){
+      urtmp += shdx[i+l*LX] * shu[l+j*LX];
+      ustmp += shdy[j+l*LX] * shu[i+l*LX];
+
+      vrtmp += shdx[i+l*LX] * shv[l+j*LX];
+      vstmp += shdy[j+l*LX] * shv[i+l*LX];
+
+      wrtmp += shdx[i+l*LX] * shw[l+j*LX];
+      wstmp += shdy[j+l*LX] * shw[i+l*LX];
+    }
+    
+    shur[ij] = H1
+	     * (G00 * urtmp
+		+ G01 * ustmp
+		+ G02 * uttmp);
+    shus[ij_p] = H1
+               * (G01 * urtmp
+                  + G11 * ustmp
+                  + G12 * uttmp);
+    rut      = H1
+	     * (G02 * urtmp
+		+ G12 * ustmp 
+		+ G22 * uttmp);
+
+    shvr[ij] = H1
+	     * (G00 * vrtmp
+		+ G01 * vstmp
+		+ G02 * vttmp);
+    shvs[ij_p] = H1
+	       * (G01 * vrtmp
+                  + G11 * vstmp
+                  + G12 * vttmp);
+    rvt      = H1
+	     * (G02 * vrtmp
+		+ G12 * vstmp 
+		+ G22 * vttmp);
+
+    shwr[ij] = H1
+	     * (G00 * wrtmp
+		+ G01 * wstmp
+		+ G02 * wttmp);
+    shws[ij_p] = H1
+	       * (G01 * wrtmp
+                  + G11 * wstmp
+                  + G12 * wttmp);
+    rwt      = H1
+	     * (G02 * wrtmp
+		+ G12 * wstmp 
+		+ G22 * wttmp);
+
+    __syncthreads();
+
+    T uwijke = 0.0;
+    T vwijke = 0.0;
+    T wwijke = 0.0;
+#pragma unroll
+    for (int l = 0; l < LX; l++){
+      uwijke += shur[l+j*LX] * shdx[l+i*LX];
+      ruw[l] += rut * shdz[k+l*LX];
+      uwijke += shus[i+l*LX] * shdy[l + j*LX];
+
+      vwijke += shvr[l+j*LX] * shdx[l+i*LX];
+      rvw[l] += rvt * shdz[k+l*LX];
+      vwijke += shvs[i+l*LX] * shdy[l + j*LX];
+
+      wwijke += shwr[l+j*LX] * shdx[l+i*LX];
+      rww[l] += rwt * shdz[k+l*LX];
+      wwijke += shws[i+l*LX] * shdy[l + j*LX];
+    }
+    ruw[k] += uwijke;
+    rvw[k] += vwijke;
+    rww[k] += wwijke;
+  }
+#pragma unroll
+  for (int k = 0; k < LX; ++k){
+   au[ij + k*LX*LX + ele] = ruw[k];
+   av[ij + k*LX*LX + ele] = rvw[k];
+   aw[ij + k*LX*LX + ele] = rww[k]; 
+  }
+}
+
 #endif // __MATH_AX_HELM_KERNEL_H__
