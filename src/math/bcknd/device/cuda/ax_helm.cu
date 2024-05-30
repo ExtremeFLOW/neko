9f204e6c97e8dd0ae2a20815ad07e0f5c62d7816
Niclas Jansson <njansson@kth.se>
2024-05-30 08:35:28 +0200

    Add HIP versions of vector ax helm kernels

--- src/math/bcknd/device/cuda/ax_helm.cu
+++ src/math/bcknd/device/cuda/ax_helm.cu
@@ -1,5 +1,5 @@
 /*
- Copyright (c) 2021-2023, The Neko Authors
+ Copyright (c) 2021-2024, The Neko Authors
  All rights reserved.
 
  Redistribution and use in source and binary forms, with or without
