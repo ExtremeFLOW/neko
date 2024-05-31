6df5ab831c161f70b842e51a7a4261b04af7a5e1
Niclas Jansson <njansson@kth.se>
2024-05-31 14:24:20 +0200

    remove whitespace

--- src/math/bcknd/device/cuda/ax_helm.cu
+++ src/math/bcknd/device/cuda/ax_helm.cu
@@ -57,7 +57,7 @@ int tune_padded(void *w, void *u, void *dx, void *dy, void *dz,
 
 extern "C" {
 
-  /** 
+  /**
    * Fortran wrapper for device CUDA Ax
    */
   void cuda_ax_helm(void *w, void *u, void *dx, void *dy, void *dz,
@@ -71,7 +71,7 @@ extern "C" {
     const dim3 nblcks_1d((*nelv), 1, 1);
     const dim3 nthrds_kstep((*lx), (*lx), 1);
     const dim3 nblcks_kstep((*nelv), 1, 1);
-    
+
     const dim3 nthrds((*lx), (*lx), 1);
     const dim3 nblcks((*nelv), 1, 1);
     const cudaStream_t stream = (cudaStream_t) glb_cmd_queue;
@@ -83,7 +83,7 @@ extern "C" {
                           (real *) dxt, (real *) dyt, (real *) dzt, (real *) h1,\
                           (real *) g11, (real *) g22, (real *) g33,             \
                           (real *) g12, (real *) g13, (real *) g23);            \
-      CUDA_CHECK(cudaGetLastError());                                           
+      CUDA_CHECK(cudaGetLastError());
 
 #define CASE_KSTEP(LX)                                                          \
     ax_helm_kernel_kstep<real, LX>                                              \
@@ -91,7 +91,7 @@ extern "C" {
                            (real *) dx, (real *) dy, (real *) dz, (real *) h1,  \
                            (real *) g11, (real *) g22, (real *) g33,            \
                            (real *) g12, (real *) g13, (real *) g23);           \
-      CUDA_CHECK(cudaGetLastError());                                           
+      CUDA_CHECK(cudaGetLastError());
 
 #define CASE_KSTEP_PADDED(LX)                                                   \
     ax_helm_kernel_kstep_padded<real, LX>                                       \
@@ -99,7 +99,7 @@ extern "C" {
                            (real *) dx, (real *) dy, (real *) dz, (real *) h1,  \
                            (real *) g11, (real *) g22, (real *) g33,            \
                            (real *) g12, (real *) g13, (real *) g23);           \
-      CUDA_CHECK(cudaGetLastError());                                           
+      CUDA_CHECK(cudaGetLastError());
 
 #define CASE(LX)                                                                \
     case LX:                                                                    \
@@ -143,7 +143,7 @@ extern "C" {
       break
 
 
-    if ((*lx) < 12) {      
+    if ((*lx) < 12) {
       switch(*lx) {
         CASE(2);
         CASE(3);
@@ -174,11 +174,11 @@ extern "C" {
           fprintf(stderr, __FILE__ ": size not supported: %d\n", *lx);
           exit(1);
         }
-      } 
+      }
     }
-  } 
+  }
 
-  /** 
+  /**
    * Fortran wrapper for device CUDA Ax vector version
    */
   void cuda_ax_helm_vector(void *au, void *av, void *aw,
@@ -223,7 +223,7 @@ extern "C" {
     case LX:                                                                    \
       CASE_VECTOR_KSTEP_PADDED(LX);                                             \
        break
-      
+
     switch(*lx) {
       CASE_VECTOR(2);
       CASE_VECTOR(3);
@@ -245,11 +245,11 @@ extern "C" {
           fprintf(stderr, __FILE__ ": size not supported: %d\n", *lx);
           exit(1);
         }
-      }   
+      }
   }
 }
 
-template < const int LX > 
+template < const int LX >
 int tune(void *w, void *u, void *dx, void *dy, void *dz,
          void *dxt, void *dyt, void *dzt, void *h1,
          void *g11, void *g22, void *g33, void *g12,
@@ -263,18 +263,18 @@ int tune(void *w, void *u, void *dx, void *dy, void *dz,
   const dim3 nthrds_kstep((*lx), (*lx), 1);
   const dim3 nblcks_kstep((*nelv), 1, 1);
  const cudaStream_t stream = (cudaStream_t) glb_cmd_queue;
- 
+
   char *env_value = NULL;
   char neko_log_buf[80];
-  
+
   env_value=getenv("NEKO_AUTOTUNE");
 
   sprintf(neko_log_buf, "Autotune Ax helm (lx: %d)", *lx);
   log_section(neko_log_buf);
-  
+
   if(env_value) {
     if( !strcmp(env_value,"1D") ) {
-      CASE_1D(LX);       
+      CASE_1D(LX);
       sprintf(neko_log_buf,"Set by env : 1 (1D)");
       log_message(neko_log_buf);
       log_end_section();
@@ -285,7 +285,7 @@ int tune(void *w, void *u, void *dx, void *dy, void *dz,
       log_message(neko_log_buf);
       log_end_section();
       return 2;
-    } else {       
+    } else {
        sprintf(neko_log_buf, "Invalid value set for NEKO_AUTOTUNE");
        log_error(neko_log_buf);
     }
@@ -293,27 +293,27 @@ int tune(void *w, void *u, void *dx, void *dy, void *dz,
 
   cudaEventCreate(&start);
   cudaEventCreate(&stop);
-  
+
   cudaEventRecord(start,0);
-   
+
   for(int i = 0; i < 100; i++) {
     CASE_1D(LX);
   }
-  
-  cudaEventRecord(stop,0); 
+
+  cudaEventRecord(stop,0);
   cudaEventSynchronize(stop);
   cudaEventElapsedTime(&time1, start, stop);
-  
+
   cudaEventRecord(start,0);
-   
+
   for(int i = 0; i < 100; i++) {
      CASE_KSTEP(LX);
    }
-  
-  cudaEventRecord(stop,0); 
+
+  cudaEventRecord(stop,0);
   cudaEventSynchronize(stop);
   cudaEventElapsedTime(&time2, start, stop);
-  
+
   if(time1 < time2) {
      retval = 1;
   } else {
@@ -341,7 +341,7 @@ int tune_padded(void *w, void *u, void *dx, void *dy, void *dz,
   const dim3 nthrds_kstep((*lx), (*lx), 1);
   const dim3 nblcks_kstep((*nelv), 1, 1);
  const cudaStream_t stream = (cudaStream_t) glb_cmd_queue;
- 
+
   char *env_value = NULL;
   char neko_log_buf[80];
 
@@ -363,12 +363,12 @@ int tune_padded(void *w, void *u, void *dx, void *dy, void *dz,
       log_message(neko_log_buf);
       log_end_section();
       return 2;
-    } else {       
+    } else {
       sprintf(neko_log_buf, "Invalid value set for NEKO_AUTOTUNE");
       log_error(neko_log_buf);
     }
   }
-    
+
   cudaEventCreate(&start);
   cudaEventCreate(&stop);
 
@@ -377,7 +377,7 @@ int tune_padded(void *w, void *u, void *dx, void *dy, void *dz,
   for(int i = 0; i < 100; i++) {
     CASE_1D(LX);
   }
-  
+
   cudaEventRecord(stop, 0);
   cudaEventSynchronize(stop);
   cudaEventElapsedTime(&time1, start, stop);
@@ -391,7 +391,7 @@ int tune_padded(void *w, void *u, void *dx, void *dy, void *dz,
   cudaEventRecord(stop, 0);
   cudaEventSynchronize(stop);
   cudaEventElapsedTime(&time2, start, stop);
-  
+
   if(time1 < time2) {
     retval=1;
   } else {
