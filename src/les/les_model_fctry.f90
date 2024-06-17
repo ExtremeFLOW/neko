fb4fe53aa0d17c0181ff9d3aa537c9f44b20cb46
Timofey Mukha <timofeymukha@users.noreply.github.com>
2024-06-17 14:59:43 +0300

    Update src/les/les_model_fctry.f90
    Co-authored-by: Tim Felle Olsen <tife@dtu.dk>

--- src/les/les_model_fctry.f90
+++ src/les/les_model_fctry.f90
@@ -14,7 +14,7 @@
 !     disclaimer in the documentation and/or other materials provided
 !     with the distribution.
 !
-!   * Neither the type_name of the authors nor the names of its
+!   * Neither the name of the authors nor the names of its
 !     contributors may be used to endorse or promote products derived
 !     from this software without specific prior written permission.
 !
