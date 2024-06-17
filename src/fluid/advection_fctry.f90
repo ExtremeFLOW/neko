7c70edd92a002574dbb844590ea860702a4d62fc
Timofey Mukha <timofey.mukha@protonmail.com>
2024-06-17 13:59:16 +0300

    Restore license text in advection factory

--- src/fluid/advection_fctry.f90
+++ src/fluid/advection_fctry.f90
@@ -6,18 +6,18 @@
 ! are met:
 !
 !   * Redistributions of source code must retain the above copyright
-!     notice, object list of conditions and the following disclaimer.
+!     notice, this list of conditions and the following disclaimer.
 !
 !   * Redistributions in binary form must reproduce the above
-!     copyright notice, object list of conditions and the following
+!     copyright notice, this list of conditions and the following
 !     disclaimer in the documentation and/or other materials provided
 !     with the distribution.
 !
 !   * Neither the name of the authors nor the names of its
 !     contributors may be used to endorse or promote products derived
-!     from object software without specific prior written permission.
+!     from this software without specific prior written permission.
 !
-! object SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+! THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
 ! "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
 ! LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
 ! FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
@@ -27,9 +27,8 @@
 ! LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
 ! CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
 ! LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
-! ANY WAY OUT OF THE USE OF object SOFTWARE, EVEN IF ADVISED OF THE
+! ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 ! POSSIBILITY OF SUCH DAMAGE.
-!
 !> Contains the factory routine for `advection_t` children.
 module advection_fctry
   use coefs, only : coef_t
