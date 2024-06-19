d9a55ef0d992caa66a92a5d2f49aab06d539de51
Shiyu-Sandy-Du <shiyud@kth.se>
2024-06-19 10:06:09 +0200

    modify the test filter of dynamic smagorinsky model

--- src/les/dynamic_smagorinsky.f90
+++ src/les/dynamic_smagorinsky.f90
@@ -150,31 +150,33 @@ contains
     type(elementwise_filter_t), intent(inout) :: filter_1d
     integer :: i
 
-    if (filter_1d%nx .le. 6) then
+    if (filter_1d%nx .le. 2) then
         call neko_error("Dynamic Smagorinsky model error: test filter is not defined for the current polynomial order")
     end if
-
-    if (mod(filter_1d%nx,2) .eq. 0) then ! number of grid spacing is even
-       ! cutoff at polynomial order int((filter_1d%nx)/2) (a conservative approach)
-       filter_1d%trnsfr(int((filter_1d%nx)/2) + 2) = 0.95_rp
-       filter_1d%trnsfr(int((filter_1d%nx)/2) + 3) = 0.50_rp
-       filter_1d%trnsfr(int((filter_1d%nx)/2) + 4) = 0.05_rp
-       if ((int((filter_1d%nx)/2) + 4) .lt. filter_1d%nx) then
-          do i = int((filter_1d%nx)/2) + 5, filter_1d%nx ! make delta_ratio = (nx-1)/(nt-1) as close to 2
+    
+    if (mod(filter_1d%nx,2) .eq. 0) then ! number of grid spacing is odd
+       ! cutoff at polynomial order int((filter_1d%nx)/2)
+       filter_1d%trnsfr(int((filter_1d%nx)/2)) = 0.95_rp
+       filter_1d%trnsfr(int((filter_1d%nx)/2) + 1) = 0.50_rp
+       filter_1d%trnsfr(int((filter_1d%nx)/2) + 2) = 0.05_rp
+       if ((int((filter_1d%nx)/2) + 2) .lt. filter_1d%nx) then
+          do i = int((filter_1d%nx)/2) + 3, filter_1d%nx
              filter_1d%trnsfr(i) = 0.0_rp
           end do
        end if
+       ! make delta_ratio = (nx-1)/(nt-1) as close to 2
        filter_1d%nt = int(filter_1d%nx/2) + 1
-    else ! number of grid spacing is odd
+    else ! number of grid spacing is even
        ! cutoff at polynomial order int((filter_1d%nx-1)/2)
-       filter_1d%trnsfr(int((filter_1d%nx-1)/2) + 2) = 0.95_rp
-       filter_1d%trnsfr(int((filter_1d%nx-1)/2) + 3) = 0.50_rp
-       filter_1d%trnsfr(int((filter_1d%nx-1)/2) + 4) = 0.05_rp
-       if ((int((filter_1d%nx-1)/2) + 4) .lt. filter_1d%nx) then
-          do i = int((filter_1d%nx-1)/2) + 5, filter_1d%nx ! make delta_ratio = (nx-1)/(nt-1) = 2
+       filter_1d%trnsfr(int((filter_1d%nx-1)/2)) = 0.95_rp
+       filter_1d%trnsfr(int((filter_1d%nx-1)/2) + 1) = 0.50_rp
+       filter_1d%trnsfr(int((filter_1d%nx-1)/2) + 2) = 0.05_rp
+       if ((int((filter_1d%nx-1)/2) + 2) .lt. filter_1d%nx) then
+          do i = int((filter_1d%nx-1)/2) + 3, filter_1d%nx
              filter_1d%trnsfr(i) = 0.0_rp
           end do
        end if
+       ! make delta_ratio = (nx-1)/(nt-1) = 2
        filter_1d%nt = int((filter_1d%nx-1)/2) + 1
     end if
 
