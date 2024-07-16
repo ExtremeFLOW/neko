7167f09f31d00b7ac281f0099b56213db23719bc
Shiyu Du <115027341+Shiyu-Sandy-Du@users.noreply.github.com>
2024-07-16 16:06:55 +0800

    Update src/common/gradient_jump_penalty.f90
    Co-authored-by: Tim Felle Olsen <tife@dtu.dk>

--- src/common/gradient_jump_penalty.f90
+++ src/common/gradient_jump_penalty.f90
@@ -294,8 +294,8 @@ contains
                           HOST_TO_DEVICE, sync=.false.)
        call device_memcpy(this%n3, this%n3_d, this%n_large, &
                           HOST_TO_DEVICE, sync=.false.)
-       call device_memcpy(this%facet_factor, this%facet_factor_d, this%n_large, &
-                          HOST_TO_DEVICE, sync=.false.)
+       call device_memcpy(this%facet_factor, this%facet_factor_d, this%n_large,&
+                          HOST_TO_DEVICE, sync = .false.)
 
     end if
 
