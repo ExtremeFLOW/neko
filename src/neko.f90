bbe3cf3c8087c6dc0dee5eb02cc930b0b22fdfc4
Tim Felle Olsen <tife@dtu.dk>
2024-07-18 14:14:59 +0200

    Spaces around separators.. Would be nice if flint did not check the internals of strings.

--- src/neko.f90
+++ src/neko.f90
@@ -144,7 +144,7 @@ contains
        argc = command_argument_count()
 
        if ((argc .lt. 1) .or. (argc .gt. 1)) then
-          if (pe_rank .eq. 0) write(*,*) 'Usage: ./neko <case file>'
+          if (pe_rank .eq. 0) write(*,*) 'Usage: ./neko < case file >'
           stop
        end if
 
