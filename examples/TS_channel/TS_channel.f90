9b9e57932b959521c3518c1951b9271435d35206
Shiyu-Sandy-Du <shiyud@kth.se>
2024-07-22 22:57:58 +0800

    deal with flint faults

--- examples/TS_channel/TS_channel.f90
+++ examples/TS_channel/TS_channel.f90
@@ -60,14 +60,14 @@ contains
     real(kind=rp) :: ur_3D, ui_3D, vr_3D, vi_3D, wr_3D, wi_3D
 
     ! data reading
-    open(unit=10, file='TSwave_cheb_2D.bin', form = 'unformatted', &
+    open(unit = 10, file = 'TSwave_cheb_2D.bin', form = 'unformatted', &
                   access = 'stream')
        read(10) data_mode_cheb_2D
     close(10)
     y_GLC = data_mode_cheb_2D(:,1)
     TS2D_GLC = data_mode_cheb_2D(:,2:num_columns)
 
-    open(unit=10, file='TSwave_cheb_3D.bin', form = 'unformatted', &
+    open(unit = 10, file = 'TSwave_cheb_3D.bin', form = 'unformatted', &
                   access = 'stream')
        read(10) data_mode_cheb_3D
     close(10)
@@ -211,9 +211,9 @@ contains
 
     ub = 1.0_rp-y*y
 
-    uvw(1)  = ub + u_pert_TS_2D + u_pert_TS_3D
-    uvw(2)  = v_pert_TS_2D + v_pert_TS_3D
-    uvw(3)  = w_pert_TS_3D
+    uvw(1) = ub + u_pert_TS_2D + u_pert_TS_3D
+    uvw(2) = v_pert_TS_2D + v_pert_TS_3D
+    uvw(3) = w_pert_TS_3D
 
   end function channel_ic
 
