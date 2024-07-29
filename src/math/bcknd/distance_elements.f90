cbac9851c39237a8acdd297ced1937e7b58e6f3d
Tim Felle Olsen <tife@dtu.dk>
2024-07-29 15:54:40 +0200

    First bunch of simple geometric operators.
    Distance functions for simple types.

---
+++ src/math/bcknd/distance_elements.f90
@@ -0,0 +1,135 @@
+! Copyright (c) 2024, The Neko Authors
+! All rights reserved.
+!
+! Redistribution and use in source and binary forms, with or without
+! modification, are permitted provided that the following conditions
+! are met:
+!
+!   * Redistributions of source code must retain the above copyright
+!     notice, this list of conditions and the following disclaimer.
+!
+!   * Redistributions in binary form must reproduce the above
+!     copyright notice, this list of conditions and the following
+!     disclaimer in the documentation and/or other materials provided
+!     with the distribution.
+!
+!   * Neither the name of the authors nor the names of its
+!     contributors may be used to endorse or promote products derived
+!     from this software without specific prior written permission.
+!
+! THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+! "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+! LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+! FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+! COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+! INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+! BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+! LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+! CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+! LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+! ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+! POSSIBILITY OF SUCH DAMAGE.
+!
+
+!> \submodule distance_elements
+!! This submodule contains the operators that are related to distance
+!! calculations for mesh elements.
+!!
+!! @note Please note that this module only contains the implementations for the
+!! Geometric Operator module.
+submodule (geometric_operators) distance_elements
+  use math, only: cross
+  implicit none
+
+contains
+
+  !> @brief Distance function for a point
+  !! @details This routine computes the distance between the query point and the
+  !! given point.
+  !!
+  !! @param p Query point
+  !! @param point Point
+  !! @return Unsigned distance value
+  real(kind=dp) module function distance_point_t(p, point)
+    real(kind=dp), dimension(3), intent(in) :: p
+    type(point_t), intent(in) :: point
+
+    distance_point_t = norm2(p - point%x)
+
+  end function distance_point_t
+
+  !> @brief Distance function for a triangle.
+  !! @details This routine computes the distance to a given triangle.
+  !! We compute the barycentric coordinate of the point projected onto the
+  !! triangle. If the projection is inside the triangle, the distance is the
+  !! perpendicular distance to the plane. If the projection is outside the
+  !! triangle, the distance is the distance to the nearest edge or vertex.
+  !!
+  !! @param p Query point
+  !! @param triangle Triangle
+  !! @return Unsigned distance value
+  real(kind=dp) module function distance_triangle(p, triangle)
+    real(kind=dp), dimension(3), intent(in) :: p
+    type(tri_t), intent(in) :: triangle
+
+    real(kind=dp), dimension(3) :: v1, v2, v3
+    real(kind=dp), dimension(3) :: normal
+    real(kind=dp) :: normal_length
+    real(kind=dp) :: b1, b2, b3
+
+    real(kind=dp), dimension(3) :: projection
+    real(kind=dp), dimension(3) :: edge
+    real(kind=dp) :: tol = 1e-10_dp
+
+    real(kind=dp) :: face_distance
+    real(kind=dp) :: t
+
+    ! Get vertices and the normal vector
+    v1 = triangle%pts(1)%p%x
+    v2 = triangle%pts(2)%p%x
+    v3 = triangle%pts(3)%p%x
+
+    normal = cross(v2 - v1, v3 - v1)
+    normal_length = norm2(normal)
+
+    if (normal_length .lt. tol) then
+       distance_triangle = huge(1.0_dp)
+       return
+    end if
+    normal = normal / normal_length
+
+    ! Compute Barycentric coordinates to determine if the point is inside the
+    ! triangular prism, of along an edge or by a face.
+    face_distance = dot_product(p - v1, normal)
+
+    projection = p - normal * face_distance
+    b1 = dot_product(normal, cross(v2 - v1, projection - v1)) / normal_length
+    b2 = dot_product(normal, cross(v3 - v2, projection - v2)) / normal_length
+    b3 = dot_product(normal, cross(v1 - v3, projection - v3)) / normal_length
+
+    if (b1 .le. tol) then
+       edge = v2 - v1
+       t = dot_product(p - v1, edge) / norm2(edge)
+       t = max(0.0_dp, min(1.0_dp, t))
+
+       projection = v1 + t * edge
+    else if (b2 .le. tol) then
+       edge = v3 - v2
+       t = dot_product(p - v2, edge) / norm2(edge)
+       t = max(0.0_dp, min(1.0_dp, t))
+
+       projection = v2 + t * edge
+    else if (b3 .le. tol) then
+       edge = v1 - v3
+       t = dot_product(p - v3, edge) / norm2(edge)
+       t = max(0.0_dp, min(1.0_dp, t))
+
+       projection = v3 + t * edge
+    end if
+
+    distance_triangle = norm2(projection - p)
+
+  end function distance_triangle
+
+
+end submodule distance_elements
---
+++ src/math/geometric_operators.f90
@@ -0,0 +1,135 @@
+! Lets have a library for the geometric operators, such as signed distance and
+! boolean containment tests. This will be useful across a wide part of the code.
+
+! Further more lets try to set it up in a proper way with submodules and module
+! operators.
+
+!> \module geometric_operators
+!! This module contains geometric operators that are useful for the mesh
+!! generation and manipulation. The operators are implemented in a way that
+!! they can be used in a modular way, and can be extended with new operators
+!! easily.
+!!
+!! Please note that this module only contains the interfaces, the actual
+!! implementation is located in approproate submodules.
+module geometric_operators
+  use num_types, only: dp
+  use point, only: point_t
+  use tri, only: tri_t
+  use tet, only: tet_t
+  implicit none
+
+  ! ========================================================================== !
+  ! Distance for elements from the mesh structure.
+
+  !> \interface distance_elements
+  !! This interface defines the distance operator. The distance
+  !! operator is used to calculate the distance between a point and a geometric
+  !! object.
+  !! The distance is signed for volumetric objects, meaning that the distance is
+  !! positive if the point is outside the object, and negative if the point is
+  !! inside the object.
+  !! For non-volumetric objects, the distance is always positive.
+  interface distance_element
+     module procedure distance_point_t
+     module procedure distance_triangle
+     module procedure distance_tetrahedron
+  end interface distance_element
+
+  !> \interface distance_point
+  !! This interface defines the distance operator for a point.
+  !! The distance operator calculates the distance between a query point and a
+  !! given point.
+  !!
+  !! @param p Query point
+  !! @param point Point
+  !! @return Distance value
+  interface distance_point
+     module procedure distance_point_real
+     module procedure distance_point_t
+  end interface distance_point
+
+  ! ========================================================================== !
+  ! Distance to geometric primitives.
+
+  interface
+
+     !> Distance to a point
+     module function distance_point_real(p, point)
+       real(kind=dp), dimension(3), intent(in) :: p
+       real(kind=dp), intent(in) :: point
+       real(kind=dp) :: distance_point_real
+     end function distance_point_real
+
+     !> Distance to the infinite line defined by a point and a direction.
+     module function distance_line(p, point, direction)
+       real(kind=dp), dimension(3), intent(in) :: p
+       real(kind=dp), dimension(3), intent(in) :: point
+       real(kind=dp), dimension(3), intent(in) :: direction
+       real(kind=dp) :: distance_line
+     end function distance_line
+
+     !> Distance to the infinite ray starting at a point and going in a
+     !! direction.
+     module function distance_line_ray(p, point, direction)
+       real(kind=dp), dimension(3), intent(in) :: p
+       real(kind=dp), dimension(3), intent(in) :: point
+       real(kind=dp), dimension(3), intent(in) :: direction
+       real(kind=dp) :: distance_line_ray
+     end function distance_line_ray
+
+     !> Distance to a line segment defined by two points.
+     module function distance_line_segment(p, point_0, point_1)
+       real(kind=dp), dimension(3), intent(in) :: p
+       real(kind=dp), dimension(3), intent(in) :: point_0
+       real(kind=dp), dimension(3), intent(in) :: point_1
+       real(kind=dp) :: distance_line_segment
+     end function distance_line_segment
+
+     !> Distance to a plane defined by a point and a normal.
+     module function distance_plane(p, point, normal)
+       real(kind=dp), dimension(3), intent(in) :: p
+       real(kind=dp), dimension(3), intent(in) :: point
+       real(kind=dp), dimension(3), intent(in) :: normal
+       real(kind=dp) :: distance_plane
+     end function distance_plane
+
+     !> Distance to a sphere defined by a center and a radius.
+     module function distance_sphere(p, sphere_center, sphere_radius)
+       real(kind=dp), dimension(3), intent(in) :: p
+       real(kind=dp), dimension(3), intent(in) :: sphere_center
+       real(kind=dp), intent(in) :: sphere_radius
+       real(kind=dp) :: distance_sphere
+     end function distance_sphere
+
+  end interface
+
+  ! ========================================================================== !
+  ! Distance to elements.
+
+  interface
+
+     !> Distance to a point
+     module function distance_point_t(p, point)
+       real(kind=dp), dimension(3), intent(in) :: p
+       type(point_t), intent(in) :: point
+       real(kind=dp) :: distance_point_t
+     end function distance_point_t
+
+     !> Distance to a triangle
+     module function distance_triangle(p, triangle)
+       real(kind=dp), dimension(3), intent(in) :: p
+       type(tri_t), intent(in) :: triangle
+       real(kind=dp) :: distance_triangle
+     end function distance_triangle
+
+     !> Distance to tetrahedron
+     module function distance_tetrahedron(p, tetrahedron)
+       real(kind=dp), dimension(3), intent(in) :: p
+       type(tet_t), intent(in) :: tetrahedron
+       real(kind=dp) :: distance_tetrahedron
+     end function distance_tetrahedron
+
+  end interface
+
+end module geometric_operators
