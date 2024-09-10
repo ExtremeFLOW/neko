c2ea81f95f127d992b783d8a4c7a7eb66eccc3b1
Niclas Jansson <njansson@kth.se>
2024-09-10 17:03:19 +0200

    Fix interface to CrayPat

--- src/common/craypat.F90
+++ src/common/craypat.F90
@@ -1,11 +1,39 @@
+! Copyright (c) 2022-2024, The Neko Authors
+! All rights reserved.
+!
+! Redistribution and use in source and binary forms, with or without
+! modification, are permitted provided that the following conditions
+! are met:
+!
+!   * Redistributions of source code must retain the above copyright
+!     notice, this list of conditions and the following disclaimer.
+!
+!   * Redistributions in binary form must reproduce the above
+!     copyright notice, this list of conditions and the following
+!     disclaimer in the documentation and/or other materials provided
+!     with the distribution.
+!
+!   * Neither the name of the authors nor the names of its
+!     contributors may be used to endorse or promote products derived
+!     from this software without specific prior written permission.
+!
+! THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+! "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+! LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+! FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+! COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+! INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+! BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+! LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+! CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+! LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+! ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+! POSSIBILITY OF SUCH DAMAGE.
+!
 !> Interface to CrayPat F77 API
 module craypat
-  use, intrinsic :: iso_c_binding
-  use stack, only : stack_i4_t
-  use utils, only : neko_error
   implicit none
-
-  type(stack_i4_t), private :: region_depth
+  
   logical, private :: craypat_on = .false.
 #ifdef CRAYPAT
   include 'pat_apif.h'
@@ -15,7 +43,6 @@ contains
   !> Turn on CrayPat recording
   subroutine craypat_record_start
     integer :: ierr
-    call region_depth%init()
     call PAT_record(PAT_STATE_ON, ierr)
     craypat_on = .true.
   end subroutine craypat_record_start
@@ -28,39 +55,24 @@ contains
   end subroutine craypat_record_stop
 
   !> Start a CrayPat region
-  subroutine craypat_region_begin(name)
-    character(kind=c_char,len=*) :: name
-    integer :: ierr, region_id
+  subroutine craypat_region_begin(name, region_id)
+    character(len=*) :: name
+    integer, intent(in) :: region_id
+    integer :: ierr
 
     if (craypat_on) then
-       !> @todo Don't hardcode region names...
-       if (name .eq. 'Time-Step') then
-          region_id = 1
-       else if(name .eq. 'Pressure') then
-          region_id = 2
-       else if (name .eq. 'Velocity') then
-          region_id = 3
-       else if (name .eq. 'gather-scatter') then
-          region_id = 4
-       else ! User defined region
-          region_id = 99
-       end if
-
-       call region_depth%push(region_id)
        call PAT_region_begin(region_id, name, ierr)
     end if
 
   end subroutine craypat_region_begin
 
   !> End a CrayPat region
-  subroutine craypat_region_end
+  subroutine craypat_region_end(region_id)
+    integer, intent(in) :: region_id
     integer :: ierr
 
     if (craypat_on) then
-       if (region_depth%size() .le. 0) then
-          call neko_error('Invalid CrayPat region id')
-       end if
-       call PAT_region_end(region_depth%pop(), ierr)
+       call PAT_region_end(region_id, ierr)
     end if
 
   end subroutine craypat_region_end
