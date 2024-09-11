3637135906cfdd88f7a70c81eddbcb5711b08ec2
Tim Felle Olsen <tife@dtu.dk>
2024-09-11 08:44:38 +0200

    remove unused variables

--- src/scalar/scalar_source_term.f90
+++ src/scalar/scalar_source_term.f90
@@ -93,8 +93,6 @@ contains
     type(json_file) :: source_subdict
     ! Source type
     character(len=:), allocatable :: type
-    ! Dummy source strenth values
-    real(kind=rp) :: values(3)
     logical :: found
     integer :: n_sources, i
 
@@ -202,7 +200,7 @@ contains
     class(scalar_source_term_t), intent(inout) :: this
     real(kind=rp), intent(in) :: t
     integer, intent(in) :: tstep
-    integer :: i, n
+    integer :: i
 
     this%f = 0.0_rp
 
