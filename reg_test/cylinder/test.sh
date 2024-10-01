4f55f857200e3656c32ef3e052ba6a0e12e0836e
MartinKarp <martin.karp@outlook.com>
2024-10-01 15:17:49 +0200

    hio

--- reg_test/cylinder/test.sh
+++ reg_test/cylinder/test.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 export DYLD_FALLBACK_LIBRARY_PATH=$JSON_INSTALL/lib
 echo "Test that small cylinder is ok"
-mpirun -np $1 neko cylinder_part1.case > log1
-mpirun -np $1 neko cylinder_part2.case > log2
+mpirun -np $1 ../../src/neko cylinder_part1.case > log1
+mpirun -np $1 ../../src/neko cylinder_part2.case > log2
 awk 'c&&c--{if(c !=17){print}};/100.00%/{c=19}' log1 > l1
 awk 'c&&c--{if(c !=17){print}};/100.00%/{c=19}' log2 > l2
 awk 'c&&c--{if(c !=17){print}};/100.00%/{c=19}' ref2.log > r2
@@ -16,6 +16,7 @@ then
    echo 'Differences compared Martins laptop'
    cat res
    echo "Did not pass sanity check" >> /dev/stderr
+   exit 1
 else
    echo "Passed sanity check"
 fi
