c4fa12385bdb9f59c25437153c49626f846cbd77
MartinKarp <martin.karp@outlook.com>
2024-10-01 15:23:15 +0200

    remove --

--- reg_test/cylinder/test.sh
+++ reg_test/cylinder/test.sh
@@ -3,10 +3,10 @@ export DYLD_FALLBACK_LIBRARY_PATH=$JSON_INSTALL/lib
 echo "Test that small cylinder is ok"
 mpirun -np $1 ../../src/neko cylinder_part1.case > log1
 mpirun -np $1 ../../src/neko cylinder_part2.case > log2
-awk 'c&&c--{if(c !=17){print}};/100.00%/{c=19}' log1 > l1
-awk 'c&&c--{if(c !=17){print}};/100.00%/{c=19}' log2 > l2
-awk 'c&&c--{if(c !=17){print}};/100.00%/{c=19}' ref2.log > r2
-awk 'c&&c--{if(c !=17){print}};/100.00%/{c=19}' ref1.log > r1
+awk 'c&&c--{if(c !=17 && $0 !~ /--/){print}};/100.00%/{c=19}' log1 > l1
+awk 'c&&c--{if(c !=17 && $0 !~ /--/){print}};/100.00%/{c=19}' log2 > l2
+awk 'c&&c--{if(c !=17 && $0 !~ /--/){print}};/100.00%/{c=19}' ref2.log > r2
+awk 'c&&c--{if(c !=17 && $0 !~ /--/){print}};/100.00%/{c=19}' ref1.log > r1
 diff l1 r1 > res
 diff l2 r1 >> res
 diff l1 r2 >> res
