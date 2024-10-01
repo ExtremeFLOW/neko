139b58f827bde07fff522fecdcbc1efe20dad3af
Timofey Mukha <timofey.mukha@protonmail.com>
2024-10-01 20:07:56 +0300

    Update

--- integration_tests/test_cylinder/test_suite.py
+++ integration_tests/test_cylinder/test_suite.py
@@ -1,9 +1,26 @@
 import subprocess
 import pytest
+import os
+
+
+def get_neko():
+    """
+    Returns the path to the neko executable  via the
+    environmental variable NEKO_BIN, or just returns "neko" 
+    """
+
+    src_dir = os.getenv("NEKO_BIN")
+    if src_dir:
+        return os.path.join(src_dir, "neko")
+    else:
+        return "neko"
 
 
 def test_cylinder():
     """A very good description of the test."""
+
+    # Get the path to the neko executable
+    neko = get_neko()
 
     log_file = "cylinder.log"
 
