22ef1470cf9466f55addf975313f14f07b4f94ac
MartinKarp <martin.karp@outlook.com>
2024-10-10 20:06:51 +0200

    add call

--- src/mesh/search_tree/aabb.f90
+++ src/mesh/search_tree/aabb.f90
@@ -188,7 +188,7 @@ contains
     select type(object)
 
       type is (aabb_t)
-       box%init(object%get_min(), object%get_max())
+       call box%init(object%get_min(), object%get_max())
       type is (tri_t)
        box = get_aabb_element(object)
       type is (hex_t)
