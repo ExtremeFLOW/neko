b0c1ee45f676eb51b96ae239b4f1e32c828f3ac5
MartinKarp <martin.karp@outlook.com>
2024-10-10 19:59:17 +0200

    pick

--- src/mesh/search_tree/aabb.f90
+++ src/mesh/search_tree/aabb.f90
@@ -189,6 +189,8 @@ contains
 
       type is (point_t)
        box = get_aabb_point(object)
+      type is (aabb_t)
+       box%init(object%get_min(), object%get_max())
       type is (tri_t)
        box = get_aabb_element(object)
       type is (hex_t)
