ac6e985f8e428602509907e4e81d2f90f28ddf1a
Tim Felle Olsen <tife@dtu.dk>
2024-10-16 15:03:06 +0200

    Fix regression paths

--- tests/regression/cylinder/test_easy.sh
+++ tests/regression/cylinder/test_easy.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 export DYLD_FALLBACK_LIBRARY_PATH=$JSON_INSTALL/lib
 echo "Easy test for small cylinder (restart and deformations)"
-mpirun -np $1 ../../src/neko cylinder_part1.case > log1
-mpirun -np $1 ../../src/neko cylinder_part2.case > log2
+mpirun -np $1 ../../../src/neko cylinder_part1.case > log1
+mpirun -np $1 ../../../src/neko cylinder_part2.case > log2
 echo "Initial run"
 cat log1
 echo "Restarted run"
