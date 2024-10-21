764112920fcb1f51d3bb74846a8cf0dc51133778
MartinKarp <martin.karp@outlook.com>
2024-10-21 16:01:35 +0200

    on host

--- src/math/tensor.f90
+++ src/math/tensor.f90
@@ -184,13 +184,15 @@ contains
 
   !> Tensor product \f$ v =(C \otimes B \otimes A) u \f$
   !! performed on a subset of the  elements.
-  subroutine tnsr3d_el_list(v, nv, u, nu, A, Bt, Ct, el_list, n_pt)
+  subroutine tnsr3d_el_list(v, nv, u, nu, A, Bt, Ct, el_list, n_pt, on_host)
     integer, intent(in) :: nv, nu, n_pt, el_list(n_pt)
     real(kind=rp), intent(inout) :: v(nv*nv*nv, n_pt), u(nu*nu*nu,1)
     real(kind=rp), intent(inout) :: A(nv,nu,n_pt),Bt(nu, nv,n_pt),Ct(nu,nv,n_pt)
+    logical, intent(in) :: on_host
     type(c_ptr) :: v_d, u_d, A_d, Bt_d, Ct_d, el_list_d
+    
+   
     integer :: i
-
     if (NEKO_BCKND_SX .eq. 1) then
        do i = 1, n_pt
           call tnsr3d_el_sx(v(1,i), nv, u(1,el_list(i)), nu, A(1,1,i), Bt(1,1,i), Ct(1,1,i))
@@ -199,7 +201,7 @@ contains
        do i = 1, n_pt
           call tnsr3d_el_xsmm(v(1,i), nv, u(1,el_list(i)), nu, A(1,1,i), Bt(1,1,i), Ct(1,1,i))
        end do
-    else if (NEKO_BCKND_DEVICE .eq. 1) then
+    else if (NEKO_BCKND_DEVICE .eq. 1 .and. .not. on_host) then
        v_d = device_get_ptr(v)
        u_d = device_get_ptr(u)
        A_d = device_get_ptr(A)
