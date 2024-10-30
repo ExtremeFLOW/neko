2e8267c0d21ead247bfc092c95e9440639810b0b
Shiyu-Sandy-Du <shiyud@kth.se>
2024-10-30 15:53:34 +0100

    fix some typos

--- src/math/bcknd/device/ax_helm_full_device.F90
+++ src/math/bcknd/device/ax_helm_full_device.F90
@@ -52,28 +52,34 @@ module ax_helm_full_device
   interface
      subroutine hip_ax_helm_stress_vector(au_d, av_d, aw_d, u_d, v_d, w_d, &
           dx_d, dy_d, dz_d, dxt_d, dyt_d, dzt_d,&
-          h1_d, g11_d, g22_d, g33_d, g12_d, g13_d, g23_d, nelv, lx) &
+          h1_d, drdx_d, drdy_d, drdz_d, &
+          dsdx_d, dsdy_d, dsdz_d, &
+          dtdx_d, dtdy_d, dtdz_d, jacinv_d, weight3_d, nelv, lx) &
           bind(c, name='hip_ax_helm_stress_vector')
        use, intrinsic :: iso_c_binding
        type(c_ptr), value :: au_d, av_d, aw_d
        type(c_ptr), value :: u_d, v_d, w_d
        type(c_ptr), value :: dx_d, dy_d, dz_d
        type(c_ptr), value :: dxt_d, dyt_d, dzt_d
-       type(c_ptr), value :: h1_d, g11_d, g22_d, g33_d, g12_d, g13_d, g23_d
+       type(c_ptr), value :: h1_d
+       type(c_ptr), value :: drdx_d, drdy_d, drdz_d
+       type(c_ptr), value :: dsdx_d, dsdy_d, dsdz_d
+       type(c_ptr), value :: dtdx_d, dtdy_d, dtdz_d
+       type(c_ptr), value :: jacinv_d, weight3_d
        integer(c_int) :: nel, lx
      end subroutine hip_ax_helm_stress_vector
   end interface
 
-  interface
-     subroutine hip_ax_helm_stress_vector_part2(au_d, av_d, aw_d, u_d, v_d, w_d, &
-          h2_d, B_d, n) bind(c, name='hip_ax_helm_stress_vector_part2')
-       use, intrinsic :: iso_c_binding
-       type(c_ptr), value :: au_d, av_d, aw_d
-       type(c_ptr), value :: u_d, v_d, w_d
-       type(c_ptr), value :: h2_d, B_d
-       integer(c_int) :: n
-     end subroutine hip_ax_helm_stress_vector_part2
-  end interface
+  ! interface
+  !    subroutine hip_ax_helm_stress_vector_part2(au_d, av_d, aw_d, u_d, v_d, w_d, &
+  !         h2_d, B_d, n) bind(c, name='hip_ax_helm_stress_vector_part2')
+  !      use, intrinsic :: iso_c_binding
+  !      type(c_ptr), value :: au_d, av_d, aw_d
+  !      type(c_ptr), value :: u_d, v_d, w_d
+  !      type(c_ptr), value :: h2_d, B_d
+  !      integer(c_int) :: n
+  !    end subroutine hip_ax_helm_stress_vector_part2
+  ! end interface
 
 ! #elif HAVE_CUDA
 !   interface
@@ -146,20 +152,20 @@ contains
     call neko_error('OPENCL is not implemented for full stress formulation')
 #endif
 
-    if (coef%ifh2) then
-#ifdef HAVE_HIP
-       call hip_ax_helm_stress_vector_part2(au_d, av_d, aw_d, u_d, v_d, w_d, &
-                                     coef%h2_d, coef%B_d, coef%dof%size())
-#elif HAVE_CUDA
-       call neko_error('CUDA is not implemented for full stress formulation')
-      !  call cuda_ax_helm_stress_vector_part2(au_d, av_d, aw_d, u_d, v_d, w_d, &
-      !                                 coef%h2_d, coef%B_d, coef%dof%size())
-#else
-       call device_addcol4(au_d ,coef%h2_d, coef%B_d, u_d, coef%dof%size())
-       call device_addcol4(av_d ,coef%h2_d, coef%B_d, v_d, coef%dof%size())
-       call device_addcol4(aw_d ,coef%h2_d, coef%B_d, w_d, coef%dof%size())
-#endif
-    end if
+!     if (coef%ifh2) then
+! #ifdef HAVE_HIP
+!        call hip_ax_helm_stress_vector_part2(au_d, av_d, aw_d, u_d, v_d, w_d, &
+!                                      coef%h2_d, coef%B_d, coef%dof%size())
+! #elif HAVE_CUDA
+!        call neko_error('CUDA is not implemented for full stress formulation')
+!       !  call cuda_ax_helm_stress_vector_part2(au_d, av_d, aw_d, u_d, v_d, w_d, &
+!       !                                 coef%h2_d, coef%B_d, coef%dof%size())
+! #else
+!        call device_addcol4(au_d ,coef%h2_d, coef%B_d, u_d, coef%dof%size())
+!        call device_addcol4(av_d ,coef%h2_d, coef%B_d, v_d, coef%dof%size())
+!        call device_addcol4(aw_d ,coef%h2_d, coef%B_d, w_d, coef%dof%size())
+! #endif
+!     end if
 
   end subroutine ax_helm_full_device_compute_vector
 
