98f212f1d72a1f247dc7d49a4301500a86b329b3
Niclas Jansson <njansson@kth.se>
2024-11-14 11:38:29 +0100

    Make gs comm. backend selectable

--- src/gs/gs_comm.f90
+++ src/gs/gs_comm.f90
@@ -39,7 +39,8 @@ module gs_comm
   implicit none
   private
 
-  integer, public, parameter :: GS_COMM_MPI = 1, GS_COMM_MPIGPU = 2
+  integer, public, parameter :: GS_COMM_MPI = 1, GS_COMM_MPIGPU = 2, &
+       GS_COMM_NVSHMEM = 3
 
   !> Gather-scatter communication method
   type, public, abstract :: gs_comm_t
