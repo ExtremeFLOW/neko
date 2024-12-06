04ed259650816cd6502a4774d8ac2b6a7dff360a
Shiyu-Sandy-Du <shiyud@kth.se>
2024-12-06 14:53:08 +0100

    Merge Mij and nut computation together in device, and segragate the code into parts for different kernels

--- src/les/bcknd/device/dynamic_smagorinsky_device.f90
+++ src/les/bcknd/device/dynamic_smagorinsky_device.f90
@@ -114,20 +114,10 @@ contains
                                          coef%mult_d, s11%dof%size())
 
     call compute_lij_device(lij, u, v, w, test_filter, u%dof%size(), u%msh%nelv)
-    call compute_nut_device(num, den, lij, mij, &
+    call compute_nut_device(nut, c_dyn, num, den, lij, mij, &
                             s11, s22, s33, s12, s13, s23, &
                             s_abs, test_filter, delta, alpha, &
                             u%dof%size(), u%msh%nelv)
-    
-    do concurrent (i =1:u%dof%size())
-       if (den%x(i,1,1,1) .gt. 0.0_rp) then
-          c_dyn%x(i,1,1,1) = 0.5_rp * (num%x(i,1,1,1)/den%x(i,1,1,1))
-       else
-          c_dyn%x(i,1,1,1) = 0.0_rp
-       end if
-       c_dyn%x(i,1,1,1) = max(c_dyn%x(i,1,1,1),0.0_rp)
-       nut%x(i,1,1,1) = c_dyn%x(i,1,1,1) * delta%x(i,1,1,1)**2 * s_abs%x(i,1,1,1)
-    end do
 
     call coef%gs_h%op(nut, GS_OP_ADD)
     call device_col2(nut%x_d, coef%mult_d, nut%dof%size())
@@ -151,7 +141,7 @@ contains
     integer, intent(inout) :: nelv
     integer :: i
     !> filtered u,v,w by the test filter
-    type(field_t) :: fu, fv, fw, fuu, fvv, fww, fuv, fuw, fvw
+    type(field_t), pointer :: fu, fv, fw, fuu, fvv, fww, fuv, fuw, fvw
     integer :: temp_indices(9)
 
     call neko_scratch_registry%request_field(fu, temp_indices(1))
@@ -169,8 +159,8 @@ contains
     call test_filter%filter_3d(fv%x, v%x, nelv)
     call test_filter%filter_3d(fw%x, w%x, nelv)
 
-    !!!!!!
-    !!!!!! PART I START
+    !!         ___ ___
+    !! Compute u_i*u_j and u_i*u_j
     call device_lij_compute_part1(lij(1)%x_d, lij(2)%x_d, lij(3)%x_d, &
                                   lij(4)%x_d, lij(5)%x_d, lij(6)%x_d, &
                                   u%x_d, v%x_d, w%x_d, &
@@ -178,132 +168,133 @@ contains
                                   fuu%x_d, fvv%x_d, fww%x_d, &
                                   fuv%x_d, fuw%x_d, fvw%x_d, n)
 
-    !! The first term
-    do concurrent (i = 1:n)
-       lij(1)%x(i,1,1,1) = fu(i) * fu(i)
-       lij(2)%x(i,1,1,1) = fv(i) * fv(i)
-       lij(3)%x(i,1,1,1) = fw(i) * fw(i)
-       lij(4)%x(i,1,1,1) = fu(i) * fv(i)
-       lij(5)%x(i,1,1,1) = fu(i) * fw(i)
-       lij(6)%x(i,1,1,1) = fv(i) * fw(i)
-    end do
-
-    !! Subtract the second term:
-    !! use test filter for the cross terms
-    !! fu and fv are used as work array
-    call col3(fuu, u%x, u%x, n)
-    call col3(fvv, v%x, v%x, n)
-    call col3(fww, w%x, w%x, n)
-    call col3(fuv, u%x, v%x, n)
-    call col3(fuw, u%x, w%x, n)
-    call col3(fvw, v%x, w%x, n)
-    !!!!!!
-    !!!!!! PART I FINISH 
-
+    !! Filter u_i*u_j by the test filter
     call test_filter%filter_3d(fuu%x, fuu%x, nelv)
     call test_filter%filter_3d(fvv%x, fvv%x, nelv)
     call test_filter%filter_3d(fww%x, fww%x, nelv)
     call test_filter%filter_3d(fuv%x, fuv%x, nelv)
     call test_filter%filter_3d(fuw%x, fuw%x, nelv)
     call test_filter%filter_3d(fvw%x, fvw%x, nelv)
-
-    !!!!!!
-    !!!!!! PART II START
+    
+    !! Assember the final form
+    !!        ___ ___   _______
+    !! L_ij = u_i*u_j - u_i*u_j
     call device_lij_compute_part2(lij(1)%x_d, lij(2)%x_d, lij(3)%x_d, &
                                   lij(4)%x_d, lij(5)%x_d, lij(6)%x_d, &
                                   fuu%x_d, fvv%x_d, fww%x_d, &
                                   fuv%x_d, fuw%x_d, fvw%x_d, n)
-    call sub2(lij(1)%x, fuu, n)
-    call sub2(lij(2)%x, fvv, n)
-    call sub2(lij(3)%x, fww, n)
-    call sub2(lij(4)%x, fuv, n)
-    call sub2(lij(5)%x, fuw, n)
-    call sub2(lij(6)%x, fvw, n)
-    !!!!!!
-    !!!!!! PART II FINISH
-
   end subroutine compute_lij_device
-
-  !> Compute M_ij on the CPU.
+  
+  !> Compute M_ij and nut on the device.
   !!                              _____ ____   __________
   !! M_ij = ((delta_test/delta)^2 s_abs*s_ij - s_abs*s_ij)*(delta^2)
+  !! @param nut The SGS viscosity array
+  !! @param c_dyn The DS model coefficient
+  !! @param num The numerator in the expression of c_dyn, i.e. <mij*lij>
+  !! @param den The denominator in the expression of c_dyn, i.e. <mij*mij>
+  !! @param lij The Germano identity
   !! @param Mij
-  !! @param u x-velocity resolved (only filtered once)
-  !! @param v y-velocity resolved (only filtered once)
-  !! @param w z-velocity resolved (only filtered once)
+  !! @param s11
+  !! @param s22
+  !! @param s33
+  !! @param s12
+  !! @param s13
+  !! @param s23
+  !! @param s_abs
   !! @param test_filter
-  subroutine compute_mij_cpu(mij, s11, s22, s33, s12, s13, s23, &
-                             s_abs, test_filter, delta, n, nelv)
+  !! @param delta The filter size
+  !! @param alpha The moving average coefficient
+  !! @param n The number of points
+  !! @param nelv The number of elements
+  subroutine compute_nut_device(nut, c_dyn, num, den, lij, mij, &
+                            s11, s22, s33, s12, s13, s23, &
+                            s_abs, test_filter, delta, alpha, &
+                            n, nelv)
+    type(field_t), intent(inout) :: nut, c_dyn
+    type(field_t), intent(inout) :: num, den
+    type(field_t), intent(in) :: lij(6)
     type(field_t), intent(inout) :: mij(6)
     type(field_t), intent(inout) :: s11, s22, s33, s12, s13, s23, s_abs
-    type(elementwise_filter_t), intent(inout) :: test_filter
+    type(elementwise_filter_t), intent(in) :: test_filter
     type(field_t), intent(in) :: delta
+    real(kind=rp), intent(in) :: alpha
     integer, intent(in) :: n
     integer, intent(inout) :: nelv
 
-    real(kind=rp), dimension(n) :: fs11, fs22, fs33, fs12, fs13, fs23, fs_abs
-    real(kind=rp) :: delta_ratio2 !test- to grid- filter ratio, squared
-    integer :: i
-    real(kind=rp) :: delta2
+    real(kind=rp) :: delta_ratio2
+    integer :: temp_indices(13)
+    type(field_t), pointer :: fs11, fs22, fs33, fs12, fs13, fs23, fs_abs &
+                              fsabss11, fsabss22, fsabss33, &
+                              fsabss12, fsabss13, fsabss23
 
     delta_ratio2 = ((test_filter%nx-1)/(test_filter%nt-1))**2
 
-    !! The first term:
-    !!                      _____ ____
-    !! (delta_test/delta)^2 s_abs*s_ij
+    call neko_scratch_registry%request_field(fs11, temp_indices(1))
+    call neko_scratch_registry%request_field(fs22, temp_indices(2))
+    call neko_scratch_registry%request_field(fs33, temp_indices(3))
+    call neko_scratch_registry%request_field(fs12, temp_indices(4))
+    call neko_scratch_registry%request_field(fs13, temp_indices(5))
+    call neko_scratch_registry%request_field(fs23, temp_indices(6))
+    call neko_scratch_registry%request_field(fsabss11, temp_indices(7))
+    call neko_scratch_registry%request_field(fsabss22, temp_indices(8))
+    call neko_scratch_registry%request_field(fsabss33, temp_indices(9))
+    call neko_scratch_registry%request_field(fsabss12, temp_indices(10))
+    call neko_scratch_registry%request_field(fsabss13, temp_indices(11))
+    call neko_scratch_registry%request_field(fsabss23, temp_indices(12))
+    call neko_scratch_registry%request_field(fs_abs, temp_indices(13))
+    
+    !! Compute M_ij
+    !!         _____     ____
+    !! Compute s_abs and s_ij
     call test_filter%filter_3d(fs_abs, s_abs%x, nelv)
 
     call test_filter%filter_3d(fs11, s11%x, nelv)
+    call test_filter%filter_3d(fs22, s22%x, nelv)
+    call test_filter%filter_3d(fs33, s33%x, nelv)
+    call test_filter%filter_3d(fs12, s12%x, nelv)
+    call test_filter%filter_3d(fs13, s13%x, nelv)
+    call test_filter%filter_3d(fs23, s23%x, nelv)
+    
+    !!! PART I START
+    !!                              _____ ____
+    !! Compute (delta_test/delta)^2 s_abs*s_ij and s_abs*s_ij
     call col3(mij(1)%x, fs_abs, fs11, n)
     call cmult(mij(1)%x, delta_ratio2, n)
-
-    call test_filter%filter_3d(fs22, s22%x, nelv)
-    call col3(mij(2)%x, fs_abs, fs11, n)
+    call col3(mij(2)%x, fs_abs, fs22, n)
     call cmult(mij(2)%x, delta_ratio2, n)
-
-    call test_filter%filter_3d(fs33, s33%x, nelv)
-    call col3(mij(3)%x, fs_abs, fs11, n)
+    call col3(mij(3)%x, fs_abs, fs33, n)
     call cmult(mij(3)%x, delta_ratio2, n)
-
-    call test_filter%filter_3d(fs12, s12%x, nelv)
-    call col3(mij(4)%x, fs_abs, fs11, n)
+    call col3(mij(4)%x, fs_abs, fs12, n)
     call cmult(mij(4)%x, delta_ratio2, n)
-
-    call test_filter%filter_3d(fs13, s13%x, nelv)
-    call col3(mij(5)%x, fs_abs, fs11, n)
+    call col3(mij(5)%x, fs_abs, fs13, n)
     call cmult(mij(5)%x, delta_ratio2, n)
-
-    call test_filter%filter_3d(fs23, s23%x, nelv)
-    call col3(mij(6)%x, fs_abs, fs11, n)
+    call col3(mij(6)%x, fs_abs, fs23, n)
     call cmult(mij(6)%x, delta_ratio2, n)
 
-    !! Substract the second term:
-    !!                      _____ ____   __________
-    !! (delta_test/delta)^2 s_abs*s_ij - s_abs*s_ij
-    !! fs11 and fs22 are used as work array
-    call col3(fs11, s_abs%x, s11%x, n)
-    call test_filter%filter_3d(fs22, fs11, nelv)
-    call sub2(mij(1)%x, fs22, n)
+    call col3(fsabss11, s_abs%x, s11%x, n)
+    call col3(fsabss22, s_abs%x, s22%x, n)
+    call col3(fsabss33, s_abs%x, s33%x, n)
+    call col3(fsabss12, s_abs%x, s12%x, n)
+    call col3(fsabss13, s_abs%x, s13%x, n)
+    call col3(fsabss23, s_abs%x, s23%x, n)
+    !!! PART I FINISH
 
-    call col3(fs11, s_abs%x, s22%x, n)
-    call test_filter%filter_3d(fs22, fs11, nelv)
-    call sub2(mij(2)%x, fs22, n)
+    !!         
+    !! Filter s_abs*s_ij by the test filter
+    call test_filter%filter_3d(fsabss11%x, fsabss11%x, nelv)
+    call test_filter%filter_3d(fsabss22%x, fsabss22%x, nelv)
+    call test_filter%filter_3d(fsabss33%x, fsabss33%x, nelv)
+    call test_filter%filter_3d(fsabss12%x, fsabss12%x, nelv)
+    call test_filter%filter_3d(fsabss13%x, fsabss13%x, nelv)
+    call test_filter%filter_3d(fsabss23%x, fsabss23%x, nelv)
 
-    call col3(fs11, s_abs%x, s33%x, n)
-    call test_filter%filter_3d(fs22, fs11, nelv)
-    call sub2(mij(3)%x, fs22, n)
-
-    call col3(fs11, s_abs%x, s12%x, n)
-    call test_filter%filter_3d(fs22, fs11, nelv)
-    call sub2(mij(4)%x, fs22, n)
-
-    call col3(fs11, s_abs%x, s13%x, n)
-    call test_filter%filter_3d(fs22, fs11, nelv)
-    call sub2(mij(5)%x, fs22, n)
-
-    call col3(fs11, s_abs%x, s23%x, n)
-    call test_filter%filter_3d(fs22, fs11, nelv)
-    call sub2(mij(6)%x, fs22, n)
+    !!! PART II START
+    call sub2(mij(1)%x, fsabss11, n)
+    call sub2(mij(2)%x, fsabss22, n)
+    call sub2(mij(3)%x, fsabss33, n)
+    call sub2(mij(4)%x, fsabss12, n)
+    call sub2(mij(5)%x, fsabss13, n)
+    call sub2(mij(6)%x, fsabss23, n)
 
     !! Lastly multiplied by delta^2
     do concurrent (i = 1:n)
@@ -314,26 +305,7 @@ contains
        mij(4)%x(i,1,1,1) = mij(4)%x(i,1,1,1) * delta2
        mij(5)%x(i,1,1,1) = mij(5)%x(i,1,1,1) * delta2
        mij(6)%x(i,1,1,1) = mij(6)%x(i,1,1,1) * delta2
-    end do
 
-  end subroutine compute_mij_cpu
-
-  !> Compute numerator and denominator for c_dyn on the CPU.
-  !! @param num The numerator in the expression of c_dyn, i.e. <mij*lij>
-  !! @param den The denominator in the expression of c_dyn, i.e. <mij*mij>
-  !! @param mij
-  !! @param lij The Germano identity.
-  !! @param alpha The moving average coefficient 
-  subroutine compute_num_den_cpu(num, den, lij, mij, alpha, n)
-    type(field_t), intent(inout) :: num, den
-    type(field_t), intent(in) :: lij(6), mij(6)
-    real(kind=rp), intent(in) :: alpha
-    integer, intent(in) :: n
-
-    real(kind=rp), dimension(n) :: num_curr, den_curr
-    integer :: i
-
-    do concurrent (i = 1:n)
        num_curr(i) = mij(1)%x(i,1,1,1)*lij(1)%x(i,1,1,1) + &
                      mij(2)%x(i,1,1,1)*lij(2)%x(i,1,1,1) + &
                      mij(3)%x(i,1,1,1)*lij(3)%x(i,1,1,1) + &
@@ -346,15 +318,22 @@ contains
                      2.0_rp*(mij(4)%x(i,1,1,1)*mij(4)%x(i,1,1,1) + &
                      mij(5)%x(i,1,1,1)*mij(5)%x(i,1,1,1) + &
                      mij(6)%x(i,1,1,1)*mij(6)%x(i,1,1,1))
-    end do
 
-    ! running average over time
-    do concurrent (i = 1:n)
        num%x(i,1,1,1) = alpha * num%x(i,1,1,1) + (1.0_rp - alpha) * num_curr(i)
        den%x(i,1,1,1) = alpha * den%x(i,1,1,1) + (1.0_rp - alpha) * den_curr(i)
-    end do
 
-  end subroutine compute_num_den_cpu
+       if (den%x(i,1,1,1) .gt. 0.0_rp) then
+          c_dyn%x(i,1,1,1) = 0.5_rp * (num%x(i,1,1,1)/den%x(i,1,1,1))
+       else
+          c_dyn%x(i,1,1,1) = 0.0_rp
+       end if
+
+       c_dyn%x(i,1,1,1) = max(c_dyn%x(i,1,1,1),0.0_rp)
+       nut%x(i,1,1,1) = c_dyn%x(i,1,1,1) * delta%x(i,1,1,1)**2 * s_abs%x(i,1,1,1)
+
+    end do
+    !!! PART II FINISH
+  end subroutine
 
 end module dynamic_smagorinsky_device
 
