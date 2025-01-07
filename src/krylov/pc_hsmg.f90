19c2aead2cfa7c45436830ef14e569bb33345edf
pohm01 <peter.ohm@riken.jp>
2025-01-07 08:40:18 +0100

    tamg: add amg call to pc_hsmg

--- src/krylov/pc_hsmg.f90
+++ src/krylov/pc_hsmg.f90
@@ -254,9 +254,9 @@ contains
     ! Create a backend specific krylov solver
     if (present(crs_pctype)) then
        if (trim(crs_pctype) .eq. 'tamg') then
-          if (NEKO_BCKND_DEVICE .eq. 1) then
-             call neko_error('Tree-amg only supported for CPU')
-          end if
+          !if (NEKO_BCKND_DEVICE .eq. 1) then
+          !   call neko_error('Tree-amg only supported for CPU')
+          !end if
 
           allocate(this%amg_solver)
 
@@ -420,12 +420,16 @@ contains
           call this%grids(1)%bclst%apply_scalar(this%wf%x, &
                this%grids(1)%dof%size())
           call profiler_start_region('HSMG_coarse_solve', 11)
-          crs_info = this%crs_solver%solve(this%Ax, this%grids(1)%e, &
+          if (allocated(this%amg_solver)) then
+            call this%amg_solver%device_solve(this%grids(1)%e%x, this%r, this%grids(1)%e%x_d, this%r_d, this%grids(1)%dof%size())
+          else
+            crs_info = this%crs_solver%solve(this%Ax, this%grids(1)%e, &
                                        this%wf%x, &
                                        this%grids(1)%dof%size(), &
                                        this%grids(1)%coef, &
                                        this%grids(1)%bclst, &
                                        this%grids(1)%gs_h, this%niter)
+          end if
           call profiler_end_region('HSMG_coarse_solve', 11)
           call this%grids(1)%bclst%apply_scalar(this%grids(1)%e%x,&
                this%grids(1)%dof%size())
