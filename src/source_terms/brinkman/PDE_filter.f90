5dc9f000562abf40a1eb8f949ebd5143e3a9a237
Shiyu Du <115027341+Shiyu-Sandy-Du@users.noreply.github.com>
2025-01-13 16:04:40 +0100

    Update PDE_filter.f90

--- src/source_terms/brinkman/PDE_filter.f90
+++ src/source_terms/brinkman/PDE_filter.f90
@@ -120,7 +120,7 @@ contains
     type(coef_t), intent(in) :: coef
 
     ! user parameters
-    call json_get(json, "filter.radius")
+    call json_get(json, "filter.radius", this%r)
 
     call json_get_or_default(json, "filter.tolerance", this%abstol_filt, &
          1.0e-10_rp)
