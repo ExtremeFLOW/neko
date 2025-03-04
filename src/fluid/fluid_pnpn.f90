04471096e6986fcfcdc1461a34cfa1c86f37e7ad
Shiyu Du <115027341+Shiyu-Sandy-Du@users.noreply.github.com>
2025-03-04 15:25:58 +0100

    add error message if delta type is unknown & leave the user to decide whether extrapolate (#1720)
    Co-authored-by: Tim Felle Olsen <tife@dtu.dk>

--- src/fluid/fluid_pnpn.f90
+++ src/fluid/fluid_pnpn.f90
@@ -665,10 +665,8 @@ contains
          event => this%event)
 
       ! Extrapolate the velocity if it's not done in nut_field estimation
-      if (this%variable_material_properties .eqv. .false.) then
-         call sumab%compute_fluid(u_e, v_e, w_e, u, v, w, &
-              ulag, vlag, wlag, ext_bdf%advection_coeffs, ext_bdf%nadv)
-      end if
+      call sumab%compute_fluid(u_e, v_e, w_e, u, v, w, &
+           ulag, vlag, wlag, ext_bdf%advection_coeffs, ext_bdf%nadv)
 
       ! Compute the source terms
       call this%source_term%compute(t, tstep)
