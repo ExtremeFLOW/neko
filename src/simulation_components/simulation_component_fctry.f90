9c78ae79cfe52a070633ebd48ff7a5f023d11358
Timofey Mukha <timofey.mukha@protonmail.com>
2025-04-24 16:14:44 +0300

    Fix simcomp factory

--- src/simulation_components/simulation_component_fctry.f90
+++ src/simulation_components/simulation_component_fctry.f90
@@ -45,6 +45,7 @@ submodule (simulation_component) simulation_component_fctry
   use derivative, only : derivative_t
   use spectral_error, only: spectral_error_t
   use utils, only : neko_type_error, neko_type_registration_error
+  implicit none
 
   ! List of all possible types created by the factory routine
   character(len=20) :: SIMCOMPS_KNOWN_TYPES(9) = [character(len=20) :: &
@@ -93,6 +94,7 @@ contains
   module subroutine simulation_component_allocator(object, type_name)
     class(simulation_component_t), allocatable, intent(inout) :: object
     character(len=*), intent(in):: type_name
+    integer :: i
 
     select case (trim(type_name))
     case ("vorticity")
@@ -137,6 +139,7 @@ contains
     character(len=*), intent(in) :: type_name
     procedure(simulation_component_allocate), pointer, intent(in) :: allocator
     type(allocator_entry), allocatable :: temp(:)
+    integer :: i
 
     do i = 1, size(SIMCOMPS_KNOWN_TYPES)
        if (trim(type_name) .eq. trim(SIMCOMPS_KNOWN_TYPES(i))) then
@@ -145,7 +148,7 @@ contains
        end if
     end do
 
-    do i = 1, simulation_component_registry_size
+    do i = 1, simcomp_registry_size
        if (trim(type_name) .eq. &
             trim(simcomp_registry(i)%type_name)) then
           call neko_type_registration_error("simulation component", type_name, &
