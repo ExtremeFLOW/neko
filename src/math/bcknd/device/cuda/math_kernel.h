fbcbc5d54d200e35f44365a6201f5e1f0094aabb
Timofey Mukha <timofey.mukha@protonmail.com>
2025-04-29 11:11:10 +0300

    Use n_mask

--- src/math/bcknd/device/cuda/math_kernel.h
+++ src/math/bcknd/device/cuda/math_kernel.h
@@ -51,38 +51,38 @@ __global__ void cmult_kernel(T * __restrict__ a,
 }
 
 /**
- * Device kernel for masked red copy
+ * Device kernel for masked gather copy
  */
 template< typename T >
 __global__ void masked_gather_copy_kernel(T * __restrict__ a,
                                    T * __restrict__ b,
                                    int * __restrict__ mask,
                                    const int n,
-                                   const int m) {
+                                   const int n_mask) {
 
   const int idx = blockIdx.x * blockDim.x + threadIdx.x;
   const int str = blockDim.x * gridDim.x;
 
-  for (int i = idx; i < m; i += str) {
+  for (int i = idx; i < n_mask; i += str) {
     a[i] = b[mask[i+1]-1];
   }
 }
 
 
 /**
- * Device kernel for masked red copy
+ * Device kernel for masked scatter copy
  */
 template< typename T >
 __global__ void masked_scatter_copy_kernel(T * __restrict__ a,
                                    T * __restrict__ b,
                                    int * __restrict__ mask,
                                    const int n,
-                                   const int m) {
+                                   const int n_mask) {
 
   const int idx = blockIdx.x * blockDim.x + threadIdx.x;
   const int str = blockDim.x * gridDim.x;
 
-  for (int i = idx; i < n; i += str) {
+  for (int i = idx; i < n_mask; i += str) {
     a[mask[i+1]-1] = b[i];
   }
 }
@@ -116,12 +116,12 @@ __global__ void masked_copy_kernel(T * __restrict__ a,
                                    T * __restrict__ b,
                                    int * __restrict__ mask,
                                    const int n,
-                                   const int m) {
+                                   const int n_mask) {
 
   const int idx = blockIdx.x * blockDim.x + threadIdx.x;
   const int str = blockDim.x * gridDim.x;
 
-  for (int i = idx; i < m; i += str) {
+  for (int i = idx; i < n_mask; i += str) {
     a[mask[i+1]-1] = b[mask[i+1]-1];
   }
 }
