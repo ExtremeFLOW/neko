a4ab30b223a4c5c765cb4e2a5f01f64602be1357
Niclas Jansson <njansson@kth.se>
2025-05-14 21:07:18 +0200

    Change pyneko example to the cylinder

--- examples/api/pyneko/cylinder.py
+++ examples/api/pyneko/cylinder.py
@@ -6,22 +6,16 @@ pyneko.init()
 pyneko.job_info()
 
 # Create a Neko case from a JSON file
-hemi_json = json.load(open('hemi.case'))
-hemi_case = pyneko.case_init(hemi_json)
+cylinder_json = json.load(open('cylinder.case'))
+cylinder_case = pyneko.case_init(cylinder_json)
 
 # To solve the entire case we can call solve()
-# pyneko.solve(hemi_case
-
+#pyneko.solve(cylinder_case)
+              
 # To manually step forward in time, call step()
-t = 0.0
-for tstep in range (1, 3):
-    pyneko.step(hemi_case, t, tstep)
-    t = t + hemi_json['case']['timestep']
-    pyneko.output(hemi_case, t, tstep)
-
-# Force an output
-pyneko.output(hemi_case, t, tstep, force=True)
+while pyneko.time(cylinder_case) < pyneko.end_time(cylinder_case):
+    pyneko.step(cylinder_case)
 
 # Cleanup
-pyneko.case_free(hemi_case)
+pyneko.case_free(cylinder_case)
 pyneko.finalize()
