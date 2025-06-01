30cccd60a8e4acd72b4b9d0abf76eb0a96e46000
Timofey Mukha <timofey.mukha@protonmail.com>
2025-06-01 17:42:55 +0200

    Update

--- integration_tests/tests/test_demo/test_suite.py
+++ integration_tests/tests/test_demo/test_suite.py
@@ -2,36 +2,24 @@ import subprocess
 import pytest
 import os
 from os.path import join
-from testlib import get_neko
+from testlib import get_neko, run_neko
 
 
-def test_cylinder(launcher_script, request):
+def test_demo(launcher_script, log_file):
     """A very good description of the test."""
 
     # Get the path to the neko executable
     neko = get_neko()
 
-    # Get the name of the test from the request object
-    test_name = request.node.name
-
-
     # Number of ranks to launch on
     nprocs = 2
-
-    log_file = "logs/" + test_name + ".log"
 
     # Either specify a case, or load it here into a json, manipulate
     # and save a new case file.
     case_file = join("case_templates", "cylinder.case")
 
     # Run Neko
-    with open(log_file, "w") as log:
-        result = subprocess.run(
-            [launcher_script, str(nprocs), case_file, neko],
-            stdout=log,
-            stderr=subprocess.STDOUT,
-            text=True
-        )
+    result = run_neko(launcher_script, nprocs, case_file, neko, log_file)
 
     # Check if the process completed successfully
     assert (
