2b9ce9fa2ce3a1705fd473a4c2c39a7c0546b3e4
tuananhdao <kitsudo1412@gmail.com>
2025-07-01 01:08:56 +0200

    fix cu hip

--- src/math/bcknd/device/cuda/opr_compute_max_wave_speed.cu
+++ src/math/bcknd/device/cuda/opr_compute_max_wave_speed.cu
@@ -32,17 +32,16 @@
  POSSIBILITY OF SUCH DAMAGE.
 */
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <cuda.h>
 #include <cuda_runtime.h>
 #include <cublas.h>
 #include <device/device_config.h>
+#include <device/cuda/check.h>
 #include <math/bcknd/device/cuda/mathops_kernel.h>
+
+extern "C" {
 
 void cuda_compute_max_wave_speed(real *max_wave_speed_d, 
                                  real *u_d, real *v_d, real *w_d,
@@ -51,22 +50,21 @@ void cuda_compute_max_wave_speed(real *max_wave_speed_d,
   
   const dim3 nthrds(256);
   const dim3 nblcks((n + 256 - 1) / 256);
+  const cudaStream_t stream = (cudaStream_t) glb_cmd_queue;
   
 #ifdef HAVE_REAL_SINGLE
   compute_max_wave_speed_kernel<float>
-    <<<nblcks, nthrds, 0, (cudaStream_t) glb_cmd_strm>>>(max_wave_speed_d, 
-                                                          u_d, v_d, w_d, 
-                                                          gamma, p_d, rho_d, n);
+    <<<nblcks, nthrds, 0, stream>>>(max_wave_speed_d, 
+                                     u_d, v_d, w_d, 
+                                     gamma, p_d, rho_d, n);
 #else
   compute_max_wave_speed_kernel<double>
-    <<<nblcks, nthrds, 0, (cudaStream_t) glb_cmd_strm>>>(max_wave_speed_d, 
-                                                          u_d, v_d, w_d, 
-                                                          gamma, p_d, rho_d, n);
+    <<<nblcks, nthrds, 0, stream>>>(max_wave_speed_d, 
+                                     u_d, v_d, w_d, 
+                                     gamma, p_d, rho_d, n);
 #endif
   CUDA_CHECK(cudaGetLastError());
   
 }
 
-#ifdef __cplusplus
-}
-#endif 
+} 
