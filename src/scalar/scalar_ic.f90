c403d2e30e27d36ddeb8da1ad07f33275774e99a
Victor Baconnet <baconnet@kth.se>
2025-06-30 15:53:28 +0200

    Change generate_interpolator function to subroutine

--- src/scalar/scalar_ic.f90
+++ src/scalar/scalar_ic.f90
@@ -360,7 +360,7 @@ contains
        end select
 
        ! Generates an interpolator object and performs the point search
-       global_interp = fld_data%generate_interpolator(s%dof, s%msh, tolerance)
+       call fld_data%generate_interpolator(global_interp, s%dof, s%msh, tolerance)
 
        ! Evaluate scalar
        call global_interp%evaluate(s%x, fld_data%t%x)
