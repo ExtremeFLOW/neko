fc80664669dd3f2110e0cef0534c8c86acaab08c
Tim Felle Olsen <tife@dtu.dk>
2025-07-09 10:28:08 +0200

    `parameter` not allowed in type

--- src/common/checkpoint.f90
+++ src/common/checkpoint.f90
@@ -79,7 +79,7 @@ module checkpoint
      real(kind=dp) :: t !< Restart time (valid after load)
      type(mesh_t) :: previous_mesh
      type(space_t) :: previous_Xh
-     real(kind=rp), parameter :: mesh2mesh_tol = NEKO_EPS*1e3_rp
+     real(kind=rp) :: mesh2mesh_tol = NEKO_EPS*1e3_rp
 
    contains
      procedure, pass(this) :: init => chkp_init
