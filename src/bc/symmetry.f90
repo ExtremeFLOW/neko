0bfe8cc347f2d04121d9a0c6173022af95fe3eb2
Timofey Mukha <timofey.mukha@protonmail.com>
2025-07-24 10:14:43 +0300

    Update src/bc/symmetry.f90
    Co-authored-by: Tim Felle Olsen <timfelle@hotmail.com>

--- src/bc/symmetry.f90
+++ src/bc/symmetry.f90
@@ -263,9 +263,9 @@ contains
   subroutine symmetry_apply_vector_dev(this, x_d, y_d, z_d, &
        time, strong, strm)
     class(symmetry_t), intent(inout), target :: this
-    type(c_ptr) :: x_d
-    type(c_ptr) :: y_d
-    type(c_ptr) :: z_d
+    type(c_ptr), intent(inout) :: x_d
+    type(c_ptr), intent(inout) :: y_d
+    type(c_ptr), intent(inout) :: z_d
     type(time_state_t), intent(in), optional :: time
     logical, intent(in), optional :: strong
     type(c_ptr) :: strm
