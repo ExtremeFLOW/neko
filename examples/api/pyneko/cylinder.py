60cb9f7f5286eb4e71ae444989c476df3d289230
Niclas Jansson <njansson@kth.se>
2025-07-29 21:58:06 +0200

    Update comments

--- examples/api/pyneko/cylinder.py
+++ examples/api/pyneko/cylinder.py
@@ -16,8 +16,8 @@ cylinder_case = pyneko.case_init(cylinder_json)
 while pyneko.time(cylinder_case) < pyneko.end_time(cylinder_case):
     pyneko.step(cylinder_case)
 
-# To retrive a numpy array of the data in a field from Neko's field
-# registry use field()
+# To retrive a dataclass representation of a field with a numpy array
+# of the data in a field from Neko's field registry use field()
 u = pyneko.field(b'u')
 
 # Cleanup
