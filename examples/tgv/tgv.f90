76a031b982b06e2b7632140ff1f45df2b1ab60d0
Timofey Mukha <timofey.mukha@protonmail.com>
2025-08-06 10:03:13 +0300

    Update examples/tgv/tgv.f90
    Co-authored-by: Tim Felle Olsen <timfelle@hotmail.com>

--- examples/tgv/tgv.f90
+++ examples/tgv/tgv.f90
@@ -147,7 +147,7 @@ contains
     call field_addcol3(w1, v, v)
     call field_addcol3(w1, w, w)
     if (NEKO_BCKND_DEVICE .eq. 1) then
-       e1 = 0.5 * device_glsc2(w1%x_d, coef%B_d, w1%size()) / coef%volume
+       e1 = 0.5_rp * device_glsc2(w1%x_d, coef%B_d, w1%size()) / coef%volume
     else
        e1 = 0.5 * glsc2(w1%x, coef%B, w1%size()) / coef%volume
     end if
