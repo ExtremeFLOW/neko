7ad8af5748bd61b984564b59a354e45835ac1d83
Tim Felle Olsen <tife@dtu.dk>
2025-08-08 09:26:27 +0200

    Reduce reliance on implicit device pointers (#2047)
    Unified the interfaces for BC's a bit and started work which should make
    it easier to phase out the get_device_ptr from BC system.
    
    (cherry picked from commit a1081128eff4517c996a2a4d25aedd8c1cac776d)

--- src/bc/bc.f90
+++ src/bc/bc.f90
@@ -34,7 +34,7 @@
 module bc
   use neko_config, only : NEKO_BCKND_DEVICE
   use num_types, only : rp
-  use device, only : device_get_ptr, HOST_TO_DEVICE, device_memcpy, &
+  use device, only : HOST_TO_DEVICE, device_memcpy, &
        device_free, device_map, DEVICE_TO_HOST, glb_cmd_queue
   use iso_c_binding, only: c_associated
   use dofmap, only : dofmap_t
@@ -45,12 +45,15 @@ module bc
   use stack, only : stack_i4t2_t
   use tuple, only : tuple_i4_t
   use field, only : field_t
-  use gather_scatter, only : GS_OP_ADD
+  use gs_ops, only : GS_OP_ADD
   use math, only : relcmp
   use utils, only : neko_error, linear_index, split_string
   use, intrinsic :: iso_c_binding, only : c_ptr, C_NULL_PTR
   use json_module, only : json_file
   use time_state, only : time_state_t
+  use field, only : field_t
+  use file, only : file_t
+
   implicit none
   private
 
@@ -292,22 +295,30 @@ contains
   !! @param x The x comp of the field for which to apply the bc.
   !! @param y The y comp of the field for which to apply the bc.
   !! @param z The z comp of the field for which to apply the bc.
-  !! @param n The size of x, y, and z.
   !! @param time Current time state.
+  !! @param strong Whether we are setting a strong or a weak bc.
   !! @param Device stream
-  subroutine bc_apply_vector_generic(this, x, y, z, n, time, strm)
+  subroutine bc_apply_vector_generic(this, x, y, z, time, strong, strm)
     class(bc_t), intent(inout) :: this
-    integer, intent(in) :: n
-    real(kind=rp), intent(inout), dimension(n) :: x
-    real(kind=rp), intent(inout), dimension(n) :: y
-    real(kind=rp), intent(inout), dimension(n) :: z
+    type(field_t), intent(inout) :: x
+    type(field_t), intent(inout) :: y
+    type(field_t), intent(inout) :: z
     type(time_state_t), intent(in), optional :: time
-    type(c_ptr), optional :: strm
+    logical, intent(in), optional :: strong
+    type(c_ptr), intent(inout), optional :: strm
     type(c_ptr) :: strm_
-    type(c_ptr) :: x_d
-    type(c_ptr) :: y_d
-    type(c_ptr) :: z_d
-    integer :: i
+    integer :: n
+    character(len=256) :: msg
+
+    ! Get the size of the fields
+    n = x%size()
+
+    ! Ensure all fields are the same size
+    if (y%size() .ne. n .or. z%size() .ne. n) then
+       msg = "Fields x, y, z must have the same size in " // &
+            "bc_list_apply_vector_field"
+       call neko_error(trim(msg))
+    end if
 
     if (NEKO_BCKND_DEVICE .eq. 1) then
 
@@ -317,16 +328,10 @@ contains
           strm_ = glb_cmd_queue
        end if
 
-       x_d = device_get_ptr(x)
-       y_d = device_get_ptr(y)
-       z_d = device_get_ptr(z)
-       if (present(time)) then
-          call this%apply_vector_dev(x_d, y_d, z_d, time = time, strm = strm_)
-       else
-          call this%apply_vector_dev(x_d, y_d, z_d, strm = strm_)
-       end if
+       call this%apply_vector_dev(x%x_d, y%x_d, z%x_d, time = time, &
+            strong = strong, strm = strm_)
     else
-       call this%apply_vector(x, y, z, n, time = time)
+       call this%apply_vector(x%x, y%x, z%x, n, time = time, strong = strong)
     end if
 
   end subroutine bc_apply_vector_generic
@@ -334,21 +339,20 @@ contains
   !> Apply the boundary condition to a scalar field. Dispatches to the CPU
   !! or the device version.
   !! @param x The x comp of the field for which to apply the bc.
-  !! @param y The y comp of the field for which to apply the bc.
-  !! @param z The z comp of the field for which to apply the bc.
-  !! @param n The size of x, y, and z.
   !! @param time Current time state.
+  !! @param strong Whether we are setting a strong or a weak bc.
   !! @param strm Device stream
-  subroutine bc_apply_scalar_generic(this, x, n, time, strm)
+  subroutine bc_apply_scalar_generic(this, x, time, strong, strm)
     class(bc_t), intent(inout) :: this
-    integer, intent(in) :: n
-    real(kind=rp), intent(inout), dimension(n) :: x
+    type(field_t), intent(inout) :: x
     type(time_state_t), intent(in), optional :: time
-    type(c_ptr), optional :: strm
+    logical, intent(in), optional :: strong
+    type(c_ptr), intent(inout), optional :: strm
     type(c_ptr) :: strm_
-    type(c_ptr) :: x_d
-    integer :: i
+    integer :: n
 
+    ! Get the size of the field
+    n = x%size()
 
     if (NEKO_BCKND_DEVICE .eq. 1) then
 
@@ -358,10 +362,10 @@ contains
           strm_ = glb_cmd_queue
        end if
 
-       x_d = device_get_ptr(x)
-       call this%apply_scalar_dev(x_d, time = time, strm = strm_)
+       call this%apply_scalar_dev(x%x_d, time = time, strong = strong, &
+            strm = strm_)
     else
-       call this%apply_scalar(x, n, time = time)
+       call this%apply_scalar(x%x, n, time = time)
     end if
 
   end subroutine bc_apply_scalar_generic
@@ -563,9 +567,6 @@ contains
   !! @details The mask will be marked with 1.
   !! @param file_name The name of the fld file.
   subroutine bc_debug_mask(this, file_name)
-    use field, only : field_t
-    use file, only : file_t
-
     class(bc_t), intent(inout) :: this
     character(len=*), intent(in) :: file_name
     type(field_t) :: bdry_field
