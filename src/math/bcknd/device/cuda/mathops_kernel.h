ExtremeFLOW/neko

commit 2e89c12f7459165fe255f7f349ab539e696f4d70

Reorganize compressible kernels (#2061)
## Problem

- Kernels to compute max wave speed the compressible module was placed
in math `operators.f90` which is not correct

## Changes

- Move above kernels to separate fluid kernels
- Add a kernel to compute the entropy function to prepare for entropy
viscosity

Manually tested

- [x] CPU (Mac)
- [x] OpenCL (Mac)
- [x] HIP (LUMI)
- [ ] CUDA

src/.depends_device
@@ -106,6 +106,8 @@ bc/bcknd/device/opencl/dong_outflow.lo : bc/bcknd/device/opencl/dong_outflow.c b
 common/bcknd/device/opencl/rhs_maker.lo : common/bcknd/device/opencl/rhs_maker.c common/bcknd/device/opencl/rhs_maker_kernel.cl.h
 fluid/bcknd/device/opencl/pnpn_res.lo : fluid/bcknd/device/opencl/pnpn_res.c fluid/bcknd/device/opencl/pnpn_res_kernel.cl.h
 fluid/bcknd/device/opencl/euler_res.lo : fluid/bcknd/device/opencl/euler_res.c fluid/bcknd/device/opencl/euler_res_kernel.cl.h
+fluid/bcknd/device/opencl/compressible_ops_compute_max_wave_speed.lo : fluid/bcknd/device/opencl/compressible_ops_compute_max_wave_speed.c fluid/bcknd/device/opencl/compressible_ops_compute_max_wave_speed_kernel.cl.h
+fluid/bcknd/device/opencl/compressible_ops_compute_entropy.lo : fluid/bcknd/device/opencl/compressible_ops_compute_entropy.c fluid/bcknd/device/opencl/compressible_ops_compute_entropy_kernel.cl.h
 scalar/bcknd/device/opencl/scalar_residual.lo : scalar/bcknd/device/opencl/scalar_residual.c scalar/bcknd/device/opencl/scalar_residual_kernel.cl.h
 sem/bcknd/device/opencl/coef.lo : sem/bcknd/device/opencl/coef.c sem/bcknd/device/opencl/coef_kernel.cl.h
 filter/bcknd/device/cuda/mappings.lo : filter/bcknd/device/cuda/mappings.cu filter/bcknd/device/cuda/mapping_kernels.h

src/Makefile.am
@@ -250,6 +250,8 @@ neko_fortran_SOURCES = \
 	fluid/bcknd/cpu/euler_res_cpu.f90\
 	fluid/bcknd/sx/euler_res_sx.f90\
 	fluid/bcknd/device/euler_res_device.F90\
+	fluid/bcknd/cpu/compressible_ops_cpu.f90\
+	fluid/bcknd/device/compressible_ops_device.F90\
 	fluid/fluid_source_term.f90\
 	common/time_step_controller.f90\
 	simulation.f90\
@@ -428,6 +430,8 @@ libneko_la_SOURCES += \
 	common/bcknd/device/hip/projection.hip\
 	fluid/bcknd/device/hip/pnpn_res.hip\
 	fluid/bcknd/device/hip/euler_res.hip\
+	fluid/bcknd/device/hip/compressible_ops_compute_max_wave_speed.hip\
+	fluid/bcknd/device/hip/compressible_ops_compute_entropy.hip\
 	fluid/stress_formulation/bcknd/device/hip/pnpn_stress_res.hip\
 	scalar/bcknd/device/hip/scalar_residual.hip\
 	sem/bcknd/device/hip/coef.hip\
@@ -478,6 +482,8 @@ libneko_la_SOURCES += \
 	common/bcknd/device/cuda/projection.cu\
 	fluid/bcknd/device/cuda/pnpn_res.cu\
 	fluid/bcknd/device/cuda/euler_res.cu\
+	fluid/bcknd/device/cuda/compressible_ops_compute_max_wave_speed.cu\
+	fluid/bcknd/device/cuda/compressible_ops_compute_entropy.cu\
 	fluid/stress_formulation/bcknd/device/cuda/pnpn_stress_res.cu\
 	scalar/bcknd/device/cuda/scalar_residual.cu\
 	sem/bcknd/device/cuda/coef.cu\
@@ -527,6 +533,8 @@ libneko_la_SOURCES += \
 	common/bcknd/device/opencl/rhs_maker.c\
 	fluid/bcknd/device/opencl/pnpn_res.c\
 	fluid/bcknd/device/opencl/euler_res.c\
+	fluid/bcknd/device/opencl/compressible_ops_compute_max_wave_speed.c\
+	fluid/bcknd/device/opencl/compressible_ops_compute_entropy.c\
 	scalar/bcknd/device/opencl/scalar_residual.c\
 	sem/bcknd/device/opencl/coef.c\
 	filter/bcknd/device/opencl/mappings.c
@@ -647,6 +655,7 @@ EXTRA_DIST = \
 	fluid/bcknd/device/cuda/prs_res_kernel.h\
 	fluid/bcknd/device/cuda/vel_res_update_kernel.h\
 	fluid/bcknd/device/cuda/euler_res_kernel.h\
+	fluid/bcknd/device/cuda/compressible_ops_kernel.h\
 	fluid/stress_formulation/bcknd/device/cuda/prs_stress_res_kernel.h\
 	scalar/bcknd/device/cuda/scalar_residual_update_kernel.h\
 	sem/bcknd/device/cuda/coef_kernel.h\
@@ -687,6 +696,7 @@ EXTRA_DIST = \
 	fluid/bcknd/device/hip/prs_res_kernel.h\
 	fluid/bcknd/device/hip/vel_res_update_kernel.h\
 	fluid/bcknd/device/hip/euler_res_kernel.h\
+	fluid/bcknd/device/hip/compressible_ops_kernel.h\
 	fluid/stress_formulation/bcknd/device/hip/prs_stress_res_kernel.h\
 	scalar/bcknd/device/hip/scalar_residual_update_kernel.h\
 	krylov/bcknd/device/cuda/pipecg_kernel.h\
@@ -730,6 +740,8 @@ EXTRA_DIST = \
 	common/bcknd/device/opencl/rhs_maker_kernel.cl\
 	fluid/bcknd/device/opencl/pnpn_res_kernel.cl\
 	fluid/bcknd/device/opencl/euler_res_kernel.cl\
+	fluid/bcknd/device/opencl/compressible_ops_compute_max_wave_speed_kernel.cl\
+	fluid/bcknd/device/opencl/compressible_ops_compute_entropy_kernel.cl\
 	scalar/bcknd/device/opencl/scalar_residual_kernel.cl\
 	gs/bcknd/device/opencl/gs_kernels.cl\
 	krylov/bcknd/device/opencl/jacobi_kernel.cl\

examples/euler_1d_sod/sod.case
@@ -29,7 +29,7 @@
         "simulation_components": [
             {
                 "type": "field_writer",
-                "fields": ["fluid_rho", "max_wave_speed"],
+                "fields": ["S"],
                 "output_control": "nsamples",
                 "output_value": 100
             }

src/.depends
@@ -218,7 +218,7 @@ fluid/stress_formulation/bcknd/cpu/pnpn_res_stress_cpu.lo : fluid/stress_formula
 fluid/stress_formulation/bcknd/device/pnpn_res_stress_device.lo : fluid/stress_formulation/bcknd/device/pnpn_res_stress_device.F90 math/bcknd/device/device_math.lo math/bcknd/device/device_mathops.lo sem/space.lo config/num_types.lo mesh/mesh.lo field/scratch_registry.lo fluid/pnpn_res.lo bc/facet_normal.lo sem/coef.lo math/ax.lo field/field.lo math/operators.lo common/utils.lo gs/gather_scatter.lo 
 fluid/fluid_scheme_base.lo : fluid/fluid_scheme_base.f90 common/time_state.lo field/field_list.lo bc/bc_list.lo common/utils.lo common/user_intf.lo common/time_step_controller.lo time_schemes/time_scheme_controller.lo sem/space.lo mesh/mesh.lo common/checkpoint.lo config/num_types.lo common/log.lo gs/gather_scatter.lo field/field_series.lo field/field.lo sem/dofmap.lo bc/dirichlet.lo sem/coef.lo 
 fluid/fluid_scheme_incompressible.lo : fluid/fluid_scheme_incompressible.f90 common/time_state.lo device/device.lo math/field_math.lo common/utils.lo common/user_intf.lo field/scratch_registry.lo common/json_utils.lo field/field_registry.lo common/log.lo math/operators.lo math/math.lo mesh/mesh.lo bc/bc_list.lo bc/bc.lo fluid/fluid_stats.lo krylov/precon.lo multigrid/phmg.lo krylov/pc_hsmg.lo krylov/bcknd/device/pc_jacobi_device.lo krylov/bcknd/sx/pc_jacobi_sx.lo krylov/bcknd/cpu/pc_jacobi.lo bc/dirichlet.lo sem/coef.lo krylov/krylov.lo sem/dofmap.lo sem/space.lo field/field.lo fluid/fluid_source_term.lo config/num_types.lo fluid/mean_flow.lo common/checkpoint.lo config/neko_config.lo fluid/mean_sqr_flow.lo gs/gather_scatter.lo fluid/fluid_scheme_base.lo 
-fluid/fluid_scheme_compressible.lo : fluid/fluid_scheme_compressible.f90 common/time_state.lo math/operators.lo common/json_utils.lo common/user_intf.lo sem/space.lo field/scratch_registry.lo mesh/mesh.lo config/num_types.lo fluid/fluid_scheme_base.lo field/field_registry.lo math/field_math.lo field/field.lo 
+fluid/fluid_scheme_compressible.lo : fluid/fluid_scheme_compressible.f90 common/time_state.lo config/neko_config.lo fluid/bcknd/device/compressible_ops_device.lo fluid/bcknd/cpu/compressible_ops_cpu.lo math/operators.lo common/json_utils.lo common/user_intf.lo sem/space.lo field/scratch_registry.lo mesh/mesh.lo config/num_types.lo fluid/fluid_scheme_base.lo field/field_registry.lo math/field_math.lo field/field.lo 
 fluid/fluid_scheme_compressible_euler.lo : fluid/fluid_scheme_compressible_euler.f90 common/time_state.lo common/log.lo common/utils.lo bc/bc.lo bc/bc_list.lo time_schemes/runge_kutta_scheme.lo config/neko_config.lo fluid/euler_res.lo sem/coef.lo math/ax.lo common/time_step_controller.lo common/user_intf.lo common/profiler.lo common/json_utils.lo common/checkpoint.lo mesh/mesh.lo config/num_types.lo gs/gs_ops.lo fluid/fluid_scheme_compressible.lo field/field.lo math/bcknd/device/device_math.lo math/math.lo math/field_math.lo device/device.lo fluid/advection.lo comm/comm.lo 
 fluid/fluid_aux.lo : fluid/fluid_aux.f90 common/time_state.lo common/utils.lo common/log.lo krylov/krylov.lo 
 fluid/fluid_pnpn.lo : fluid/fluid_pnpn.f90 bc/dong_outflow.lo bc/field_dirichlet_vector.lo bc/blasius.lo bc/field_dirichlet.lo bc/inflow.lo comm/comm.lo common/time_state.lo math/operators.lo io/file.lo bc/bc.lo math/field_math.lo common/utils.lo bc/zero_dirichlet.lo bc/bc_list.lo math/mathops.lo config/neko_config.lo gs/gs_ops.lo common/time_step_controller.lo common/user_intf.lo mesh/mesh.lo common/checkpoint.lo bc/non_normal.lo bc/facet_normal.lo bc/wall_model_bc.lo bc/shear_stress.lo bc/dirichlet.lo field/field.lo math/ax.lo common/json_utils.lo common/profiler.lo fluid/advection.lo device/device.lo common/projection_vel.lo common/projection.lo fluid/fluid_aux.lo math/bcknd/device/device_mathops.lo fluid/fluid_scheme_incompressible.lo fluid/fluid_volflow.lo common/rhs_maker.lo fluid/pnpn_res.lo krylov/krylov.lo config/num_types.lo common/log.lo field/field_registry.lo bc/symmetry.lo sem/coef.lo 
@@ -247,6 +247,8 @@ fluid/bcknd/device/pnpn_res_device.lo : fluid/bcknd/device/pnpn_res_device.F90 f
 fluid/bcknd/cpu/euler_res_cpu.lo : fluid/bcknd/cpu/euler_res_cpu.f90 field/field_list.lo time_schemes/runge_kutta_scheme.lo field/scratch_registry.lo gs/gs_ops.lo math/math.lo math/operators.lo config/num_types.lo gs/gather_scatter.lo sem/coef.lo math/ax.lo field/field.lo fluid/euler_res.lo 
 fluid/bcknd/sx/euler_res_sx.lo : fluid/bcknd/sx/euler_res_sx.f90 field/field_list.lo time_schemes/runge_kutta_scheme.lo field/scratch_registry.lo gs/gs_ops.lo math/math.lo math/operators.lo config/num_types.lo gs/gather_scatter.lo sem/coef.lo math/ax.lo field/field.lo fluid/euler_res.lo 
 fluid/bcknd/device/euler_res_device.lo : fluid/bcknd/device/euler_res_device.F90 math/bcknd/device/device_math.lo field/field_list.lo time_schemes/runge_kutta_scheme.lo math/field_math.lo math/operators.lo common/utils.lo field/scratch_registry.lo config/num_types.lo gs/gather_scatter.lo sem/coef.lo math/ax.lo field/field.lo fluid/euler_res.lo 
+fluid/bcknd/cpu/compressible_ops_cpu.lo : fluid/bcknd/cpu/compressible_ops_cpu.f90 config/num_types.lo 
+fluid/bcknd/device/compressible_ops_device.lo : fluid/bcknd/device/compressible_ops_device.F90 common/utils.lo field/field.lo config/num_types.lo 
 fluid/fluid_source_term.lo : fluid/fluid_source_term.f90 common/user_intf.lo sem/coef.lo field/field_list.lo field/field.lo source_terms/source_term_handler.lo source_terms/source_term.lo source_terms/user_source_term.lo 
 common/time_step_controller.lo : common/time_step_controller.f90 common/time_state.lo common/json_utils.lo common/log.lo config/num_types.lo 
 simulation.lo : simulation.f90 common/time_step_controller.lo common/time_state.lo common/json_utils.lo simulation_components/simcomp_executor.lo common/profiler.lo common/jobctrl.lo common/log.lo io/file.lo time_schemes/time_scheme_controller.lo config/num_types.lo common/checkpoint.lo case.lo 

src/device/opencl/prgm_lib.F90
@@ -68,6 +68,10 @@ module opencl_prgm_lib
   !> Device euler residual kernels
   type(c_ptr), public, bind(c) :: euler_res_program = C_NULL_PTR
 
+  !> Device compressible ops kernels
+  type(c_ptr), public, bind(c) :: compressible_ops_compute_max_wave_speed_program = C_NULL_PTR
+  type(c_ptr), public, bind(c) :: compressible_ops_compute_entropy_program = C_NULL_PTR
+
   !> Device fdm kernels
   type(c_ptr), public, bind(c) :: fdm_program = C_NULL_PTR
 
@@ -232,6 +236,20 @@ contains
           call neko_error('Failed to release program')
        end if
        euler_res_program = C_NULL_PTR
+    end if
+
+    if (c_associated(compressible_ops_compute_max_wave_speed_program)) then
+       if(clReleaseProgram(compressible_ops_compute_max_wave_speed_program) .ne. CL_SUCCESS) then
+          call neko_error('Failed to release program')
+       end if
+       compressible_ops_compute_max_wave_speed_program = C_NULL_PTR
+    end if
+
+    if (c_associated(compressible_ops_compute_entropy_program)) then
+       if(clReleaseProgram(compressible_ops_compute_entropy_program) .ne. CL_SUCCESS) then
+          call neko_error('Failed to release program')
+       end if
+       compressible_ops_compute_entropy_program = C_NULL_PTR
     end if
 
     if (c_associated(fdm_program)) then

src/device/opencl/prgm_lib.h
@@ -65,6 +65,10 @@ extern void *pnpn_res_program;
 /** Device euler residual kernels */
 extern void *euler_res_program;
 
+/** Device compressible ops kernels */
+extern void *compressible_ops_compute_max_wave_speed_program;
+extern void *compressible_ops_compute_entropy_program;
+
 /** Device fdm kernels */
 extern void *fdm_program;
 

commit dd6fc67d9461ccf7354c78cee308ef0c870fa323

fix: Remove duplicate symbols in compressible kernels (#2067)
## Changes

Fix issue #2065 caused by PR #2061

src/.depends_device
@@ -90,7 +90,6 @@ math/bcknd/device/opencl/opr_conv1.lo : math/bcknd/device/opencl/opr_conv1.c mat
 math/bcknd/device/opencl/opr_opgrad.lo : math/bcknd/device/opencl/opr_opgrad.c math/bcknd/device/opencl/opgrad_kernel.cl.h
 math/bcknd/device/opencl/opr_lambda2.lo : math/bcknd/device/opencl/opr_lambda2.c math/bcknd/device/opencl/lambda2_kernel.cl.h
 math/bcknd/device/opencl/opr_cfl.lo : math/bcknd/device/opencl/opr_cfl.c math/bcknd/device/opencl/cfl_kernel.cl.h
-math/bcknd/device/opencl/opr_compute_max_wave_speed.lo : math/bcknd/device/opencl/opr_compute_max_wave_speed.c math/bcknd/device/opencl/compute_max_wave_speed_kernel.cl.h
 math/bcknd/device/opencl/ax_helm.lo : math/bcknd/device/opencl/ax_helm.c math/bcknd/device/opencl/ax_helm_kernel.cl.h
 krylov/bcknd/device/opencl/pc_jacobi.lo : krylov/bcknd/device/opencl/pc_jacobi.c krylov/bcknd/device/opencl/jacobi_kernel.cl.h
 gs/bcknd/device/opencl/gs.lo : gs/bcknd/device/opencl/gs.c gs/bcknd/device/opencl/gs_kernels.cl.h

src/Makefile.am
@@ -409,7 +409,6 @@ libneko_la_SOURCES += \
 	math/bcknd/device/hip/opr_opgrad.hip\
 	math/bcknd/device/hip/opr_lambda2.hip\
 	math/bcknd/device/hip/opr_cfl.hip\
-	math/bcknd/device/hip/opr_compute_max_wave_speed.hip\
 	math/bcknd/device/hip/ax_helm.hip\
 	math/bcknd/device/hip/ax_helm_full.hip\
 	krylov/bcknd/device/hip/pc_jacobi.hip\
@@ -460,7 +459,6 @@ libneko_la_SOURCES += \
 	math/bcknd/device/cuda/opr_opgrad.cu\
 	math/bcknd/device/cuda/opr_lambda2.cu\
 	math/bcknd/device/cuda/opr_cfl.cu\
-	math/bcknd/device/cuda/opr_compute_max_wave_speed.cu\
 	math/bcknd/device/cuda/ax_helm.cu\
 	math/bcknd/device/cuda/ax_helm_full.cu\
 	krylov/bcknd/device/cuda/pc_jacobi.cu\
@@ -518,7 +516,6 @@ libneko_la_SOURCES += \
 	math/bcknd/device/opencl/opr_opgrad.c\
 	math/bcknd/device/opencl/opr_lambda2.c\
 	math/bcknd/device/opencl/opr_cfl.c\
-	math/bcknd/device/opencl/opr_compute_max_wave_speed.c\
 	math/bcknd/device/opencl/ax_helm.c\
 	krylov/bcknd/device/opencl/pc_jacobi.c\
 	gs/bcknd/device/opencl/gs.c\
@@ -736,7 +733,6 @@ EXTRA_DIST = \
 	math/bcknd/device/opencl/conv1_kernel.cl\
 	math/bcknd/device/opencl/lambda2_kernel.cl\
 	math/bcknd/device/opencl/cfl_kernel.cl\
-	math/bcknd/device/opencl/compute_max_wave_speed_kernel.cl\
 	common/bcknd/device/opencl/rhs_maker_kernel.cl\
 	fluid/bcknd/device/opencl/pnpn_res_kernel.cl\
 	fluid/bcknd/device/opencl/euler_res_kernel.cl\

src/math/bcknd/device/cuda/mathops_kernel.h
@@ -194,28 +194,5 @@ __global__ void opadd2col_kernel(T * __restrict__ a1,
 
 }
 
-/**
- * Device kernel for compute_max_wave_speed
- */
-template< typename T>
-__global__ void compute_max_wave_speed_kernel(T * __restrict__ max_wave_speed,
-                                              const T * __restrict__ u,
-                                              const T * __restrict__ v,
-                                              const T * __restrict__ w,
-                                              const T gamma,
-                                              const T * __restrict__ p,
-                                              const T * __restrict__ rho,
-                                              const int n) {
-
-  const int idx = blockIdx.x * blockDim.x + threadIdx.x;
-  const int str = blockDim.x * gridDim.x;
-
-  for (int i = idx; i < n; i += str) {
-    T vel_mag = sqrt(u[i]*u[i] + v[i]*v[i] + w[i]*w[i]);
-    T sound_speed = sqrt(gamma * p[i] / rho[i]);
-    max_wave_speed[i] = vel_mag + sound_speed;
-  }
-
-}
 
 #endif // __MATH_MATHOPS_KERNEL_H__

src/math/bcknd/device/cuda/opr_compute_max_wave_speed.cu
@@ -1,63 +1,0 @@
-/*
- Copyright (c) 2025, The Neko Authors
- All rights reserved.
-
- Redistribution and use in source and binary forms, with or without
- modification, are permitted provided that the following conditions
- are met:
-
-   * Redistributions of source code must retain the above copyright
-     notice, this list of conditions and the following disclaimer.
-
-   * Redistributions in binary form must reproduce the above
-     copyright notice, this list of conditions and the following
-     disclaimer in the documentation and/or other materials provided
-     with the distribution.
-
-   * Neither the name of the authors nor the names of its
-     contributors may be used to endorse or promote products derived
-     from this software without specific prior written permission.
-
- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
- FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
- COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
- INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
- BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
- ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- POSSIBILITY OF SUCH DAMAGE.
-*/
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <cuda.h>
-#include <cuda_runtime.h>
-#include <cublas.h>
-#include <device/device_config.h>
-#include <device/cuda/check.h>
-#include <math/bcknd/device/cuda/mathops_kernel.h>
-
-extern "C" {
-
-void cuda_compute_max_wave_speed(void *max_wave_speed_d, 
-                                 void *u_d, void *v_d, void *w_d,
-                                 real *gamma, void *p_d, void *rho_d, 
-                                 int *n) {
-  
-  const dim3 nthrds(1024, 1, 1);
-  const dim3 nblcks(((*n) + 1024 - 1) / 1024, 1, 1);
-  const cudaStream_t stream = (cudaStream_t) glb_cmd_queue;
-  
-  compute_max_wave_speed_kernel<real>
-    <<<nblcks, nthrds, 0, stream>>>((real *) max_wave_speed_d, 
-                                     (real *) u_d, (real *) v_d, (real *) w_d, 
-                                     *gamma, (real *) p_d, (real *) rho_d, *n);
-  CUDA_CHECK(cudaGetLastError());
-  
-}
-
-} 

src/math/bcknd/device/hip/mathops_kernel.h
@@ -194,28 +194,6 @@ __global__ void opadd2col_kernel(T * __restrict__ a1,
 
 }
 
-/**
- * Device kernel for compute_max_wave_speed
- */
-template< typename T>
-__global__ void compute_max_wave_speed_kernel(T * __restrict__ max_wave_speed,
-                                              const T * __restrict__ u,
-                                              const T * __restrict__ v,
-                                              const T * __restrict__ w,
-                                              const T gamma,
-                                              const T * __restrict__ p,
-                                              const T * __restrict__ rho,
-                                              const int n) {
 
-  const int idx = blockIdx.x * blockDim.x + threadIdx.x;
-  const int str = blockDim.x * gridDim.x;
-
-  for (int i = idx; i < n; i += str) {
-    T vel_mag = sqrt(u[i]*u[i] + v[i]*v[i] + w[i]*w[i]);
-    T sound_speed = sqrt(gamma * p[i] / rho[i]);
-    max_wave_speed[i] = vel_mag + sound_speed;
-  }
-
-}
 
 #endif // __MATH_MATHOPS_KERNEL_H__

src/math/bcknd/device/hip/opr_compute_max_wave_speed.hip
@@ -1,59 +1,0 @@
-/*
- Copyright (c) 2025, The Neko Authors
- All rights reserved.
-
- Redistribution and use in source and binary forms, with or without
- modification, are permitted provided that the following conditions
- are met:
-
-   * Redistributions of source code must retain the above copyright
-     notice, this list of conditions and the following disclaimer.
-
-   * Redistributions in binary form must reproduce the above
-     copyright notice, this list of conditions and the following
-     disclaimer in the documentation and/or other materials provided
-     with the distribution.
-
-   * Neither the name of the authors nor the names of its
-     contributors may be used to endorse or promote products derived
-     from this software without specific prior written permission.
-
- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
- FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
- COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
- INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
- BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
- ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- POSSIBILITY OF SUCH DAMAGE.
-*/
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <hip/hip_runtime.h>
-#include <device/device_config.h>
-#include <device/hip/check.h>
-#include <math/bcknd/device/hip/mathops_kernel.h>
-
-extern "C" {
-
-void hip_compute_max_wave_speed(void *max_wave_speed_d, 
-                                void *u_d, void *v_d, void *w_d,
-                                real *gamma, void *p_d, void *rho_d, 
-                                int *n) {
-  
-  const dim3 nthrds(1024, 1, 1);
-  const dim3 nblcks(((*n) + 1024 - 1) / 1024, 1, 1);
-  
-  hipLaunchKernelGGL(compute_max_wave_speed_kernel<real>, nblcks, nthrds, 0,
-                     (hipStream_t) glb_cmd_queue, (real *) max_wave_speed_d, 
-                     (real *) u_d, (real *) v_d, (real *) w_d, *gamma, (real *) p_d, (real *) rho_d, *n);
-  HIP_CHECK(hipGetLastError());
-  
-}
-
-} 

src/math/bcknd/device/opencl/compute_max_wave_speed_kernel.cl
@@ -1,66 +1,0 @@
-/*
- Copyright (c) 2025, The Neko Authors
- All rights reserved.
-
- Redistribution and use in source and binary forms, with or without
- modification, are permitted provided that the following conditions
- are met:
-
-   * Redistributions of source code must retain the above copyright
-     notice, this list of conditions and the following disclaimer.
-
-   * Redistributions in binary form must reproduce the above
-     copyright notice, this list of conditions and the following
-     disclaimer in the documentation and/or other materials provided
-     with the distribution.
-
-   * Neither the name of the authors nor the names of its
-     contributors may be used to endorse or promote products derived
-     from this software without specific prior written permission.
-
- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
- FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
- COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
- INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
- BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
- ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- POSSIBILITY OF SUCH DAMAGE.
-*/
-
-#ifndef __OPENCL_COMPUTE_MAX_WAVE_SPEED_KERNEL__
-#define __OPENCL_COMPUTE_MAX_WAVE_SPEED_KERNEL__
-
-/**
- * Device kernel for compute_max_wave_speed
- */
-__kernel void compute_max_wave_speed_kernel(__global real * max_wave_speed,
-                                            __global const real * u,
-                                            __global const real * v,
-                                            __global const real * w,
-                                            const real gamma,
-                                            __global const real * p,
-                                            __global const real * rho,
-                                            const int n) {
-
-  const int idx = get_global_id(0);
-  const int str = get_global_size(0);
-
-  for (int i = idx; i < n; i += str) {
-    if (i < n) {
-      real u_sq = u[i] * u[i];
-      real v_sq = v[i] * v[i]; 
-      real w_sq = w[i] * w[i];
-      real vel_mag = sqrt(u_sq + v_sq + w_sq);
-      real sound_speed = sqrt(gamma * p[i] / rho[i]);
-      max_wave_speed[i] = vel_mag + sound_speed;
-    }
-  }
-
-}
-
-#endif // __OPENCL_COMPUTE_MAX_WAVE_SPEED_KERNEL__ 

src/math/bcknd/device/opencl/opr_compute_max_wave_speed.c
@@ -1,74 +1,0 @@
-/*
- Copyright (c) 2025, The Neko Authors
- All rights reserved.
-
- Redistribution and use in source and binary forms, with or without
- modification, are permitted provided that the following conditions
- are met:
-
-   * Redistributions of source code must retain the above copyright
-     notice, this list of conditions and the following disclaimer.
-
-   * Redistributions in binary form must reproduce the above
-     copyright notice, this list of conditions and the following
-     disclaimer in the documentation and/or other materials provided
-     with the distribution.
-
-   * Neither the name of the authors nor the names of its
-     contributors may be used to endorse or promote products derived
-     from this software without specific prior written permission.
-
- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
- FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
- COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
- INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
- BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
- ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- POSSIBILITY OF SUCH DAMAGE.
-*/
-
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-
-#include <device/device_config.h>
-#include <device/opencl/jit.h>
-#include <device/opencl/prgm_lib.h>
-#include <device/opencl/check.h>
-
-#include "compute_max_wave_speed_kernel.cl.h"
-
-void opencl_compute_max_wave_speed(void *max_wave_speed, void *u, void *v, void *w,
-                                   real gamma, void *p, void *rho, int n) {
-
-  cl_int err;
-
-  if (compute_max_wave_speed_program == NULL)
-    opencl_kernel_jit(compute_max_wave_speed_kernel, (cl_program *) &compute_max_wave_speed_program);
-
-  cl_kernel kernel = clCreateKernel((cl_program) compute_max_wave_speed_program, 
-                                    "compute_max_wave_speed_kernel", &err);
-  CL_CHECK(err);
-
-  CL_CHECK(clSetKernelArg(kernel, 0, sizeof(cl_mem), (void *) &max_wave_speed));
-  CL_CHECK(clSetKernelArg(kernel, 1, sizeof(cl_mem), (void *) &u));
-  CL_CHECK(clSetKernelArg(kernel, 2, sizeof(cl_mem), (void *) &v));
-  CL_CHECK(clSetKernelArg(kernel, 3, sizeof(cl_mem), (void *) &w));
-  CL_CHECK(clSetKernelArg(kernel, 4, sizeof(real), (void *) &gamma));
-  CL_CHECK(clSetKernelArg(kernel, 5, sizeof(cl_mem), (void *) &p));
-  CL_CHECK(clSetKernelArg(kernel, 6, sizeof(cl_mem), (void *) &rho));
-  CL_CHECK(clSetKernelArg(kernel, 7, sizeof(int), (void *) &n));
-
-  const int nb = (n + 256 - 1) / 256;
-  const size_t global_item_size = 256 * nb;
-  const size_t local_item_size = 256;
-
-  CL_CHECK(clEnqueueNDRangeKernel((cl_command_queue) glb_cmd_queue, kernel, 1,
-                                  NULL, &global_item_size, &local_item_size,
-                                  0, NULL, NULL));
-} 