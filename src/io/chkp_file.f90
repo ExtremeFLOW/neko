af7c1a232bd0ee3ebfa0633e03a3638b32138ec2
Tim Felle Olsen <tife@dtu.dk>
2025-09-03 17:18:04 +0200

    Add message and limit scope of use statements

--- src/io/chkp_file.f90
+++ src/io/chkp_file.f90
@@ -31,7 +31,7 @@
 ! POSSIBILITY OF SUCH DAMAGE.
 !
 !> Neko checkpoint file format
-!! @details this module defines interface to read/write Neko's ceckpoint files
+!! @details this module defines interface to read/write Neko's checkpoint files
 module chkp_file
   use generic_file, only : generic_file_t
   use field_series, only : field_series_t
@@ -47,8 +47,13 @@ module chkp_file
   use neko_mpi_types, only : MPI_REAL_PREC_SIZE, MPI_INTEGER_SIZE, &
        MPI_DOUBLE_PRECISION_SIZE, MPI_REAL_PREC_SIZE
   use global_interpolation, only : global_interpolation_t
-  use comm
-  use mpi_f08
+  use logger, only : neko_log, NEKO_LOG_VERBOSE
+  use comm, only : NEKO_COMM, pe_rank, MPI_REAL_PRECISION
+  use mpi_f08, only : MPI_File, MPI_Status, MPI_OFFSET_KIND, MPI_MODE_CREATE, &
+       MPI_MODE_RDONLY, MPI_MODE_WRONLY, MPI_INFO_NULL, MPI_INTEGER, &
+       MPI_DOUBLE_PRECISION, MPI_LOGICAL, MPI_SUCCESS, &
+       MPI_File_open, MPI_File_close, MPI_File_read_all, MPI_File_write_all, &
+       MPI_File_read_at_all, MPI_File_write_at_all, MPI_Bcast
   implicit none
   private
 
@@ -387,6 +392,7 @@ contains
     logical :: file_exists
 
     ! If the raw file does not exist, then use the counter and check again
+    file_exists = .false.
     if (pe_rank .eq. 0) inquire(file = this%fname, exist = file_exists)
     call MPI_Bcast(file_exists, 1, MPI_LOGICAL, 0, NEKO_COMM, ierr)
 
@@ -397,7 +403,8 @@ contains
        write(id_str, '(i5.5)') this%counter
        fname = trim(this%fname(1:suffix_pos-1)) // id_str // '.chkp'
 
-       if (pe_rank .eq. 0) inquire(file = this%fname, exist = file_exists)
+       file_exists = .false.
+       if (pe_rank .eq. 0) inquire(file = fname, exist = file_exists)
        call MPI_Bcast(file_exists, 1, MPI_LOGICAL, 0, NEKO_COMM, ierr)
 
        if (.not. file_exists) fname = trim(this%fname)
@@ -476,6 +483,8 @@ contains
        call neko_error('Invalid data')
     end select
 
+    call neko_log%message("Reading checkpoint from file: " // trim(fname), &
+         NEKO_LOG_VERBOSE)
     call MPI_File_open(NEKO_COMM, trim(fname), &
          MPI_MODE_RDONLY, MPI_INFO_NULL, fh, ierr)
     call MPI_File_read_all(fh, glb_nelv, 1, MPI_INTEGER, status, ierr)
