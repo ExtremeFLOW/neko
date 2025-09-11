ExtremeFLOW/neko

commit 21dfe753272aa25dbc98937b869053576bae7012

File names (#2080)
Updates to the filename structure.
Counters are now handled in the generic_file_t while still allowing
filetypes to define their own patterns.

See fld files for an example of how the filenames are still custom to
that file alone.

src/io/output_controller.f90
@@ -179,7 +179,7 @@ contains
     ! The code below only prints to console
     call neko_log%section('Adding write output')
     call neko_log%message('File name        : '// &
-         trim(this%output_list(this%n)%ptr%file_%file_type%fname))
+         trim(this%output_list(this%n)%ptr%file_%file_type%get_fname()))
     call neko_log%message('Write control    : '//trim(write_control))
 
     ! Show the output precision if we are outputting an fld file
@@ -274,7 +274,7 @@ contains
        do i = 1, this%n
           if (this%controllers(i)%check(time, force)) then
              call neko_log%message('File name     : '// &
-                  trim(samp%output_list(i)%ptr%file_%file_type%fname))
+                  trim(samp%output_list(i)%ptr%file_%file_type%get_fname()))
 
              write(log_buf, '(A,I6)') 'Output number :', &
                   int(this%controllers(i)%nexecutions)

src/.depends
@@ -144,7 +144,7 @@ io/file.lo : io/file.f90 io/hdf5_file.lo io/csv_file.lo io/stl_file.lo io/vtk_fi
 io/output.lo : io/output.f90 io/file.lo config/num_types.lo 
 io/fluid_output.lo : io/fluid_output.f90 field/field.lo field/field_registry.lo scalar/scalars.lo io/output.lo device/device.lo config/neko_config.lo field/field_list.lo scalar/scalar_scheme.lo fluid/fluid_scheme_base.lo fluid/fluid_scheme_compressible.lo fluid/fluid_scheme_incompressible.lo config/num_types.lo 
 io/fld_file_output.lo : io/fld_file_output.f90 io/output.lo device/device.lo config/neko_config.lo field/field_list.lo config/num_types.lo 
-io/chkp_output.lo : io/chkp_output.f90 common/utils.lo config/num_types.lo io/output.lo common/checkpoint.lo 
+io/chkp_output.lo : io/chkp_output.f90 config/num_types.lo io/output.lo common/checkpoint.lo 
 io/mean_flow_output.lo : io/mean_flow_output.f90 io/output.lo device/device.lo config/num_types.lo fluid/mean_flow.lo 
 io/fluid_stats_output.lo : io/fluid_stats_output.f90 math/matrix.lo io/output.lo device/device.lo io/fld_file_data.lo sem/map_2d.lo sem/map_1d.lo config/num_types.lo config/neko_config.lo fluid/fluid_stats.lo 
 io/mean_sqr_flow_output.lo : io/mean_sqr_flow_output.f90 io/output.lo config/num_types.lo fluid/mean_sqr_flow.lo 

src/common/utils.f90
@@ -44,7 +44,8 @@ module utils
   end interface neko_error
 
   public :: neko_error, neko_warning, nonlinear_index, filename_chsuffix, &
-       filename_suffix, filename_suffix_pos, filename_tslash_pos, &
+       filename_path, filename_name, filename_suffix, &
+       filename_suffix_pos, filename_tslash_pos, &
        linear_index, split_string, NEKO_FNAME_LEN, index_is_on_facet, &
        concat_string_array, extract_fld_file_index, neko_type_error, &
        neko_type_registration_error
@@ -66,12 +67,73 @@ contains
     tslash_pos = scan(trim(fname), '/', back = .true.)
   end function filename_tslash_pos
 
+  !> Extract the path to a file
+  subroutine filename_path(fname, path)
+    character(len=*), intent(in) :: fname
+    character(len=*), intent(out) :: path
+    integer :: tslash_pos
+
+    tslash_pos = filename_tslash_pos(fname)
+    if (tslash_pos .gt. 0) then
+       path = trim(fname(1:tslash_pos))
+    else
+       path = './'
+    end if
+
+  end subroutine filename_path
+
+  !> Extract the base name of a file (without path and suffix)
+  subroutine filename_name(fname, name)
+    character(len=*), intent(in) :: fname
+    character(len=*), intent(out) :: name
+    integer :: tslash_pos, suffix_pos, start, end
+
+    tslash_pos = filename_tslash_pos(fname)
+    suffix_pos = filename_suffix_pos(fname)
+    if (tslash_pos .eq. 0) then
+       start = 1
+    else
+       start = tslash_pos + 1
+    end if
+    if (suffix_pos .eq. 0) then
+       end = len_trim(fname)
+    else
+       end = suffix_pos - 1
+    end if
+    name = trim(fname(start:end))
+  end subroutine filename_name
+
   !> Extract a filename's suffix
   subroutine filename_suffix(fname, suffix)
     character(len=*) :: fname
     character(len=*) :: suffix
     suffix = trim(fname(filename_suffix_pos(fname) + 1:len_trim(fname)))
   end subroutine filename_suffix
+
+  !> Extract file name components
+  subroutine filename_split(fname, path, name, suffix)
+    character(len=*), intent(in) :: fname
+    character(len=*), intent(out) :: path, name, suffix
+    integer :: tslash_pos, suffix_pos
+
+    tslash_pos = filename_tslash_pos(fname)
+    suffix_pos = filename_suffix_pos(fname)
+
+    if (tslash_pos .gt. 0) then
+       path = trim(fname(1:tslash_pos))
+    else
+       path = './'
+    end if
+
+    if (suffix_pos .gt. 0) then
+       name = trim(fname(tslash_pos + 1:suffix_pos - 1))
+       suffix = trim(fname(suffix_pos:len_trim(fname)))
+    else
+       name = trim(fname(tslash_pos + 1:len_trim(fname)))
+       suffix = ''
+    end if
+
+  end subroutine filename_split
 
   !> Change a filename's suffix
   subroutine filename_chsuffix(fname, new_fname, new_suffix)

src/io/bp_file.F90
@@ -93,7 +93,7 @@ contains
     character(len=132) :: hdr
     character :: rdcode(10)
     character(len=8) :: id_str
-    character(len=1024) :: fname
+    character(len=1024) :: fname, base_fname
     character(len=1024) :: start_field
     integer :: i, j, ierr, n, suffix_pos, tslash_pos
     integer :: lx, ly, lz, lxyz, gdim, glb_nelv, nelv, offset_el
@@ -321,9 +321,10 @@ contains
     ! Adapt filename with counter
     !> @todo write into single file with multiple steps
     !> @todo write into function because of code duplication
-    suffix_pos = filename_suffix_pos(this%fname)
+    base_fname = this%get_base_fname()
+    suffix_pos = filename_suffix_pos(base_fname)
     write(id_str, '(i5.5,a)') this%counter, '.bp'
-    fname = trim(this%fname(1:suffix_pos-1)) // "0." // id_str
+    fname = trim(base_fname(1:suffix_pos-1)) // "0." // id_str
 
     if (.not. adios%valid) then
        !> @todo enable parsing XML filename
@@ -430,13 +431,13 @@ contains
 
     ! Write metadata file
     if (pe_rank .eq. 0) then
-       tslash_pos = filename_tslash_pos(this%fname)
+       tslash_pos = filename_tslash_pos(base_fname)
        write(start_field, "(I5,A7)") this%start_counter, '.adios2'
        open(unit = newunit(file_unit), &
-            file = trim(this%fname(1:suffix_pos - 1)) &
+            file = trim(base_fname(1:suffix_pos - 1)) &
             // trim(adjustl(start_field)), status='replace')
        write(file_unit, fmt = '(A,A,A)') 'filetemplate:         ', &
-            this%fname(tslash_pos+1:suffix_pos-1),'%01d.%05d.bp'
+            base_fname(tslash_pos+1:suffix_pos-1),'%01d.%05d.bp'
        write(file_unit, fmt = '(A,i5)') 'firsttimestep: ', this%start_counter
        write(file_unit, fmt = '(A,i5)') 'numtimesteps: ', &
             (this%counter + 1) - this%start_counter
@@ -456,7 +457,7 @@ contains
     class(*), target, intent(inout) :: data
     character(len=132) :: hdr
     integer :: ierr, suffix_pos, i, j
-    character(len=1024) :: fname, meta_fname, string
+    character(len=1024) :: fname, meta_fname, string, base_fname
     logical :: meta_file, read_mesh, read_velocity, read_pressure
     logical :: read_temp
     character(len=8) :: id_str
@@ -474,8 +475,9 @@ contains
 
     select type (data)
     type is (fld_file_data_t)
-       suffix_pos = filename_suffix_pos(this%fname)
-       meta_fname = trim(this%fname(1:suffix_pos-1))
+       base_fname = this%get_base_fname()
+       suffix_pos = filename_suffix_pos(base_fname)
+       meta_fname = trim(base_fname(1:suffix_pos-1))
        call filename_chsuffix(meta_fname, meta_fname,'adios2')
 
        !> @ todo debug and check if correct strings and filenames are extracted
@@ -516,10 +518,10 @@ contains
           end if
        else
           ! @todo write into function because of code duplication
-          !suffix_pos = filename_suffix_pos(this%fname)
+          !suffix_pos = filename_suffix_pos(base_fname)
           !write(id_str, '(i5.5,a)') this%counter, '.bp'
-          !fname = trim(this%fname(1:suffix_pos-1))//'.'//id_str
-          fname = this%fname
+          !fname = trim(base_fname(1:suffix_pos-1))//'.'//id_str
+          fname = base_fname
        end if
 
        if (.not.adios%valid) then

src/io/chkp_file.f90
@@ -64,7 +64,6 @@ module chkp_file
      type(interpolator_t) :: space_interp !< Interpolation when only changing lx
      type(global_interpolation_t) :: global_interp !< Interpolation for different meshes
      logical :: mesh2mesh !< Flag if previous mesh difers from current.
-     logical, private :: overwrite = .false.
    contains
      procedure :: read => chkp_file_read
      procedure :: read_field => chkp_read_field
@@ -174,16 +173,12 @@ contains
        call neko_error('Invalid data')
     end select
 
-    suffix_pos = filename_suffix_pos(this%fname)
-    if (this%overwrite) then
-       fname = trim(this%fname)
-    else !< Append the counter to the filename
-       write(id_str, '(i5.5)') this%counter
-       fname = trim(this%fname(1:suffix_pos-1)) // id_str // '.chkp'
-    end if
-
     dof_offset = int(msh%offset_el, i8) * int(u%Xh%lx * u%Xh%ly * u%Xh%lz, i8)
     n_glb_dofs = int(u%Xh%lx * u%Xh%ly * u%Xh%lz, i8) * int(msh%glb_nelv, i8)
+
+    ! Retrieve the filename and increment the counter if we are not overwriting
+    if (.not. this%overwrite) call this%increment_counter()
+    fname = trim(this%get_fname())
 
     call MPI_File_open(NEKO_COMM, trim(fname), &
          MPI_MODE_WRONLY + MPI_MODE_CREATE, MPI_INFO_NULL, fh, ierr)
@@ -352,8 +347,6 @@ contains
        call neko_error('Error writing checkpoint file ' // trim(fname))
     end if
 
-    this%counter = this%counter + 1
-
   end subroutine chkp_file_write
 
   !> Load a checkpoint from file
@@ -389,28 +382,8 @@ contains
     real(kind=rp) :: center_x, center_y, center_z
     integer :: i, e
     type(dofmap_t) :: dof
-    logical :: file_exists
 
-    ! If the raw file does not exist, then use the counter and check again
-    file_exists = .false.
-    if (pe_rank .eq. 0) inquire(file = this%fname, exist = file_exists)
-    call MPI_Bcast(file_exists, 1, MPI_LOGICAL, 0, NEKO_COMM, ierr)
-
-    if (file_exists) then
-       fname = trim(this%fname)
-    else
-       suffix_pos = filename_suffix_pos(this%fname)
-       write(id_str, '(i5.5)') this%counter
-       fname = trim(this%fname(1:suffix_pos-1)) // id_str // '.chkp'
-
-       file_exists = .false.
-       if (pe_rank .eq. 0) inquire(file = fname, exist = file_exists)
-       call MPI_Bcast(file_exists, 1, MPI_LOGICAL, 0, NEKO_COMM, ierr)
-
-       if (.not. file_exists) fname = trim(this%fname)
-    end if
-
-    call this%check_exists(fname)
+    call this%check_exists()
 
     select type(data)
     type is (chkp_t)
@@ -483,6 +456,7 @@ contains
        call neko_error('Invalid data')
     end select
 
+    fname = trim(this%get_fname())
     call neko_log%message("Reading checkpoint from file: " // trim(fname), &
          NEKO_LOG_VERBOSE)
     call MPI_File_open(NEKO_COMM, trim(fname), &
@@ -664,7 +638,7 @@ contains
     call MPI_File_close(fh, ierr)
 
     if (ierr .ne. MPI_SUCCESS) then
-       call neko_error('Error reading checkpoint file ' // trim(this%fname))
+       call neko_error('Error reading checkpoint file ' // trim(fname))
     end if
 
     call this%global_interp%free()

src/io/chkp_output.f90
@@ -35,7 +35,6 @@ module chkp_output
   use checkpoint, only : chkp_t
   use output
   use num_types, only : rp
-  use utils, only : filename_suffix_pos
   implicit none
   private
 
@@ -57,7 +56,6 @@ contains
     logical, intent(in), optional :: overwrite
     character(len=1024) :: fname
     character(len=10) :: suffix
-    integer :: suffix_pos
 
     suffix = '.chkp'
     if (present(fmt)) then
@@ -69,10 +67,7 @@ contains
     fname = 'fluid'
     if (present(name)) fname = trim(name)
     if (present(path)) fname = trim(path) // trim(fname)
-
-    ! Add suffix if not present
-    suffix_pos = filename_suffix_pos(fname)
-    if (suffix_pos .eq. 0) fname = trim(fname) // trim(suffix)
+    fname = trim(fname) // trim(suffix)
 
     call this%init_base(fname, overwrite = overwrite)
     this%chkp => chkp

src/io/csv_file.f90
@@ -45,7 +45,6 @@ module csv_file
   type, public, extends(generic_file_t) :: csv_file_t
      character(len=1024) :: header = "" !< Contains header of file.
      logical :: header_is_written = .false. !< Has header already been written?
-     logical :: overwrite = .false. !< Should the file be overwritten?
    contains
      !> Writes data to an output file.
      procedure :: write => csv_file_write
@@ -101,7 +100,7 @@ contains
     ! Write is performed on rank 0
     if (pe_rank .eq. 0) then
 
-       call neko_log%message("Writing to " // trim(this%fname))
+       call neko_log%message("Writing to " // trim(this%get_fname()))
        if (associated(vec)) then
           call csv_file_write_vector(this, vec, t)
        else if (associated(mat)) then
@@ -128,13 +127,15 @@ contains
 
     ! Delete file if overwrite is enabled and header hasn't been written yet
     if (f%overwrite .and. .not. f%header_is_written) then
-       open(unit=999, file=trim(f%fname), status="old", iostat=ierr)
+       open(unit=999, file=trim(f%get_fname()), status="old", iostat=ierr)
        if (ierr == 0) close(999, status="delete")
     end if
 
-    open(file = trim(f%fname), position = "append", iostat = ierr, &
+    open(file = trim(f%get_fname()), position = "append", iostat = ierr, &
          newunit = file_unit)
-    if (ierr .ne. 0) call neko_error("Error while opening " // trim(f%fname))
+    if (ierr .ne. 0) then
+       call neko_error("Error while opening " // trim(f%get_fname()))
+    end if
 
     ! write header if not empty and if not already written
     if (f%header .ne. "" .and. .not. f%header_is_written) then
@@ -166,13 +167,15 @@ contains
 
     ! Delete file if overwrite is enabled and header hasn't been written yet
     if (f%overwrite .and. .not. f%header_is_written) then
-       open(unit=999, file=trim(f%fname), status="old", iostat=ierr)
+       open(unit=999, file=trim(f%get_fname()), status="old", iostat=ierr)
        if (ierr == 0) close(999, status="delete")
     end if
 
-    open(file = trim(f%fname), position = "append", iostat = ierr, &
+    open(file = trim(f%get_fname()), position = "append", iostat = ierr, &
          newunit = file_unit)
-    if (ierr .ne. 0) call neko_error("Error while opening " // trim(f%fname))
+    if (ierr .ne. 0) then
+       call neko_error("Error while opening " // trim(f%get_fname()))
+    end if
 
     ! write header if not empty and if not already written
     if (f%header .ne. "" .and. .not. f%header_is_written) then
@@ -232,7 +235,7 @@ contains
     if (pe_rank .eq. 0) then
 
        call neko_log%newline()
-       call neko_log%message("Reading csv file " // trim(this%fname))
+       call neko_log%message("Reading csv file " // trim(this%get_fname()))
        if (associated(vec)) then
           call csv_file_read_vector(this, vec)
        else if (associated(mat)) then
@@ -258,9 +261,11 @@ contains
 
     n_lines = f%count_lines()
 
-    open(file = trim(f%fname), status = 'old', newunit = file_unit, &
+    open(file = trim(f%get_fname()), status = 'old', newunit = file_unit, &
          iostat = ierr)
-    if (ierr .ne. 0) call neko_error("Error while opening " // trim(f%fname))
+    if (ierr .ne. 0) then
+       call neko_error("Error while opening " // trim(f%get_fname()))
+    end if
 
     ! If there is more than 1 line, assume that means there is a header
     if (n_lines .gt. 1) then
@@ -287,9 +292,11 @@ contains
 
     n_lines = f%count_lines()
 
-    open(file = trim(f%fname), status = 'old', newunit = file_unit, &
+    open(file = trim(f%get_fname()), status = 'old', newunit = file_unit, &
          iostat = ierr)
-    if (ierr .ne. 0) call neko_error("Error while opening " // trim(f%fname))
+    if (ierr .ne. 0) then
+       call neko_error("Error while opening " // trim(f%get_fname()))
+    end if
 
     ! If the number of lines is larger than the number of rows in the
     ! matrix, assume that means there is a header
@@ -327,9 +334,11 @@ contains
 
     call this%check_exists()
 
-    open(file = trim(this%fname), status = 'old', newunit = file_unit, &
+    open(file = trim(this%get_fname()), status = 'old', newunit = file_unit, &
          iostat = ierr)
-    if (ierr .ne. 0) call neko_error("Error while opening " // trim(this%fname))
+    if (ierr .ne. 0) then
+       call neko_error("Error while opening " // trim(this%get_fname()))
+    end if
     rewind(file_unit)
 
     n = 0

src/io/file.f90
@@ -61,6 +61,10 @@ module file
      procedure :: write => file_write
      !> Read @a data from a file.
      procedure :: read => file_read
+     !> Get a file's name.
+     procedure :: get_fname => file_get_fname
+     !> Get a file's base name.
+     procedure :: get_base_fname => file_get_base_fname
      !> Get a file's counter.
      procedure :: get_counter => file_get_counter
      !> Set a file's counter.
@@ -178,6 +182,34 @@ contains
 
   end subroutine file_read
 
+  !> Get a file's name.
+  function file_get_fname(this) result(fname)
+    class(file_t), intent(in) :: this
+    character(len=1024) :: fname
+
+    fname = ""
+
+    select type (ft => this%file_type)
+    class is (generic_file_t)
+       fname = ft%get_fname()
+    end select
+
+  end function file_get_fname
+
+  !> Get a file's base name.
+  function file_get_base_fname(this) result(fname)
+    class(file_t), intent(in) :: this
+    character(len=1024) :: fname
+
+    fname = ""
+
+    select type (ft => this%file_type)
+    class is (generic_file_t)
+       fname = ft%get_base_fname()
+    end select
+
+  end function file_get_base_fname
+
   !> Get a file's counter.
   function file_get_counter(this) result(n)
     class(file_t), intent(inout) :: this
@@ -186,7 +218,7 @@ contains
 
     select type (ft => this%file_type)
     class is (generic_file_t)
-       n = ft%counter
+       n = ft%get_counter()
     end select
 
   end function file_get_counter
@@ -225,7 +257,7 @@ contains
     class is (csv_file_t)
        call ft%set_header(hd)
     class default
-       call filename_suffix(this%file_type%fname, suffix)
+       call filename_suffix(this%file_type%get_fname(), suffix)
        call neko_warning("No set_header defined for " // trim(suffix) // " yet")
     end select
 
@@ -244,7 +276,7 @@ contains
     type is (bp_file_t)
        call ft%set_precision(precision)
     class default
-       call filename_suffix(this%file_type%fname, suffix)
+       call filename_suffix(this%file_type%get_fname(), suffix)
        call neko_warning("No precision strategy defined for " // trim(suffix) &
             // " files")
     end select
@@ -262,7 +294,7 @@ contains
     type is (bp_file_t)
        call ft%set_layout(layout)
     class default
-       call filename_suffix(this%file_type%fname, suffix)
+       call filename_suffix(this%file_type%get_fname(), suffix)
        call neko_warning("No set_layout defined for " // trim(suffix) // " yet")
     end select
 
@@ -275,12 +307,8 @@ contains
     character(len=80) :: suffix
 
     select type (ft => this%file_type)
-    type is (csv_file_t)
+    class is (generic_file_t)
        call ft%set_overwrite(overwrite)
-    class default
-       call filename_suffix(this%file_type%fname, suffix)
-       call neko_warning("No set_overwrite defined for " // trim(suffix) // &
-            " yet")
     end select
   end subroutine file_set_overwrite
 

commit 0a642ffe70f2d7e30912c4cded3bcb73ff333f94

Fix a bug introduced in #2080

src/io/output_controller.f90
@@ -313,8 +313,8 @@ contains
     do i = 1, this%n
        if (this%controllers(i)%nsteps .eq. 0) then
           call this%controllers(i)%set_counter(time)
-          nexecutions =this%controllers(i)%nexecutions
-          call this%output_list(i)%ptr%set_counter(nexecutions)
+          nexecutions = this%controllers(i)%nexecutions
+          call this%output_list(i)%ptr%set_counter(-1)
           call this%output_list(i)%ptr%set_start_counter(nexecutions)
        end if
     end do