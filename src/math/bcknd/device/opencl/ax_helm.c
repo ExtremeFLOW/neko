5b4c03b9b1a903a41818f389b45fe184e475273c
Niclas Jansson <njansson@kth.se>
2025-09-21 22:07:02 +0200

    Add kstep and start of autotune framework

--- src/math/bcknd/device/opencl/ax_helm.c
+++ src/math/bcknd/device/opencl/ax_helm.c
@@ -1,5 +1,5 @@
 /*
- Copyright (c) 2021-2022, The Neko Authors
+ Copyright (c) 2021-2025, The Neko Authors
  All rights reserved.
 
  Redistribution and use in source and binary forms, with or without
@@ -40,14 +40,18 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include <device/device_config.h>
 #include <device/opencl/jit.h>
 #include <device/opencl/prgm_lib.h>
 #include <device/opencl/check.h>
+#include <common/neko_log.h>
 
 #include "ax_helm_kernel.cl.h"
 
-/** 
+int *autotune_ax_helm = NULL;
+
+/**
  * Fortran wrapper for device OpenCL Ax
  */
 void opencl_ax_helm(void *w, void *u, void *dx, void *dy, void *dz,
@@ -56,16 +60,27 @@ void opencl_ax_helm(void *w, void *u, void *dx, void *dy, void *dz,
                     void *g13, void *g23, int *nelv, int *lx) {
 
   cl_int err;
-  
+
   if (ax_helm_program == NULL)
       opencl_kernel_jit(ax_helm_kernel, (cl_program *) &ax_helm_program);
-  
+
   const size_t global_item_size = 256 * (*nelv);
-  const size_t local_item_size = 256;    
+  const size_t local_item_size = 256;
+
+  size_t global_kstep[2];
+  size_t local_kstep[2];
+  local_kstep[0] = (*lx);
+  local_kstep[1] = (*lx);
+  global_kstep[0] = (*nelv) * (*lx);
+  global_kstep[1] = (*lx);
+
+  if (autotune_ax_helm == NULL) {
+    autotune_ax_helm = malloc(13 * sizeof(int));
+    memset(autotune_ax_helm, 0, 13 * sizeof(int));
+  }
 
 #define STR(X) #X
-#define CASE(LX)                                                                \
-  case LX:                                                                      \
+#define CASE_1D(LX)                                                             \
     {                                                                           \
       cl_kernel kernel = clCreateKernel(ax_helm_program,                        \
                                         STR(ax_helm_kernel_lx##LX), &err);      \
@@ -91,8 +106,67 @@ void opencl_ax_helm(void *w, void *u, void *dx, void *dy, void *dz,
                                      kernel, 1, NULL, &global_item_size,        \
                                      &local_item_size, 0, NULL, NULL));         \
                                                                                 \
-    }                                                                           \
-    break
+    }
+
+
+#define CASE_KSTEP(LX)                                                          \
+    {                                                                           \
+      cl_kernel kernel = clCreateKernel(ax_helm_program,                        \
+                                        STR(ax_helm_kernel_kstep_lx##LX), &err);\
+      CL_CHECK(err);                                                            \
+                                                                                \
+      CL_CHECK(clSetKernelArg(kernel, 0, sizeof(cl_mem), (void *) &w));         \
+      CL_CHECK(clSetKernelArg(kernel, 1, sizeof(cl_mem), (void *) &u));         \
+      CL_CHECK(clSetKernelArg(kernel, 2, sizeof(cl_mem), (void *) &dx));        \
+      CL_CHECK(clSetKernelArg(kernel, 3, sizeof(cl_mem), (void *) &dy));        \
+      CL_CHECK(clSetKernelArg(kernel, 4, sizeof(cl_mem), (void *) &dz));        \
+      CL_CHECK(clSetKernelArg(kernel, 5, sizeof(cl_mem), (void *) &h1));        \
+      CL_CHECK(clSetKernelArg(kernel, 6, sizeof(cl_mem), (void *) &g11));       \
+      CL_CHECK(clSetKernelArg(kernel, 7, sizeof(cl_mem), (void *) &g22));       \
+      CL_CHECK(clSetKernelArg(kernel, 8, sizeof(cl_mem), (void *) &g33));       \
+      CL_CHECK(clSetKernelArg(kernel, 9, sizeof(cl_mem), (void *) &g12));       \
+      CL_CHECK(clSetKernelArg(kernel, 10, sizeof(cl_mem), (void *) &g13));      \
+      CL_CHECK(clSetKernelArg(kernel, 11, sizeof(cl_mem), (void *) &g23));      \
+                                                                                \
+     CL_CHECK(clEnqueueNDRangeKernel((cl_command_queue) glb_cmd_queue,          \
+                                     kernel, 2, NULL, global_kstep,             \
+                                     local_kstep, 0, NULL, NULL));              \
+                                                                                \
+    }
+
+
+#define CASE(LX)                                                                \
+  case LX:                                                                      \
+      if(autotune_ax_helm[LX] == 0 ) {                                          \
+        char *env_value = NULL;                                                 \
+        char neko_log_buf[80];                                                  \
+        env_value = getenv("NEKO_AUTOTUNE");                                    \
+                                                                                \
+        sprintf(neko_log_buf, "Autotune Ax helm (lx: %d)", *lx);                \
+        log_section(neko_log_buf);                                              \
+        if(env_value) {                                                         \
+          if( !strcmp(env_value,"1D") ) {                                       \
+            CASE_1D(LX);                                                        \
+            sprintf(neko_log_buf,"Set by env : 1 (1D)");                        \
+            log_message(neko_log_buf);                                          \
+            autotune_ax_helm[LX] = 1;                                           \
+          } else if( !strcmp(env_value,"KSTEP") ) {                             \
+            CASE_KSTEP(LX);                                                     \
+            sprintf(neko_log_buf,"Set by env : 2 (KSTEP)");                     \
+            log_message(neko_log_buf);                                          \
+            autotune_ax_helm[LX] = 2;                                           \
+          } else {                                                              \
+            sprintf(neko_log_buf, "Invalid value set for NEKO_AUTOTUNE");       \
+            log_error(neko_log_buf);                                            \
+          }                                                                     \
+        }                                                                       \
+        log_end_section();                                                      \
+      } else if (autotune_ax_helm[LX] == 1 ) {                                  \
+          CASE_1D(LX);                                                          \
+      } else if (autotune_ax_helm[LX] == 2 ) {                                  \
+          CASE_KSTEP(LX);                                                       \
+      }                                                                         \
+      break
 
   switch(*lx) {
     CASE(2);
@@ -112,4 +186,84 @@ void opencl_ax_helm(void *w, void *u, void *dx, void *dy, void *dz,
       exit(1);
     }
   }
+
+}
+
+/**
+ * Fortran wrapper for device OpenCL Ax (vector version)
+ */
+void opencl_ax_helm_vector(void *au, void *av, void *aw,
+                           void *u, void *v, void *w,
+                           void *dx, void *dy, void *dz,
+                           void *dxt, void *dyt, void *dzt, void *h1,
+                           void *g11, void *g22, void *g33, void *g12,
+                           void *g13, void *g23, int *nelv, int *lx) {
+
+  cl_int err;
+
+  if (ax_helm_program == NULL)
+      opencl_kernel_jit(ax_helm_kernel, (cl_program *) &ax_helm_program);
+
+  size_t * global_kstep = (size_t *) malloc(sizeof(size_t) * 2);
+  size_t * local_kstep = (size_t *) malloc(sizeof(size_t) * 2);
+  local_kstep[0] = (*lx);
+  local_kstep[1] = (*lx);
+  global_kstep[0] = (*nelv) * (*lx);
+  global_kstep[1] = (*lx);
+
+
+#define STR(X) #X
+#define CASE_VECTOR(LX)                                                         \
+  case LX:                                                                      \
+    {                                                                           \
+      cl_kernel kernel =                                                        \
+        clCreateKernel(ax_helm_program,                                         \
+                       STR(ax_helm_kernel_vector_kstep_lx##LX), &err);          \
+      CL_CHECK(err);                                                            \
+                                                                                \
+      CL_CHECK(clSetKernelArg(kernel, 0, sizeof(cl_mem), (void *) &au));        \
+      CL_CHECK(clSetKernelArg(kernel, 1, sizeof(cl_mem), (void *) &av));        \
+      CL_CHECK(clSetKernelArg(kernel, 2, sizeof(cl_mem), (void *) &aw));        \
+      CL_CHECK(clSetKernelArg(kernel, 3, sizeof(cl_mem), (void *) &u));         \
+      CL_CHECK(clSetKernelArg(kernel, 4, sizeof(cl_mem), (void *) &v));         \
+      CL_CHECK(clSetKernelArg(kernel, 5, sizeof(cl_mem), (void *) &w));         \
+      CL_CHECK(clSetKernelArg(kernel, 6, sizeof(cl_mem), (void *) &dx));        \
+      CL_CHECK(clSetKernelArg(kernel, 7, sizeof(cl_mem), (void *) &dy));        \
+      CL_CHECK(clSetKernelArg(kernel, 8, sizeof(cl_mem), (void *) &dz));        \
+      CL_CHECK(clSetKernelArg(kernel, 9, sizeof(cl_mem), (void *) &h1));        \
+      CL_CHECK(clSetKernelArg(kernel, 10, sizeof(cl_mem), (void *) &g11));      \
+      CL_CHECK(clSetKernelArg(kernel, 11, sizeof(cl_mem), (void *) &g22));      \
+      CL_CHECK(clSetKernelArg(kernel, 12, sizeof(cl_mem), (void *) &g33));      \
+      CL_CHECK(clSetKernelArg(kernel, 13, sizeof(cl_mem), (void *) &g12));      \
+      CL_CHECK(clSetKernelArg(kernel, 14, sizeof(cl_mem), (void *) &g13));      \
+      CL_CHECK(clSetKernelArg(kernel, 15, sizeof(cl_mem), (void *) &g23));      \
+                                                                                \
+     CL_CHECK(clEnqueueNDRangeKernel((cl_command_queue) glb_cmd_queue,          \
+                                     kernel, 2, NULL, global_kstep,             \
+                                     local_kstep, 0, NULL, NULL));              \
+                                                                                \
+    }                                                                           \
+    break
+
+  switch(*lx) {
+    CASE_VECTOR(2);
+    CASE_VECTOR(3);
+    CASE_VECTOR(4);
+    CASE_VECTOR(5);
+    CASE_VECTOR(6);
+    CASE_VECTOR(7);
+    CASE_VECTOR(8);
+    CASE_VECTOR(9);
+    CASE_VECTOR(10);
+    CASE_VECTOR(11);
+    CASE_VECTOR(12);
+  default:
+    {
+      fprintf(stderr, __FILE__ ": size not supported: %d\n", *lx);
+      exit(1);
+    }
+  }
+
+  free(global_kstep);
+  free(local_kstep);
 }
