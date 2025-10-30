1be6d5ad343f4c7bf36e968066695c8613d74387
Niclas Jansson <njansson@kth.se>
2025-10-30 17:00:23 +0900

    Please the linter

--- src/math/signed_distance.f90
+++ src/math/signed_distance.f90
@@ -71,10 +71,10 @@ contains
     end if
 
     select type(object)
-      type is (tri_mesh_t)
+    type is (tri_mesh_t)
        call signed_distance_field_tri_mesh(field_data, object, max_dist)
 
-      class default
+    class default
        call neko_error("signed_distance_field: Object type not supported.")
     end select
 
@@ -318,10 +318,10 @@ contains
     real(kind=dp), intent(out), optional :: weighted_sign
 
     select type(element)
-      type is (tri_t)
+    type is (tri_t)
        call element_distance_triangle(element, p, distance, weighted_sign)
 
-      class default
+    class default
        print *, "Error: Element type not supported."
        stop
     end select
