515b90dd0c7e6b22943ba12c5453aa472cfeb8e8
Tim Felle Olsen <tife@dtu.dk>
2025-10-31 10:20:32 +0100

    Align with fluid

--- src/scalar/scalar_aux.f90
+++ src/scalar/scalar_aux.f90
@@ -1,14 +1,14 @@
 !> Auxiliary routines for fluid solvers
 module scalar_aux
-  use logger, only: LOG_SIZE
-  use krylov, only : ksp_monitor_t
-  use time_state, only : time_state_t
-  use utils, only : neko_error, neko_warning
   use, intrinsic :: ieee_arithmetic, only: ieee_is_nan
+  use krylov, only : ksp_monitor_t
+  use logger, only : LOG_SIZE
+  use utils, only : neko_error, neko_warning
+  use time_state, only : time_state_t
   implicit none
   private
 
-  public :: scalar_step_info
+  public :: scalar_step_info, scalar_step_info_reset_stabilized
 
   !> To track if the solver is stabilized
   logical :: stabilized = .false.
@@ -21,9 +21,15 @@ contains
     type(ksp_monitor_t), dimension(:), intent(in) :: ksp_results
     type(time_state_t), intent(in) :: time
     logical, intent(in), optional :: strict_convergence
-    logical :: converged
+    logical :: converged, strict_conv
     character(len=LOG_SIZE) :: log_buf
     integer :: i
+
+    if (present(strict_convergence)) then
+       strict_conv = strict_convergence
+    else
+       strict_conv = .false.
+    end if
 
     ! Do the printing
     call ksp_results(1)%print_header()
@@ -39,24 +45,29 @@ contains
                trim(ksp_results(i)%name))
        end if
 
-       if (present(strict_convergence)) then
-          if (.not. ksp_results(i)%converged) then
-             converged = .false.
-             log_buf = 'Scalar solver did not converge for ' &
-                  // trim(ksp_results(i)%name)
+       if (.not. ksp_results(i)%converged) then
+          converged = .false.
+          log_buf = 'Scalar solver did not converge for ' &
+               // trim(ksp_results(i)%name)
 
-             if (.not. stabilized)then
-                continue
-             else if (strict_convergence) then
-                call neko_error(log_buf)
-             else
-                call neko_warning(log_buf)
-             end if
+          if (.not. stabilized)then
+             continue
+          else if (strict_conv) then
+             call neko_error(log_buf)
+          else
+             call neko_warning(log_buf)
           end if
        end if
     end do
 
+    ! Update stabilized status
     if (.not. stabilized) stabilized = converged
 
   end subroutine scalar_step_info
+
+  !> Resets the stabilized flag to false
+  subroutine scalar_step_info_reset_stabilized()
+    stabilized = .false.
+  end subroutine scalar_step_info_reset_stabilized
+
 end module scalar_aux
