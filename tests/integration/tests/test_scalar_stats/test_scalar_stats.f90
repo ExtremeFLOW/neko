a5e171ae3e9227320a5d8eec5725761b4e7fd2a2
Timofey Mukha <timofey.mukha@protonmail.com>
2025-12-02 16:40:26 +0300

    Update tests

--- tests/integration/tests/test_scalar_stats/test_scalar_stats.f90
+++ tests/integration/tests/test_scalar_stats/test_scalar_stats.f90
@@ -3,18 +3,18 @@ module user
   implicit none
 
 
-  real(kind=rp), parameter :: U0 = 1.0_rp
-  real(kind=rp), parameter :: U1 = 1.0_rp
-  real(kind=rp), parameter :: V0 = 2.0_rp
-  real(kind=rp), parameter :: V1 = 2.0_rp
-  real(kind=rp), parameter :: W0 = 3.0_rp
-  real(kind=rp), parameter :: W1 = 3.0_rp
-  real(kind=rp), parameter :: P0 = 4.0_rp
-  real(kind=rp), parameter :: P1 = 4.0_rp
-  real(kind=rp), parameter :: omega = 1.0_rp
-  real(kind=rp), parameter :: phi = 3.14192_rp / 4.0_rp
-  real(kind=rp), parameter :: psi = 3.14192_rp / 2.0_rp
-  real(kind=rp), parameter :: xi = 3.14192_rp / 3.0_rp
+real(kind=rp), parameter :: U0 = 1.0_rp
+real(kind=rp), parameter :: U1 = 1.0_rp
+real(kind=rp), parameter :: V0 = 2.0_rp
+real(kind=rp), parameter :: V1 = 2.0_rp
+real(kind=rp), parameter :: W0 = 3.0_rp
+real(kind=rp), parameter :: W1 = 3.0_rp
+real(kind=rp), parameter :: S0 = 4.0_rp
+real(kind=rp), parameter :: S1 = 4.0_rp
+real(kind=rp), parameter :: omega = 1.0_rp
+real(kind=rp), parameter :: phi = 3.14192_rp / 4.0_rp
+real(kind=rp), parameter :: psi = 3.14192_rp / 2.0_rp
+real(kind=rp), parameter :: xi = 3.14192_rp / 3.0_rp
 
 contains
   !> Register user defined functions (see user_intf.f90)
@@ -29,35 +29,35 @@ contains
     character(len=*), intent(in) :: scheme_name
     type(field_list_t), intent(inout) :: fields
 
-    type (field_t), pointer :: u, v, w, p
+    type (field_t), pointer :: u, v, w, s
 
     if (scheme_name .eq. 'fluid') then
        u => fields%get("u")
        v => fields%get("v")
        w => fields%get("w")
-       p => fields%get("p")
 
        call field_cfill(u, U0 + U1 * cos(omega * 0.0_rp))
        call field_cfill(v, V0 + V1 * cos(omega * 0.0_rp + phi))
        call field_cfill(w, W0 + W1 * cos(omega * 0.0_rp + psi))
-       call field_cfill(p, P0 + P1 * sin(omega * 0.0_rp + xi))
+    else
+       call field_cfill(s, S0 + S1 * sin(omega * 0.0_rp + xi))
     end if
 
   end subroutine initial_conditions
 
   subroutine compute(time)
     type(time_state_t), intent(in) :: time
-    type (field_t), pointer :: u, v, w, p
+    type (field_t), pointer :: u, v, w, s
     integer :: i
 
     u => neko_field_registry%get_field("u")
     v => neko_field_registry%get_field("v")
     w => neko_field_registry%get_field("w")
-    p => neko_field_registry%get_field("p")
+    s => neko_field_registry%get_field("s")
 
     call field_cfill(u, U0 + U1 * cos(omega * time%t))
     call field_cfill(v, V0 + V1 * cos(omega * time%t + phi))
     call field_cfill(w, W0 + W1 * cos(omega * time%t + psi))
-    call field_cfill(p, P0 + P1 * sin(omega * time%t + xi))
+    call field_cfill(s, S0 + S1 * sin(omega * time%t + xi))
   end subroutine compute
 end module user
--- tests/integration/tests/test_scalar_stats/test_scalar_stats.py
+++ tests/integration/tests/test_scalar_stats/test_scalar_stats.py
@@ -8,46 +8,38 @@ from pysemtools.io.ppymech.neksuite import pynekread
 from mpi4py import MPI
 
 
-def test_fluid_stats(launcher_script, request, log_file, tmp_path):
+def test_scalar_stats(launcher_script, request, log_file, tmp_path):
     """
-    Synthetic test for basic fluid statistics.
+    Synthetic test for basic scalar statistics.
 
-    This module defines simple bounded, periodic fields u(t), v(t), w(t), and p(t)
-    for use in verifying the correctness of a statistics.
-    The fields are constructed so that all first- and second-order raw moments
-    (<u>, <v>, <w>, <p>, <uu>, <vv>, <ww>, <pp>, <uv>, <uw>, <vw>) have known,
-    closed-form analytical values.
-    Note that that the fields have no spatial variation, so they are not
-    suitable for testing the full statistics output, only the basic one.
+    This module defines simple bounded, periodic fields u(t), v(t), w(t), and
+    p(t) for use in verifying the correctness of a statistics. The fields are
+    constructed so that all first- and second-order raw moments (<s>, <us>,
+    <vs>, <ws>, <ss>) have known, closed-form analytical values. Note that that
+    the fields have no spatial variation, so they are not suitable for testing
+    the full statistics output, only the basic one.
 
     All fields use a common base frequency ω and are defined as
 
         u(t) = U0 + U1 * cos(ω t)
         v(t) = V0 + V1 * cos(ω t + φ)
         w(t) = W0 + W1 * cos(ω t + ψ)
-        p(t) = P0 + P1 * sin(ω t + χ)
+        s(t) = S0 + S1 * sin(ω t + χ)
 
     Because each signal is purely harmonic, averaging over an integer number of
     periods yields analytical time-averaged statistics:
 
     First-order statistics:
-        <u> = U0
-        <v> = V0
-        <w> = W0
-        <p> = P0
-
+        <s> = S0
     Second-order diagonal moments:
-        <uu> = U0**2 + 0.5 * U1**2
-        <vv> = V0**2 + 0.5 * V1**2
-        <ww> = W0**2 + 0.5 * W1**2
-        <pp> = P0**2 + 0.5 * P1**2
-
+        <ss> = S0**2 + 0.5 * S1**2
     Cross moments:
-        <uv> = U0*V0 + 0.5 * U1*V1 * cos(φ)
-        <uw> = U0*W0 + 0.5 * U1*W1 * cos(ψ)
-        <vw> = V0*W0 + 0.5 * V1*W1 * cos(φ - ψ)
+        <us> = U0*S0 + 0.5 * U1*S1 * sin(χ)
+        <vs> = V0*S0 + 0.5 * V1*S1 * sin(χ - φ)
+        <ws> = W0*S0 + 0.5 * W1*S1 * sin(χ - ψ)
 
-    The test runs 3 fluid_stats simcomps one with xy averaging and one with just
+
+    The test runs 3 scalar_stats simcomps one with xy averaging and one with just
     x. All outputs are tested against analytical values.
     """
 
@@ -60,7 +52,7 @@ def test_fluid_stats(launcher_script, request, log_file, tmp_path):
     makeneko = get_makeneko()
 
     result = subprocess.run(
-        [makeneko, join("tests", "test_fluid_stats", "test_fluid_stats.f90")],
+        [makeneko, join("tests", "test_scalar_stats", "test_scalar_stats.f90")],
         stdout=subprocess.PIPE,
         stderr=subprocess.STDOUT,
         text=True)
@@ -84,7 +76,7 @@ def test_fluid_stats(launcher_script, request, log_file, tmp_path):
 
     nprocs = configure_nprocs(max_nprocs)
 
-    case_file = join("tests", "test_fluid_stats", "test_fluid_stats.case")
+    case_file = join("tests", "test_scalar_stats", "test_scalar_stats.case")
 
     # Run Neko
     result = run_neko(launcher_script, nprocs, case_file, neko, log_file)
@@ -101,34 +93,30 @@ def test_fluid_stats(launcher_script, request, log_file, tmp_path):
     V1 = 2.0
     W0 = 3.0
     W1 = 3.0
-    P0 = 4.0
-    P1 = 4.0
+    S0 = 4.0
+    S1 = 4.0
     omega = 4.0
     phi = 3.14192 / 4.0
     psi = 3.14192 / 2.0
     xi = 3.14192 / 3.0
 
-    correct = np.array([P0, U0, V0, W0,
-        P0**2 + 0.5 * P1**2,
-        U0**2 + 0.5 * U1**2,
-        V0**2 + 0.5 * V1**2,
-        W0**2 + 0.5 * W1**2,
-        U0 * V0 + 0.5 * U1 * V1 * np.cos(phi),
-        U0 * W0 + 0.5 * U1 * W1 * np.cos(psi),
-        V0 * W0 + 0.5 * V1 * W1 * np.cos(phi - psi)
+    correct = np.array([
+        S0,
+        U0 * S0 + 0.5 * U1 * S1 * np.sin(xi),
+        V0 * S0 + 0.5 * V1 * S1 * np.sin(xi - phi),
+        W0 * S0 + 0.5 * W1 * S1 * np.sin(xi - psi),
+        S0**2 + 0.5 * S1**2,
     ])
-
 
     #
     # 1D statistics output
     #
 
-    csv = np.genfromtxt(join("tests", "test_fluid_stats", "fluid_stats0.csv"),
+
+    csv = np.genfromtxt(join("tests", "test_scalar_stats", "scalar_stats0.csv"),
                         delimiter=",")[12, 2:]
 
-    quants = ["<p>", "<u>", "<v>", "<w>",
-              "<p^2>", "<u^2>", "<v^2>", "<w^2>",
-              "<uv>", "<uw>", "<vw>"]
+    quants = ["<s>", "<us>", "<vs>", "<ws>", "<s^2>"]
 
     for i, q in enumerate(quants):
         error = (csv[i] - correct[i]) / correct[i]
@@ -144,12 +132,11 @@ def test_fluid_stats(launcher_script, request, log_file, tmp_path):
 
     mesh = Mesh(comm, create_connectivity=True)
     fld = FieldRegistry(comm)
-    pynekread(join("tests", "test_fluid_stats", "stats2d0.f00001"), comm,
+    pynekread(join("tests", "test_scalar_stats", "stats2d0.f00001"), comm,
               data_dtype=np.single, msh=mesh, fld=fld)
 
     # Ordering of keys corresponds to 'correct' array above
-    ordered_keys = ["p", "u", "v", "s6", "t",
-        "s0", "s1", "s2", "s3", "s4", "s5"]
+    ordered_keys = ["p", "u", "v", "s0", "t"]
 
     # We check at an essentially random node, the should all have the same
     # values
@@ -166,12 +153,11 @@ def test_fluid_stats(launcher_script, request, log_file, tmp_path):
 
     mesh = Mesh(comm, create_connectivity=True)
     fld = FieldRegistry(comm)
-    pynekread(join("tests", "test_fluid_stats", "stats3d0.f00001"), comm,
+    pynekread(join("tests", "test_scalar_stats", "stats3d0.f00001"), comm,
               data_dtype=np.single, msh=mesh, fld=fld)
 
     # Ordering of keys corresponds to 'correct' array above
-    ordered_keys = ["p", "u", "v", "w", "t",
-        "s0", "s1", "s2", "s3", "s4", "s5"]
+    ordered_keys = ["p", "u", "v", "w", "t"]
 
     # We check at an essentially random node, the should all have the same
     # values
