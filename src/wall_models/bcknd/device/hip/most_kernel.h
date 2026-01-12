e5810caa9e14ece03741d3c2ca53eaa5dfb4c3e3
Lorenzo Donati <lorenzoluca.donati@gmail.com>
2026-01-12 15:05:42 +0100

    changed 'most_convective' to 'most'

--- src/wall_models/bcknd/device/hip/most_kernel.h
+++ src/wall_models/bcknd/device/hip/most_kernel.h
@@ -1,5 +1,5 @@
-#ifndef MOST_CONVECTIVE_KERNEL_H
-#define MOST_CONVECTIVE_KERNEL_H
+#ifndef MOST_KERNEL_H
+#define MOST_KERNEL_H
 
 #include <cmath>
 #include <algorithm>
@@ -12,7 +12,7 @@
  * 
  */
 template<typename T>
-__global__ void MOST_CONVECTIVE_compute(const T* __restrict__ u_d,
+__global__ void most_compute(const T* __restrict__ u_d,
                                       const T* __restrict__ v_d,
                                       const T* __restrict__ w_d,
                                       const T* __restrict__ temp_d,
@@ -73,4 +73,4 @@ __global__ void MOST_CONVECTIVE_compute(const T* __restrict__ u_d,
     }
 }
 
-#endif // MOST_CONVECTIVE_KERNEL_H
+#endif // MOST_KERNEL_H
