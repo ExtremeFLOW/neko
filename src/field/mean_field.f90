4d38b666800399ec83ad8c71a8b0397cb3cf561e
Victor Baconnet <baconnet@kth.se>
2026-01-27 15:43:02 +0100

    linter

--- src/field/mean_field.f90
+++ src/field/mean_field.f90
@@ -84,7 +84,7 @@ contains
        write(name, '(A,A)') 'mean_', trim(f%name)
     end if
 
-    call neko_registry%add_field(f%dof, name) 
+    call neko_registry%add_field(f%dof, name)
     this%mf => neko_registry%get_field(name)
 
   end subroutine mean_field_init
