e9a4edd991ad7dee3abf6e73eac74c41cc249814
Linnea Huusko <linnea.huusko@misu.su.se>
2026-01-28 12:02:55 +0100

    Make indices of `aij` terms consistent

--- src/les/bcknd/cpu/vreman_cpu.f90
+++ src/les/bcknd/cpu/vreman_cpu.f90
@@ -184,11 +184,11 @@ contains
 
                     ! Shear component (denominator in Ri definition)
                     ! Directional derivative of velocity
-                    du_n(1) = a11%x(i,1,1,e)*n(1) + a21%x(i,1,1,e)*n(2) +&
-                            a31%x(i,1,1,e)*n(3)
-                    du_n(2) = a12%x(i,1,1,e)*n(1) + a22%x(i,1,1,e)*n(2) +&
-                            a32%x(i,1,1,e)*n(3)
-                    du_n(3) = a13%x(i,1,1,e)*n(1) + a23%x(i,1,1,e)*n(2) +&
+                    du_n(1) = a11%x(i,1,1,e)*n(1) + a12%x(i,1,1,e)*n(2) +&
+                            a13%x(i,1,1,e)*n(3)
+                    du_n(2) = a21%x(i,1,1,e)*n(1) + a22%x(i,1,1,e)*n(2) +&
+                            a23%x(i,1,1,e)*n(3)
+                    du_n(3) = a31%x(i,1,1,e)*n(1) + a32%x(i,1,1,e)*n(2) +&
                             a33%x(i,1,1,e)*n(3)
 
                     ! Component parallel to n
