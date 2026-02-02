471e63fe6994e6b55e8c6e1467a14a00b6e400df
Adalberto <adalberto0524@gmail.com>
2026-02-02 11:29:25 +0100

    Ran formatter

--- src/io/fluid_output.f90
+++ src/io/fluid_output.f90
@@ -195,8 +195,8 @@ contains
 
     end if
 
-    select type (ft => this%file_%file_type) 
-    ! Only fld files have the option to write the mesh at command
+    select type (ft => this%file_%file_type)
+       ! Only fld files have the option to write the mesh at command
     type is (fld_file_t)
        ft%write_mesh = this%always_write_mesh
        call ft%write(this%fluid, t)
