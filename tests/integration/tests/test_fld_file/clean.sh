70394d8d142deda0ee0e1e6b7e15610ef99a8416
Timofey Mukha <timofey.mukha@protonmail.com>
2026-02-04 18:02:39 +0300

    Add test

---
+++ tests/integration/tests/test_fld_file/clean.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+set -eu
+
+rm -f -- \
+  test0.f00000 \
+  test0.nek5000 \
+  *.log \
+  test_fld_file_*.json
--- tests/integration/tests/test_fld_file/test_fld_file.f90
+++ tests/integration/tests/test_fld_file/test_fld_file.f90
@@ -2,12 +2,29 @@ module user
   use neko
   implicit none
 
+  logical :: skip_pressure = .false.
+  logical :: skip_temperature = .false.
+  logical :: skip_velocity = .false.
+  logical :: is_list = .false.
+  integer :: n_items = 0
+
 contains
   !> Register user defined functions (see user_intf.f90)
   subroutine user_setup(user)
     type(user_t), intent(inout) :: user
     user%compute => compute
+    user%startup => startup
   end subroutine user_setup
+
+  subroutine startup(params)
+    type(json_file), intent(inout) :: params
+
+    call json_get(params, "skip_pressure", skip_pressure)
+    call json_get(params, "skip_temperature", skip_temperature)
+    call json_get(params, "skip_velocity", skip_velocity)
+    call json_get(params, "is_list", is_list)
+    call json_get(params, "n_items", n_items)
+  end subroutine startup
 
   subroutine compute(time)
     type(time_state_t), intent(in) :: time
@@ -17,18 +34,26 @@ contains
     type(file_t) :: file
 
     u => neko_registry%get_field("u")
-    v => neko_registry%get_field("v")
-    w => neko_registry%get_field("w")
 
-    call list%init(3)
-    call list%assign(1, u)
-    call list%assign(2, v)
-    call list%assign(3, w)
+    call list%init(n_items)
+    do i = 1 , n_items
+       call list%assign(i, u)
+    end do
 
-    call file%init("test.fld")
+    call file%init("./tests/test_fld_file/test.fld")
 
-    call file%write(list)
+    select type (ft => file%file_type)
+    type is (fld_file_t)
+       ft%skip_pressure = skip_pressure
+       ft%skip_temperature = skip_temperature
+       ft%skip_velocity = skip_velocity
+    end select
+
+    if (is_list) then
+       call file%write(list)
+    else
+       call file%write(u)
+    end if
     call file%free()
-
   end subroutine compute
 end module user
