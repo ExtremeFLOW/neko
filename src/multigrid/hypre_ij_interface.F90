0d5abe705ff605a7b6a8119ac853daa8251bc274
pohm01 <peter.ohm@riken.jp>
2026-02-13 01:29:24 +0100

    more ij interface

--- src/multigrid/hypre_ij_interface.F90
+++ src/multigrid/hypre_ij_interface.F90
@@ -159,4 +159,114 @@ module hypre_ij_interface
      end function HYPRE_IJVectorGetObject
   end interface
 
+contains
+
+  !-----------------------------------------------------------------------------
+  ! Matrix
+  !-----------------------------------------------------------------------------
+
+  !> Initialize a hypre matrix
+  !! Each processor knows only of its own rows
+  !! - the range is denoted by ijlower and ijupper.
+  subroutine hypre_matrix_init(mpi_comm, ilower, iupper, jlower, jupper, A)
+     integer, intent(in) :: mpi_comm, ilower, iupper, jlower, jupper
+     type(c_ptr), intent(inout) :: A
+     integer :: ierr
+     !TODO: may need 1->0 shift on indexing
+     ! Create the matrix object
+     ierr = HYPRE_IJMatrixCreate(mpi_comm, ilower, iupper, jlower, jupper, A)
+     ! Choose a parallel csr format storage (see hypre's user manual)
+     ierr = HYPRE_IJ_MatrixSetObjectType(A, HYPRE_PARCSR)
+     ! Initialize before setting coefficients
+     ierr = HYPRE_IJMatrixInitialize(A)
+  end subroutine hypre_matrix_init
+
+  subroutine hypre_matrix_fill(A, nrows, ncols, rows, cols, values)
+     type(c_ptr), intent(in) :: A
+     integer, intent(in) :: nrows, ncols(:)
+     integer, intent(in) :: rows(:), cols(:)
+     double precision, intent(in) :: values(:)
+     integer :: ierr
+     ! Fill the matrix with values.
+     ierr = HYPRE_IJMatrixSetValues(A, nrows, c_loc(ncols), &
+         c_loc(rows), c_loc(cols), c_loc(values))
+  end subroutine hypre_matrix_fill
+
+  subroutine hypre_matrix_assemble(A, parcsr_A)
+     type(c_ptr), intent(in) :: A
+     type(c_ptr), intent(inout) :: parcsr_A
+     integer :: ierr
+     ! Assemble after setting the coefficients
+     ierr = HYPRE_IJMatrixAssemble(A)
+     ! Get parcsr matrix pointer (for solver use)
+     ierr = HYPRE_IJMatrixGetObject(A, parcsr_A)
+  end subroutine hypre_matrix_assemble
+
+  subroutine hypre_matrix_destroy(A)
+     type(c_ptr), intent(inout) :: A
+     integer :: ierr
+     ierr = HYPRE_IJMatrixDestroy(A)
+  end subroutine hypre_matrix_destroy
+
+  !-----------------------------------------------------------------------------
+  ! Vector
+  !-----------------------------------------------------------------------------
+
+  !> Interface to initialize a vector for hypre
+  !! mpi_comm
+  !! jlower
+  !! jupper
+  !! v
+  subroutine hypre_vector_init(mpi_comm, jlower, jupper, v)
+    integer, intent(in) :: mpi_comm, jlower, jupper
+    type(c_ptr), intent(inout) :: v
+    integer :: ierr
+    !TODO: indexing may need 1->0 shift
+    ! Create the vector
+    ierr = HYPRE_IJVectorCreate(mpi_comm, jlower, jupper, v)
+    ierr = HYPRE_IJVectorSetObjectType(v, HYPRE_PARCSR)
+    ierr = HYPRE_IJVectorInitialize(v)
+  end subroutine hypre_vector_init
+
+  subroutine hypre_vector_fill(v, nvalues, indices, values)
+    type(c_ptr), intent(in) :: v
+    integer, intent(in) :: nvalues
+    integer, intent(in) :: indices(:)
+    double precision, intent(in) :: values(:)
+    integer :: ierr
+    ! Set vector values
+    ierr = HYPRE_IJVectorSetValues(v, nvalues, c_loc(indices), c_loc(values))
+  end subroutine hypre_vector_fill
+
+  subroutine hypre_vector_assemble(v, par_v)
+    type(c_ptr), intent(in) :: v
+    type(c_ptr), intent(inout) :: par_v
+    integer :: ierr
+    ! Finalize and assemble vector object
+    ierr = HYPRE_IJVectorAssemble(v)
+    ierr = HYPRE_IJVectorGetObject(v, par_v)
+  end subroutine hypre_vector_assemble
+
+  subroutine copy_to_vector(v, par_v, nvalues, indices, values)
+    type(c_ptr), intent(in) :: v
+    type(c_ptr), intent(inout) :: par_v
+    integer, intent(in) :: nvalues
+    integer, intent(in) :: indices(:)
+    double precision, intent(in) :: values(:)
+    integer :: ierr
+    ! re-initialize vector
+    ierr = HYPRE_IJVectorInitialize(v)
+    ! Set vector values
+    ierr = HYPRE_IJVectorSetValues(v, nvalues, c_loc(indices), c_loc(values))
+    ! Finalize and assemble vector object
+    ierr = HYPRE_IJVectorAssemble(v)
+    ierr = HYPRE_IJVectorGetObject(v, par_v)
+  end subroutine copy_to_vector
+
+  subroutine hypre_vector_destroy(v)
+     type(c_ptr), intent(inout) :: v
+     integer :: ierr
+     ierr = HYPRE_IJVectorDestroy(A)
+  end subroutine hypre_vector_destroy
+
 end module hypre_ij_interface
