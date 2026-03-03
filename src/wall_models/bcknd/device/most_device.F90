89a9b485f09ac6cb9a781226ef1fa4a79f694efc
Lorenzo Donati <lorenzoluca.donati@gmail.com>
2026-03-03 14:09:29 +0100

    fixed handling of strings in C/Fortran

--- src/wall_models/bcknd/device/most_device.F90
+++ src/wall_models/bcknd/device/most_device.F90
@@ -59,6 +59,7 @@ contains
        ind_r_d, ind_s_d, ind_t_d, ind_e_d, &
        n_x_d, n_y_d, n_z_d, h_d, tau_x_d, tau_y_d, tau_z_d, &
        n_nodes, lx, kappa, z0, z0h_in, bc_type, bc_value, tstep)
+    use, intrinsic :: iso_c_binding, only : c_loc ! needed for C-string handling
     integer, intent(in) :: n_nodes, lx, tstep
     type(c_ptr), intent(in) :: u_d, v_d, w_d, temp_d
     type(c_ptr), intent(in) :: ind_r_d, ind_s_d, ind_t_d, ind_e_d
@@ -69,7 +70,7 @@ contains
     character(len=*), intent(in) :: bc_type    ! passed as a normal Fortran string 
 
     ! bc_type must be C-compatible string
-    character(kind=c_char, len=len_trim(bc_type)+1) :: bc_type_c
+    character(kind=c_char, len=len_trim(bc_type)+1), target :: bc_type_c
     ! add null terminator for C/C++ compatibility
     bc_type_c = trim(bc_type) // c_null_char
 
@@ -78,13 +79,13 @@ contains
          ind_r_d, ind_s_d, ind_t_d, ind_e_d, &
          n_x_d, n_y_d, n_z_d, h_d, &
          tau_x_d, tau_y_d, tau_z_d, n_nodes, &
-         lx, kappa, z0, z0h_in, bc_type_c, bc_value, tstep)
+         lx, kappa, z0, z0h_in, c_loc(bc_type_c), bc_value, tstep)
 #elif HAVE_CUDA
     call cuda_most_compute(u_d, v_d, w_d,temp_d, &
          ind_r_d, ind_s_d, ind_t_d, ind_e_d, &
          n_x_d, n_y_d, n_z_d, h_d, &
          tau_x_d, tau_y_d, tau_z_d, n_nodes, &
-         lx, kappa, z0, z0h_in, bc_type_c, bc_value, tstep)
+         lx, kappa, z0, z0h_in, c_loc(bc_type_c), bc_value, tstep)
 #elif HAVE_OPENCL
     call neko_error("OPENCL is not implemented for the MOST wall model")
 #else
