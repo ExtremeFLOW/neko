2465ffded0938ca297e8e63712f822aa93e2353b
Tim Felle Olsen <tife@dtu.dk>
2026-03-17 15:36:06 +0100

    Move utilities to VTK file

--- src/io/vtkhdf_file.F90
+++ src/io/vtkhdf_file.F90
@@ -48,6 +48,7 @@ module vtkhdf_file
   use mpi_f08, only : MPI_INFO_NULL, MPI_Allreduce, MPI_Allgather, &
        MPI_IN_PLACE, MPI_INTEGER, MPI_SUM, MPI_MAX, MPI_Comm_size, MPI_Exscan, &
        MPI_Barrier
+  use vtk, only: vtk_ordering
 #ifdef HAVE_HDF5
   use hdf5
 #endif
@@ -307,16 +308,12 @@ contains
     idx = 0
 
     select case (vtk_type)
-    case (12) ! VTK_HEXAHEDRON
-       ! No special ordering needed for linear hexes
     case (9) ! VTK_QUAD
        ! No special ordering needed for linear quads
-    case (72) ! VTK_LAGRANGE_HEXAHEDRON
-       node_order = vtk_lagrange_hex_ordering(lx, ly, lz)
-    case (70) ! VTK_LAGRANGE_QUADRILATERAL
-       node_order = vtk_lagrange_quad_ordering(lx, ly)
-    case default
-       call neko_error('Unsupported VTK cell type')
+    case (12) ! VTK_HEXAHEDRON
+       ! No special ordering needed for linear hexes
+    case default ! Standard VTK node types
+       node_order = vtk_ordering(vtk_type, lx, ly, lz)
     end select
 
     do ie = 1, nelv
@@ -1327,156 +1324,5 @@ contains
   end subroutine vtkhdf_file_read
 
 #endif
-
-  ! -------------------------------------------------------------------------- !
-  ! Pure functions for VTK Lagrange node ordering
-  ! These are HDF5-independent and placed outside the preprocessor block.
-
-  !> Build the VTK Lagrange hexahedron node ordering for a given lx.
-  !! Returns an array of size lx*ly*lz mapping VTK node position to the
-  !! 0-based tensor-product index (i + lx*j + lx*ly*k).
-  !! Implements VTK's PointIndexFromIJK for Lagrange hexahedra.
-  !! Node ordering: 8 corners, 4 * (lx - 2 + ly - 2 + lz - 2) edge interiors,
-  !! 6*(lx - 2)*(ly - 2) face interiors, (lx - 2)*(ly - 2)*(lz - 2) body
-  !! interior.
-  !! @param lx Number of points per edge in x-direction (polynomial order + 1)
-  !! @param ly Number of points per edge in y-direction (polynomial order + 1)
-  !! @param lz Number of points per edge in z-direction (polynomial order + 1)
-  !! @return Array of 0-based tensor-product indices in VTK order
-  pure function vtk_lagrange_hex_ordering(lx, ly, lz) result(ordering)
-    integer, intent(in) :: lx, ly, lz
-    integer, allocatable :: ordering(:)
-    integer :: i, j, k, vtk_idx
-    integer :: ibdy, jbdy, kbdy, nbdy, offset
-    integer :: n_corners, n_edges, n_faces, n_interior, n_total
-
-    n_corners = 8
-    n_edges = 4 * ((lx - 2) + (ly - 2) + (lz - 2))
-    n_faces = 2 * ((lx - 2) * (ly - 2) + (lx - 2) * (lz - 2) + (ly - 2) * (lz - 2))
-    n_interior = (lx - 2) * (ly - 2) * (lz - 2)
-    n_total = n_corners + n_edges + n_faces + n_interior
-    allocate(ordering(lx * ly * lz))
-
-    do concurrent (i = 1:lx, j = 1:ly, k = 1:lz)
-       ibdy = merge(1, 0, i .eq. 1 .or. i .eq. lx)
-       jbdy = merge(1, 0, j .eq. 1 .or. j .eq. ly)
-       kbdy = merge(1, 0, k .eq. 1 .or. k .eq. lz)
-       nbdy = ibdy + jbdy + kbdy
-
-       if (nbdy .eq. 3) then
-          ! Corner node
-          vtk_idx = merge( &
-               merge(2, 1, j .ne. 1), &
-               merge(3, 0, j .ne. 1), i .ne. 1) &
-               + merge(4, 0, k .ne. 1)
-
-       else if (nbdy .eq. 2) then
-          ! Edge interior node
-          offset = n_corners
-          if (ibdy .eq. 0) then
-             vtk_idx = (i - 2) &
-                  + merge((lx - 2) + (ly - 2), 0, j .ne. 1) &
-                  + merge(2 * ((lx - 2) + (ly - 2)), 0, k .ne. 1) &
-                  + offset
-          else if (jbdy .eq. 0) then
-             vtk_idx = (j - 2) &
-                  + merge(lx - 2, 2 * (lx - 2) + (ly - 2), i .ne. 1) &
-                  + merge(2 * ((lx - 2) + (ly - 2)), 0, k .ne. 1) &
-                  + offset
-          else
-             vtk_idx = (k - 2) + (lz - 2) &
-                  * merge(merge(2, 1, j .ne. 1), &
-                  merge(3, 0, j .ne. 1), i .ne. 1) &
-                  + offset + 4 * ((lx - 2) + (ly - 2))
-          end if
-
-       else if (nbdy .eq. 1) then
-          ! Face interior node
-          offset = n_corners + n_edges
-          if (ibdy .eq. 1) then
-             vtk_idx = (j - 2) + (ly - 2) * (k - 2) &
-                  + merge((ly - 2) * (lz - 2), 0, i .ne. 1) &
-                  + offset
-          else if (jbdy .eq. 1) then
-             offset = offset + 2 * (ly - 2) * (lz - 2)
-             vtk_idx = (i - 2) + (lx - 2) * (k - 2) &
-                  + merge((lx - 2) * (lz - 2), 0, j .ne. 1) &
-                  + offset
-          else if (kbdy .eq. 1) then
-             offset = offset + 2 * (ly - 2) * (lz - 2) + 2 * (lx - 2) * (lz - 2)
-             vtk_idx = (i - 2) + (lx - 2) * (j - 2) &
-                  + merge((lx - 2) * (ly - 2), 0, k .ne. 1) &
-                  + offset
-          end if
-
-       else
-          ! Body interior node
-          offset = n_corners + n_edges + n_faces
-          vtk_idx = offset &
-               + (i - 2) + (lx - 2) * ((j - 2) + (ly - 2) * (k - 2))
-       end if
-
-       ! ordering(vtk_position + 1) = tensor-product index
-       ordering(vtk_idx + 1) = linear_index(i, j, k, 1, lx, ly, lz) - 1
-    end do
-
-  end function vtk_lagrange_hex_ordering
-
-  !> Build the VTK Lagrange quadrilateral node ordering for a given lx, ly.
-  !! Returns an array of size lx*ly mapping VTK node position to the
-  !! 0-based tensor-product index (i + lx*j).
-  !! Implements VTK's PointIndexFromIJK for Lagrange quadrilaterals.
-  !! Node ordering: 4 corners, 2*(lx-2) + 2*(ly-2) edge interiors,
-  !! (lx-2)*(ly-2) face interior.
-  !! @param lx Number of points per edge in x-direction (polynomial order + 1)
-  !! @param ly Number of points per edge in y-direction (polynomial order + 1)
-  !! @return Array of 0-based tensor-product indices in VTK order
-  pure function vtk_lagrange_quad_ordering(lx, ly) result(ordering)
-    integer, intent(in) :: lx, ly
-    integer, allocatable :: ordering(:)
-    integer :: i, j, vtk_idx
-    integer :: ibdy, jbdy, nbdy, offset
-    integer :: n_corners, n_edges, n_faces, n_total
-
-    n_corners = 4
-    n_edges = 2 * ((lx - 2) + (ly - 2))
-    n_faces = (lx - 2) * (ly - 2)
-    n_total = n_corners + n_edges + n_faces
-    allocate(ordering(lx * ly))
-
-    do concurrent (i = 1:lx, j = 1:ly)
-       ibdy = merge(1, 0, i .eq. 1 .or. i .eq. lx)
-       jbdy = merge(1, 0, j .eq. 1 .or. j .eq. ly)
-       nbdy = ibdy + jbdy
-
-       if (nbdy .eq. 2) then
-          ! Corner node
-          vtk_idx = merge( &
-               merge(2, 1, j .ne. 1), &
-               merge(3, 0, j .ne. 1), i .ne. 1)
-
-       else if (nbdy .eq. 1) then
-          ! Edge interior node
-          offset = n_corners
-          if (ibdy .eq. 0) then
-             vtk_idx = (i - 2) &
-                  + merge((lx - 2) + (ly - 2), 0, j .ne. 1) &
-                  + offset
-          else
-             vtk_idx = (j - 2) &
-                  + merge(lx - 2, 2 * (lx - 2) + (ly - 2), i .ne. 1) &
-                  + offset
-          end if
-
-       else
-          ! Face interior node
-          offset = n_corners + n_edges
-          vtk_idx = offset + (i - 2) + (lx - 2) * (j - 2)
-       end if
-
-       ordering(vtk_idx + 1) = linear_index(i, j, 1, 1, lx, ly, 1) - 1
-    end do
-
-  end function vtk_lagrange_quad_ordering
 
 end module vtkhdf_file
