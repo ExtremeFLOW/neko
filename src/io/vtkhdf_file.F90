d77bf09e8d62fd79b4785f3f3494e40c1c8a8d35
Tim Felle Olsen <tife@dtu.dk>
2026-03-25 11:45:42 +0100

    Missing import

--- src/io/vtkhdf_file.F90
+++ src/io/vtkhdf_file.F90
@@ -50,18 +50,22 @@ module vtkhdf_file
        MPI_Barrier
   use vtk, only : vtk_ordering
 #ifdef HAVE_HDF5
-   use hdf5, only : &
-      h5open_f, h5close_f, &
-      h5fcreate_f, h5fopen_f, h5fclose_f, &
-      h5gcreate_f, h5gopen_f, h5gclose_f, &
-      h5acreate_f, h5awrite_f, h5aclose_f, &
-      h5dcreate_f, h5dopen_f, h5dwrite_f, h5dclose_f, &
-      h5screate_f, h5screate_simple_f, h5sclose_f, h5sselect_hyperslab_f, &
-      h5pcreate_f, h5pclose_f, h5pset_fapl_mpio_f, h5pset_dxpl_mpio_f, &
-      h5lexists_f, h5tset_strpad_f, h5tset_size_f, h5tcopy_f, &
-      H5P_FILE_ACCESS_F, H5P_DATASET_XFER_F, H5F_ACC_TRUNC_F, H5F_ACC_RDWR_F, &
-      H5T_NATIVE_INTEGER, &
-      H5S_SCALAR_F, H5S_SELECT_SET_F, H5FD_MPIO_COLLECTIVE_F, h5p_default_f
+  use hdf5, only : &
+       hid_t, hsize_t, &
+       h5open_f, h5close_f, &
+       h5fcreate_f, h5fopen_f, h5fclose_f, &
+       h5gcreate_f, h5gopen_f, h5gclose_f, &
+       h5acreate_f, h5awrite_f, h5aclose_f, &
+       h5dcreate_f, h5dopen_f, h5dwrite_f, h5dclose_f, &
+       h5screate_f, h5screate_simple_f, h5sclose_f, h5sselect_hyperslab_f, &
+       h5pcreate_f, h5pclose_f, h5pset_fapl_mpio_f, h5pset_dxpl_mpio_f, &
+       h5lexists_f, h5tset_strpad_f, h5tset_size_f, h5tcopy_f, &
+       H5P_FILE_ACCESS_F, H5P_DATASET_XFER_F, H5P_DATASET_CREATE_F, &
+       H5F_ACC_TRUNC_F, H5F_ACC_RDWR_F, &
+       H5T_STD_U8LE, H5T_NATIVE_INTEGER, H5T_FORTRAN_S1, H5T_STR_NULLTERM_F, &
+       h5kind_to_type, H5_REAL_KIND, H5_INTEGER_KIND, &
+       H5S_SCALAR_F, H5S_SELECT_SET_F, H5FD_MPIO_COLLECTIVE_F, h5p_default_f, &
+       H5S_UNLIMITED_F
 #endif
   implicit none
   private
