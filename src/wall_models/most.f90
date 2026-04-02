2264bb220733f15a84f40aebbd31438f67afab31
Lorenzo Donati <lorenzoluca.donati@gmail.com>
2026-04-02 15:48:28 +0200

    fixed some variable names

--- src/wall_models/most.f90
+++ src/wall_models/most.f90
@@ -105,7 +105,7 @@ contains
     integer, intent(in) :: msk(:)
     integer, intent(in) :: facet(:)
     integer, intent(in) :: h_index
-    real(kind=rp) :: mu, rho
+    real(kind=rp) :: mu_val, rho_val
     type(json_file), intent(inout) :: json
     real(kind=rp) :: kappa, z0, z0h_in
     character(len=:), allocatable :: bc_type
@@ -123,8 +123,8 @@ contains
     ! If z0h is to specified, assign default value of -0.8,
     ! corresponding to the Zilitinkevich constant value used in Zilitinkevich 1995.
     call json_get_or_default(json, "z0h", z0h_in, -0.8_rp)
-    call json_get_or_default(json, "mu", mu, 1e-10_rp)
-    call json_get_or_default(json, "rho", rho, 1.0_rp)
+    call json_get_or_default(json, "mu", mu_val, 1e-10_rp)
+    call json_get_or_default(json, "rho", rho_val, 1.0_rp)
     call json_get(json, "type_of_temp_bc", bc_type)
     call json_get(json, "scalar_field", scalar_name)
     call json_get(json, "bottom_bc_flux_or_temp", bc_value)
@@ -138,7 +138,7 @@ contains
     deallocate(g_tmp)
 
     call this%init_from_components(scheme_name, scalar_name, coef, msk, facet, h_index, &
-         kappa, mu, rho, g, z0, z0h_in, bc_type, bc_value)
+         kappa, mu_val, rho_val, g, z0, z0h_in, bc_type, bc_value)
     
     deallocate(bc_type)
     deallocate(scalar_name)
@@ -159,6 +159,17 @@ contains
     call json_get_or_default(json, "z0h", this%z0h_in, -0.8_rp)
     call json_get(json, "type_of_temp_bc", this%bc_type)
     call json_get(json, "bottom_bc_flux_or_temp", this%bc_value)
+    call json_get_or_default(json, "mu", this%mu_val, 1e-10_rp)
+    call json_get_or_default(json, "rho", this%rho_val, 1.0_rp)
+
+    call json_get(json, "g", g_tmp)
+    if (size(g_tmp) == 3) then
+       this%g = g_tmp
+    else
+       call neko_error("Richardson WM: Gravity vector must have 3 components")
+    end if
+    deallocate(g_tmp)
+
 
   end subroutine most_partial_init
 
@@ -181,8 +192,8 @@ contains
   !! @param facet The boundary facets.
   !! @param h_index The off-wall index of the sampling cell.
   !! @param kappa The von Karman coefficient.
-  !! @param rho fluid density
-  !! @param mu fluid dynamic viscosity
+  !! @param rho_val fluid density
+  !! @param mu_val fluid dynamic viscosity
   !! @param g The gravity vector.
   !! @param z0 The roughness height.
   !! @param z0h_in The thermal roughness height. If negative, set automatically from Zilitinkevich, 1995.
@@ -190,7 +201,7 @@ contains
   !! @param scalar_name The name of the scalar field (temperature) for MOST.
   !! @param bc_value The heat flux at the surface boundary condition.
   subroutine most_init_from_components(this, scheme_name, scalar_name, coef, msk, &
-       facet, h_index, kappa, mu, rho, g, z0, z0h_in, bc_type, bc_value)
+       facet, h_index, kappa, mu_val, rho_val, g, z0, z0h_in, bc_type, bc_value)
     class(most_t), intent(inout) :: this
     character(len=*), intent(in) :: scheme_name
     character(len=*), intent(in) :: bc_type
@@ -202,7 +213,7 @@ contains
     real(kind=rp), intent(in) :: g(3)
     real(kind=rp) :: g_mag, g_dot_n, cos_alpha, max_ang
     integer :: i
-    real(kind=rp), intent(in) :: kappa, mu, rho
+    real(kind=rp), intent(in) :: kappa, mu_val, rho_val
     real(kind=rp), intent(in) :: z0, z0h_in, bc_value
     character(len=LOG_SIZE) :: log_buf
 
@@ -210,8 +221,8 @@ contains
 
     this%kappa = kappa
     this%g = g
-    this%mu_val = mu
-    this%rho_val = rho
+    this%mu_val = mu_val
+    this%rho_val = rho_val
     this%z0 = z0
     this%z0h_in = z0h_in
     this%bc_type = bc_type
