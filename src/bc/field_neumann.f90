f2791aad52371a8c46b7723a588b521b9e6cfa42
Shiyu-Sandy-Du <shiyud@kth.se>
2026-04-12 17:38:40 +0200

    documentation

--- src/bc/field_neumann.f90
+++ src/bc/field_neumann.f90
@@ -110,6 +110,7 @@ contains
     character(len=:), allocatable :: field_name
 
     call json_get(json, "field_name", field_name)
+write(*,*) field_name
     call this%init_from_components(coef, field_name)
 
   end subroutine field_neumann_init
