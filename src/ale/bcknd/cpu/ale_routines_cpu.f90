bce68e1036bb25fedf8d01f365d58543cce4ba0c
Mohammad Moniripiri <m.moniripiri@gmail.com>
2026-04-16 09:38:53 +0200

    removed unused use statement

--- src/ale/bcknd/cpu/ale_routines_cpu.f90
+++ src/ale/bcknd/cpu/ale_routines_cpu.f90
@@ -47,7 +47,6 @@ module ale_routines_cpu
   use mpi_f08, only : MPI_WTIME, MPI_Barrier
   use logger, only : neko_log
   use ale_rigid_kinematics, only : ale_config_t, body_kinematics_t
-  use ale_routines_device, only : compute_cheap_dist_device
   use device, only : device_map, device_memcpy, device_unmap, &
        HOST_TO_DEVICE, DEVICE_TO_HOST, glb_cmd_queue
   use neko_config, only : NEKO_BCKND_DEVICE
