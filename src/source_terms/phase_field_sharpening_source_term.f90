25f2dbec67643ac00f722c48dbd32d4052631d64
Harrison Nobis <58177854+gloopydoop@users.noreply.github.com>
2026-04-24 12:08:28 +0200

    Multiphase over integration (#2217)
    Finally a commit to multiphase!
    In this PR I add a way of performing over integration (dealiasing) on
    the term
    
    $\nabla \cdot \left( \phi (1-\phi) \frac{\nabla \phi}{|\nabla
    \phi|}\right)$
    
    If one considers the test function too, this is 4 polynomials multiplied
    together, so it's not a 3/2 rules, it's a 4/2=2 rule.
    
    You should be able to enable it with `"dealias_normal": true` under
    `scalar`.
    
    There's also a little profiler wrapping each computations, I'm seeing
    (from small tests)
    ```
    
                            Total time        Avg. time       Range  +/-
     --------------------------------------------------------------------
     n_dealias            0.9233649E+02    0.7694707E-02    0.1670377E-01
     n_no_dealias         0.3231977E+01    0.2693314E-03    0.2046465E-04
     ```

--- src/source_terms/phase_field_sharpening_source_term.f90
+++ src/source_terms/phase_field_sharpening_source_term.f90
@@ -45,6 +45,7 @@
 !! - "scalar_field": Name of the phase field, defaults to "phase".
 !! - "gamma": Sharpening coefficient.
 !! - "u_max": Reference velocity scale.
+!! - "dealias": Use dealias for the normal/divergence term.
 module phase_field_sharpening_source_term
   use num_types, only : rp
   use field, only : field_t
@@ -53,11 +54,18 @@ module phase_field_sharpening_source_term
   use json_utils, only : json_get, json_get_or_default
   use source_term, only : source_term_t
   use coefs, only : coef_t
-  use operators, only : grad, div
-  use gather_scatter, only : GS_OP_ADD
-  use scratch_registry, only : neko_scratch_registry
+  use device_math, only : device_add2, device_cmult, device_col2, &
+       device_copy, device_invcol2
+  use dofmap, only : dofmap_t
+  use gather_scatter, only : GS_OP_ADD, gs_t
+  use interpolation, only : interpolator_t
+  use neko_config, only : NEKO_BCKND_DEVICE
+  use operators, only : grad, div, dudxyz
+  use profiler, only : profiler_start_region, profiler_end_region
+  use scratch_registry, only : neko_scratch_registry, scratch_registry_t
   use field_registry, only : neko_field_registry
-  use math, only : col2, add2, cmult
+  use math, only : col2, add2, cmult, copy, invcol2
+  use space, only : space_t, GL
   use utils, only : neko_error
   use time_state, only : time_state_t
   implicit none
@@ -69,6 +77,7 @@ module phase_field_sharpening_source_term
   !! - "scalar_field": The name of the phase field, defaults to "phase".
   !! - "gamma": Sharpening coefficient.
   !! - "u_max": Reference velocity scale.
+  !! - "dealias": Use dealias for the normal/divergence term.
   type, public, extends(source_term_t) :: phase_field_sharpening_source_term_t
      !> The phase field.
      type(field_t), pointer :: phi => null()
@@ -76,6 +85,20 @@ module phase_field_sharpening_source_term
      real(kind=rp) :: gamma
      !> Reference velocity scale.
      real(kind=rp) :: u_max
+     !> Use dealias for the normal/divergence evaluation.
+     logical :: dealias = .false.
+     !> Function space used for the dealias evaluation.
+     type(space_t) :: Xh_GL
+     !> Dofmap associated with Xh_GL.
+     type(dofmap_t) :: dm_Xh_GL
+     !> Gather-scatter associated with Xh_GL.
+     type(gs_t) :: gs_Xh_GL
+     !> Coefficients associated with Xh_GL.
+     type(coef_t) :: coef_GL
+     !> Interpolator between the original and dealias spaces.
+     type(interpolator_t) :: GLL_to_GL
+     !> Scratch registry on the dealias space.
+     type(scratch_registry_t) :: scratch_GL
    contains
      !> The common constructor using a JSON object.
      procedure, pass(this) :: init => &
@@ -106,15 +129,18 @@ contains
     character(len=*), intent(in) :: variable_name
     real(kind=rp) :: start_time, end_time, gamma, u_max
     character(len=:), allocatable :: scalar_name
+    logical :: dealias
 
     call json_get_or_default(json, "start_time", start_time, 0.0_rp)
     call json_get_or_default(json, "end_time", end_time, huge(0.0_rp))
     call json_get_or_default(json, "scalar_field", scalar_name, "phase")
     call json_get(json, "gamma", gamma)
     call json_get(json, "u_max", u_max)
+    call json_get_or_default(json, "dealias", dealias, .false.)
 
     call phase_field_sharpening_source_term_init_from_components(this, fields, &
-         scalar_name, gamma, u_max, coef, start_time, end_time)
+         scalar_name, gamma, u_max, coef, start_time, end_time, &
+         dealias)
 
   end subroutine phase_field_sharpening_source_term_init_from_json
 
@@ -127,13 +153,16 @@ contains
   !! @param start_time When to start adding the source term.
   !! @param end_time When to stop adding the source term.
   subroutine phase_field_sharpening_source_term_init_from_components(this, &
-       fields, scalar_name, gamma, u_max, coef, start_time, end_time)
+       fields, scalar_name, gamma, u_max, coef, start_time, end_time, &
+       dealias)
     class(phase_field_sharpening_source_term_t), intent(inout) :: this
     type(field_list_t), intent(in), target :: fields
     character(len=*), intent(in) :: scalar_name
     real(kind=rp), intent(in) :: gamma, u_max
     type(coef_t), intent(in), target :: coef
     real(kind=rp), intent(in) :: start_time, end_time
+    logical, intent(in), optional :: dealias
+    integer :: lx, lxd
 
     if (.not. fields%size() == 1) then
        call neko_error("Phase-field sharpening term expects 1 field to work on.")
@@ -149,6 +178,23 @@ contains
 
     this%gamma = gamma
     this%u_max = u_max
+    this%dealias = .false.
+    if (present(dealias)) then
+       this%dealias = dealias
+    end if
+
+    if (this%dealias) then
+       lx = coef%Xh%lx
+       lxd = 2 * lx
+
+       call this%Xh_GL%init(GL, lxd, lxd, lxd)
+       call this%dm_Xh_GL%init(this%coef%msh, this%Xh_GL)
+       call this%gs_Xh_GL%init(this%dm_Xh_GL)
+       call this%coef_GL%init(this%gs_Xh_GL)
+       call this%GLL_to_GL%init(this%Xh_GL, this%coef%Xh)
+       call this%scratch_GL%init(size=5, expansion_size=2, &
+            dof=this%dm_Xh_GL)
+    end if
 
   end subroutine phase_field_sharpening_source_term_init_from_components
 
@@ -157,6 +203,15 @@ contains
     class(phase_field_sharpening_source_term_t), intent(inout) :: this
 
     nullify(this%phi)
+    if (this%dealias) then
+       call this%scratch_GL%free()
+       call this%GLL_to_GL%free()
+       call this%coef_GL%free()
+       call this%gs_Xh_GL%free()
+       call this%dm_Xh_GL%free()
+       call this%Xh_GL%free()
+       this%dealias = .false.
+    end if
     call this%free_base()
   end subroutine phase_field_sharpening_source_term_free
 
@@ -167,11 +222,25 @@ contains
     type(time_state_t), intent(in) :: time
 
     type(field_t), pointer :: rhs_s
+
+    rhs_s => this%fields%get(1)
+
+    if (this%dealias) then
+       call phase_field_sharpening_source_term_compute_dealias(this, rhs_s)
+    else
+       call phase_field_sharpening_source_term_compute_gll(this, rhs_s)
+    end if
+
+  end subroutine phase_field_sharpening_source_term_compute
+
+  !> Computes the source term on the original GLL space.
+  subroutine phase_field_sharpening_source_term_compute_gll(this, rhs_s)
+    class(phase_field_sharpening_source_term_t), intent(inout) :: this
+    type(field_t), pointer, intent(inout) :: rhs_s
     type(field_t), pointer :: work1, work2, work3, work4
     integer :: ind(4), i, n
     real(kind=rp) :: absgrad
 
-    rhs_s => this%fields%get(1)
     n = rhs_s%size()
 
     call neko_scratch_registry%request_field(work1, ind(1))
@@ -218,6 +287,109 @@ contains
 
     call neko_scratch_registry%relinquish_field(ind)
 
-  end subroutine phase_field_sharpening_source_term_compute
+  end subroutine phase_field_sharpening_source_term_compute_gll
+
+  !> Computes the source term on a dealias GL space.
+  subroutine phase_field_sharpening_source_term_compute_dealias(this, rhs_s)
+    class(phase_field_sharpening_source_term_t), intent(inout) :: this
+    type(field_t), pointer, intent(inout) :: rhs_s
+    type(field_t), pointer :: work1_GL, work2_GL, work3_GL, work4_GL
+    type(field_t), pointer :: phi_GL, accum_GLL, term_GLL
+    integer :: ind_GL(5), ind_GLL(2), i, n_GL, n_GLL, nel
+    real(kind=rp) :: absgrad
+
+    call this%scratch_GL%request_field(work1_GL, ind_GL(1))
+    call this%scratch_GL%request_field(work2_GL, ind_GL(2))
+    call this%scratch_GL%request_field(work3_GL, ind_GL(3))
+    call this%scratch_GL%request_field(work4_GL, ind_GL(4))
+    call this%scratch_GL%request_field(phi_GL, ind_GL(5))
+    call neko_scratch_registry%request_field(accum_GLL, ind_GLL(1))
+    call neko_scratch_registry%request_field(term_GLL, ind_GLL(2))
+
+    n_GL = work1_GL%size()
+    n_GLL = rhs_s%size()
+    nel = this%coef%msh%nelv
+
+    call this%GLL_to_GL%map(phi_GL%x, this%phi%x, nel, this%Xh_GL)
+    call grad(work1_GL%x, work2_GL%x, work3_GL%x, phi_GL%x, this%coef_GL)
+
+    do i = 1, n_GL
+       absgrad = sqrt(work1_GL%x(i,1,1,1)**2 + work2_GL%x(i,1,1,1)**2 &
+            + work3_GL%x(i,1,1,1)**2)
+       if (absgrad < 1.0e-12_rp) then
+          work1_GL%x(i,1,1,1) = 0.0_rp
+          work2_GL%x(i,1,1,1) = 0.0_rp
+          work3_GL%x(i,1,1,1) = 0.0_rp
+       else
+          work1_GL%x(i,1,1,1) = &
+               -phi_GL%x(i,1,1,1)*(1.0_rp - phi_GL%x(i,1,1,1)) &
+               * work1_GL%x(i,1,1,1) / absgrad
+          work2_GL%x(i,1,1,1) = &
+               -phi_GL%x(i,1,1,1)*(1.0_rp - phi_GL%x(i,1,1,1)) &
+               * work2_GL%x(i,1,1,1) / absgrad
+          work3_GL%x(i,1,1,1) = &
+               -phi_GL%x(i,1,1,1)*(1.0_rp - phi_GL%x(i,1,1,1)) &
+               * work3_GL%x(i,1,1,1) / absgrad
+       end if
+    end do
+
+    call dudxyz(work4_GL%x, work1_GL%x, this%coef_GL%drdx, &
+         this%coef_GL%dsdx, this%coef_GL%dtdx, this%coef_GL)
+    call phase_field_sharpening_source_term_map_to_gll(this, term_GLL, &
+         work4_GL, n_GL, n_GLL, nel)
+    if (NEKO_BCKND_DEVICE .eq. 1) then
+       call device_copy(accum_GLL%x_d, term_GLL%x_d, n_GLL)
+    else
+       call copy(accum_GLL%x, term_GLL%x, n_GLL)
+    end if
+
+    call dudxyz(work4_GL%x, work2_GL%x, this%coef_GL%drdy, &
+         this%coef_GL%dsdy, this%coef_GL%dtdy, this%coef_GL)
+    call phase_field_sharpening_source_term_map_to_gll(this, term_GLL, &
+         work4_GL, n_GL, n_GLL, nel)
+    if (NEKO_BCKND_DEVICE .eq. 1) then
+       call device_add2(accum_GLL%x_d, term_GLL%x_d, n_GLL)
+    else
+       call add2(accum_GLL%x, term_GLL%x, n_GLL)
+    end if
+
+    call dudxyz(work4_GL%x, work3_GL%x, this%coef_GL%drdz, &
+         this%coef_GL%dsdz, this%coef_GL%dtdz, this%coef_GL)
+    call phase_field_sharpening_source_term_map_to_gll(this, term_GLL, &
+         work4_GL, n_GL, n_GLL, nel)
+    if (NEKO_BCKND_DEVICE .eq. 1) then
+       call device_add2(accum_GLL%x_d, term_GLL%x_d, n_GLL)
+       call device_cmult(accum_GLL%x_d, this%gamma * this%u_max, n_GLL)
+       call device_add2(rhs_s%x_d, accum_GLL%x_d, n_GLL)
+    else
+       call add2(accum_GLL%x, term_GLL%x, n_GLL)
+       call cmult(accum_GLL%x, this%gamma * this%u_max, n_GLL)
+       call add2(rhs_s%x, accum_GLL%x, n_GLL)
+    end if
+
+    call this%scratch_GL%relinquish_field(ind_GL)
+    call neko_scratch_registry%relinquish_field(ind_GLL)
+
+  end subroutine phase_field_sharpening_source_term_compute_dealias
+
+  !> Maps a GL-space derivative contribution to the GLL RHS space.
+  subroutine phase_field_sharpening_source_term_map_to_gll(this, term_GLL, &
+       term_GL, n_GL, n_GLL, nel)
+    class(phase_field_sharpening_source_term_t), intent(inout) :: this
+    type(field_t), pointer, intent(inout) :: term_GLL
+    type(field_t), pointer, intent(inout) :: term_GL
+    integer, intent(in) :: n_GL, n_GLL, nel
+
+    if (NEKO_BCKND_DEVICE .eq. 1) then
+       call device_col2(term_GL%x_d, this%coef_GL%B_d, n_GL)
+       call this%GLL_to_GL%map(term_GLL%x, term_GL%x, nel, this%coef%Xh)
+       call device_invcol2(term_GLL%x_d, this%coef%B_d, n_GLL)
+    else
+       call col2(term_GL%x, this%coef_GL%B, n_GL)
+       call this%GLL_to_GL%map(term_GLL%x, term_GL%x, nel, this%coef%Xh)
+       call invcol2(term_GLL%x, this%coef%B, n_GLL)
+    end if
+
+  end subroutine phase_field_sharpening_source_term_map_to_gll
 
 end module phase_field_sharpening_source_term
