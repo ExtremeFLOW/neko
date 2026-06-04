c80e4809188b1fe4f2b61a127e92e2d2c7b522bb
Shiyu-Sandy-Du <shiyud@kth.se>
2026-06-04 13:14:14 +0200

    have an independent type for particles

--- src/simulation_components/lpt.f90
+++ src/simulation_components/lpt.f90
@@ -64,6 +64,17 @@ module lagrangian_particle_tracking
   real(kind=rp), parameter :: LPT_PERIODIC_TOL = 1.0e-8_rp
   integer, parameter :: LPT_VEL_HISTORY_LEN = 2
 
+  type, private :: particles_t
+     real(kind=rp), allocatable :: xyz(:,:)
+     integer, allocatable :: ids(:)
+     real(kind=rp), allocatable :: vel_lag(:,:,:)
+     integer :: n = 0
+     integer :: n_global = 0
+   contains
+     procedure, pass(this) :: init => particles_init
+     procedure, pass(this) :: free => particles_free
+  end type particles_t
+
   !> A simulation component for passive Lagrangian particle tracking.
   !! Particles are redistributed to the rank that owns their current location.
   type, public, extends(simulation_component_t) :: lpt_t
@@ -71,17 +82,13 @@ module lagrangian_particle_tracking
      type(field_t), pointer :: v => null()
      type(field_t), pointer :: w => null()
      type(global_interpolation_t) :: global_interp
-     real(kind=rp), allocatable :: xyz_particles(:,:)
-     integer, allocatable :: particle_ids(:)
-     real(kind=rp), allocatable :: vel_particles_lag(:,:,:)
+     type(particles_t) :: particles
      type(file_t) :: output_file
      logical :: output_enabled = .false.
      logical :: log = .true.
      logical :: periodic_enabled = .false.
      logical :: rotational_periodic_enabled = .false.
      real(kind=rp) :: start_time = -huge(0.0_rp)
-     integer :: n_particles = 0
-     integer :: n_global_particles = 0
      integer :: n_periodic_dirs = 0
      real(kind=rp) :: periodic_shift(3, 3) = 0.0_rp
      real(kind=rp) :: periodic_dir(3, 3) = 0.0_rp
@@ -122,6 +129,39 @@ module lagrangian_particle_tracking
   end type lpt_t
 
 contains
+
+  subroutine particles_init(this, xyz)
+    class(particles_t), intent(inout) :: this
+    real(kind=rp), intent(in) :: xyz(:,:)
+    integer :: i
+
+    if (size(xyz, 1) .ne. 3) then
+       call neko_error("particles coordinates must have size 3 in dim 1")
+    end if
+
+    call this%free()
+
+    this%n = size(xyz, 2)
+    this%n_global = this%n
+    allocate(this%xyz(3, this%n))
+    allocate(this%ids(this%n))
+    allocate(this%vel_lag(3, LPT_VEL_HISTORY_LEN, this%n))
+    this%xyz = xyz
+    this%vel_lag = 0.0_rp
+    do i = 1, this%n
+       this%ids(i) = i
+    end do
+  end subroutine particles_init
+
+  subroutine particles_free(this)
+    class(particles_t), intent(inout) :: this
+
+    if (allocated(this%xyz)) deallocate(this%xyz)
+    if (allocated(this%ids)) deallocate(this%ids)
+    if (allocated(this%vel_lag)) deallocate(this%vel_lag)
+    this%n = 0
+    this%n_global = 0
+  end subroutine particles_free
 
   !> Construct from JSON.
   !! Supported particle input is either a flat `coordinates` array
@@ -178,7 +218,7 @@ contains
     class(lpt_t), intent(inout) :: this
     type(json_file), intent(inout) :: json
     real(kind=rp), allocatable :: coords(:)
-    integer :: i
+    real(kind=rp), allocatable :: empty_xyz(:,:)
 
     if (pe_rank .eq. 0) then
        if (json%valid_path("coordinates")) then
@@ -187,28 +227,16 @@ contains
              call neko_error("lpt coordinates must contain 3 values per " // &
                   "particle")
           end if
-          this%n_particles = size(coords) / 3
-          allocate(this%xyz_particles(3, this%n_particles))
-          this%xyz_particles = reshape(coords, [3, this%n_particles])
+          call this%particles%init(reshape(coords, [3, size(coords) / 3]))
        else if (json%valid_path("points_file")) then
           call this%read_particles_csv(json)
        else
           call neko_error("lpt requires either coordinates or points_file")
        end if
     else
-       this%n_particles = 0
-       allocate(this%xyz_particles(3, 0))
+       allocate(empty_xyz(3, 0))
+       call this%particles%init(empty_xyz)
     end if
-
-    allocate(this%particle_ids(this%n_particles))
-    do i = 1, this%n_particles
-       this%particle_ids(i) = i
-    end do
-
-    allocate(this%vel_particles_lag(3, LPT_VEL_HISTORY_LEN, this%n_particles))
-    this%vel_particles_lag = 0.0_rp
-
-    this%n_global_particles = this%n_particles
   end subroutine read_particles_json
 
   !> Read particle coordinates from a three-column CSV file.
@@ -226,11 +254,9 @@ contains
 
     select type (ft => file_in%file_type)
     type is (csv_file_t)
-       this%n_particles = ft%count_lines()
-       call mat_in%init(this%n_particles, 3)
+       call mat_in%init(ft%count_lines(), 3)
        call ft%read(mat_in)
-       allocate(this%xyz_particles(3, this%n_particles))
-       this%xyz_particles = transpose(mat_in%x)
+       call this%particles%init(transpose(mat_in%x))
     class default
        call neko_error("lpt points_file must be a csv file")
     end select
@@ -248,19 +274,19 @@ contains
     real(kind=rp), allocatable :: vel_particles_lag_local(:,:,:)
     integer :: ierr
 
-    n_particles_old = this%n_particles
+    n_particles_old = this%particles%n
     call this%wrap_particles_periodic()
-    call this%global_interp%find_points_and_redist(this%xyz_particles, &
-         this%n_particles)
+    call this%global_interp%find_points_and_redist(this%particles%xyz, &
+         this%particles%n)
     call this%init_particle_redist_comm(redist_comm)
     call this%redistribute_particle_ids(redist_comm, n_particles_old, &
          particle_ids_local)
     call this%redistribute_velocity_history(redist_comm, n_particles_old, &
          vel_particles_lag_local)
     call redist_comm%free()
-    call move_alloc(particle_ids_local, this%particle_ids)
-    call move_alloc(vel_particles_lag_local, this%vel_particles_lag)
-    call MPI_Allreduce(this%n_particles, this%n_global_particles, 1, &
+    call move_alloc(particle_ids_local, this%particles%ids)
+    call move_alloc(vel_particles_lag_local, this%particles%vel_lag)
+    call MPI_Allreduce(this%particles%n, this%particles%n_global, 1, &
          MPI_INTEGER, MPI_SUM, NEKO_COMM, ierr)
   end subroutine redistribute_particles
 
@@ -309,7 +335,7 @@ contains
     real(kind=rp), allocatable :: recvbuf(:)
     integer :: n_local
 
-    n_local = this%n_particles
+    n_local = this%particles%n
     allocate(particle_ids_local(n_local))
     particle_ids_local = 0
 
@@ -317,7 +343,7 @@ contains
 
     allocate(sendbuf(n_particles_old))
     allocate(recvbuf(n_local))
-    sendbuf = real(this%particle_ids, rp)
+    sendbuf = real(this%particles%ids, rp)
     recvbuf = 0.0_rp
     call redist_comm%sendrecv(sendbuf, recvbuf, n_particles_old, n_local)
     particle_ids_local = nint(recvbuf)
@@ -335,7 +361,7 @@ contains
     integer :: i
     integer :: j
 
-    n_local = this%n_particles
+    n_local = this%particles%n
     allocate(vel_particles_lag_local(3, LPT_VEL_HISTORY_LEN, n_local))
     vel_particles_lag_local = 0.0_rp
 
@@ -345,7 +371,7 @@ contains
     allocate(recvbuf(n_local))
     do j = 1, LPT_VEL_HISTORY_LEN
        do i = 1, 3
-          sendbuf = this%vel_particles_lag(i, j, :)
+          sendbuf = this%particles%vel_lag(i, j, :)
           recvbuf = 0.0_rp
           call redist_comm%sendrecv(sendbuf, recvbuf, n_particles_old, n_local)
           vel_particles_lag_local(i, j, :) = recvbuf
@@ -388,7 +414,7 @@ contains
     integer :: n_iters
     logical :: mapped
 
-    if (this%n_particles .eq. 0) return
+    if (this%particles%n .eq. 0) return
 
     if (this%rotational_periodic_enabled) then
        call this%wrap_particles_rotational()
@@ -396,7 +422,7 @@ contains
     end if
 
     if (this%n_periodic_maps .gt. 0) then
-       do i = 1, this%n_particles
+       do i = 1, this%particles%n
           do n_iters = 1, 3
              mapped = .false.
              do k = 1, this%n_periodic_maps
@@ -500,10 +526,10 @@ contains
     real(kind=rp) :: pi
 
     pi = acos(-1.0_rp)
-    do i = 1, this%n_particles
-       radius = norm2(this%xyz_particles(1:2, i))
-       theta = modulo(atan2(this%xyz_particles(2, i), &
-            this%xyz_particles(1, i)) + 2.0_rp * pi, 2.0_rp * pi)
+    do i = 1, this%particles%n
+       radius = norm2(this%particles%xyz(1:2, i))
+       theta = modulo(atan2(this%particles%xyz(2, i), &
+            this%particles%xyz(1, i)) + 2.0_rp * pi, 2.0_rp * pi)
 
        do while (theta .lt. this%rotational_theta_min - LPT_PERIODIC_TOL)
           theta = theta + this%rotational_theta_len
@@ -513,8 +539,8 @@ contains
           theta = theta - this%rotational_theta_len
        end do
 
-       this%xyz_particles(1, i) = radius * cos(theta)
-       this%xyz_particles(2, i) = radius * sin(theta)
+       this%particles%xyz(1, i) = radius * cos(theta)
+       this%particles%xyz(2, i) = radius * sin(theta)
     end do
   end subroutine wrap_particles_rotational
 
@@ -526,18 +552,18 @@ contains
 
     if (.not. this%periodic_enabled) return
 
-    do i = 1, this%n_particles
+    do i = 1, this%particles%n
        do j = 1, this%n_periodic_dirs
-          coord = dot_product(this%xyz_particles(:, i), this%periodic_dir(:, j))
+          coord = dot_product(this%particles%xyz(:, i), this%periodic_dir(:, j))
 
           do while (coord .lt. this%periodic_min(j) - LPT_PERIODIC_TOL)
-             this%xyz_particles(:, i) = this%xyz_particles(:, i) + &
+             this%particles%xyz(:, i) = this%particles%xyz(:, i) + &
                   this%periodic_shift(:, j)
              coord = coord + this%periodic_len(j)
           end do
 
           do while (coord .gt. this%periodic_max(j) + LPT_PERIODIC_TOL)
-             this%xyz_particles(:, i) = this%xyz_particles(:, i) - &
+             this%particles%xyz(:, i) = this%particles%xyz(:, i) - &
                   this%periodic_shift(:, j)
              coord = coord - this%periodic_len(j)
           end do
@@ -551,8 +577,8 @@ contains
     real(kind=rp), allocatable, intent(out) :: vel(:,:)
     logical :: do_interp_on_host
 
-    allocate(vel(3, this%n_particles))
-    if (this%n_particles .eq. 0) return
+    allocate(vel(3, this%particles%n))
+    if (this%particles%n .eq. 0) return
 
     do_interp_on_host = .false.
     call this%global_interp%evaluate(vel(1,:), this%u%x, do_interp_on_host)
@@ -577,9 +603,9 @@ contains
     call this%evaluate_velocity(vel)
     nadv = this%case%fluid%ext_bdf%nadv
 
-    if (nadv .gt. 1 .and. all(this%vel_particles_lag .eq. 0.0_rp)) then
+    if (nadv .gt. 1 .and. all(this%particles%vel_lag .eq. 0.0_rp)) then
        do j = 1, LPT_VEL_HISTORY_LEN
-          this%vel_particles_lag(:, j, :) = vel
+          this%particles%vel_lag(:, j, :) = vel
        end do
     end if
 
@@ -592,21 +618,21 @@ contains
 
     dtc = time%dt * this%case%fluid%ext_bdf%advection_coeffs%x(1)
     do i = 1, 3
-       call add2s2(this%xyz_particles(i, :), vel(i, :), dtc, this%n_particles)
+       call add2s2(this%particles%xyz(i, :), vel(i, :), dtc, this%particles%n)
     end do
 
     do j = 2, nadv
        dtc = time%dt * this%case%fluid%ext_bdf%advection_coeffs%x(j)
        do i = 1, 3
-          call add2s2(this%xyz_particles(i, :), this%vel_particles_lag(i, j - 1, :), &
-               dtc, this%n_particles)
+          call add2s2(this%particles%xyz(i, :), this%particles%vel_lag(i, j - 1, :), &
+               dtc, this%particles%n)
        end do
     end do
 
     do j = LPT_VEL_HISTORY_LEN, 2, -1
-       this%vel_particles_lag(:, j, :) = this%vel_particles_lag(:, j - 1, :)
+       this%particles%vel_lag(:, j, :) = this%particles%vel_lag(:, j - 1, :)
     end do
-    this%vel_particles_lag(:, 1, :) = vel
+    this%particles%vel_lag(:, 1, :) = vel
 
     if (allocated(vel)) deallocate(vel)
   end subroutine lpt_compute
@@ -628,16 +654,16 @@ contains
     integer :: i
     integer :: ierr
 
-    n_local = this%n_particles
+    n_local = this%particles%n
 
     allocate(local_data(9, n_local))
     do i = 1, n_local
        local_data(1,i) = real(time%tstep, rp)
        local_data(2,i) = time%t
-       local_data(3,i) = real(this%particle_ids(i), rp)
-       local_data(4,i) = this%xyz_particles(1,i)
-       local_data(5,i) = this%xyz_particles(2,i)
-       local_data(6,i) = this%xyz_particles(3,i)
+       local_data(3,i) = real(this%particles%ids(i), rp)
+       local_data(4,i) = this%particles%xyz(1,i)
+       local_data(5,i) = this%particles%xyz(2,i)
+       local_data(6,i) = this%particles%xyz(3,i)
        local_data(7,i) = vel(1,i)
        local_data(8,i) = vel(2,i)
        local_data(9,i) = vel(3,i)
@@ -692,9 +718,7 @@ contains
   subroutine lpt_free(this)
     class(lpt_t), intent(inout) :: this
 
-    if (allocated(this%xyz_particles)) deallocate(this%xyz_particles)
-    if (allocated(this%particle_ids)) deallocate(this%particle_ids)
-    if (allocated(this%vel_particles_lag)) deallocate(this%vel_particles_lag)
+    call this%particles%free()
     if (allocated(this%periodic_map_npts)) deallocate(this%periodic_map_npts)
     if (allocated(this%periodic_src_center)) deallocate(this%periodic_src_center)
     if (allocated(this%periodic_src_basis)) deallocate(this%periodic_src_basis)
@@ -711,8 +735,6 @@ contains
     this%output_enabled = .false.
     this%log = .true.
     this%start_time = -huge(0.0_rp)
-    this%n_particles = 0
-    this%n_global_particles = 0
     this%n_periodic_maps = 0
     this%periodic_enabled = .false.
     this%rotational_periodic_enabled = .false.
@@ -739,7 +761,7 @@ contains
     write(log_buf, '(A,A)') "Name: ", trim(this%name)
     call neko_log%message(log_buf)
     write(log_buf, '(A,I0)') "Global seeded particles: ", &
-         this%n_global_particles
+         this%particles%n_global
     call neko_log%message(log_buf)
     if (this%periodic_enabled) then
        write(log_buf, '(A,I0)') "Periodic wrap directions: ", &
@@ -760,7 +782,7 @@ contains
        call neko_log%message(log_buf)
     end if
     write(log_buf, '(A,I0)') "Local particles on rank 0 at init: ", &
-         this%n_particles
+         this%particles%n
     if (pe_rank .eq. 0) call neko_log%message(log_buf)
     call neko_log%end_section()
   end subroutine log_status
@@ -933,7 +955,7 @@ contains
     real(kind=rp) :: xi(3)
 
     mapped = .false.
-    rel = this%xyz_particles(:, i_particle) - this%periodic_src_center(:, i_map)
+    rel = this%particles%xyz(:, i_particle) - this%periodic_src_center(:, i_map)
     if (this%periodic_map_npts(i_map) .eq. 4) then
        xi(1) = dot_product(rel, this%periodic_src_basis(:, 1, i_map))
        xi(2) = dot_product(rel, this%periodic_src_basis(:, 2, i_map))
@@ -946,7 +968,7 @@ contains
             LPT_PERIODIC_TOL .and. &
             xi(2) .le. this%periodic_src_bounds_max(2, i_map) + &
             LPT_PERIODIC_TOL .and. xi(3) .lt. -LPT_PERIODIC_TOL) then
-          this%xyz_particles(:, i_particle) = this%periodic_tgt_center(:, i_map) + &
+          this%particles%xyz(:, i_particle) = this%periodic_tgt_center(:, i_map) + &
                xi(1) * this%periodic_tgt_basis(:, 1, i_map) + &
                xi(2) * this%periodic_tgt_basis(:, 2, i_map) + &
                xi(3) * this%periodic_tgt_basis(:, 3, i_map)
@@ -959,7 +981,7 @@ contains
             LPT_PERIODIC_TOL .and. &
             xi(1) .le. this%periodic_src_bounds_max(1, i_map) + &
             LPT_PERIODIC_TOL .and. xi(2) .lt. -LPT_PERIODIC_TOL) then
-          this%xyz_particles(:, i_particle) = this%periodic_tgt_center(:, i_map) + &
+          this%particles%xyz(:, i_particle) = this%periodic_tgt_center(:, i_map) + &
                xi(1) * this%periodic_tgt_basis(:, 1, i_map) + &
                xi(2) * this%periodic_tgt_basis(:, 3, i_map)
           mapped = .true.
