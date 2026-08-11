4a8023ef96bb26ea6e286b3b561410010720a78a
Victor Baconnet <baconnet@kth.se>
2026-08-11 09:44:53 +0200

    changes to time schemes

--- src/simulation.f90
+++ src/simulation.f90
@@ -224,7 +224,7 @@ contains
           time%tlag(2) = time%t
        end if
 
-       call ext_bdf%set_coeffs(real(time%dtlag, kind=rp))
+       call ext_bdf%set_coeffs(time%dtlag)
     end if
 
     time%tstep = time%tstep + 1
@@ -283,7 +283,7 @@ contains
     ! Restart the time state and BDF coefficients
     call chkp%set_time_state(C%time)
     do i = 1, size(C%time%dtlag)
-       call C%fluid%ext_bdf%set_coeffs( real(C%time%dtlag, kind=rp) )
+       call C%fluid%ext_bdf%set_coeffs(C%time%dtlag)
     end do
 
     ! Restart the fluid and scalars
