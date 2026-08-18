c17130260692a25a9d6e846e4ddda0110e11d819
Victor Baconnet <baconnet@kth.se>
2026-08-18 16:19:40 +0200

    revert rhs maker

--- src/common/rhs_maker.f90
+++ src/common/rhs_maker.f90
@@ -36,7 +36,7 @@
 !! and the BDF scheme applied to the time derivative.
 !! Inheritance is used to define implementation for different backends.
 module rhs_maker
-  use num_types, only : rp, dp
+  use num_types, only : rp
   use field_series, only : field_series_t
   use field, only : field_t
   implicit none
@@ -120,8 +120,7 @@ module rhs_maker
        type(field_series_t), intent(in) :: ulag, vlag, wlag
        real(kind=rp), intent(inout) :: bfx(n), bfy(n), bfz(n)
        real(kind=rp), intent(in) :: B(n), Blag(n), Blaglag(n)
-       real(kind=rp), intent(in) :: rho, bd(4)
-       real(kind=rp), intent(in) :: dt
+       real(kind=rp), intent(in) :: dt, rho, bd(4)
      end subroutine rhs_maker_bdf
   end interface
 
@@ -136,8 +135,7 @@ module rhs_maker
        type(field_series_t), intent(in) :: s_lag
        real(kind=rp), intent(inout) :: fs(n)
        real(kind=rp), intent(in) :: B(n)
-       real(kind=rp), intent(in) :: bd(4)
-       real(kind=rp), intent(in) :: dt
+       real(kind=rp), intent(in) :: dt, bd(4)
      end subroutine scalar_rhs_maker_bdf
   end interface
 
@@ -145,8 +143,7 @@ module rhs_maker
      subroutine rhs_maker_oifs(phi_x, phi_y, phi_z, bf_x, bf_y, bf_z, &
           rho, dt, n)
        import rp
-       real(kind=rp), intent(in) :: rho
-       real(kind=rp), intent(in) :: dt
+       real(kind=rp), intent(in) :: rho, dt
        integer, intent(in) :: n
        real(kind=rp), intent(inout) :: bf_x(n), bf_y(n), bf_z(n)
        real(kind=rp), intent(inout) :: phi_x(n), phi_y(n), phi_z(n)
