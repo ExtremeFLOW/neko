1008ce1927487af1602b6437a4d6a56dedcd0b92
Victor Baconnet <baconnet@kth.se>
2026-08-21 14:32:49 +0200

    update copyright date

--- src/math/bcknd/device/cuda/opr_cfl.cu
+++ src/math/bcknd/device/cuda/opr_cfl.cu
@@ -1,5 +1,5 @@
 /*
- Copyright (c) 2022-2025, The Neko Authors
+ Copyright (c) 2022-2026, The Neko Authors
  All rights reserved.
 
  Redistribution and use in source and binary forms, with or without
