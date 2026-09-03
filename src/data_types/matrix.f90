c8a9db82f5f72e8dc1ac2ac69291b6863e46ca21
Tim Felle Olsen <tife@dtu.dk>
2026-09-03 15:38:35 +0200

    Extend to matrix

--- src/data_types/matrix.f90
+++ src/data_types/matrix.f90
@@ -32,49 +32,36 @@
 !
 !> Defines a matrix
 module matrix
-  use neko_config, only : NEKO_BCKND_DEVICE
+  use array, only : array_t
   use num_types, only : rp, xp
-  use device, only : device_map, device_unmap, device_memcpy, &
-       device_sync
-  use device_math, only : device_copy, device_cfill
-  use utils, only : neko_error, NEKO_VARNAME_LEN
-  use, intrinsic :: iso_c_binding
+  use neko_config, only : NEKO_BCKND_DEVICE
+  use device, only : HOST_TO_DEVICE, DEVICE_TO_HOST
+  use utils, only : neko_warning, neko_error
   implicit none
   private
 
-  type, public :: matrix_t
-     real(kind=rp), allocatable :: x(:,:) !< Matrix entries.
-     character(len=NEKO_VARNAME_LEN) :: name = "" !< Name of the matrix
-     type(c_ptr) :: x_d = C_NULL_PTR !< Device pointer.
-     integer, private :: nrows = 0 !< Number of matrix rows.
-     integer, private :: ncols = 0 !< Number of matrix columns.
-     integer, private :: n = 0 !< Total size nows*ncols.
+  type, public, extends(array_t) :: matrix_t
+     !> Matrix shaped pointer.
+     real(kind=rp), pointer, dimension(:,:) :: x => null()
+     !> Number of matrix rows.
+     integer, private :: nrows = 0
+     !> Number of matrix columns.
+     integer, private :: ncols = 0
+
    contains
      !> Initialise a matrix of size `nrows*ncols`.
-     procedure, pass(m) :: init => matrix_init
+     procedure, pass(this) :: init => matrix_init
      !> Deallocate a matrix.
-     procedure, pass(m) :: free => matrix_free
-     !> Returns the number of entries in the matrix.
-     procedure, pass(m) :: size => matrix_size
-     !> Copy between host and device
-     procedure, pass(m) :: copy_from => matrix_copy_from
+     procedure, pass(this) :: free => matrix_free
      !> Returns the number of rows in the matrix.
-     procedure, pass(m) :: get_nrows => matrix_nrows
+     procedure, pass(this) :: get_nrows => matrix_nrows
      !> Returns the number of columns in the matrix.
-     procedure, pass(m) :: get_ncols => matrix_ncols
-     !> Assignment \f$ m = w \f$
-     procedure, pass(m) :: matrix_assign_matrix
-     !> Assignment \f$ m = s \f$.
-     procedure, pass(m) :: matrix_assign_scalar
+     procedure, pass(this) :: get_ncols => matrix_ncols
+
      !> Inverse a matrix.
-     procedure, pass(m) :: inverse => matrix_bcknd_inverse
-     procedure, pass(m) :: inverse_on_host => cpu_matrix_inverse
+     procedure, pass(this) :: inverse => matrix_inverse
+     procedure, pass(this) :: inverse_on_host => cpu_matrix_inverse
 
-     generic :: assignment(=) => matrix_assign_matrix, &
-          matrix_assign_scalar
-
-     !> Allocate a matrix of size `nrows*ncols`.
-     procedure, pass(m), private :: alloc => matrix_allocate
   end type matrix_t
 
   type, public :: matrix_ptr_t
@@ -86,194 +73,83 @@ contains
   !> Initialise a matrix of size `nrows*ncols`.
   !! @param nrows Number of rows.
   !! @param ncols Number of columns.
-  subroutine matrix_init(m, nrows, ncols, name)
-    class(matrix_t), intent(inout) :: m
-    integer, intent(in) :: nrows
-    integer, intent(in) :: ncols
+  !! @param name Optional name of the object.
+  subroutine matrix_init(this, nrows, ncols, name)
+    class(matrix_t), intent(inout), target :: this
+    integer, intent(in) :: nrows, ncols
     character(len=*), intent(in), optional :: name
 
-    ! m%alloc zeroes the device side (and synchronizes) before any
-    ! host-side touch: under zero-copy the device then faults the
-    ! pages (device first touch), which gives contiguous physical
-    ! mappings and thus better GPU TLB utilisation; rewriting the
-    ! zeros on the host afterwards is benign
-    call m%alloc(nrows, ncols)
-    m%x = 0.0_rp
-
-    if (present(name)) then
-       m%name = name
-    end if
-
+    call this%init_base(nrows*ncols, name)
+    this%nrows = nrows
+    this%ncols = ncols
+    this%x(1:nrows, 1:ncols) => this%data(:)
 
   end subroutine matrix_init
 
-  !> Allocate a matrix of size `nrows*ncols`.
-  !! @param nrows Number of rows.
-  !! @param ncols Number of columns.
-  subroutine matrix_allocate(m, nrows, ncols)
-    class(matrix_t), intent(inout) :: m
-    integer, intent(in) :: nrows
-    integer, intent(in) :: ncols
-
-    call m%free()
-
-    allocate(m%x(nrows, ncols))
-    m%nrows = nrows
-    m%ncols = ncols
-    m%n = nrows*ncols
-
-    if (NEKO_BCKND_DEVICE .eq. 1) then
-       call device_map(m%x, m%x_d, m%n)
-       call device_cfill(m%x_d, 0.0_rp, m%n)
-       call device_sync()
-    end if
-
-  end subroutine matrix_allocate
-
   !> Deallocate a matrix.
-  subroutine matrix_free(m)
-    class(matrix_t), intent(inout) :: m
+  subroutine matrix_free(this)
+    class(matrix_t), intent(inout) :: this
 
-    if (allocated(m%x)) then
-       if (NEKO_BCKND_DEVICE .eq. 1) then
-          call device_unmap(m%x, m%x_d)
-       end if
-       deallocate(m%x)
-    end if
-
-    m%nrows = 0
-    m%ncols = 0
-    m%n = 0
-    m%name = ""
+    call this%free_base()
+    this%nrows = 0
+    this%ncols = 0
+    nullify(this%x)
 
   end subroutine matrix_free
 
-  !> Returns the number of entries in the matrix.
-  pure function matrix_size(m) result(s)
-    class(matrix_t), intent(in) :: m
-    integer :: s
-    s = m%n
-  end function matrix_size
-
-
-  !> Easy way to copy between host and device.
-  !! @param m matrix to copy to/from device/host
-  !! @memdir direction to copy (HOST_TO_DEVICE or DEVICE_TO_HOST)
-  !! @sync whether the memcopy to be blocking or not
-  subroutine matrix_copy_from(m, memdir, sync)
-    class(matrix_t), intent(inout) :: m
-    integer, intent(in) :: memdir
-    logical, intent(in) :: sync
-
-    if (NEKO_BCKND_DEVICE .eq. 1) then
-       call device_memcpy(m%x, m%x_d, m%n, memdir, sync)
-    end if
-
-  end subroutine matrix_copy_from
-
   !> Returns the number of rows in the matrix.
-  pure function matrix_nrows(m) result(nr)
-    class(matrix_t), intent(in) :: m
+  pure function matrix_nrows(this) result(nr)
+    class(matrix_t), intent(in) :: this
     integer :: nr
-    nr = m%nrows
+    nr = this%nrows
   end function matrix_nrows
 
   !> Returns the number of columns in the matrix.
-  pure function matrix_ncols(m) result(nc)
-    class(matrix_t), intent(in) :: m
+  pure function matrix_ncols(this) result(nc)
+    class(matrix_t), intent(in) :: this
     integer :: nc
-    nc = m%ncols
+    nc = this%ncols
   end function matrix_ncols
 
-  !> Assignment \f$ m = w \f$
-  subroutine matrix_assign_matrix(m, w)
-    class(matrix_t), intent(inout) :: m
-    type(matrix_t), intent(in) :: w
-
-    if (allocated(m%x)) then
-       call m%free()
-    end if
-
-    if (.not. allocated(m%x)) then
-
-       m%nrows = w%nrows
-       m%ncols = w%ncols
-       m%n = w%n
-       allocate(m%x(m%nrows, m%ncols))
-
-       if (NEKO_BCKND_DEVICE .eq. 1) then
-          call device_map(m%x, m%x_d, m%n)
-       end if
-
-    end if
-
-    if (NEKO_BCKND_DEVICE .eq. 1) then
-       call device_copy(m%x_d, w%x_d, m%n)
-    else
-       m%x = w%x
-    end if
-
-    m%name = w%name
-
-  end subroutine matrix_assign_matrix
-
-  !> Assignment \f$ m = s \f$.
-  subroutine matrix_assign_scalar(m, s)
-    class(matrix_t), intent(inout) :: m
-    real(kind=rp), intent(in) :: s
-
-    if (.not. allocated(m%x)) then
-       call neko_error('matrix not allocated')
-    end if
-
-    if (NEKO_BCKND_DEVICE .eq. 1) then
-       call device_cfill(m%x_d, s, m%n)
-    else
-       m%x = s
-    end if
-
-  end subroutine matrix_assign_scalar
-
-  subroutine matrix_bcknd_inverse(m, bcknd)
-    class(matrix_t), intent(inout) :: m
+  !> Compute the inverse of the matrix.
+  subroutine matrix_inverse(this, bcknd)
+    class(matrix_t), intent(inout) :: this
     integer, optional :: bcknd
 
-    if (NEKO_BCKND_DEVICE .eq. 1 .and. &
-         bcknd .eq. NEKO_BCKND_DEVICE) then
-       call neko_error("matrix_bcknd_inverse not &
-       &implemented on accelarators.")
+    if (NEKO_BCKND_DEVICE .eq. 1 .and. bcknd .eq. NEKO_BCKND_DEVICE) then
+       call neko_error("matrix_bcknd_inverse not implemented on accelarators.")
     else
-       call cpu_matrix_inverse(m)
+       call this%inverse_on_host()
     end if
 
-  end subroutine matrix_bcknd_inverse
+  end subroutine matrix_inverse
 
-  subroutine cpu_matrix_inverse(m)
+  subroutine cpu_matrix_inverse(this)
     ! Gauss-Jordan matrix inversion with full pivoting
     ! Num. Rec. p. 30, 2nd Ed., Fortran
-    ! m%x     is an sqaure matrix
-    ! rmult is m  work array of length nrows = ncols
-    class(matrix_t), intent(inout) :: m
-    integer :: indr(m%nrows), indc(m%ncols), ipiv(m%ncols)
-    real(kind=xp) :: rmult(m%nrows), amx, tmp, piv, eps
+    ! this%x     is an square matrix
+    ! rmult is this  work array of length nrows = ncols
+    class(matrix_t), intent(inout) :: this
+    integer :: indr(this%nrows), indc(this%ncols), ipiv(this%ncols)
+    real(kind=xp) :: rmult(this%nrows), amx, tmp, piv, eps
     integer :: i, j, k, ir, jc
 
-    if (.not. (m%ncols .eq. m%nrows)) then
-       call neko_error("Fatal error: trying to invert m matrix that is not &
+    if (.not. (this%ncols .eq. this%nrows)) then
+       call neko_error("Fatal error: trying to invert this matrix that is not &
        &square")
     end if
 
     eps = 1e-9_rp
     ipiv = 0
 
-    do k = 1, m%nrows
+    do k = 1, this%nrows
        amx = 0.0_rp
-       do i = 1, m%nrows ! Pivot search
+       do i = 1, this%nrows ! Pivot search
           if (ipiv(i) .ne. 1) then
-             do j = 1, m%nrows
+             do j = 1, this%nrows
                 if (ipiv(j) .eq. 0) then
-                   if (abs(m%x(i, j)) .ge. amx) then
-                      amx = abs(m%x(i, j))
+                   if (abs(this%x(i, j)) .ge. amx) then
+                      amx = abs(this%x(i, j))
                       ir = i
                       jc = j
                    end if
@@ -287,59 +163,58 @@ contains
 
        !  Swap rows
        if (ir .ne. jc) then
-          do j = 1, m%ncols
-             tmp = m%x(ir, j)
-             m%x(ir, j) = m%x(jc, j)
-             m%x(jc, j) = tmp
+          do j = 1, this%ncols
+             tmp = this%x(ir, j)
+             this%x(ir, j) = this%x(jc, j)
+             this%x(jc, j) = tmp
           end do
        end if
        indr(k) = ir
        indc(k) = jc
 
-       if (abs(m%x(jc, jc)) .lt. eps) then
+       if (abs(this%x(jc, jc)) .lt. eps) then
           call neko_error("matrix_inverse error: small Gauss Jordan Piv")
        end if
-       piv = 1.0_xp/m%x(jc, jc)
-       m%x(jc, jc) = 1.0_xp
-       do j = 1, m%ncols
-          m%x(jc, j) = m%x(jc, j)*piv
+       piv = 1.0_xp/this%x(jc, jc)
+       this%x(jc, jc) = 1.0_xp
+       do j = 1, this%ncols
+          this%x(jc, j) = this%x(jc, j)*piv
        end do
 
-       do j = 1, m%ncols
-          tmp = m%x(jc, j)
-          m%x(jc, j) = m%x(1 , j)
-          m%x(1 , j) = tmp
+       do j = 1, this%ncols
+          tmp = this%x(jc, j)
+          this%x(jc, j) = this%x(1 , j)
+          this%x(1 , j) = tmp
        end do
-       do i = 2, m%nrows
-          rmult(i) = m%x(i, jc)
-          m%x(i, jc) = 0.0_rp
+       do i = 2, this%nrows
+          rmult(i) = this%x(i, jc)
+          this%x(i, jc) = 0.0_rp
        end do
 
-       do j = 1, m%ncols
-          do i = 2, m%nrows
-             m%x(i, j) = m%x(i, j) - rmult(i)*m%x(1, j)
+       do j = 1, this%ncols
+          do i = 2, this%nrows
+             this%x(i, j) = this%x(i, j) - rmult(i)*this%x(1, j)
           end do
        end do
 
-       do j = 1, m%ncols
-          tmp = m%x(jc, j)
-          m%x(jc, j) = m%x(1 , j)
-          m%x(1 , j) = tmp
+       do j = 1, this%ncols
+          tmp = this%x(jc, j)
+          this%x(jc, j) = this%x(1 , j)
+          this%x(1 , j) = tmp
        end do
     end do
 
     ! Unscramble matrix
-    do j = m%nrows, 1, -1
+    do j = this%nrows, 1, -1
        if (indr(j) .ne. indc(j)) then
-          do i = 1, m%nrows
-             tmp = m%x(i, indr(j))
-             m%x(i, indr(j)) = m%x(i, indc(j))
-             m%x(i, indc(j)) = tmp
+          do i = 1, this%nrows
+             tmp = this%x(i, indr(j))
+             this%x(i, indr(j)) = this%x(i, indc(j))
+             this%x(i, indc(j)) = tmp
           end do
        end if
     end do
 
-    return
   end subroutine cpu_matrix_inverse
 
 end module matrix
