7a70d78ff16a754155cd5f83174357c607204880
Tim Felle Olsen <tife@dtu.dk>
2026-09-03 15:38:36 +0200

    make matrix more generic.

--- src/data_types/matrix.f90
+++ src/data_types/matrix.f90
@@ -44,15 +44,18 @@ module matrix
      !> Matrix shaped pointer.
      real(kind=rp), pointer, dimension(:,:) :: x => null()
      !> Number of matrix rows.
-     integer, private :: nrows = 0
-     !> Number of matrix columns.
-     integer, private :: ncols = 0
+     integer, dimension(2), private :: dims = [0, 0]
 
    contains
      !> Initialise a matrix of size `nrows*ncols`.
      procedure, pass(this) :: init => matrix_init
      !> Deallocate a matrix.
      procedure, pass(this) :: free => matrix_free
+
+     !> Returns the dimensions of the matrix.
+     procedure, pass(this) :: get_dims => matrix_dims
+     !> Return the specific dimension of the matrix.
+     procedure, pass(this) :: get_dim => matrix_dim
      !> Returns the number of rows in the matrix.
      procedure, pass(this) :: get_nrows => matrix_nrows
      !> Returns the number of columns in the matrix.
@@ -65,7 +68,12 @@ module matrix
   end type matrix_t
 
   type, public :: matrix_ptr_t
-     type(matrix_t), pointer :: ptr
+     type(matrix_t), pointer :: ptr => null()
+   contains
+     !> Constructor. Just assigns the pointer
+     procedure, pass(this) :: init => matrix_ptr_init
+     !> Destructor. Just nullifies the pointer.
+     procedure, pass(this) :: free => matrix_ptr_free
   end type matrix_ptr_t
 
 contains
@@ -78,10 +86,9 @@ contains
     class(matrix_t), intent(inout), target :: this
     integer, intent(in) :: nrows, ncols
     character(len=*), intent(in), optional :: name
+    this%dims = [nrows, ncols]
 
     call this%init_base(nrows*ncols, name)
-    this%nrows = nrows
-    this%ncols = ncols
     this%x(1:nrows, 1:ncols) => this%data(:)
 
   end subroutine matrix_init
@@ -91,24 +98,44 @@ contains
     class(matrix_t), intent(inout) :: this
 
     call this%free_base()
-    this%nrows = 0
-    this%ncols = 0
+    this%dims = [0, 0]
     nullify(this%x)
 
   end subroutine matrix_free
+
+  !> Returns the dimensions of the matrix.
+  pure function matrix_dims(this) result(dims)
+    class(matrix_t), intent(in) :: this
+    integer :: dims(2)
+    dims = this%dims
+  end function matrix_dims
+
+  !> Return the specific dimension of the matrix.
+  function matrix_dim(this, dim) result(d)
+    class(matrix_t), intent(in) :: this
+    integer, intent(in) :: dim
+    integer :: d
+    if (dim .eq. 1) then
+       d = this%dims(1)
+    else if (dim .eq. 2) then
+       d = this%dims(2)
+    else
+       call neko_error("matrix_dim: invalid dimension requested")
+    end if
+  end function matrix_dim
 
   !> Returns the number of rows in the matrix.
   pure function matrix_nrows(this) result(nr)
     class(matrix_t), intent(in) :: this
     integer :: nr
-    nr = this%nrows
+    nr = this%dims(1)
   end function matrix_nrows
 
   !> Returns the number of columns in the matrix.
   pure function matrix_ncols(this) result(nc)
     class(matrix_t), intent(in) :: this
     integer :: nc
-    nc = this%ncols
+    nc = this%dims(2)
   end function matrix_ncols
 
   !> Compute the inverse of the matrix.
@@ -130,11 +157,11 @@ contains
     ! this%x     is an square matrix
     ! rmult is this  work array of length nrows = ncols
     class(matrix_t), intent(inout) :: this
-    integer :: indr(this%nrows), indc(this%ncols), ipiv(this%ncols)
-    real(kind=xp) :: rmult(this%nrows), amx, tmp, piv, eps
+    integer :: indr(this%dims(1)), indc(this%dims(2)), ipiv(this%dims(2))
+    real(kind=xp) :: rmult(this%dims(1)), amx, tmp, piv, eps
     integer :: i, j, k, ir, jc
 
-    if (.not. (this%ncols .eq. this%nrows)) then
+    if (.not. (this%dims(2) .eq. this%dims(1))) then
        call neko_error("Fatal error: trying to invert this matrix that is not &
        &square")
     end if
@@ -142,11 +169,11 @@ contains
     eps = 1e-9_rp
     ipiv = 0
 
-    do k = 1, this%nrows
+    do k = 1, this%dims(1)
        amx = 0.0_rp
-       do i = 1, this%nrows ! Pivot search
+       do i = 1, this%dims(1) ! Pivot search
           if (ipiv(i) .ne. 1) then
-             do j = 1, this%nrows
+             do j = 1, this%dims(1)
                 if (ipiv(j) .eq. 0) then
                    if (abs(this%x(i, j)) .ge. amx) then
                       amx = abs(this%x(i, j))
@@ -163,7 +190,7 @@ contains
 
        !  Swap rows
        if (ir .ne. jc) then
-          do j = 1, this%ncols
+          do j = 1, this%dims(2)
              tmp = this%x(ir, j)
              this%x(ir, j) = this%x(jc, j)
              this%x(jc, j) = tmp
@@ -177,27 +204,27 @@ contains
        end if
        piv = 1.0_xp/this%x(jc, jc)
        this%x(jc, jc) = 1.0_xp
-       do j = 1, this%ncols
+       do j = 1, this%dims(2)
           this%x(jc, j) = this%x(jc, j)*piv
        end do
 
-       do j = 1, this%ncols
+       do j = 1, this%dims(2)
           tmp = this%x(jc, j)
           this%x(jc, j) = this%x(1 , j)
           this%x(1 , j) = tmp
        end do
-       do i = 2, this%nrows
+       do i = 2, this%dims(1)
           rmult(i) = this%x(i, jc)
           this%x(i, jc) = 0.0_rp
        end do
 
-       do j = 1, this%ncols
-          do i = 2, this%nrows
+       do j = 1, this%dims(2)
+          do i = 2, this%dims(1)
              this%x(i, j) = this%x(i, j) - rmult(i)*this%x(1, j)
           end do
        end do
 
-       do j = 1, this%ncols
+       do j = 1, this%dims(2)
           tmp = this%x(jc, j)
           this%x(jc, j) = this%x(1 , j)
           this%x(1 , j) = tmp
@@ -205,9 +232,9 @@ contains
     end do
 
     ! Unscramble matrix
-    do j = this%nrows, 1, -1
+    do j = this%dims(1), 1, -1
        if (indr(j) .ne. indc(j)) then
-          do i = 1, this%nrows
+          do i = 1, this%dims(1)
              tmp = this%x(i, indr(j))
              this%x(i, indr(j)) = this%x(i, indc(j))
              this%x(i, indc(j)) = tmp
@@ -216,5 +243,25 @@ contains
     end do
 
   end subroutine cpu_matrix_inverse
+
+  ! ========================================================================== !
+  ! matrix pointer type subroutines
+
+  subroutine matrix_ptr_init(this, ptr)
+    class(matrix_ptr_t), intent(inout) :: this
+    type(matrix_t), target, intent(in) :: ptr
+
+    call this%free()
+    this%ptr => ptr
+  end subroutine matrix_ptr_init
+
+  subroutine matrix_ptr_free(this)
+    class(matrix_ptr_t), intent(inout) :: this
+
+    if (associated(this%ptr)) then
+       nullify(this%ptr)
+    end if
+
+  end subroutine matrix_ptr_free
 
 end module matrix
