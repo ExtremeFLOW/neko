fb8730d16c60af0d269083eb03884da192ad0974
Niclas Jansson <njansson@kth.se>
2026-09-06 22:05:31 +0200

    Please the linter

--- src/les/bcknd/device/device_sigma_nut.F90
+++ src/les/bcknd/device/device_sigma_nut.F90
@@ -36,7 +36,7 @@ module device_sigma_nut
   use utils, only: neko_error
   use comm, only: NEKO_COMM, pe_size, MPI_REAL_PRECISION
   use mpi_f08, only: MPI_SUM, MPI_IN_PLACE, MPI_Allreduce
-  
+
   implicit none
   private
 
@@ -150,5 +150,5 @@ contains
 #endif
   end subroutine device_sigma_nut_compute
 
-  
+
 end module device_sigma_nut
