407ea9cb1d0651dec267effaf893e1fe69690286
Tim Felle Olsen <tife@dtu.dk>
2026-09-16 14:59:29 +0200

    Apply new array type to tensors

--- src/registries/scratch_registry.f90
+++ src/registries/scratch_registry.f90
@@ -489,8 +489,7 @@ contains
             end if
 
             m => entries(index)%get_matrix()
-            if (m%get_nrows() .ne. nrows .or. &
-                 m%get_ncols() .ne. ncols) then
+            if (any(m%get_dims() .ne. [nrows, ncols])) then
                nullify(m)
                cycle
             end if
@@ -542,9 +541,7 @@ contains
             end if
 
             t => entries(index)%get_tensor3()
-            if (t%get_n1() .ne. n .or. &
-                 t%get_n2() .ne. m .or. &
-                 t%get_n3() .ne. l) then
+            if (any(t%get_dims() .ne. [n, m, l])) then
                nullify(t)
                cycle
             end if
@@ -601,10 +598,7 @@ contains
             end if
 
             t => entries(index)%get_tensor4()
-            if (t%get_n1() .ne. n .or. &
-                 t%get_n2() .ne. m .or. &
-                 t%get_n3() .ne. l .or. &
-                 t%get_n4() .ne. k) then
+            if (any(t%get_dims() .ne. [n, m, l, k])) then
                nullify(t)
                cycle
             end if
